fc563bd76c5649d5fbe0749420604688fb1deb05
MartinS <MartinS@0b88b48e-9a22-11de-825e-994487f65616>
2009-09-05 14:51:51 +0000

    Add missing files

---
+++ trunk/progress-doibot.php
@@ -0,0 +1,10 @@
+<?
+$linkto2 = '';
+include("/home/verisimilus/public_html/Bot/DOI_bot/expandFns$linkto2.php");
+if (preg_match("~\d{8}~", $_GET['date'], $date)) $date = $date[0]; else $date = 0;
+
+// Return count of how many left to do
+$result = mysql_query ("SELECT page FROM citation WHERE fast > $date") or die(mysql_error());
+print "\nStatus: done " . mysql_num_rows($result).  " pages " . (($date>0)?" since $date; ":" in total; ");
+$result = mysql_query ("SELECT page FROM citation") or die(mysql_error());
+print mysql_num_rows($result)." in database.\n\n";
