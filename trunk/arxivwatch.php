a7fd662af360cbbb80fa821e25bba08b42c973fe
MartinS <MartinS@0b88b48e-9a22-11de-825e-994487f65616>
2009-09-05 15:08:08 +0000

    Make include paths relative; bot will now work from anywhere in ~verisimilus

--- trunk/arxivwatch.php
+++ trunk/arxivwatch.php
@@ -1,10 +1,11 @@
-#!/usr/bin/php
 <?php
+// $Id: $
+
 $accountSuffix = '_2';
 $editInitiator = '[ax]';
-require_once("/home/verisimilus/public_html/Bot/DOI_bot/expandFns.php"); //Must include first
-require_once("/home/verisimilus/public_html/DOItools.php");
-require_once("/home/verisimilus/public_html/Bot/wikiFunctions.php");
+require_once("expandFns.php"); //Must include first
+require_once("DOItools.php");
+require_once("wikiFunctions.php");
 define("wikiroot", "http://en.wikipedia.org/w/index.php?");
 
 $toDo = categoryMembers("Articles with missing Cite arXiv inputs");
@@ -18,5 +19,5 @@ function nextPage(){
 }
 $page = nextPage();
 $ON = true;
-require_once("/home/verisimilus/public_html/Bot/DOI_bot/expand.php");
+require_once("expand.php");
 print "\n===End===\n\n";
