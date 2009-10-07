b0a160b28fd431b601c3f493634a9d45dfa01396
MartinS <MartinS@0b88b48e-9a22-11de-825e-994487f65616>
2009-10-07 04:12:41 +0000

    Author detection if only one author present

--- console-doibot.php
+++ console-doibot.php
@@ -3,16 +3,16 @@
 error_reporting(E_ALL^E_NOTICE);
 $slowMode=false;
 $fastMode=false;
-$editInitiator = '[Pu37a]';
+$editInitiator = '[Pu37]';
 $accountSuffix='_1';
 
 $ON = true;
-$ON = false;
+#$ON = false;
 include("expandFns.php");
 
 function nextPage(){
 	#return 'User:DOI bot/Zandbox';
-	die ("\n**EXIT: nextPage is disabled!\n");
+	#die ("\n**EXIT: nextPage is disabled!\n");
   //return 'Template:Cite doi/10.1001.2Farchinternmed.2009.6';
 	global $db;
 	$result = mysql_query ("SELECT page FROM citation ORDER BY fast ASC") or die(mysql_error());
