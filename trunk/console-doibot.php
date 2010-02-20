a6cfc0226b68459367d47df2570c9b19cf15477f
MartinS <MartinS@0b88b48e-9a22-11de-825e-994487f65616>
2010-02-20 16:58:34 +0000

    only accept dois from PubMed if they are forematted liek a DOI

--- trunk/console-doibot.php
+++ trunk/console-doibot.php
@@ -10,8 +10,6 @@ $htmlOutput = false;
 $editInitiator = '[Pu' . revisionID() . ']';
 $ON = true; // Override later if necessary
 
-print revisionID(); exit;
-
 function updateQueue() {
   print "** Updating backlog...\nSeeing what links to 'Cite Journal'...";
   $cite_journal = whatTranscludes2("Cite_journal", 0);
@@ -45,6 +43,6 @@ $ON = false;
 $page = "User:DOI bot/Zandbox";  // Leave this line as is.  It'll be over-written when the bot is turned on.
 if ($ON) $page = nextPage();
 #$page = " Template:Cite doi/10.1002.2F.28SICI.291097-0290.2819980420.2958:2.2F3.3C121::AID-BIT2.3E3.0.CO.3B2-N";
-//$ON = true; // Uncomment this line to test edits in teh Zandbox; but remember to break the bot after it touches the page or it'll keep on going!
+//$ON = true; // Uncomment this line to test edits in the Zandbox; but remember to break the bot after it touches the page or it'll keep on going!
 #$page = "";
 include("expand.php"); // i.e. GO!
