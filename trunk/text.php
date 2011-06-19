dc4d7f7b95593c5662f03875c086c7ced772f6d6
MartinS <MartinS@0b88b48e-9a22-11de-825e-994487f65616>
2011-06-19 02:44:08 +0000

    Character encoding in text.php

--- trunk/text.php
+++ trunk/text.php
@@ -1,8 +1,12 @@
 <html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+  </head>
   <body onload="form.wpDiff.click()">
     <h1>Citation bot is running...</h1>
     <h3>Wait a moment whilst the bot runs.  You'll be returned to Wikipedia when it's done.</h3>
-    <form id="form" method="post" action="<?=str_replace("&action=edit", "&action=submit", $_SERVER["HTTP_REFERER"])?>">
+    <form id="form" method="post" accept-charset="UTF-8"
+          action="<?=str_replace("&action=edit", "&action=submit", $_SERVER["HTTP_REFERER"])?>">
       <textarea rows="20" cols="90" name="wpTextbox1"><?php
       error_reporting(E_ALL^E_NOTICE);
       $accountSuffix = '_1'; // Keep this before including expandFns
@@ -12,7 +16,7 @@
 
       $postvars = $_POST;
       echo htmlentities(expand_text(
-              mb_convert_encoding($postvars["wpTextbox1"], "ISO-8859-1")));
+              mb_convert_encoding($postvars["wpTextbox1"], "UTF-8")), ENT_QUOTES, 'UTF-8');
     ?></textarea>
       <?php
 unset ($postvars["wpTextbox1"]);
