4c1bfc0b5529a2a397a8a9ed06f0346cb661f16f
martins <martins@gmail.com>
2014-06-28 15:29:05 +0000

    Draw in Dev channel

--- category.php
+++ category.php
@@ -19,13 +19,12 @@ foreach ($argv as $arg) {
   }
 }
 
-$slowMode = false;
-$fastMode = false;
+$slow_mode = false;
 $account_suffix='_4'; // Whilst testing
 $account_suffix='_1'; // Keep this before including expandFns
 include("expandFns.php");
 $htmlOutput = false;
-$edit_initiator = '[Cat' . revisionID() . '-dev]';
+$edit_initiator = '[Cat' . revisionID() . ']';
 
 $category = $argument["cat"] ? $argument["cat"][0] : $_GET["cat"];
 if (!$category) $category = "Pages_using_citations_with_old-style_implicit_et_al.";
