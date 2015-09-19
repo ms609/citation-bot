83ef3891d26e911b8d72906782d4a6607d59be7e
Kaldari <rkaldari@wikimedia.org>
2015-09-18 17:01:32 -0700

    Oops, missing parentheses

--- Template.php
+++ Template.php
@@ -900,7 +900,7 @@ class Template extends Item {
           $this->add_if_new('issue', $crossRef->issue);
         }
         if ($this->blank("page")) {
-          if ($crossRef->last_page && ($crossRef->first_page != $crossRef->last_page) {
+          if ($crossRef->last_page && ($crossRef->first_page != $crossRef->last_page)) {
             $this->add_if_new("pages", $crossRef->first_page . "-" . $crossRef->last_page); //replaced by an endash later in script
           } else {
             $this->add_if_new("pages", $crossRef->first_page);
