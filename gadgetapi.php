538005808e30e4987cf9cbb9a7708baec59dbeb3
Frances Hocutt <fhocutt@wikimedia.org>
2015-11-07 08:39:48 +0000

    Replace old gadget backend with new API
    * Remove text.php, as the new gadget onwiki works through AJAX calls
    * Add new page-expanding API backend
    * Misc. style change to Template.php
    
    Written together with Niharika Kohli.

---
+++ gadgetapi.php
@@ -0,0 +1,33 @@
+ <?php
+header("Access-Control-Allow-Origin: *"); //This is ok because the API is not authenticated
+header("Content-Type: text/json");
+
+//Configure setting to suppress buffered output
+global $html_output;
+$html_output = -1;
+
+//Set up tool requirements
+require_once('/data/project/citations-dev/public_html/expandFns.php');
+
+$originalText = $_POST['text'];
+$editSummary = $_POST['summary'];
+
+//Expand text from postvars
+$page = new Page();
+$page->text = $originalText;
+$page->expand_text();
+
+//Modify edit summary to identify bot-assisted edits
+if ($editSummary) {
+  $editSummary .= " | ";
+}
+$editSummary .= "[[WP:UCB|Assisted by Citation bot]]";
+
+$result = array(
+	'expandedtext' => $page->text,
+	'editsummary' => $editSummary
+);
+
+echo json_encode($result);
+
+?>
