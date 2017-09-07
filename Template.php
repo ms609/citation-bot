53cf8c03f1954c45692304f5811bced58938df28
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-09-06 19:05:35 -0700

    Update Template.php

--- Template.php
+++ Template.php
@@ -413,7 +413,10 @@ class Template extends Item {
       return false;
       case 'volume':
         if ($this->blank($param)) {
-          if ($this->get('journal') == "ZookKeys" ) return $this->add('issue', $value); // This journal has no volume
+          if ($this->get('journal') == "ZookKeys" ) {
+            this->add_if_new('issue',$value) // This journal has no volume
+            return true;
+          }
           return $this->add($param, $value);
         }
       return false;
