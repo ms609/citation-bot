e7d99be36f4e2907472e043d6ecb93f6f0b3c0ee
Martin R. Smith <martins@gmail.com>
2017-09-09 21:06:34 +0800

    Move constant definitions to separate file

--- expandFns.php
+++ expandFns.php
@@ -1,12 +1,11 @@
 <?php
 /*
- * expandFns.php sets up most of the page expansion. A number of constants and variables
- * are set here. HTTP handing takes place using an instance of the Snoopy class. Most of
- * the page expansion depends on the classes in objects.php, particularly Template and Page.
+ * expandFns.php sets up most of the page expansion. HTTP handing takes place using an instance 
+ * of the Snoopy class. Most of the page expansion depends on the classes in objects.php, 
+ * particularly Template and Page.
  */
 
 ini_set("user_agent", "Citation_bot; citations@tools.wmflabs.org");
-define('HOME', dirname(__FILE__) . '/');
 
 function quiet_echo($text, $alternate_text = '') {
   global $html_output;
@@ -16,35 +15,10 @@ function quiet_echo($text, $alternate_text = '') {
     echo $alternate_text;
 }
 
-define("editinterval", 10);
-define("PIPE_PLACEHOLDER", '%%CITATION_BOT_PIPE_PLACEHOLDER%%');
-define("comment_placeholder", "### Citation bot : comment placeholder %s ###");
-define("to_en_dash", "--?|\&mdash;|\xe2\x80\x94|\?\?\?"); // regexp for replacing to ndashes using mb_ereg_replace
-define("en_dash", "\xe2\x80\x93"); // regexp for replacing to ndashes using mb_ereg_replace
-define("wikiroot", "https://en.wikipedia.org/w/index.php?");
-define("api", "https://en.wikipedia.org/w/api.php"); // wiki's API endpoint
-define("bibcode_regexp", "~^(?:" . str_replace(".", "\.", implode("|", Array(
-                    "http://(?:\w+.)?adsabs.harvard.edu",
-                    "http://ads.ari.uni-heidelberg.de",
-                    "http://ads.inasan.ru",
-                    "http://ads.mao.kiev.ua",
-                    "http://ads.astro.puc.cl",
-                    "http://ads.on.br",
-                    "http://ads.nao.ac.jp",
-                    "http://ads.bao.ac.cn",
-                    "http://ads.iucaa.ernet.in",
-                    "http://ads.lipi.go.id",
-                    "http://cdsads.u-strasbg.fr",
-                    "http://esoads.eso.org",
-                    "http://ukads.nottingham.ac.uk",
-                    "http://www.ads.lipi.go.id",
-                ))) . ")/.*(?:abs/|bibcode=|query\?|full/)([12]\d{3}[\w\d\.&]{15})~");
-//define("doiRegexp", "(10\.\d{4}/([^\s;\"\?&<])*)(?=[\s;\"\?&]|</)");
-#define("doiRegexp", "(10\.\d{4}(/|%2F)[^\s\"\?&]*)(?=[\s\"\?&]|</)"); //Note: if a DOI is superceded by a </span>, it will pick up this tag. Workaround: Replace </ with \s</ in string to search.
-
+require_once("constants.php");
 require_once(HOME . "credentials/doiBot.login");
 # Snoopy's ini files should be modified so the host name is en.wikipedia.org.
-require_once('Snoopy.class.php');
+require_once("Snoopy.class.php");
 require_once("DOItools.php");
 require_once("objects.php");
 require_once("wikiFunctions.php");
