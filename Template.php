e88522b287b47d57d29945de787fdb2ad4a0d979
Martin R. Smith <martins@gmail.com>
2017-09-11 09:29:45 +0800

    Check page range exists

--- Template.php
+++ Template.php
@@ -531,7 +531,7 @@ class Template extends Item {
     $volume = $this->get('volume');
     $page_range = $this->page_range();
     $start_page = $page_range[1];
-    $end_page = $page_range[2];
+    $end_page = isset($page_range[2]) ? $page_range[2] : null;
     $issn = $this->get('issn');
     $url1 = trim($this->get('url'));
     $input = array($title, $journal, $author, $year, $volume, $start_page, $end_page, $issn, $url1);
