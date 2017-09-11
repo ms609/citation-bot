fb9fede66b276ecefde04512d688c41412bc36f7
Martin R. Smith <martins@gmail.com>
2017-09-12 00:21:04 +0800

    Backward compatibility?
    https://stackoverflow.com/questions/42811164/class-phpunit-framework-testcase-not-found

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -1,5 +1,11 @@
 <?php
 
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+}
+
 /*
  * Tests for Template.php, called from expandFns.php.
  */
