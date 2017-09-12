0b639004fb8dc8e496ac919ca81453a64a57191e
Martin R. Smith <martins@gmail.com>
2017-09-12 18:39:57 +0800

    Encoding failure?

--- expandFns.php
+++ expandFns.php
@@ -175,7 +175,7 @@ function format_title_text($title, $capitalize = TRUE) {
 }
 
 function remove_accents($input) {
-  $search = explode(",", "ç,æ,,á,é,í,ó,ú,à,è,ì,ò,ù,ä,ë,ï,ö,ü,ÿ,â,ê,î,ô,û,å,e,i,ø,u");
+  $search = explode(",", "ç,æ,œ,á,é,í,ó,ú,à,è,ì,ò,ù,ä,ë,ï,ö,ü,ÿ,â,ê,î,ô,û,å,e,i,ø,u");
   $replace = explode(",", "c,ae,oe,a,e,i,o,u,a,e,i,o,u,a,e,i,o,u,y,a,e,i,o,u,a,e,i,o,u");
   return str_replace($search, $replace, $input);
 }
