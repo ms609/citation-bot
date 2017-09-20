451c326bb569d62df52ec77825439b219127b58d
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-09-20 08:49:41 -0700

    did not mean to do that change

--- Template.php
+++ Template.php
@@ -1371,7 +1371,7 @@ class Template extends Item {
       }
       
       if (preg_match('~^(https?://|www\.)\S+~', $dat, $match)) { # Takes priority over more tenative matches
-        $this->add('url', $match[0]);
+        $this->set('url', $match[0]);
         $dat = str_replace($match[0], '', $dat);
       }
       
