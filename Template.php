f1aeb572e78cdfd33da9edd6f50a63f92e961cba
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-09-26 09:07:48 -0700

    Better URL validation

--- Template.php
+++ Template.php
@@ -452,13 +452,12 @@ class Template extends Item {
       if (strtolower(substr( $url, 0, 4 )) !== "http" ) {
         $url = "http://" . $url; // Try it with http
       }
-      if (filter_var($url, FILTER_VALIDATE_URL,FILTER_FLAG_HOST_REQUIRED |  FILTER_FLAG_PATH_REQUIRED ) ) { // is a valid http(s) URL with a full hostname and path of some type
-        $this->rename('website', 'url'); // Rename it first, so that parameters stay in same order
-        $this->set('url',$url);
-        quiet_echo("\n   ~ website is actually HTTP URL; converting to use url parameter.");
-      } else { // Not a URL
-        return ; // Nothing to be done with website.  Maybe we should convert to work or publisher
-      }
+      if (filter_var($url, FILTER_VALIDATE_URL,FILTER_FLAG_HOST_REQUIRED | FILTER_FLAG_PATH_REQUIRED ) === FALSE ) return ; // PHP does not like it
+      $pattern = '_^(?:(?:https?|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?!10(?:\\.\\d{1,3}){3})(?!127(?:\\.\\d{1,3}){3})(?!169\\.254(?:\\.\\d{1,3}){2})(?!192\\.168(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,})))(?::\\d{2,5})?(?:/[^\\s]*)?$_iuS';
+      if (preg_match ($pattern, $url) !== 1) return ;  // See https://mathiasbynens.be/demo/url-regex/  This regex is more exact than validator.  We only spend time on this after quick and dirty check is passed
+      $this->rename('website', 'url'); // Rename it first, so that parameters stay in same order
+      $this->set('url',$url);
+      quiet_echo("\n   ~ website is actually HTTP URL; converting to use url parameter.");
     }
     $url = $this->get('url');
     if (strtolower(substr( $url, 0, 6 )) === "ttp://" ) { // Not unusual to lose first character in copy and paste
