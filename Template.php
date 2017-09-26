086977d370297964501e72566a2e26a5c1db15cb
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-09-26 07:54:06 -0700

    http more likely to work than https.
    also, there are bots that will convert http to https for some websites

--- Template.php
+++ Template.php
@@ -447,7 +447,7 @@ class Template extends Item {
     if ($this->blank('url') && !$this->blank('website')) {  // No URL, but a website
       $url = trim($this->get('website'));
       if (strtolower(substr( $url, 0, 4 )) !== "http" ) {
-        $url = "https://" . $url; // Try it with http
+        $url = "http://" . $url; // Try it with http
       }
       if (filter_var($url, FILTER_VALIDATE_URL,FILTER_FLAG_HOST_REQUIRED |  FILTER_FLAG_PATH_REQUIRED ) ) { // is a valid http(s) URL with a full hostname and path of some type
         $this->set('url',$url);
