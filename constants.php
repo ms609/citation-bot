1a1017b0c1c1ab684094f59557a30e9579c573cc
Martin R. Smith <martins@gmail.com>
2017-09-29 17:01:13 +0100

    Whitespace

--- constants.php
+++ constants.php
@@ -12,7 +12,7 @@ const api = "https://en.wikipedia.org/w/api.php"; // wiki's API endpoint
 const template_regexp = "~\{\{\s*([^\|\}]+)([^\{]|\{[^\{])*?\}\}~";
 const BRACESPACE = "!BOTCODE-spaceBeforeTheBrace";
 const bibcode_regexp = "~^(?:http://(?:\w+.)?adsabs.harvard.edu|http://ads\.ari\.uni-heidelberg\.de|http://ads\.inasan\.ru|http://ads\.mao\.kiev\.ua|http://ads\.astro\.puc\.cl|http://ads\.on\.br|http://ads\.nao\.ac\.jp|http://ads\.bao\.ac\.cn|http://ads\.iucaa\.ernet\.in|http://ads\.lipi\.go\.id|http://cdsads\.u-strasbg\.fr|http://esoads\.eso\.org|http://ukads\.nottingham\.ac\.uk|http://www\.ads\.lipi\.go\.id)/.*(?:abs/|bibcode=|query\?|full/)([12]\d{3}[\w\d\.&]{15})~";
-const IN_PRESS_ALIASES = array("in press","pending","published","unpublished","unknown","tba","forthcoming","in the press","na","submitted","tbd","missing");         
+const IN_PRESS_ALIASES = array("in press","pending","published","unpublished","unknown","tba","forthcoming","in the press","na","submitted","tbd","missing");
 
 //Note: if a DOI is superceded by a </span>, it will pick up this tag. Workaround: Replace </ with \s</ in string to search.
 const doiRegexp = "(10\.\d{4}\d?(/|%2F)..([^\s\|\"\?&>]|&l?g?t;|<[^\s\|\"\?&]*>))(?=[\s\|\"\?]|</)";
