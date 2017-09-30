b414a9ab7cdce9c94b1d08388c87ba1a8fb326f2
Martin R. Smith <martins@gmail.com>
2017-09-30 19:05:51 +0100

    Change positioning of defining VERBOSE

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -9,11 +9,11 @@ if (!class_exists('\PHPUnit\Framework\TestCase') &&
     class_exists('\PHPUnit_Framework_TestCase')) {
     class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
 }
+if (!defined('VERBOSE')) define('VERBOSE', TRUE);
  
 class TemplateTest extends PHPUnit\Framework\TestCase {
 
   protected function setUp() {
-    define('VERBOSE', TRUE);
   }
 
   protected function tearDown() {
