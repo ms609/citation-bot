4de50d23555f2c9cf5f3a8d6aad60942dbf25fac
Martin R. Smith <martins@gmail.com>
2017-10-02 14:31:40 +0100

    Missing >

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -418,7 +418,7 @@ ER -  }}';
     public function testJstorSICI() {
        $text = '{{Cite journal|url=https://www.jstor.org/sici?sici=0003-0279(196101%2F03)81%3A1%3C43%3AWLIMP%3E2.0.CO%3B2-9}}';
        $expanded = $this->process_citation($text);
-       $this-assertEquals('594900',$expanded->get('jstor'));
+       $this->assertEquals('594900', $expanded->get('jstor'));
    }
   /* TODO 
   Test adding a paper with > 4 editors; this should trigger displayeditors
