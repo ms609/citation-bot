534871894e4da94258d5c2f1ac671daef76f11b4
Martin R. Smith <martins@gmail.com>
2017-10-13 21:12:54 +0100

    Typo

--- Template.php
+++ Template.php
@@ -918,7 +918,7 @@ class Template extends Item {
         $result = query_adsabs("bibcode:" . urlencode($this->get("bibcode")));
       } elseif ($this->has('doi')) {
         $result = query_adsabs("doi:" . urlencode($this->get('doi')));
-      } elseif ($this->hase('title')) {
+      } elseif ($this->has('title')) {
         $result = query_adsabs("title:" . urlencode('"' .  $this->get("title") . '"'));
         $record = $result->docs[0];
         $inTitle = str_replace(array(" ", "\n", "\r"), "", (mb_strtolower($record->title)));
