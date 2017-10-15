6c48af7c1fe5d0b2d5533289a11b3c2e0fd10052
Martin R. Smith <martins@gmail.com>
2017-10-15 15:24:56 +0100

    Restore edit summary tester

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -28,26 +28,31 @@ class PageTest extends PHPUnit\Framework\TestCase {
     $page->expand_text();
     return $page;
   }
-/*
+
   public function testReadExpandWrite() {
     $page = new TestPage();
     $page->get_text_from('User:Blocked Testing Account/readtest');
     $this->assertEquals('This page tests bots', $page->parsed_text());
     
-    $writeTestPage = 'User talk:Blocked Testing Account';
-    $trialCitation = '{{Cite journal | title Bot Testing | doi_broken_date=1986-01-01 | doi = 10.1038/nature09068}}';
-    
     $page = new TestPage();
+    $writeTestPage = 'User talk:Blocked Testing Account';
     $page->get_text_from($writeTestPage);
+    $trialCitation = '{{Cite journal | title Bot Testing | ' .
+      'doi_broken_date=1986-01-01 | doi = 10.1038/nature09068}}';
     $page->overwrite_text($trialCitation);
     $page->write("Testing bot write function");
+    
     $page->get_text_from($writeTestPage);
     $this->assertEquals($trialCitation, $page->parsed_text());
     $page->expand_text();
     $page->write();
-    $this->assertTrue(strpos($page->parsed_text(), 'Nature') > 5);
+  // TODO Restore this test once we've got a bot account that can make edits 
+  #  $page->get_text_from($writeTestPage);
+  #  $this->assertTrue(strpos($page->parsed_text(), 'Nature') > 5);
+  #  $this->assertTrue(strpos($page->edit_summary(), 'journal, ') > 3);
   }
-  */
+  
+  
   public function testRedirects() {
     $page = new Page();
     $page->get_text_from('NoSuchPage:ThereCan-tBe');
