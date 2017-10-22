25db384aba5645de8dbf529746641728a0413cd5
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-10-22 09:45:43 -0700

    Those comments are wrong

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -248,7 +248,7 @@ class TemplateTest extends PHPUnit\Framework\TestCase {
   public function testId2Param() {
       $text = '{{cite book |id=ISBN 978-1234-9583-068, DOI 10.1234/bashifbjaksn.ch2, {{arxiv|1234.5678}} 
         {{oclc|12354|4567}} {{oclc|1234}} {{ol|12345}} }}';
-      $expanded = $this->process_citation($text); // Not process_citation as there's an embedded template
+      $expanded = $this->process_citation($text);
       $this->assertEquals('978-1234-9583-068', $expanded->get('isbn'));
       $this->assertEquals('1234.5678', $expanded->get('arxiv'));
       $this->assertEquals('10.1234/bashifbjaksn.ch2', $expanded->get('doi'));
@@ -258,23 +258,23 @@ class TemplateTest extends PHPUnit\Framework\TestCase {
       $this->assertEquals(1, preg_match('~' . sprintf(Template::PLACEHOLDER_TEXT, '\d+') . '~i', $expanded->get('id')));
       
       $text = '{{cite book | id={{arxiv|id=1234.5678}}}}';
-      $expanded = $this->process_citation($text); // Not process_citation as there's an embedded template
+      $expanded = $this->process_citation($text);
       $this->assertEquals('1234.5678', $expanded->get('arxiv'));
       
       $text = '{{cite book | id={{arxiv|astr.ph|1234.5678}} }}';
-      $expanded = $this->process_citation($text); // Not process_citation as there's an embedded template
+      $expanded = $this->process_citation($text);
       $this->assertEquals('astr.ph/1234.5678', $expanded->get('arxiv'));     
   }
   
   
   public function testOrigYearHandling() {
       $text = '{{cite book |year=2009 | origyear = 2000 }}';
-      $expanded = $this->process_citation($text); // Not process_citation as there's an embedded template
+      $expanded = $this->process_citation($text);
       $this->assertEquals('2000', $expanded->get('origyear'));
       $this->assertEquals('2009', $this->getDateAndYear($expanded));
       
       $text = '{{cite book | origyear = 2000 }}';
-      $expanded = $this->process_citation($text); // Not process_citation as there's an embedded template
+      $expanded = $this->process_citation($text);
       $this->assertEquals('2000', $this->getDateAndYear($expanded));
       $this->assertNull($expanded->get('origyear'));
   }
