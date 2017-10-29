c4084ccf54d312ff39b85ef8e2a7a0cd7c888a2a
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-10-29 15:43:52 -0700

    SLOW Mode

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -130,7 +130,9 @@ class TemplateTest extends PHPUnit\Framework\TestCase {
      $SLOW_MODE = FALSE; // Otherwise we'll find a bibcode
      $text = '{{cite journal| p=546 |doi=10.1103/PhysRev.57.546|title=Nuclear Fission of Separated Uranium Isotopes |journal=Physical Review |volume=57 |issue=6 |year=1940 |last1=Nier |first1=Alfred O. |last2=Booth |first2=E. T. |last3=Dunning |first3=J. R. |last4=Grosse |first4=A. V. }}';
      $expanded = $this->process_citation($text);
+     $SLOW_MODE = TRUE;  // Reset it
      $this->assertEquals($text, $expanded->parsed_text());
+     
    }
   
   public function testUnknownJournal() {
