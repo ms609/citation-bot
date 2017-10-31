bc8175a16ec65cd497f5784cb31d5af10cf55d1b
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-10-31 07:02:04 -0700

    Just ignore them

--- expandFns.php
+++ expandFns.php
@@ -202,7 +202,10 @@ function restore_italics ($text) {
  */
 function title_capitalization($in, $caps_after_punctuation = TRUE) {
   // Use 'straight quotes' per WP:MOS
-  $new_case = straighten_quotes($in);
+  $new_case = straighten_quotes(trim($in));
+  if(substr($new_case,0,2) === "[[" && substr($new_case,-2) === "]]") {
+     return $new_case;  // And now we ignore wikilinked names since who knows what's going on there.  We would need to not break links.  Deal with [[Journal YZ|J. YZ]] etc.
+  }
   
   if ( $new_case == mb_strtoupper($new_case) 
      && mb_strlen(str_replace(array("[", "]"), "", trim($in))) > 6
@@ -234,15 +237,10 @@ function title_capitalization($in, $caps_after_punctuation = TRUE) {
     create_function('$matches', 'return "\'\'" . ucfirst(strtolower($matches[\'taxon\'])) . "\'\' " . strtolower($matches["nova"]);'),
     $new_case);
   
-  // Capitalization exceptions, e.g. Elife -> eLife
+  // Capitalization exceptions, e.g. Elife -> eLife  
   $new_case = str_replace(UCFIRST_JOURNAL_ACRONYMS, JOURNAL_ACRONYMS, " " .  $new_case . " ");
   $new_case = substr($new_case, 1, strlen($new_case) - 2); // remove spaces, needed for matching in LC_SMALL_WORDS
-  // And now with wikilinked names, e.g. [[Elife]] -> [[eLife]]
-  if(substr($new_case,0,2) === "[[" && substr($new_case,-2) === "]]") {
-    $new_case = substr(substr($new_case,2),0,-2); // Remove [[ and ]]
-    $new_case = str_replace(UCFIRST_JOURNAL_ACRONYMS, JOURNAL_ACRONYMS, " " .  $new_case . " ");
-    $new_case = '[[' . substr($new_case, 1, strlen($new_case) - 2) . ']]';
-  }
+
   /* I believe we can do without this now
   if (preg_match("~^(the|into|at?|of)\b~", $new_case)) {
     // If first word is a little word, it should still be capitalized
