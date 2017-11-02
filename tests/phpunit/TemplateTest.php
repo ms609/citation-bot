8ecb93a7aef2db0183e054afefd4fb13ec4842be
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-11-01 20:54:48 -0700

    One more check

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -69,6 +69,7 @@ class TemplateTest extends PHPUnit\Framework\TestCase {
     $expanded = $this->process_citation($text);
     $this->assertEquals('cite journal', $expanded->wikiname());
     $this->assertEquals('1941451', $expanded->get('pmid'));
+    $this->assertEquals('Brawn',$expanded->get('last1'));
       
     $text = "{{cite journal|pmid=1234567890}}";
     $expanded = $this->process_citation($text);
