ba48237e8f33069b487684c0867ce5affa942d98
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-11-02 21:06:47 -0700

    add test for testing servers

--- Template.php
+++ Template.php
@@ -1296,7 +1296,9 @@ class Template extends Item {
       if ($oclc) $url = $url . "+oclc:" . $oclc;
       if ($url === '') return FALSE;
       $url = "https://www.googleapis.com/books/v1/volumes?q=" . $url ;
-      // $url = $url . "&key=" . GOOGLE_KEY ;
+      if (strpos(gethostname(),'travis') !== FALSE ) { // Do not use on testing servers
+        $url = $url . "&key=" . GOOGLE_KEY ;
+      }
       $string = @file_get_contents($url); 
       if ($string === FALSE) {
         echo "\n Google APIs search failed for" ;
