ac7009023cea4491e3ec6ab14746b1c80116159b
Martin R. Smith <martins@gmail.com>
2017-11-05 07:02:27 +0000

    Revert "de-encode SICI"

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -535,7 +535,7 @@ ER -  }}';
   }   
   
   public function testJstorSICI() {
-       $text = '{{Cite journal|url=https://www.jstor.org/sici?sici=0003-0279(196101/03)81:1<43:WLIMP>2.0.CO;2-9}}';
+       $text = '{{Cite journal|url=https://www.jstor.org/sici?sici=0003-0279(196101%2F03)81%3A1%3C43%3AWLIMP%3E2.0.CO%3B2-9}}';
        $expanded = $this->process_citation($text);
        $this->assertEquals('594900', $expanded->get('jstor'));
    }
