0031b1da38b9d3ac2a70861f609ebb4a2a01a484
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-11-10 09:51:03 -0800

    better way

--- constants.php
+++ constants.php
@@ -29,6 +29,7 @@ const DOT_DECODE = array("/", "[", "{", "}", "]", "<", ">", ";", "(", ")");
 // Use lower case for all of these, and then compare to a lower cased version
 const BAD_AUTHORS = array("unknown","missing");
 const AUTHORS_ARE_PUBLISHERS = array(); // Things from google like "hearst magazines", "time inc", "nielsen business media, inc" that the catch alls do not detect
+const AUTHORS_ARE_PUBLISHERS_ENDINGS = array("inc.","inc","magazines","press","publishing","publishers","publishing","books","corporation");
 const HAS_NO_VOLUME = array("zookeys");  // Some journals have issues only, no volume numbers
 const BAD_TITLES = array("unknown","missing");
 
