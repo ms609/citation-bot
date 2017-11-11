b296d00dfba12ea827dbe5e74fa0f24cfa8119bf
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-11-11 14:11:15 -0800

    Update Comment.php

--- Comment.php
+++ Comment.php
@@ -1,14 +1,14 @@
 <?php
 /*
- * Extends Item. Contains constants and text-parsing functions for wikitext comments.
+ * Contains constants and text-parsing functions for wikitext comments.
  */
 
-require_once('Item.php');
-
-class Comment extends Item {
+class Comment {
   const PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_COMMENT %s # # #';
   const REGEXP = '~<!--.*?-->~us';
   const TREAT_IDENTICAL_SEPARATELY = FALSE;
+  protected $rawtext;
+  public $occurrences, $page;
 
   public function parse_text($text) {
     $this->rawtext = $text;
@@ -23,6 +23,8 @@ class Nowiki extends Item {
   const PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_NOWIKI %s # # #';  // Have space in nowiki so that it does not through some crazy bug match itself recursively
   const REGEXP = '~<nowiki>.*?</nowiki>~us'; 
   const TREAT_IDENTICAL_SEPARATELY = FALSE;
+  protected $rawtext;
+  public $occurrences, $page;
   
   public function parse_text($text) {
     $this->rawtext = $text;
