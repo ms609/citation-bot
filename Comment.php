042e4c7d9f13e0ff2482c49b94847c42d0639481
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-11-12 09:10:37 -0800

    Update Comment.php

--- Comment.php
+++ Comment.php
@@ -3,7 +3,7 @@
  * Contains constants and text-parsing functions for wikitext comments.
  */
 
-class Comment {
+final class Comment {
   const PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_COMMENT %s # # #';
   const REGEXP = '~<!--.*?-->~us';
   const TREAT_IDENTICAL_SEPARATELY = FALSE;
@@ -19,7 +19,7 @@ class Comment {
   }
 }
 
-class Nowiki {
+final class Nowiki {
   const PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_NOWIKI %s # # #';  // Have space in nowiki so that it does not through some crazy bug match itself recursively
   const REGEXP = '~<nowiki>.*?</nowiki>~us'; 
   const TREAT_IDENTICAL_SEPARATELY = FALSE;
