7a4925f5f1e48d6c44fe9a6ed868ba5153e6973f
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-11-13 06:54:11 -0800

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -309,8 +309,8 @@ class TemplateTest extends PHPUnit\Framework\TestCase {
       error_reporting(E_ALL^E_NOTICE);
       $expanded_page = $this->process_page($text);
       error_reporting(E_ALL);
-      $expanded_temp = $this->process_template($text);
-      $this->assertEquals($expanded_temp->parsed_text(), $expanded_page->parsed_text());
+      $expanded_citr = $this->process_citation($text);
+      $this->assertEquals($expanded_cite->parsed_text(), $expanded_page->parsed_text());
   }
     
   public function testOrigYearHandling() {
