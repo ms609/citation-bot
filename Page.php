d804e9263135194fe835814dcb1110f6fff595d6
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-11-15 13:12:47 -0800

    Internal templates parsed in page()

--- Page.php
+++ Page.php
@@ -115,6 +115,7 @@ final class Page {
     }
 
     for ($i = 0; $i < count($templates); $i++) {
+      $templates[$i]->internal_templates = &$templates;
       $templates[$i]->process();
       $template_mods = $templates[$i]->modifications();
       foreach (array_keys($template_mods) as $key) {
