b96aa1edf9cef526f32639d3589667bc9cfbf610
GlazerMann <kurt.glaesemann@pnnl.gov>
2017-11-19 18:47:01 -0800

    Update category.php

--- category.php
+++ category.php
@@ -44,7 +44,7 @@ if ($category) {
         . get_last_revision($page) . ">last edit</a></small></i>\n\n<br>"
         , ".");
       } else {
-         echo "\n # Failed. \n" . htmlspecialchars($page->text);
+         echo "\n # Failed. \n";
       }
     } else {
       echo "\n # " . ($page->text ? 'No changes required.' : 'Blank page') . "\n # # # ";
