53bb02bb4169e34300f047944ee66975b051e92a
GlazerMann <kurt@ioiowa.com>
2017-11-22 07:20:07 -0800

    add DOI in comments

--- tests/phpunit/expandFnsTest.php
+++ tests/phpunit/expandFnsTest.php
@@ -31,11 +31,12 @@ final class expandFnsTest extends PHPUnit\Framework\TestCase {
   }
      
   public function testMathInTitle() {
-    // This MML code comes from a real CroffRef search.  The <math> is the correct final code.
+    // This MML code comes from a real CroffRef search of DOI 10.1016/j.newast.2009.05.001
+    // $text_math is the correct final output
     $text_math = 'Spectroscopic analysis of the candidate <math><mrow>β</mrow></math> Cephei star <math><mrow>σ</mrow></math> Cas: Atmospheric characterization and line-profile variability';
     $text_mml  = 'Spectroscopic analysis of the candidate <mml:math altimg="si37.gif" overflow="scroll" xmlns:xocs="http://www.elsevier.com/xml/xocs/dtd" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.elsevier.com/xml/ja/dtd" xmlns:ja="http://www.elsevier.com/xml/ja/dtd" xmlns:mml="http://www.w3.org/1998/Math/MathML" xmlns:tb="http://www.elsevier.com/xml/common/table/dtd" xmlns:sb="http://www.elsevier.com/xml/common/struct-bib/dtd" xmlns:ce="http://www.elsevier.com/xml/common/dtd" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:cals="http://www.elsevier.com/xml/common/cals/dtd"><mml:mrow><mml:mi>β</mml:mi></mml:mrow></mml:math> Cephei star <mml:math altimg="si38.gif" overflow="scroll" xmlns:xocs="http://www.elsevier.com/xml/xocs/dtd" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.elsevier.com/xml/ja/dtd" xmlns:ja="http://www.elsevier.com/xml/ja/dtd" xmlns:mml="http://www.w3.org/1998/Math/MathML" xmlns:tb="http://www.elsevier.com/xml/common/table/dtd" xmlns:sb="http://www.elsevier.com/xml/common/struct-bib/dtd" xmlns:ce="http://www.elsevier.com/xml/common/dtd" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:cals="http://www.elsevier.com/xml/common/cals/dtd"><mml:mrow><mml:mi>σ</mml:mi></mml:mrow></mml:math> Cas: Atmospheric characterization and line-profile variability';
     $this->assertEquals($text_math,sanitize_string($text_math));      // Should not change
-    $this->assertEquals($text_math,wikify_external_text($text_math)); // Should not changeg
+    $this->assertEquals($text_math,wikify_external_text($text_math)); // Should not change
     $this->assertEquals($text_math,wikify_external_text($text_mml));  // The most important test: mml converstion to <math>
   }
 }
