9fd269792b3404e544147fc466e40ff18cd188c2
Martin R. Smith <martins@gmail.com>
2017-12-03 08:31:54 +0000

    Update to use new WikipediaBot class

--- wikiFunctions.php
+++ wikiFunctions.php
@@ -1,5 +1,8 @@
 <?php
 
+global $api = new WikipediaBot();
+$api->log_in();
+
 function category_members($cat){
   $vars = Array(
     "cmtitle" => "Category:$cat", // Don't URLencode.
@@ -11,9 +14,10 @@ function category_members($cat){
   $qc = "query-continue";
   $list = array();
 
+  global $api;
   do {
     set_time_limit(40);
-    $res = load_xml_via_bot($vars);
+    $res = $api->fetch($vars, 'POST');
     if ($res) {
       foreach ($res->query->categorymembers->cm as $page) {
           $list[] = (string) $page["title"];
@@ -32,29 +36,30 @@ function what_transcludes($template, $namespace=99){
 }
 
 function what_transcludes_2($template, $namespace = 99) {
-    $vars = Array (
-      "action" => "query",
-      "list" => "embeddedin",
-      "eilimit" => "5000",
-      "format" => "xml",
-      "eititle" => "Template:" . $template,
-      "einamespace" => ($namespace==99)?"":$namespace,
-   );
-   $list = ['title' => NULL];
+  $vars = Array (
+    "action" => "query",
+    "list" => "embeddedin",
+    "eilimit" => "5000",
+    "format" => "xml",
+    "eititle" => "Template:" . $template,
+    "einamespace" => ($namespace==99)?"":$namespace,
+  );
+  $list = ['title' => NULL];
   
-   do {
-        set_time_limit(20);
-        $res = load_xml_via_bot($vars);
-        if (!$res) {
-          echo 'Error reading API from ' . htmlspecialchars($url) . "\n";
-        } else {
-          foreach($res->query->embeddedin->ei as $page) {
-            $list["title"][] = (string) $page["title"];
-            $list["id"][] = (integer) $page["pageid"];
-          }
-        }
-    } while ($vars["eicontinue"] = (string) $res->{"query-continue"}->embeddedin["eicontinue"]);
-    return $list;
+  global $api;  
+  do {
+    set_time_limit(20);
+    $res = $api->fetch($vars, 'POST');
+    if (!$res) {
+      echo 'Error reading API from ' . htmlspecialchars($url) . "\n";
+    } else {
+      foreach($res->query->embeddedin->ei as $page) {
+        $list["title"][] = (string) $page["title"];
+        $list["id"][] = (integer) $page["pageid"];
+    }
+    }
+  } while ($vars["eicontinue"] = (string) $res->{"query-continue"}->embeddedin["eicontinue"]);
+  return $list;
 }
 
 /**
@@ -65,18 +70,19 @@ function wikititle_encode($in) {
   return str_replace(DOT_DECODE, DOT_ENCODE, $in);
 }
 
-function get_last_revision($page){
-  $xml = load_xml_via_bot(Array(
+function get_last_revision($page) {
+  global $api;
+  $res = $api->fetch(Array(
       "action" => "query",
       "prop" => "revisions",
       "format" => "xml",
       "titles" => $page,
     ));
-  if ($xml === FALSE) {
+  if (!isset($api->query->pages->page->revisions->rev)) {
       echo "\n Failed to get article last revision \n";
       return FALSE;
   }
-  return $xml->query->pages->page->revisions->rev["revid"];
+  return $api->query->pages->page->revisions->rev["revid"];
 }
 
 function get_prefix_index($prefix, $namespace = 0, $start = "") {
@@ -91,9 +97,10 @@ function get_prefix_index($prefix, $namespace = 0, $start = "") {
     "apprefix" => $prefix,
     "aplimit" => "5000",
   );
+  global $api;
   do {
     set_time_limit(10);
-    $res = load_xml_via_bot($vars);
+    $res = $api->fetch($vars, 'POST');
     if ($res && !$res->error) {
       foreach ($res->query->allpages->p as $page) {
         $page_titles[] = (string) $page["title"];
@@ -113,13 +120,14 @@ function get_prefix_index($prefix, $namespace = 0, $start = "") {
  * @codeCoverageIgnore
  */
 function get_article_id($page) {
-  $xml = load_xml_via_bot(Array(
+  global $api;
+  $res = $api->fetch((Array(
       "action" => "query",
       "format" => "xml",
       "prop" => "info",
       "titles" => $page,
-      ));
-  if ($xml === FALSE) {
+      ), 'POST');
+  if (!isset($xml->query->pages->page)) {
       echo "\n Failed to get article ID \n";
       return FALSE;
   }
@@ -127,12 +135,13 @@ function get_article_id($page) {
 }
 
 function get_namespace($page) {
-    $xml = load_xml_via_bot(Array("action" => "query",
+  global $api;
+  $res = $api->fetch(Array("action" => "query",
       "format" => "xml",
       "prop" => "info",
       "titles" => $page,
       ));
-  if ($xml === FALSE) {
+  if (!isset($res->query->pages->page)) {
       echo "\n Failed to get article namespace \n";
       return FALSE;
   }
@@ -140,21 +149,21 @@ function get_namespace($page) {
 }
 
 function is_redirect($page) {
-  $url = Array(
+  global $api;
+  $res = $api->fetch(Array(
       "action" => "query",
       "format" => "xml",
       "prop" => "info",
       "titles" => $page,
-      );
-  $xml = load_xml_via_bot($url);
-  if ($xml === FALSE) {
+      ), 'POST');
+  if (!isset($res->query->pages->page)) {
       echo "\n Failed to get redirect status \n";
       return array (-1, NULL);
   }
-  if ($xml->query->pages->page["pageid"]) {
+  if ($res->query->pages->page["pageid"]) {
     // Page exists
-    return array ((($xml->query->pages->page["redirect"]) ? 1 : 0),
-                    $xml->query->pages->page["pageid"]);
+    return array ((($res->query->pages->page["redirect"]) ? 1 : 0),
+                    $res->query->pages->page["pageid"]);
   } else {
       return array (-1, NULL);
   }
@@ -165,14 +174,14 @@ function is_redirect($page) {
  * @codeCoverageIgnore
  */
 function redirect_target($page) {
-  $url = Array(
+  global $api;
+  $res = $api->fetch(Array(
       "action" => "query",
       "format" => "xml",
       "redirects" => "1",
       "titles" => $page,
-      );
-  $xml = load_xml_via_bot($url);
-  if ($xml === FALSE) {
+      ), 'POST');
+  if (!isset($res->pages->page)) {
       echo "\n Failed to get redirect target \n";
       return FALSE;
   }
@@ -184,20 +193,24 @@ function redirect_target($page) {
  * @codeCoverageIgnore
  */
 function parse_wikitext($text, $title = "API") {
-  $bot = new WikipediaBot();
+  global $api;
   $vars = array(
         'format' => 'json',
         'action' => 'parse',
         'text'   => $text,
         'title'  => $title,
     );
-  $a = @json_decode($bot->post(API_ROOT, $vars));
-  if (!$a) {
+  $res = $api->fetch($vars, 'POST');
+  if (!$res) {
     // Wait a sec and try again
     sleep(2);
-    $a = @json_decode($bot->post(API_ROOT, $vars));
+    $res = $api->fetch($vars), 'POST');
   }
-  return $a->parse->text->{"*"};
+  if (!isset($res->parse->text)) {
+    trigger_error("Could not parse text of $title.", E_USER_WARNING);
+    return FALSE;
+  }
+  return $res->parse->text->{"*"};
 }
 
 function namespace_id($name) {
@@ -257,11 +270,6 @@ function wiki_link($page, $style = "#036;", $target = NULL) {
   if (!$target) $target = $page;
   $css = $style?" style='color:$style !important'":"";
   return "<a href='" . WIKI_ROOT . "?title=" . urlencode($target) . "' title='$page ($target) on Wikipedia'$css>$page</a>";
-}
-
-function load_xml_via_bot($vars) {
-  $bot = new WikipediaBot();
-  return @simplexml_load_string($bot->post(API_ROOT, $vars));
 }
 
 /**
