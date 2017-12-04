cbda25608c5e56f3bfdef5b5fb2ff4b04d7b3a5c
GlazerMann <kurt@ioiowa.com>
2017-12-04 15:19:10 -0800

    Update wikiFunctions.php

--- wikiFunctions.php
+++ wikiFunctions.php
@@ -12,7 +12,7 @@ function wiki_link($page, $style = "#036;", $target = NULL) {
 
 function is_valid_user($user) {
   if (!$user) return FALSE;
-  $headers_test = @get_headers('https://en.wikipedia.org/wiki/User:' . urlencode($user), 1);
+  $headers_test = FALSE;
   if ($headers_test === FALSE) return FALSE;
   if (strpos((string) $headers_test[0], '404')) return FALSE;  // Even non-existent pages for valid users do exist.  They redirect, but do exist
   return TRUE;
