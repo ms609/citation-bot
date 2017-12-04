98307639ab93c110ed263047656dce146e4e7a74
Martin R. Smith <martins@gmail.com>
2017-12-04 09:01:20 +0000

    Remove duplicate test

--- tests/phpunit/WikipediaBotTest.php
+++ tests/phpunit/WikipediaBotTest.php
@@ -19,33 +19,7 @@ class WikipediaBotTest extends PHPUnit\Framework\TestCase {
 
   protected function tearDown() {
   }
-  
-  public function testReadExpandWrite() {
-    $api = new WikipediaBot();
-    $page = new TestPage();
-    $page->get_text_from('User:Blocked Testing Account/readtest');
-    $this->assertEquals('This page tests bots', $page->parsed_text());
     
-    $page = new TestPage();
-    $writeTestPage = 'User:Blocked Testing Account/writetest';
-    $page->get_text_from($writeTestPage);
-    $trialCitation = '{{Cite journal | title Bot Testing | ' .
-      'doi_broken_date=1986-01-01 | doi = 10.1038/nature09068}}';
-    $page->overwrite_text($trialCitation);
-    $this->assertFalse(@$page->write("Testing bot write function")); // False because Travis IPs are blocked from editing
-    
-    $page->get_text_from($writeTestPage);
-    $this->assertEquals($trialCitation, $page->parsed_text());
-    $page->expand_text();
-    $this->assertTrue(strpos($page->edit_summary(), 'journal, ') > 3);
-    $this->assertTrue(strpos($page->edit_summary(), ' Removed ') > 3);
-    $page->write($api);
-    
-    $page->get_text_from($writeTestPage);
-    print $page->parsed_text();
-    $this->assertTrue(strpos($page->parsed_text(), 'Nature') > 5);
-  }
-  
   public function testCategoryMembers() {
     $api = new WikipediaBot();
     $this->assertTrue(count($api->category_members('GA-Class cricket articles')) > 10);
