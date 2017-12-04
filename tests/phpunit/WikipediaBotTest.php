7766fcc8264edbe9973598941eded159aeebb823
Martin R. Smith <martins@gmail.com>
2017-12-04 08:44:46 +0000

    Move tests to files corresponding to the file they're testing.

--- tests/phpunit/WikipediaBotTest.php
+++ tests/phpunit/WikipediaBotTest.php
@@ -49,5 +49,78 @@ class WikipediaBotTest extends PHPUnit\Framework\TestCase {
     $this->assertTrue(strpos($page->parsed_text(), 'Nature') > 5);
   }
   
+  public function testCategoryMembers() {
+    $this->assertTrue(count(category_members('GA-Class cricket articles')) > 10);
+    $this->assertEquals(0, count(category_members('A category we expect to be empty')));
+  }
+  
+  public function testWhatTranscludes() {
+    $this->assertTrue(count(what_transcludes('Graphical timeline')) > 10);
+  }
+    
+  public function testGetPrefixIndex() {
+    $namespace = get_namespace('Template:Cite journal');
+    $this->assertEquals(namespace_id('Template'), $namespace);
+    $results = get_prefix_index('Cite jo', $namespace); // too many results if we just use 'Cite'
+    $this->assertTrue(array_search('Template:Cite journal', $results) !== FALSE);
+    $results = get_prefix_index("If we retrieve anything here, it's an error", $namespace);
+    $this->assertTrue(empty($results));
+  }
+  
+  public function testRedirects() {
+    $this->assertEquals(-1, is_redirect('NoSuchPage:ThereCan-tBe'));
+    $this->assertEquals( 0, is_redirect('User:Citation_bot'));
+    $this->assertEquals( 1, is_redirect('WP:UCB'));
+  }  
+  
+  public function testNamespaces() {
+    global $api;
+    $vars = array(
+          'format' => 'json',
+          'action' => 'query',
+          'meta'   => 'siteinfo',
+          'siprop'  => 'namespaces',
+      );
+    $namespaces = $api->fetch($vars, 'POST');
+    
+    foreach ($namespaces->query->namespaces as $ns) {
+      $ns_name = isset($ns->canonical)? $ns->canonical : '';
+      $ns_id = (string) $ns->id;
+      $this->assertEquals($ns_id, namespace_id($ns_name));
+      $this->assertEquals($ns_name, namespace_name($ns_id));
+    }
+    
+    /*
+    // If the above assertions are throwing an error, you can generate an updated 
+    // version of constants/namespace.php by running the below and pasting the content:
+    echo "\n\n! Namespaces are out of date. Please update constants/namespace.php with the below:\n\n";
+    echo "<?php\nconst NAMESPACES = Array(";
+    foreach ($namespaces->query->namespaces as $ns) {
+      $ns_name = isset($ns->canonical)? $ns->canonical : '';
+      echo ("\n  " . (string) $ns->id . " => '" . $ns_name . "',");
+    }
+    echo ");\n\nconst NAMESPACE_ID = Array(";
+    foreach ($namespaces->query->namespaces as $ns) {
+      $ns_name = isset($ns->canonical)? $ns->canonical : '';
+      echo ("\n  '" . strtolower($ns_name) . "' => " . (string) $ns->id . ",");
+    }
+    echo "\n);\n?" . ">\n";
+    exit(0);
+    */
+  }
+  
+  public function testIsValidUser() {
+      $result = is_valid_user('Smith609');
+      $this->assertEquals(TRUE, $result);
+      $result = is_valid_user('Stanlha'); // Random user who at this point (Nov 2017) does not have page, but does exists
+      $this->assertEquals(TRUE, $result);
+      $result = is_valid_user('Not_a_valid_user_at_Dec_2017'); 
+      $this->assertEquals(FALSE, $result);
+  }
+    
+  public function testGetLastRevision() {
+    $this->assertTrue(is_int(1 * get_last_revision('User talk:Citation bot')));
+  }
+   
 }
 
