5a86d1882d11c195d8de000c8ad41d870b14822b
GlazerMann <kurt@ioiowa.com>
2017-12-16 20:13:30 -0800

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -46,22 +46,4 @@ final class TemplateTest extends PHPUnit\Framework\TestCase {
      $this->assertEquals('1932Natur.129Q..18.', $expanded->get('bibcode'));
   }
    
-
- public function testBadBibcodeARXIVPages() { // Some bibcodes have pages set to arXiv:1711.02260
-    $text = '{{cite journal|bibcode=2017arXiv171102260L}}';
-    $expanded = $this->process_citation($text);
-    $pages = $expanded->get('pages');
-    $volume = $expanded->get('volume');
-    $this->assertEquals(FALSE, stripos($pages, 'arxiv'));
-    $this->assertEquals(FALSE, stripos('1711', $volume));
-    $this->assertNull($expanded->get('journal'));  // if we get a journal, the the data is updated and test probably no longer gets bad data
- }
-    
-
-
-  /* TODO 
-  Test adding a paper with > 4 editors; this should trigger displayeditors
-  Test finding a DOI and using it to expand a paper [See testLongAuthorLists - Arxiv example?]
-  Test adding a doi-is-broken modifier to a broken DOI.
-  */    
 }
