9ebda5348395d9f01e7f5cd3015d4aea3be2cd39
GlazerMann <kurt@ioiowa.com>
2017-12-16 17:19:10 -0800

    Update constantsTest.php

--- tests/phpunit/constantsTest.php
+++ tests/phpunit/constantsTest.php
@@ -19,14 +19,5 @@ final class constantsTest extends PHPUnit\Framework\TestCase {
   protected function tearDown() {
   }
 
-  public function testConstantsDefined() {
-    $this->assertEquals(PIPE_PLACEHOLDER, '# # # CITATION_BOT_PLACEHOLDER_PIPE # # #');
-    for ($i = 0; $i < sizeof(JOURNAL_ACRONYMS); $i++) {
-      $this->assertEquals(UCFIRST_JOURNAL_ACRONYMS[$i], mb_convert_case(JOURNAL_ACRONYMS[$i], MB_CASE_TITLE, "UTF-8"));
-    }
-    for ($i = 1; $i < sizeof(LC_SMALL_WORDS); $i++) { ## Not 0, which is "and Then"
-      $this->assertEquals(UC_SMALL_WORDS[$i], mb_convert_case(LC_SMALL_WORDS[$i], MB_CASE_TITLE, "UTF-8"));
-    }
-  }
   
 }
