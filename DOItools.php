39541d941c04254aea19d2ad0a595653cfeb3733
GlazerMann <kurt@ioiowa.com>
2018-07-08 20:51:21 -0700

    Remove duplicate comment

--- DOItools.php
+++ DOItools.php
@@ -112,7 +112,7 @@ function format_author($author){
 	// Requires an author who is formatted as SURNAME, FORENAME or SURNAME FORENAME or FORENAME SURNAME. Substitute initials for forenames if nec.
   $surname = NULL;
   // Google sometimes has these
-  $author=str_ireplace("(sir.)","",$author);  // Google sometimes has these
+  $author=str_ireplace("(sir.)","",$author);
   $author=str_ireplace("(sir)","",$author);
 
   if (substr(trim($author), -1) === ".") {
