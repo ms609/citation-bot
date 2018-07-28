88ae5754dd2b5a2bc0cb70b1e2caf34a6a9e764a
ms609 <martins@gmail.com>
2018-07-28 12:33:57 +0100

    Verbose return

--- gitpull.php
+++ gitpull.php
@@ -1,11 +1,11 @@
 <html>
-Pulling <a href="https://github.com/ms609/citation-bot">latest Git repo</a>...
+Pulling <a href="https://github.com/ms609/citation-bot">latest Git repo</a>:
 <?php
 exec ("git pull", $output, $return_var);
 var_dump($return_var);
 ?>
 <pre>
-<?php print_r($output); ?>
+<?php for ($line in $output) print "$line \n"; ?>
 </pre>
 Operation complete.
 </html>
