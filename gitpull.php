9e7aedf59d021779fd529bab25f636407c28173c
ms609 <martins@gmail.com>
2018-07-28 12:32:50 +0100

    Better display

--- gitpull.php
+++ gitpull.php
@@ -1,7 +1,11 @@
 <html>
 Pulling <a href="https://github.com/ms609/citation-bot">latest Git repo</a>...
 <?php
-exec ("git pull");
+exec ("git pull", $output, $return_var);
+var_dump($return_var);
 ?>
+<pre>
+<?php print_r($output); ?>
+</pre>
 Operation complete.
 </html>
