5172cfd39dd9024db5d28866ff1401276a8de52d
GlazerMann <kurt@ioiowa.com>
2018-07-29 06:38:19 -0700

    Test matches

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -580,7 +580,7 @@ ER -  }}';
   public function testCapsAfterColonAndPeriod() {
       $text = '{{Cite journal |journal=Historical Biology: An International Journal. Of Paleobiology}}';
       $expanded = $this->process_citation($text);
-      $this->assertEquals('Historical Biology: An International Journal. Of Paleobiology', $expanded->get('journal'));
+      $this->assertEquals('Historical Biology: An International Journal. of Paleobiology', $expanded->get('journal'));
   }  
   public function testExistingWikiText() { // checks for formating in tidy() not breaking things
       $text = '{{cite journal|title=[[Zootimeboys]] and Girls|journal=[[Zootimeboys]] and Girls}}';
