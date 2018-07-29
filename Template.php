6be8805ebf30c6d5a03ae6a3cf42590e59260bb0
GlazerMann <kurt@ioiowa.com>
2018-07-28 19:31:37 -0700

    typo

--- Template.php
+++ Template.php
@@ -2263,7 +2263,7 @@ final class Template {
               $p->val = mb_ereg_replace(TO_EN_DASH, EN_DASH, $p->val);
             }
             if ( ($pmatch[1] !== 'issue') && (substr_count($p->val, "–") === 1) && (stripos($p->val, "http") === FALSE)) { // Exactly one EN_DASH.  Do not change issue 3-3
-              preg_match("~(.+)"–~" , $p->val, $part1);
+              preg_match("~(.+)–~" , $p->val, $part1);
               $part1[0] = substr($part1[0], 0, -1); // Remove ending dash
               preg_match("~–(.+)~" , $p->val, $part2);
               $part2[0] = substr($part2[0], 1); // Remove starting dash
