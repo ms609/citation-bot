549a05304e1ee180b1a37629d392f313f2848f71
GlazerMann <kurt@ioiowa.com>
2018-07-30 15:42:18 -0700

    Update constantsTest.php

--- tests/phpunit/constantsTest.php
+++ tests/phpunit/constantsTest.php
@@ -10,14 +10,28 @@ if (!class_exists('\PHPUnit\Framework\TestCase') &&
     class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
 }
 
-
+function arxiv_callable_error_handler($errno,$errstr,$errfile,$errline) {
+      if ($errno === 1024 && $errstr === "API Error in query_adsabs: Unauthorized" && getenv('TRAVIS')) {
+          echo "\n -API Error in query_adsabs: Unauthorized";
+          return TRUE;
+      } elseif ($errno === 1024 && $errstr === "Error in query_adsabs: Could not decode AdsAbs response" && getenv('TRAVIS')) {
+          echo "\n -Error in query_adsabs: Could not decode AdsAbs response";
+          return TRUE;
+      } else {
+          echo "\n STRING IS " . $errstr ;
+          echo "\n ERRNUM IS " . $errno ;
+          return FALSE;
+      }
+}
 final class constantsTest extends PHPUnit\Framework\TestCase {
 
   protected function setUp() {
+     set_error_handler("arxiv_callable_error_handler");
   }
-
   protected function tearDown() {
+     set_error_handler(NULL);
   }
+  
 
   public function testConstantsDefined() {
     $this->assertEquals(PIPE_PLACEHOLDER, '# # # CITATION_BOT_PLACEHOLDER_PIPE # # #');
