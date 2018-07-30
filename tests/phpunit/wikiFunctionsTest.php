2ed6291a2870b6bca453f2ca9880d65569a2f410
GlazerMann <kurt@ioiowa.com>
2018-07-30 15:42:24 -0700

    Update wikiFunctionsTest.php

--- tests/phpunit/wikiFunctionsTest.php
+++ tests/phpunit/wikiFunctionsTest.php
@@ -8,13 +8,28 @@ if (!class_exists('\PHPUnit\Framework\TestCase') &&
 if (!defined('VERBOSE')) define('VERBOSE', TRUE);
 $SLOW_MODE = TRUE;
  
+function arxiv_callable_error_handler($errno,$errstr,$errfile,$errline) {
+      if ($errno === 1024 && $errstr === "API Error in query_adsabs: Unauthorized" && getenv('TRAVIS')) {
+          echo "\n -API Error in query_adsabs: Unauthorized";
+          return TRUE;
+      } elseif ($errno === 1024 && $errstr === "Error in query_adsabs: Could not decode AdsAbs response" && getenv('TRAVIS')) {
+          echo "\n -Error in query_adsabs: Could not decode AdsAbs response";
+          return TRUE;
+      } else {
+          echo "\n STRING IS " . $errstr ;
+          echo "\n ERRNUM IS " . $errno ;
+          return FALSE;
+      }
+}
 final class wikiFunctionsTest extends PHPUnit\Framework\TestCase {
   
   protected function setUp() {
+     set_error_handler("arxiv_callable_error_handler");
   }
-
   protected function tearDown() {
+     set_error_handler(NULL);
   }
+  
   
   public function testIsValidUser() {
     $result = is_valid_user('Smith609');
