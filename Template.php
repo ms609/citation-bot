c2a6033654053819593de79755fa634fcd7327e0
GlazerMann <kurt@ioiowa.com>
2018-08-11 13:10:18 -0700

    minimize changes

--- Template.php
+++ Template.php
@@ -1631,34 +1631,34 @@ final class Template {
         }
         // Check if best location is already linked -- avoid double linking
         if (preg_match("~^https?://europepmc\.org/articles/pmc(\d+)~", $best_location->url_for_landing_page, $match) || preg_match("~^https?://www\.pubmedcentral\.nih\.gov/articlerender.fcgi\?.*\bartid=(\d+)"
-                      . "|^https?://www\.ncbi\.nlm\.nih\.gov/pmc/articles/PMC(\d+)~",$best_location->url_for_landing_page, $match)) {
+                      . "|^https?://www\.ncbi\.nlm\.nih\.gov/pmc/articles/PMC(\d+)~", $best_location->url_for_landing_page, $match)) {
           if ($this->has('pmc') ) {
-             return TRUE; // The best location is already linked to by the PMC link
+             return TRUE;
           }
         }
         if (preg_match("~\barxiv\.org/.*(?:pdf|abs)/(.+)$~", $best_location->url_for_landing_page, $match)) {
           if ($this->has('arxiv') || $this->has('eprint')) {
-             return TRUE; // The best location is already linked to by the ARXIV link
+             return TRUE;
           }
         }
         if (strpos($best_location->url_for_landing_page, 'hdl.handle.net') !== false) {
           if ($this->has('hdl') ) {
-             return TRUE; // The best location is already linked to by the HDL link
+             return TRUE;
           }
         }
         if (strpos($best_location->url_for_landing_page, 'citeseerx.ist.ps.edu') !== false) {
           if ($this->has('citeseerx') ) {
-             return TRUE; // The best location is already linked to by the citeseerx link
+             return TRUE;
           }
         }
         if (preg_match(BIBCODE_REGEXP, urldecode($best_location->url_for_landing_page), $bibcode)) {
            if ($this->has('bibcode')) {
-             return TRUE; // The best location is already linked to by the bibcode link 
+             return TRUE;
           }
         }
         if (preg_match("~https?://www.ncbi.nlm.nih.gov/pubmed/.*?=?(\d{6,})~", $best_location->url_for_landing_page, $match)) {
           if ($this->has('pmid')) {
-             return TRUE; // The best location is already linked to by the pmid link 
+             return TRUE;
           }
         }
 
