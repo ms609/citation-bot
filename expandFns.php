8be423414b9286530fe9f584f2b4199e9c3138c5
GlazerMann <kurt@ioiowa.com>
2018-08-11 08:29:29 -0700

    Hexidecimal vs base ten

--- expandFns.php
+++ expandFns.php
@@ -222,12 +222,12 @@ function title_capitalization($in, $caps_after_punctuation) {
     trim($new_case)
   );
   
-  $new_case = preg_replace_callback("~ (L[\'\{FF07}\{055A}\{00B4}\{2019}])([a-zÀ-ÿ]+)~", function($matches) { /** French l'Words  **/
+  // '=straight, ff07=full width, 055A=armenian, 00B4=acute accent, 2019 = right quotation (All in hexadecimal)
+  $new_case = preg_replace_callback("~ (L[\'\x{FF07}\x{055A}\x{00B4}\x{2019}])([a-zÀ-ÿ]+)~", function($matches) { /** French l'Words  **/
     return " l'" . mb_ucfirst($matches[2]);
   }, ' ' . $new_case);
   $new_case = mb_ucfirst(trim($new_case));
-  // '=straight, ff07=full width, 055A=armenian, 00B4=acute accent, 2019 = right quotation
-  $new_case = preg_replace_callback("~ (D[\'\{FF07}\{055A}\{00B4}\{2019}])([a-zÀ-ÿ]+)~", function($matches) { /** French d'Words  **/
+  $new_case = preg_replace_callback("~ (D[\'\x{FF07}\x{055A}\x{00B4}\x{2019}])([a-zÀ-ÿ]+)~", function($matches) { /** French d'Words  **/
     return " d'" . mb_ucfirst($matches[2]);
   }, ' ' . $new_case);
   $new_case = mb_ucfirst(trim($new_case));
