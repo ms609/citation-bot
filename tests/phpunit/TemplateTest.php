63bba1e9b7e572ddcd95f28d7cedc7d9b704181b
GlazerMann <kurt@ioiowa.com>
2018-08-12 13:28:55 -0700

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -1,8 +1,4 @@
 <?php
-
-/*
- * Tests for Template.php, called from expandFns.php.
- */
 error_reporting(E_ALL);
 // backward compatibility
 if (!class_exists('\PHPUnit\Framework\TestCase') &&
@@ -10,9 +6,6 @@ if (!class_exists('\PHPUnit\Framework\TestCase') &&
     class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
 }
 
-// Initialize bot configuration
-if (!defined('VERBOSE')) define('VERBOSE', TRUE);
-$SLOW_MODE = TRUE;
  
 final class TemplateTest extends PHPUnit\Framework\TestCase {
 
@@ -22,14 +15,6 @@ final class TemplateTest extends PHPUnit\Framework\TestCase {
   protected function tearDown() {
   }
   
-  protected function requires_secrets($function) {
-    if (getenv('TRAVIS_PULL_REQUEST')) {
-      echo "\n - Skipping test: Risks exposing secret keys";
-      $this->assertNull(NULL); // Make Travis think we tested something
-    } else {
-      $function();
-    }
-  }
   
   protected function process_citation($text) {
     $page = new TestPage();
@@ -40,13 +25,6 @@ final class TemplateTest extends PHPUnit\Framework\TestCase {
     $template->parse_text($expanded_text);
     return $template;
   }
-  
-  protected function process_page($text) {  // Only used if more than just a citation template
-    $page = new TestPage();
-    $page->parse_text($text);
-    $page->expand_text();
-    return $page;
-  }
 
   public function testParameterWithNoParameters() {
     $text = "{{Cite web | testy1= | testy2=}}';
@@ -54,6 +32,4 @@ final class TemplateTest extends PHPUnit\Framework\TestCase {
     $expanded->rename('testy2','testy1');
     $this->assetNull($expanded->parsed_text());
   }
-  
- } 
 }
