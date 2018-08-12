4d040eaba77bfc5313e683c5457c974058db11d3
GlazerMann <kurt@ioiowa.com>
2018-08-12 13:43:14 -0700

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -30,6 +30,6 @@ final class TemplateTest extends PHPUnit\Framework\TestCase {
     $text = '{{Cite web | testy1= | testy2=}}';
     $expanded = $this->process_citation($text);
     $expanded->rename('testy2','testy1');
-    $this->assetNull($expanded->parsed_text());
+    echo $expanded->parsed_text();
   }
 }
