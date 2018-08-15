a37c10e068ccfabecd9983dc7f8ef2ce551c00c5
GlazerMann <kurt@ioiowa.com>
2018-08-14 20:12:10 -0700

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -785,7 +785,7 @@ ER -  }}';
    }
     
    public function testSmallWords() {
-       $text = '{{cite journal|journal=A Word in ny and n y ABOUT cow And Then boys the U S A and y and z}}';
+       $text = '{{cite journal|journal=A Word in ny and n y About cow And Then boys the U S A and y and z}}';
        $expanded = $this->process_citation($text);
        $this->assertEquals('A Word in NY and N Y About Cow and then Boys the U S A and y and Z', $expanded->get('journal'));
           
