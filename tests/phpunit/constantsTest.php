438ac99a74f9d37fbb8119babfe90fe864212326
GlazerMann <kurt@ioiowa.com>
2018-08-15 07:42:21 -0700

    Test that they all work in one giant title

--- tests/phpunit/constantsTest.php
+++ tests/phpunit/constantsTest.php
@@ -24,17 +24,29 @@ final class constantsTest extends PHPUnit\Framework\TestCase {
     for ($i = 0; $i < sizeof(JOURNAL_ACRONYMS); $i++) {
       $this->assertEquals(UCFIRST_JOURNAL_ACRONYMS[$i], mb_convert_case(JOURNAL_ACRONYMS[$i], MB_CASE_TITLE, "UTF-8"));
     }
+    $text_in  = 'Start ';
+    $text_out = 'Start ';
     for ($i = 0; $i < sizeof(LC_SMALL_WORDS); $i++) {
+      // Verify that they match
       if (substr_count(UC_SMALL_WORDS[$i], ' ') === 2) {
         $this->assertEquals(UC_SMALL_WORDS[$i], mb_convert_case(LC_SMALL_WORDS[$i], MB_CASE_TITLE, "UTF-8"));
       } else {  // Weaker test for things with internal spaces
         $this->assertEquals(strtolower(UC_SMALL_WORDS[$i]), strtolower(LC_SMALL_WORDS[$i]));
       }
+      // Verify that they are padded with a space
       $this->assertEquals   (' ', mb_substr(UC_SMALL_WORDS[$i], -1, 1));
       $this->assertEquals   (' ', mb_substr(UC_SMALL_WORDS[$i],  0, 1));
       $this->assertNotEquals(' ', mb_substr(UC_SMALL_WORDS[$i], -2, 1));
-      $this->assertNotEquals(' ', mb_substr(UC_SMALL_WORDS[$i],  1, 1));    
+      $this->assertNotEquals(' ', mb_substr(UC_SMALL_WORDS[$i],  1, 1)); 
+      $text_out = $text_out . LC_SMALL_WORDS[$i];
+      $text_in  = $text_in  . UC_SMALL_WORDS[$i];
     }
+    // Test that they all work in one giant title
+    $text_in  = $text_in  . ' Ending';
+    $text_out = $text_out . ' Ending';
+    $text = '{{cite journal|journal=$text_in}}';
+    $expanded = $this->process_citation($text);
+    $this->assertEquals($text_out, $expanded->get('journal')); 
   }
   
 }
