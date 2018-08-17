4131320e8871c1833944ef6f457e24408db50072
ms609 <martins@gmail.com>
2018-08-17 20:46:39 +0800

    Remove redundant (?) CSS

--- doibot.php
+++ doibot.php
@@ -6,32 +6,11 @@
 		<link rel="apple-touch-icon" href="https://en.wikipedia.org/apple-touch-icon.png" />
 		<link rel="copyright" href="https://www.gnu.org/copyleft/fdl.html" />
 		<title>Citation bot: Preparing to run</title>
-		<style type="text/css" media="screen, projection">/*<![CDATA[*/
-			@import "https://en.wikipedia.org/skins-1.5/common/shared.css?144";
-			@import "https://en.wikipedia.org/skins-1.5/monobook/main.css?144";
-		/*]]>*/</style>
-		<link rel="stylesheet" type="text/css" media="print" href="https://en.wikipedia.org/skins-1.5/common/commonPrint.css?144" />
-		<!--[if lt IE 5.5000]><style type="text/css">@import "https://en.wikipedia.org/skins-1.5/monobook/IE50Fixes.css?144";</style><![endif]-->
-		<!--[if IE 5.5000]><style type="text/css">@import "https://en.wikipedia.org/skins-1.5/monobook/IE55Fixes.css?144";</style><![endif]-->
-		<!--[if IE 6]><style type="text/css">@import "https://en.wikipedia.org/skins-1.5/monobook/IE60Fixes.css?144";</style><![endif]-->
-		<!--[if IE 7]><style type="text/css">@import "https://en.wikipedia.org/skins-1.5/monobook/IE70Fixes.css?144";</style><![endif]-->
-		<!--[if lt IE 7]><script type="text/javascript" src="https://en.wikipedia.org/skins-1.5/common/IEFixes.js?144"></script>
-		<meta http-equiv="imagetoolbar" content="no" /><![endif]-->
-   	<style type="text/css">/*<![CDATA[*/ @import "https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-navpop.css&action=raw&ctype=text/css"; /*]]>*/</style>
-
-		<script type="text/javascript" src="https://en.wikipedia.org/w/index.php?title=-&amp;action=raw&amp;smaxage=0&amp;gen=js&amp;useskin=monobook"><!-- site js --></script>
-		<style type="text/css">/*<![CDATA[*/
-@import "https://en.wikipedia.org/w/index.php?title=MediaWiki:Common.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=2678400";
-@import "https://en.wikipedia.org/w/index.php?title=MediaWiki:Monobook.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=2678400";
-@import "https://en.wikipedia.org/w/index.php?title=-&action=raw&gen=css&maxage=2678400&smaxage=0&ts=20080516172257";
-@import "https://en.wikipedia.org/w/index.php?title=User:Smith609/monobook.css&action=raw&ctype=text/css";
-/*]]>*/</style>
 	</head>
 <body class="mediawiki ns-2 ltr">
 	<div id="globalWrapper">
 		<div id="column-content">
       <div id="content">
-        <h1 class="firstHeading">Welcome to Citation Bot</h1>
         <div id="bodyContent">
           <h3 id="siteSub">Please wait while the <a href="https://en.wikipedia.org/wiki/User:Citation_bot">Citation bot</a> processes the page you requested.</h3>
             <pre><?php
