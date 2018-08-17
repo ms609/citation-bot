84bac4d05ff34c99f2ac13836937d183b8ce9cc1
ms609 <martins@gmail.com>
2018-08-17 20:51:08 +0800

    Remove redundant formatting divs

--- doibot.php
+++ doibot.php
@@ -6,14 +6,11 @@
 		<link rel="apple-touch-icon" href="https://en.wikipedia.org/apple-touch-icon.png" />
 		<link rel="copyright" href="https://www.gnu.org/copyleft/fdl.html" />
 		<title>Citation bot: Preparing to run</title>
+    <style>body {font-family: Arial;}</style>
 	</head>
 <body class="mediawiki ns-2 ltr">
-	<div id="globalWrapper">
-		<div id="column-content">
-      <div id="content">
-        <div id="bodyContent">
-          <h3 id="siteSub">Please wait while the <a href="https://en.wikipedia.org/wiki/User:Citation_bot">Citation bot</a> processes the page you requested.</h3>
-            <pre><?php
+<h3 id="siteSub">Follow the progress of <a href="https://en.wikipedia.org/wiki/User:Citation_bot">Citation bot</a> below.</h3>
+<pre><?php
 ## Set up - including DOT_DECODE array
 define("HTML_OUTPUT", TRUE);
 require_once("expandFns.php");
@@ -71,26 +68,4 @@ if ($my_page->get_text_from($_REQUEST["page"], $api)) {
 ?>
 
 </pre>
-    </div><!-- div 'bodyContent' -->
-  </div><!-- div 'content' -->
-  <div id="column-one">
-    <div id="p-cactions" class="portlet">
-      <h5>Views</h5>
-      <div class="pBody">
-        <ul>
-           <li id="ca-nstab-user" class="selected"><a href="https://en.wikipedia.org/wiki/User:Citation_bot" title="View the user page [c]" accesskey="c">User page</a></li>
-           <li id="ca-talk" class="new"><a href="https://en.wikipedia.org/w/index.php?title=User_talk:Citation_bot" title="Discussion about the content page [t]" accesskey="t">Discussion</a></li>
-           <li id="ca-edit"><a href="https://en.wikipedia.org/w/index.php?title=User_talk:Citation_bot" title="Click here to report an error [e]" accesskey="e">Report error</a></li>
-           <li id="ca-history"><a href="https://en.wikipedia.org/wiki/Special:Contributions/Citation_bot" title="This bot's contributions [x]" accesskey="x">Contributions</a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="portlet" id="p-logo">
-      <a style="background-image: url(https://wiki.ts.wikimedia.org/images/wiki-en.png);" ></a>
-    </div>
-	<script type="text/javascript"> if (window.isMSIE55) fixalpha(); </script>
-  </div><!-- end of the left (by default at least) column -->
-			<div class="visualClear"></div>
-		</div>
-</div>
 </body></html>
