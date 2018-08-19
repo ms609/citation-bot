1e337dbd417c1c8a1dab3bc0205bb84b7e199fcb
GlazerMann <kurt@ioiowa.com>
2018-08-19 16:19:51 -0700

    Update category.php

--- category.php
+++ category.php
@@ -45,7 +45,7 @@ if ($category) {
         html_echo(
         " </pre><br><small><a href=https://en.wikipedia.org/w/index.php?title=" . urlencode($page_title) . "&action=history>history</a> / "
         . "<a href=https://en.wikipedia.org/w/index.php?title=" . urlencode($page_title) . "&diff=prev&oldid="
-        . $api->get_last_revision($page_title) . ">last edit</a></small></i>\n\n<br>"
+        . $api->get_last_revision($page_title) . ">last edit</a></small></i>\n\n<br><pre>"
         , ".");
       } else {
          echo "\n # Failed. \n";
