62033e8b45ee098636cdbdd3bf24f332a2bc1d10
GlazerMann <kurt@ioiowa.com>
2018-08-20 14:01:23 -0700

    detect bare url in modifications code

--- Template.php
+++ Template.php
@@ -56,11 +56,11 @@ final class Template {
 
   // Re-assemble parsed template into string
   public function parsed_text() {
-    if ($this->has('CITATION_BOT_URL')) {
+    if ($this->has('CITATION_BOT_ORIGINAL_BARE_URL')) {
       if ($this->blank('title')) {
-        return base64_decode($this->get('CITATION_BOT_URL'));
+        return base64_decode($this->get('CITATION_BOT_PLACEHOLDER_BARE_URL'));
       } else {
-        $this->forget('CITATION_BOT_URL');
+        $this->forget('CITATION_BOT_PLACEHOLDER_BARE_URL');
       }
     }
     return '{{' . $this->name . $this->join_params() . '}}';
@@ -2959,6 +2959,7 @@ final class Template {
   protected function added($param) {return $this->modified($param, '+');}
 
   public function modifications ($type='all') {
+    if ($this->has('CITATION_BOT_PLACEHOLDER_BARE_URL')) return array(); // Never forgot it meaning that we did nothing
     if ($this->param) {
       foreach ($this->param as $p) {
         $new[$p->param] = $p->val;
