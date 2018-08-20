b4c3ef7329db591bfddd11d8643dc489280ff49e
GlazerMann <kurt@ioiowa.com>
2018-08-20 14:37:55 -0700

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -79,6 +79,6 @@ class PageTest extends PHPUnit\Framework\TestCase {
       $page = $this->process_page('  ');
       $page = $this->process_page('  move along, nothing to see here ');
       $page = $this->process_page('  move along, nothing to see here {{}} ');
-      $this-assertNull(NULL);
+      $this->assertNull(NULL);
   }
 }
