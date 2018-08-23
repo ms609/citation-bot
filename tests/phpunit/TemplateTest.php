f13632afda6d46546801cf9706a43f24c5563b51
ms609 <martins@gmail.com>
2018-08-23 09:02:51 +0100

    Spacing

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -199,7 +199,7 @@ final class TemplateTest extends PHPUnit\Framework\TestCase {
     $text = "{{Cite web | pages=10-11| Edition = 3rd ed. |journal=My Journal| issn=1234-4321 | publisher=Unwarranted |issue=0|accessdate=2013-01-01|quotes=no}}";
     $expanded = $this->process_citation($text);
     // ISSN should be retained when journal is originally present
-    $this->assertEquals('{{Cite journal| pages=10–11| edition = 3rd |journal=My Journal| issn=1234-4321}}', $expanded->parsed_text());
+    $this->assertEquals('{{Cite journal| pages=10–11| edition = 3rd |journal=My Journal| issn=1234-4321 }}', $expanded->parsed_text());
     
     $text = "{{Cite web | Journal=My Journal| issn=1234-4321 | publisher=Unwarranted }}";
     $expanded = $this->process_citation($text);
