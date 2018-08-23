83e1f4262f811fc77cfc5b4219bb356219624686
ms609 <martins@gmail.com>
2018-08-23 17:45:14 +0100

    Mark API sources

--- apiFunctions.php
+++ apiFunctions.php
@@ -133,16 +133,16 @@ function arxiv_api($ids, $templates) {
       $i++;
       $name = $auth->name;
       if (preg_match("~(.+\.)(.+?)$~", $name, $names) || preg_match('~^\s*(\S+) (\S+)\s*$~', $name, $names)) {
-        $this_template->add_if_new("last$i", $names[2]);
-        $this_template->add_if_new("first$i", $names[1]);
+        $this_template->add_if_new("last$i", $names[2], 'arxiv');
+        $this_template->add_if_new("first$i", $names[1], 'arxiv');
       } else {
-        $this_template->add_if_new("author$i", $name);
+        $this_template->add_if_new("author$i", $name, 'arxiv');
       }
     }
-    $this_template->add_if_new("title", (string) $entry->title); // Formatted by add_if_new
-    $this_template->add_if_new("class", (string) $entry->category["term"]);
-    $this_template->add_if_new("year", substr($entry->published, 0, 4));
-    $this_template->add_if_new("doi", (string) $entry->arxivdoi);
+    $this_template->add_if_new("title", (string) $entry->title, 'arxiv'); // Formatted by add_if_new
+    $this_template->add_if_new("class", (string) $entry->category["term"], 'arxiv');
+    $this_template->add_if_new("year", substr($entry->published, 0, 4), 'arxiv');
+    $this_template->add_if_new("doi", (string) $entry->arxivdoi, 'arxiv');
 
     if ($entry->arxivjournal_ref) {
       $journal_data = (string) $entry->arxivjournal_ref;
@@ -156,12 +156,12 @@ function arxiv_api($ids, $templates) {
       }
       if (preg_match("~\w?\d+-\w?\d+~", $journal_data, $match)) {
         $journal_data = str_replace($match[0], "", $journal_data);
-        $this_template->add_if_new("pages", str_replace("--", REGEXP_EN_DASH, $match[0]));
+        $this_template->add_if_new("pages", str_replace("--", REGEXP_EN_DASH, $match[0]), 'arxiv');
       }
       if (preg_match("~(\d+)(?:\D+(\d+))?~", $journal_data, $match)) {
-        $this_template->add_if_new("volume", $match[1]);
+        $this_template->add_if_new("volume", $match[1], 'arxiv');
         if (isset($match[2])) {
-          $this_template->add_if_new("issue", $match[2]);
+          $this_template->add_if_new("issue", $match[2], 'arxiv');
         }
         $journal_data = preg_replace("~[\s:,;]*$~", "",
                 str_replace($match[-0], "", $journal_data));
@@ -169,9 +169,9 @@ function arxiv_api($ids, $templates) {
       if ($this_template->has('publisher') && $journal_data) {
         $this_template->forget('publisher'); // This is either bad data, or refers to a preprint, not the journal
       }
-      $this_template->add_if_new("journal", wikify_external_text($journal_data));
+      $this_template->add_if_new("journal", wikify_external_text($journal_data), 'arxiv');
     } else {
-      $this_template->add_if_new("year", date("Y", strtotime((string)$entry->published)));
+      $this_template->add_if_new("year", date("Y", strtotime((string)$entry->published)), 'arxiv');
     }
     $this_template = next($templates);
   }
@@ -267,11 +267,11 @@ function adsabs_api($ids, $templates, $identifier) {
   foreach ($response->docs as $record) {
     $this_template = $templates[array_search((string) $record->bibcode, $templates)];
     $this_template->add_if_new('bibcode', (string) $record->bibcode, 'adsabs');
-    $this_template->add_if_new("title", (string) $record->title[0]); // add_if_new will format the title text and check for unknown
+    $this_template->add_if_new("title", (string) $record->title[0], 'adsabs'); // add_if_new will format the title text and check for unknown
     $i = 0;
     if (isset($record->author)) {
      foreach ($record->author as $author) {
-      $this_template->add_if_new("author" . ++$i, $author);
+      $this_template->add_if_new("author" . ++$i, $author, 'adsabs');
      }
     }
     if (isset($record->pub)) {
@@ -281,12 +281,12 @@ function adsabs_api($ids, $templates, $identifier) {
         // Do nothing
       } elseif (substr($journal_start, 0, 6) == "eprint") {
         if (substr($journal_start, 7, 6) == "arxiv:") {
-          if (isset($record->arxivclass)) $this_template->add_if_new("class", $record->arxivclass);
+          if (isset($record->arxivclass)) $this_template->add_if_new("class", $record->arxivclass, 'adsabs');
         } else {
           $this_template->append_to('id', ' ' . substr($journal_start, 13));
         }
       } else {
-        $this_template->add_if_new('journal', $journal_string[0]);
+        $this_template->add_if_new('journal', $journal_string[0], 'adsabs');
       }          
     }
     if (isset($record->page) && (stripos(implode('–', $record->page), 'arxiv') !== FALSE)) {  // Bad data
@@ -295,21 +295,21 @@ function adsabs_api($ids, $templates, $identifier) {
        unset($record->issue);
     }
     if (isset($record->volume)) {
-      $this_template->add_if_new("volume", (string) $record->volume);
+      $this_template->add_if_new("volume", (string) $record->volume, 'adsabs');
     }
     if (isset($record->issue)) {
-      $this_template->add_if_new("issue", (string) $record->issue);
+      $this_template->add_if_new("issue", (string) $record->issue, 'adsabs');
     }
     if (isset($record->year)) {
-      $this_template->add_if_new("year", preg_replace("~\D~", "", (string) $record->year));
+      $this_template->add_if_new("year", preg_replace("~\D~", "", (string) $record->year), 'adsabs');
     }
     if (isset($record->page)) {
-      $this_template->add_if_new("pages", implode('–', $record->page));
+      $this_template->add_if_new("pages", implode('–', $record->page), 'adsabs');
     }
     if (isset($record->identifier)) { // Sometimes arXiv is in journal (see above), sometimes here in identifier
       foreach ($record->identifier as $recid) {
         if(strtolower(substr($recid, 0, 6)) === 'arxiv:') {
-           if (isset($record->arxivclass)) $this_template->add_if_new("class", $record->arxivclass);
+           if (isset($record->arxivclass)) $this_template->add_if_new("class", $record->arxivclass, 'adsabs');
         }
       }
     }
