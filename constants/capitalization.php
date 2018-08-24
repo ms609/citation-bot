674a57a478384da034724d1d8e6cb2f12fbb3302
Martin Smith <martins@gmail.com>
2018-08-24 13:45:34 +0000

    Automated file maintenance

--- constants/capitalization.php
+++ constants/capitalization.php
@@ -1,4 +1,5 @@
 
+          
           <?php
 const LC_SMALL_WORDS = array(/* The following will be automatically updated to alphabetical order */
           ' AI ', ' AJHG ', ' an ', ' and ', ' and then ', ' as ', ' at ', ' at ', ' aus ', ' BBA ', 
