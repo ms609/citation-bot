307078f463ac0a81a5ac9de631b4206a4accc03b
ms609 <martins@gmail.com>
2018-08-24 14:11:52 +0100

    Determine branch name

--- maintain_files.sh
+++ maintain_files.sh
@@ -5,9 +5,15 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 99
 git config --global user.email "martins@gmail.com"
 git config --global user.name "Martin Smith"
-git clone -b ${TRAVIS_BRANCH} https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git file-maintenance
+if [ TRAVIS_PULL_REQUEST -gt 0 ]
+then
+  ${TRAVIS_PULL_REQUEST_BRANCH}
+else
+  BRANCH_NAME = ${TRAVIS_BRANCH}
+fi;
+git clone -b $BRANCH_NAME https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git file-maintenance
 cd file-maintenance
 php maintain_files.php
 git add --all *
 git commit -m"Automated file maintenance" || true
-git push -q origin ${TRAVIS_BRANCH}
+git push -q origin $BRANCH_NAME
