383eae6bacfdde287dab6065697232244286e651
ms609 <martins@gmail.com>
2018-08-24 13:58:13 +0100

    ls?

--- maintain_files.sh
+++ maintain_files.sh
@@ -7,6 +7,8 @@ git config --global user.email "martins@gmail.com"
 git config --global user.name "Martin Smith"
 git clone -b file-edits https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git file-maintenance
 cd file-maintenance
+ls
+echo ls
 php maintain_files.php
 git add --all *
 git commit -m"Automated file maintenance" || true
