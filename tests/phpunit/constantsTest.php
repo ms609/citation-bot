1b53ac2da68ea096fe53fcd6fb90dfb15c381a77
GlazerMann <kurt@ioiowa.com>
2018-08-24 15:57:32 -0700

    Testing to see if capital journals work

--- tests/phpunit/constantsTest.php
+++ tests/phpunit/constantsTest.php
@@ -23,6 +23,7 @@ final class constantsTest extends PHPUnit\Framework\TestCase {
     $this->assertEquals(count(UCFIRST_JOURNAL_ACRONYMS), count(JOURNAL_ACRONYMS));
     for ($i = 0; $i < sizeof(JOURNAL_ACRONYMS); $i++) {
       $this->assertEquals(UCFIRST_JOURNAL_ACRONYMS[$i], mb_convert_case(JOURNAL_ACRONYMS[$i], MB_CASE_TITLE, "UTF-8"));
+      $this->assertEquals(trim(JOURNAL_ACRONYMS[$i]), trim(title_capitalization(ucwords(UCFIRST_JOURNAL_ACRONYMS[$i]), TRUE)));
     }
     $this->assertEquals(count(LC_SMALL_WORDS), count(UC_SMALL_WORDS));
     for ($i = 0; $i < sizeof(LC_SMALL_WORDS); $i++) {
