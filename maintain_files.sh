16ad9ed6e7172208edb7a99ead93e77706506ecc
ms609 <martins@gmail.com>
2018-08-24 13:55:18 +0100

    sp

--- maintain_files.sh
+++ maintain_files.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
+[ -z "${GITHUB_PAT}" ] && exit 99
 git config --global user.email "martins@gmail.com"
 git config --global user.name "Martin Smith"
 git clone -b file-edits https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git file-maintenance
 cd file-maintenance
-php maintain-files.php
+php maintain_files.php
 git add --all *
 git commit -m"Automated file maintenance" || true
 git push -q origin file-edits
