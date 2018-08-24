983a375b90c2db274f7b4728d49db33ca0a93170
ms609 <martins@gmail.com>
2018-08-24 14:05:35 +0100

    use branch for reference

--- maintain_files.sh
+++ maintain_files.sh
@@ -5,11 +5,11 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 99
 git config --global user.email "martins@gmail.com"
 git config --global user.name "Martin Smith"
-git clone -b file-edits https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git file-maintenance
+git clone -b ${TRAVIS_BRANCH} https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git file-maintenance
 cd file-maintenance
 ls
 echo ls
 php maintain_files.php
 git add --all *
 git commit -m"Automated file maintenance" || true
-git push -q origin file-edits
+git push -q origin ${TRAVIS_BRANCH}
