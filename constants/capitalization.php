1f6efd6c29356fcdf1556a3eae8bf37e21bc0deb
GlazerMann <kurt@ioiowa.com>
2018-08-24 12:33:57 -0700

    Update capitalization.php

--- constants/capitalization.php
+++ constants/capitalization.php
@@ -8,7 +8,7 @@ const LC_SMALL_WORDS = array(/* The following will be automatically updated to a
           ' for ', ' from ', ' för ', ' für ', ' if ', ' in ', ' into ', ' is ', ' its ', 
           ' JAMA ', ' la ', ' las ', ' le ', ' les ', ' los ', ' MNRAS ', ' NEJM ', ' nor ', 
           ' nor ', ' NY ', ' NYC ', ' NYT ', ' of ', ' off ', ' on ', ' or ', ' over ', 
-          ' PCR ', ' PNAS ', ' RNA ', ' SSRN ', ' the ', ' then ', ' to ', ' U S A ', ' UK ', ' um ',
+          ' PCR ', ' PNAS ', ' RNA ', ' SSRN ', ' the ', ' then ', ' to ', ' UK ', ' um ',
           ' und ', ' up ', ' USA ', ' van ', ' von ', ' when ', ' with ', ' woor ', ' y ', 
           ' zu ', ' zur ', /* The above will be automatically updated to alphabetical order */ 
           // After this line we list exceptions that need re-capitalizing after they've been decapitalized.
@@ -22,7 +22,7 @@ const UC_SMALL_WORDS = array(/* The following will be automatically updated to a
           ' For ', ' From ', ' För ', ' Für ', ' If ', ' In ', ' Into ', ' Is ', ' Its ', 
           ' Jama ', ' La ', ' Las ', ' Le ', ' Les ', ' Los ', ' Mnras ', ' Nejm ', ' Nor ', 
           ' Nor ', ' Ny ', ' Nyc ', ' Nyt ', ' Of ', ' Off ', ' On ', ' Or ', ' Over ', 
-          ' Pcr ', ' Pnas ', ' Rna ', ' Ssrn ', ' The ', ' Then ', ' To ', ' U S A ', ' Uk ', ' Um ',
+          ' Pcr ', ' Pnas ', ' Rna ', ' Ssrn ', ' The ', ' Then ', ' To ', ' Uk ', ' Um ',
           ' Und ', ' Up ', ' Usa ', ' Van ', ' Von ', ' When ', ' With ', ' Woor ', ' Y ', 
           ' Zu ', ' Zur ', /* The above will be automatically updated to alphabetical order */ 
           // After this line we list exceptions that need re-capitalizing after they've been decapitalized.
