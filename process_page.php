361262b74dd834bced252d4e3055d31073d48878
ms609 <martins@gmail.com>
2018-08-25 06:04:44 +0100

    Rename files

--- process_page.php
+++ process_page.php
@@ -71,12 +71,12 @@ foreach (explode('|', $pages) as $title) {
       safely_echo($my_page->parsed_text());
       echo "</pre>";
   ?>
-  <form method="post" action="doibot.php">
+  <form method="post" action="process_page.php">
     <input type="hidden" name="page" value="<?php echo $title;?>" />
     <input type="hidden" name="user" value="<?php echo $user;?>" />
     <input type="hidden" name="edit" value="on" />
     <input type="hidden" name="slow" value="<?php echo $SLOW_MODE;?>" />
-    <input type=submit value="Submit edits" />
+    <input type="submit" value="Submit edits" />
   </form>
   <?php
     } else {  
