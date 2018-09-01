264148656c8ee759c7c2b0bcc172bb6d5c41e258
GlazerMann <kurt@ioiowa.com>
2018-09-01 14:16:36 -0700

    Only remove double angle quotes if some of both

--- DOItools.php
+++ DOItools.php
@@ -241,7 +241,10 @@ function format_multiple_authors($authors, $returnAsArray = FALSE){
 
 function straighten_quotes($str) {
   $str = preg_replace('~&#821[679];|&#x201[89];|[\x{2039}\x{203A}\x{FF07}\x{2018}-\x{201B}`]|&[rl]s?[ab]?quo;~u', "'", $str);
-  $str = preg_replace('~&#822[013];|[\x{00AB}\x{00BB}\x{201C}-\x{201F}]|&[rlb][ad]?quo;~u', '"', $str);
+  $str = preg_replace('~&#822[013];|[\x{00AB}\x{00BB}\x{201C}-\x{201F}]|&[rlb][d]?quo;~u', '"', $str);
+  if(mb_strpos($str, '&raquo;') !== FALSE && b_strpos($str, '&laquo;') !== FALSE) { // Only replace angle quotes if some of both
+     $str = preg_replace('~&[lr]aquo;~u', '"', $str);                               // Websites tiles: Jobs » Iowa » Cows » Ames
+  }
   return $str;
 }
 
