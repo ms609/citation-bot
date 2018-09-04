610ccbf2e6169e9b21de8d910f9c25bcc8bd19c2
GlazerMann <kurt@ioiowa.com>
2018-09-04 16:04:02 -0700

    unicode

--- Template.php
+++ Template.php
@@ -17,7 +17,7 @@ require_once("Parameter.php");
 
 final class Template {
   const PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_TEMPLATE %s # # #';
-  const REGEXP = '~\{\{(?:[^\{]|\{[^\{])+?\}\}~s';
+  const REGEXP = '~\{\{(?:[^\{]|\{[^\{])+?\}\}~su';
   const TREAT_IDENTICAL_SEPARATELY = FALSE;
   public $all_templates;  // Points to list of all the Template() on the Page() including this one
   protected $rawtext;
