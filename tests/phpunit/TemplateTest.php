7ff930ceb98837c5521a547765fcfb830e0f8d58
GlazerMann <kurt@ioiowa.com>
2018-09-05 15:32:38 -0700

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -211,12 +211,12 @@ final class TemplateTest extends PHPUnit\Framework\TestCase {
   }
   
   public function testDoiExpansion() { // using real DOIs but fake urls
-    $text = "{{Cite web | http://onlinelibrary.wiley.com/doi/10.1111/j.1475-4983.2012.01203.x/files.pdf}}";
+    $text = "{{Cite web | url = http://onlinelibrary.wiley.com/doi/10.1111/j.1475-4983.2012.01203.x/files.pdf}}";
     $expanded = $this->process_citation($text);
     $this->assertEquals('cite journal', $expanded->wikiname());
     $this->assertEquals('10.1111/j.1475-4983.2012.01203.x', $expanded->get('doi'));
     $this->assertNull($expanded->get('url'));
-    $text = "{{Cite web | http://freecopies.com/doi/10.1111/j.1475-4983.2012.01203.x/file.pdf}}";
+    $text = "{{Cite web | url = http://freecopies.com/doi/10.1111/j.1475-4983.2012.01203.x/file.pdf}}";
     $expanded= $this->process_citation($text);
     $this->assertEquals('cite journal', $expanded->wikiname());
     $this->assertEquals('10.1111/j.1475-4983.2012.01203.x', $expanded->get('doi'));
