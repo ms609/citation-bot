ba46041c96cee42c32dc8eadd48a4a5e453b96ba
GlazerMann <kurt@ioiowa.com>
2018-09-06 15:52:54 -0700

    Update Template.php

--- Template.php
+++ Template.php
@@ -18,7 +18,7 @@ require_once("Parameter.php");
 final class Template {
   const PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_TEMPLATE %s # # #';
   const REGEXP_FAST = '~\{\{[^\{\}]+?\}\}~su';
-  const REGEXP = '~\{\{(?:[^\{]|\{[^\{])+?\}\}~su';
+  const REGEXP = '~\{\{(?:!.*\{\{|\}\}|\<math\>|\<\/math\>|\<chem\>|\<\/chem\>).*\}\}~su';
   const TREAT_IDENTICAL_SEPARATELY = FALSE;
   public $all_templates;  // Points to list of all the Template() on the Page() including this one
   protected $rawtext;
