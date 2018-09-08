fa1179b76e855ca9672e588d63060fe1131125b2
GlazerMann <kurt@ioiowa.com>
2018-09-07 17:48:47 -0700

    Update Page.php

--- Page.php
+++ Page.php
@@ -133,8 +133,8 @@ class Page {
                       function($matches) {return $matches[1] . '{{cite web | url=' . $matches[3] . ' | ' . strtolower('CITATION_BOT_PLACEHOLDER_BARE_URL') .'=' . base64_encode($matches[2]) . '}}' . $matches[4] ;},
                       $this->text
                       );
-     // PLAIN DOIS Conerted to templates 
-     $this->text = preg_replace_callback(   // Ones like <ref>10.1244/abc</ref>
+     // PLAIN DOIS Converted to templates 
+     $this->text = preg_replace_callback(   // like <ref>10.1244/abc</ref>
                       "~(<ref[^>]*?>)(\s*10\.[0-9]+\/\S+\s*)(<\s*?\/\s*?ref>)~",
                       function($matches) {return $matches[1] . '{{cite journal | doi=' . $matches[2] . ' | ' . strtolower('CITATION_BOT_PLACEHOLDER_BARE_URL') .'=' . base64_encode($matches[2]) . '}}' . $matches[3] ;},
                       $this->text
