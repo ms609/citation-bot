b89bc52f4995475461ecf84c10330104ef2816a0
GlazerMann <kurt@ioiowa.com>
2018-09-11 14:32:50 -0700

    Update Template.php

--- Template.php
+++ Template.php
@@ -563,7 +563,7 @@ final class Template {
         if (    $all_page_parameters == ""     // Nothing
                 || (strpos(strtolower($all_page_parameters), 'no') !== FALSE && $this->blank('at')) // "None" or "no" contained within something other than "at"
                 || (strcasecmp($all_page_parameters,'no')===0 || strcasecmp($all_page_parameters,'none')===0) // Is exactly "no" or "none"
-                || (strpos($value, chr(2013)) || (strpos($value, '-'))    // New pages have dash, old did not
+                || (strpos($value, chr(2013)) || (strpos($value, '-'))
                   && !strpos($all_page_parameters, chr(2013))
                   && !strpos($all_page_parameters, chr(150)) // Also en-dash
                   && !strpos($all_page_parameters, chr(226)) // Also en-dash
