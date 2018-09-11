19e238c6218d4d1daf596774f46cbdd14da0e6ec
GlazerMann <kurt@ioiowa.com>
2018-09-10 19:57:44 -0700

    Update Template.php

--- Template.php
+++ Template.php
@@ -1111,8 +1111,9 @@ final class Template {
     }
     if ($check_for_errors && $xml->ErrorList) {
       if (isset($xml->ErrorList->PhraseNotFound)) {
-        report_warning("Phrase not found in PMID search with query $query: "
+        fwrite(STDERR, "\n Phrase not found in PMID search with query $query: "
         . echoable(print_r($xml->ErrorList, 1)));
+        fwrite(STDERR, "\n " . $url . " \n";
       } else {
         report_inline('no results.');
       }
