66139f4c8880350da402e32cbceca872df0e0491
GlazerMann <kurt@ioiowa.com>
2018-09-13 11:24:07 -0700

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -36,6 +36,7 @@ class PageTest extends PHPUnit\Framework\TestCase {
       $this->assertEquals('Misc citation tidying. You can [[WP:UCB|use this bot]] yourself. [[WP:DBUG|Report bugs here]].',$page->edit_summary());
       $page = $this->process_page('<ref>http://onlinelibrary.wiley.com/doi/10.1111/j.1475-4983.2012.01203.x</ref>');
       $this->assertFalse(strpos($page->parsed_text(), 'onlinelibrary.wiley.com')); // URL is gone
+      fwrite(STDERR, "\n XXX " . $page->parsed_text() . " XXX\n");
       $this->assertEquals('Converted bare reference to cite template. You can [[WP:UCB|use this bot]] yourself. [[WP:DBUG|Report bugs here]].',$page->edit_summary());
   }
 
