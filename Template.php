2f460d7b4ea061f30818ab97fccc15eb56b7c788
GlazerMann <kurt@ioiowa.com>
2018-09-15 21:08:54 -0700

    Support [[a|b|c|d|e]]

--- Template.php
+++ Template.php
@@ -75,8 +75,23 @@ final class Template {
   // Parts of each param: | [pre] [param] [eq] [value] [post]
   protected function split_params($text) {
     // Replace | characters that are inside template parameter/value pairs
-    $text = preg_replace('~(\[\[[^\[\]\|]+)\|([^\[\]\|]+\]\])~', "$1" . PIPE_PLACEHOLDER . "$2", $text);
-    $text = preg_replace('~(\[\[[^\[\]\|]+)\|([^\[\]\|]*)\|([^\[\]\|]+\]\])~', "$1" . PIPE_PLACEHOLDER . "$2" . PIPE_PLACEHOLDER . "$3", $text);
+    const START = "~(\[\[[^\[\]\|]+)";
+    const STOP  =      "([^\[\]\|]+\]\])~";
+    const MID   =      "([^\[\]\|]+)";
+    const PIPE  = "\|";
+    $text = preg_replace(START . PIPE . STOP, "$1" . PIPE_PLACEHOLDER . "$2", $text);
+    $text = preg_replace(START . PIPE . MID . PIPE . STOP,
+                         "$1" . PIPE_PLACEHOLDER . "$2" .
+                         PIPE_PLACEHOLDER . "$3", $text);
+    $text = preg_replace(START . PIPE . MID . PIPE . MID . PIPE . STOP,
+                         "$1" . PIPE_PLACEHOLDER . "$2" .
+                         PIPE_PLACEHOLDER . "$3" .
+                         PIPE_PLACEHOLDER . "$4", $text);
+    $text = preg_replace(START . PIPE . MID . PIPE . MID . PIPE . MID . PIPE . STOP,
+                         "$1" . PIPE_PLACEHOLDER . "$2" .
+                         PIPE_PLACEHOLDER . "$3" .
+                         PIPE_PLACEHOLDER . "$4" .
+                         PIPE_PLACEHOLDER . "$5", $text);
     $params = explode('|', $text);
     // TODO: this naming is confusing, distinguish between $text above and
     //       $text in the loop (derived from $text above via $params)
