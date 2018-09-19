90aca295244a699611173b17e48eeb29b608029c
GlazerMann <kurt@ioiowa.com>
2018-09-19 14:20:05 -0700

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -219,9 +219,9 @@ final class TemplateTest extends PHPUnit\Framework\TestCase {
   
   public function testDoiExpansion() {
     $text = "{{Cite web | http://onlinelibrary.wiley.com/doi/10.1111/j.1475-4983.2012.01203.x/abstract}}";
-    $expanded = $this->prepare_citation($text);
-    $this->assertEquals('cite journal', $expanded->wikiname());
-    $this->assertEquals('10.1111/j.1475-4983.2012.01203.x', $expanded->get('doi'));
+    $prepared = $this->prepare_citation($text);
+    $this->assertEquals('cite journal', $prepared->wikiname());
+    $this->assertEquals('10.1111/j.1475-4983.2012.01203.x', $prepared->get('doi'));
 
     $text = "{{Cite web | url = http://onlinelibrary.wiley.com/doi/10.1111/j.1475-4983.2012.01203.x/abstract}}";
     $expanded = $this->prepare_citation($text);
