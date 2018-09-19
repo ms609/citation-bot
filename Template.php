e5cac587094d92e14320adc53138a9d5cf67e38d
GlazerMann <kurt@ioiowa.com>
2018-09-19 07:41:58 -0700

    remove uneeded code

--- Template.php
+++ Template.php
@@ -63,9 +63,6 @@ final class Template {
   // Re-assemble parsed template into string
   public function parsed_text() {
     if ($this->has(strtolower('CITATION_BOT_PLACEHOLDER_BARE_URL'))) {
-      //if ($this->has('doi') && !doi_active($this->get('doi')) && $this->blank(DOI_BROKEN_ALIASES) && $this->blank(['title', 'chapter'])) {
-      //  $this->mark_inactive_doi();
-      //}
       if ($this->blank(['title', 'chapter'])) {
         return base64_decode($this->get(strtolower('CITATION_BOT_PLACEHOLDER_BARE_URL')));
       } else {
