19c6832d8016bd29da859515ac6076b84651c8f8
ms609 <martins@gmail.com>
2018-09-20 08:14:49 +0100

    Move file to tests

--- tests/citoid.php
+++ tests/citoid.php
@@ -2,7 +2,7 @@
 $posted = $_POST;
 
 if (isset($posted)) {
-  require_once('citoid.php');
+  require_once('../api_handlers/citoid.php');
   echo citoid_request($posted);
 } else {
   echo json_encode(['error' => 'No POSTdata received']);
