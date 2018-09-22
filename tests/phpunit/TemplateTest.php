0a1de34f8af8a98a56214d4b2dad5821eb8520fc
GlazerMann <kurt@ioiowa.com>
2018-09-22 11:32:44 -0700

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -125,7 +125,7 @@ final class TemplateTest extends PHPUnit\Framework\TestCase {
   
   public function testBrokenDoiUrlChanges() {
      $text = '{{cite journal|url=http://dx.doi.org/10.1111/j.1471-0528.1995.tb09132.x|doi=10.00/broken_and_invalid}}';
-     $expanded = $this->process_page($text);
+     $expanded = $this->process_citation($text);
      $this->assertEquals('10.1111/j.1471-0528.1995.tb09132.x', $expanded->get('doi'));
      $this->assertNull($expanded->get('url'));
   }
