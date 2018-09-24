fab41cd1a729d196294d4f8424a5267bde77a6ba
GlazerMann <kurt@ioiowa.com>
2018-09-24 16:54:12 -0700

    typo in test

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -231,7 +231,7 @@ final class TemplateTest extends PHPUnit\Framework\TestCase {
     $expanded = $this->process_citation($text);
     $this->assertEquals('978-0470016176', $expanded->get('isbn'));
     $this->assertEquals('cite book', $expanded->wikiname());
-    $this->assertEquals('http://www.paulselden.net/uploads/7/5/3/2/7532217/elsterrestrialization.pdf', $this->get('chapter-url'));
+    $this->assertEquals('http://www.paulselden.net/uploads/7/5/3/2/7532217/elsterrestrialization.pdf', $expanded->get('chapter-url'));
     $this->assertNull($expanded->get('url'));
   }
 
