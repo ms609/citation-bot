514617e4eb2226a81ca8aadb6f90b95d20e7f9a0
GlazerMann <kurt@ioiowa.com>
2018-09-25 08:46:45 -0700

    website=archive.org is wrong if url is not archive

--- Template.php
+++ Template.php
@@ -2687,6 +2687,10 @@ final class Template {
           if (($this->wikiname() === 'cite book') && (strcasecmp((string)$this->get($param), 'google.com') === 0)) {
             $this->forget($param);
           }
+          if (stripos($param, 'archive.org') !== FALSE &&
+              stripos($this->get('url') . $this->get('chapter-url') . $this->get('chapterurl'), 'archive.org') === FALSE) {
+            $this->forget($param);
+          }
           return;
       }
     }
