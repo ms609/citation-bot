68e5dd1b84eb039126a6d1485d6ca0327f8bad18
GlazerMann <kurt@ioiowa.com>
2018-09-26 15:32:05 -0700

    Update Parameter.php

--- Parameter.php
+++ Parameter.php
@@ -48,10 +48,11 @@ final class Parameter {
   protected function set_value($value) {
     switch ($this->param) {
       case 'pages':
-        if (stripos($value, "http") === FALSE) $value = mb_ereg_replace(REGEXP_TO_EN_DASH, REGEXP_EN_DASH, $value);
+        if (stripos($value, 'http') === FALSE && stripos($value, 'CITATION_BOT_PLACEHOLDER_COMMENT') === FALSE) {
+          $value = mb_ereg_replace(REGEXP_TO_EN_DASH, REGEXP_EN_DASH, $value);
+        }
+      default:
         $this->val = $value;
-      break;
-      default: $this->val = $value;
     }
   }
 
