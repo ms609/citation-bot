3ffe6e15bd79d34f788ff71ab9c89e2c1bdcf16e
GlazerMann <kurt@ioiowa.com>
2018-09-27 08:38:45 -0700

    More tests of Zotero

--- tests/phpunit/zoteroTest.php
+++ tests/phpunit/zoteroTest.php
@@ -38,8 +38,32 @@ class ZoteroTest extends PHPUnit\Framework\TestCase {
     $page->expand_text();
     return $page;
   }
-  
-  public function testZoteroExpansion() {
+ 
+  public function testZoteroExpansionRG() {
+    $text = '{{Cite journal|url =  https://www.researchgate.net/publication/2344536}}';
+    $expanded = $this->process_citation($text);
+    $this->assertTrue(strpos($expanded->parsed_text(), $text);
+  }
+
+  public function testZoteroExpansionPII() {
+    $text = '{{Cite journal|url = https://www.sciencedirect.com/science/article/pii/S0024379512004405}}';
+    $expanded = $this->process_citation($text);
+    $this->assertTrue(strpos($expanded->parsed_text(), $text);
+  }
+
+  public function testZoteroExpansionJstorBook() {
+    $text = '{{Cite journal|url=https://www.jstor.org/stable/j.ctt6wp6td.10?seq=9#metadata_info_tab_contents}}';
+    $expanded = $this->process_citation($text);
+    $this->assertTrue(strpos($expanded->parsed_text(), $text);
+  }
+
+  public function testZoteroExpansionNBK() {
+    $text = '{{Cite journal|url=https://www.ncbi.nlm.nih.gov/books/NBK24662/}}';
+    $expanded = $this->process_citation($text);
+    $this->assertTrue(strpos($expanded->parsed_text(), $text);
+  }
+
+  public function testZoteroExpansionNYT() {
     $text = '{{Cite journal|url =https://www.nytimes.com/2018/06/11/technology/net-neutrality-repeal.html}}';
     $expanded = $this->process_citation($text);
     expand_by_zotero($expanded);
@@ -48,7 +72,8 @@ class ZoteroTest extends PHPUnit\Framework\TestCase {
     $this->assertEquals('Keith', $expanded->get('first1')); // Would be tidied to 'first' in final_parameter_tudy
     $this->assertEquals('Collins', $expanded->get('last1'));
     $this->assertEquals('cite newspaper', $expanded->wikiname());
-        
+  }
+  public function testZoteroExpansionRSRef() {
     $text = '<ref>http://rspb.royalsocietypublishing.org/content/285/1887/20181780</ref>';
     $expanded = $this->process_page($text);
     $this->assertTrue(strpos($expanded->parsed_text(), 'Hyoliths with pedicles illuminate the origin of the brachiopod body plan') !== FALSE);
