7143739730b9e8c7eac69ca60d89ec17cd04e0b8
GlazerMann <kurt@ioiowa.com>
2018-09-27 12:38:37 -0700

    be even more paranoid about zotero

--- Template.php
+++ Template.php
@@ -183,13 +183,7 @@ final class Template {
     if ($this->should_be_processed()) {
       $this->prepare();
       
-      if ($this->has('url') 
-          && $this->blank(['doi', 'pmc', 'pmid', 'jstor', 'eprint', 'arxiv', 'bibcode']) // These custom APIs are more reliable and faster
-          && $this->incomplete()  // Too slow for data that is already good
-          && !preg_match("~^https?://books\.google\.~", $this->get('url')) // We have custom Google Book code
-         )  { 
-            expand_by_zotero($this); // May modify wikiname
-      }
+      $had_a_url = $this->get('url');
 
       switch ($this->wikiname()) {
         case 'cite web':
@@ -276,6 +270,20 @@ final class Template {
           }
           break;
       }
+    }
+
+    if ($had_a_url
+          && $this->blank(['doi', 'pmc', 'pmid', 'jstor', 'eprint', 'arxiv', 'bibcode']) // These custom APIs are more reliable and faster
+          && $this->incomplete()  // Too slow for data that is already good
+          && !preg_match("~^https?://books\.google\.~", $this->get('url')) // We have custom Google Book code
+         )  {
+        if ($this->blank('url')) { // we must have eaten it, like citeceerx
+            $this->set('url', $had_a_url);
+            expand_by_zotero($this); // May modify wikiname
+            $this->quietly_forget('url');
+        } else {
+            expand_by_zotero($this); // May modify wikiname
+        }
     }
     report_action('Tying up loose ends...');
     $this->final_tidy();
