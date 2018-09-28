123afa64d8a2637592747c079c104bbd61d603f7
GlazerMann <kurt@ioiowa.com>
2018-09-28 09:15:56 -0700

    Update zoteroTest.php

--- tests/phpunit/zoteroTest.php
+++ tests/phpunit/zoteroTest.php
@@ -16,46 +16,7 @@ $SLOW_MODE = TRUE;
  
 class ZoteroTest extends PHPUnit\Framework\TestCase {
 
-  protected function setUp() {
-  }
-
-  protected function tearDown() {
-  }
-  
-  protected function prepare_citation($text) {
-    $this->assertEquals('{{', mb_substr($text, 0, 2));
-    $this->assertEquals('}}', mb_substr($text, -2));
-    $template = new Template();
-    $template->parse_text($text);
-    $template->prepare();
-    return $template;
-  }
-  
-  protected function process_citation($text) {
-    $this->assertEquals('{{', mb_substr($text, 0, 2));
-    $this->assertEquals('}}', mb_substr($text, -2));
-    $page = new TestPage();
-    $page->parse_text($text);
-    $page->expand_text();
-    $expanded_text = $page->parsed_text();
-    $template = new Template();
-    $template->parse_text($expanded_text);
-    return $template;
-  }
-  
-  protected function process_page($text) {  // Only used if more than just a citation template
-    $page = new TestPage();
-    $page->parse_text($text);
-    $page->expand_text();
-    return $page;
-  }
-  
-  protected function expand_via_zotero($text) {
-    $expanded = $this->prepare_citation($text);
-    expand_by_zotero($expanded);
-    $expanded->tidy();
-    return $expanded;
-  }
+include_once("./TestHeader.php");
 
 // TODO    
 //public function testZoteroExpansionRG() {
