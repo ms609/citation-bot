db756bb03ea0ab5dd83d4dc65bc436e32a9eea85
GlazerMann <kurt@ioiowa.com>
2018-09-28 10:05:35 -0700

    Update wikiFunctionsTest.php

--- tests/phpunit/wikiFunctionsTest.php
+++ tests/phpunit/wikiFunctionsTest.php
@@ -9,7 +9,13 @@ if (!defined('VERBOSE')) define('VERBOSE', TRUE);
 $SLOW_MODE = TRUE;
  
 final class wikiFunctionsTest extends PHPUnit\Framework\TestCase {
-  
+
+  protected function setUp() {
+  }
+
+  protected function tearDown() {
+  }
+
   require_once("./TestHeader.php");
   
   public function testIsValidUser() {
