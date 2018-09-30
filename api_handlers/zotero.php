6d86a022985e0623f2b8073d86cf4c345d996b60
GlazerMann <kurt@ioiowa.com>
2018-09-30 14:41:51 -0700

    Update zotero.php

--- api_handlers/zotero.php
+++ api_handlers/zotero.php
@@ -8,16 +8,16 @@ function query_url_api($ids, $templates) {
   }
   report_action("Using Zotero translation server to retrieve details from identifiers.");
   foreach ($templates as $template) {
-       if ($template->has('biorxiv')) expand_by_zotero($template, 'https://dx.doi.org/10.1101/' . $template->get('biorxiv'));
+       if ($template->has('biorxiv'))   expand_by_zotero($template, 'https://dx.doi.org/10.1101/' . $template->get('biorxiv'));
        if ($template->has('citeseerx')) expand_by_zotero($template, 'http://citeseerx.ist.psu.edu/viewdoc/summary?doi=' . $template->get('citeseerx'));
-       if ($template->has('hdl')) expand_by_zotero($template, 'https://hdl.handle.net/' . $template->get('hdl'));
-       if ($template->has('jfm')) expand_by_zotero($template, 'https://zbmath.org/?format=complete&q=an:' . $template->get('jfm'));
-       if ($template->has('zbl')) expand_by_zotero($template, 'https://zbmath.org/?format=complete&q=an:' . $template->get('zbl'));
-       if ($template->has('mr')) expand_by_zotero($template, 'https://mathscinet.ams.org/mathscinet-getitem?mr=' . $template->get('mr'));
-       if ($template->has('osti')) expand_by_zotero($template, 'https://www.osti.gov/biblio/' . $template->get('osti'));
-       if ($template->has('rfc')) expand_by_zotero($template, 'https://tools.ietf.org/html/rfc' . $template->get('rfc'));
-       if ($template->has('ssrn')) expand_by_zotero($template, 'https://papers.ssrn.com/sol3/papers.cfm?abstract_id=' . $template->get('ssrn'));
-       if ($template->has('doi') && doi_inactive()) expand_by_zotero($template, 'https://dx.doi.org/' . $template->get('doi')); // Non-crossref DOIs, such as 10.13140/RG.2.1.1002.9609
+       if ($template->has('hdl'))       expand_by_zotero($template, 'https://hdl.handle.net/' . $template->get('hdl'));
+       if ($template->has('jfm'))       expand_by_zotero($template, 'https://zbmath.org/?format=complete&q=an:' . $template->get('jfm'));
+       if ($template->has('zbl'))       expand_by_zotero($template, 'https://zbmath.org/?format=complete&q=an:' . $template->get('zbl'));
+       if ($template->has('mr'))        expand_by_zotero($template, 'https://mathscinet.ams.org/mathscinet-getitem?mr=' . $template->get('mr'));
+       if ($template->has('osti'))      expand_by_zotero($template, 'https://www.osti.gov/biblio/' . $template->get('osti'));
+       if ($template->has('rfc'))       expand_by_zotero($template, 'https://tools.ietf.org/html/rfc' . $template->get('rfc'));
+       if ($template->has('ssrn'))      expand_by_zotero($template, 'https://papers.ssrn.com/sol3/papers.cfm?abstract_id=' . $template->get('ssrn'));
+       if ($template->has('doi'))       expand_by_zotero($template, 'https://dx.doi.org/' . $template->get('doi')); // Non-crossref DOIs, such as 10.13140/RG.2.1.1002.9609
   }
 }
 
