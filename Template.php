235d298781fb875cb15bb07dfc25df9834fc8d28
GlazerMann <kurt@ioiowa.com>
2018-09-30 13:01:36 -0700

    Not called

--- Template.php
+++ Template.php
@@ -179,7 +179,7 @@ final class Template {
     return !$this->api_has_used($api, $param);
   }
   
-  public function process() {
+  private function process() {
     if ($this->should_be_processed()) {
       $this->prepare();
       
