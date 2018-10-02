d725b464377f308f68854ea781af7a4235e5640f
GlazerMann <kurt@ioiowa.com>
2018-10-02 15:20:19 -0700

    Update testBaseClass.php

--- testBaseClass.php
+++ testBaseClass.php
@@ -1,1 +1,19 @@
 RUBBISH to KILLS TESTS
+
+error_reporting(E_ALL); // All tests run this way
+
+ // backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+}
+
+
+class testBaseClass extends PHPUnit\Framework\TestCase {
+
+  public function setUp() {
+  }
+
+  public function tearDown() {
+  }
+}
