c3294cc853fdad851ed9003e8885bb3b82ffd4bd
GlazerMann <kurt@ioiowa.com>
2018-10-02 15:40:36 -0700

    Update ParameterTest.php

--- tests/phpunit/ParameterTest.php
+++ tests/phpunit/ParameterTest.php
@@ -3,7 +3,8 @@
 /*
  * Tests for Parameter.php.
  */
-require_once __DIR__ . '/testBaseClass.php';
+
+require_once __DIR__ . '../../testBaseClass.php';
 
 final class ParameterTest extends testBaseClass {
 
