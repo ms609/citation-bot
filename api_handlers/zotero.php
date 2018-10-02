56a14bd89ae40bca8aff449e84e17b5a7e24e3ae
GlazerMann <kurt@ioiowa.com>
2018-10-02 16:43:00 -0700

    Update zotero.php

--- api_handlers/zotero.php
+++ api_handlers/zotero.php
@@ -36,7 +36,8 @@ function expand_by_zotero(&$template, $url = NULL) {
   }
 
   if(preg_match("~^https?://books\.google\.~", $url)) return FALSE;  // We have special google gooks code
-
+  if(stristr($url, 'CITATION_BOT_PLACEHOLDER') !== FALSE) return FALSE; // That's a bad url
+  
   $zotero_response = zotero_request($url);
   switch (trim($zotero_response)) {
     case '':
