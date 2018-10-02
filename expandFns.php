77d870f2fbbdc9bfc484216357a0771a78a5eb6f
Martin R. Smith <martins@gmail.com>
2018-10-02 07:37:39 +0100

    Tougher check for garbage numbers
    Some (if not many) things were written a long time ago.
    2000 BC picked somewhat arbitrarily...

--- expandFns.php
+++ expandFns.php
@@ -329,7 +329,7 @@ function tidy_date($string) {
     }
   }
   if (preg_match('~^(.*\d{4}\-\d?\d(?:\-?\d\d?))\S*~', $string, $matches)) return $matches[1];
-  if (preg_match('~^\d+$~', $string)) return "" ; // just a string of number????
+  if (is_numeric($string) && ($string < -2000 || $string > date("Y") + 10)) return ''; // A number that is not a year; probably garbage
   return $string;
 }
 
