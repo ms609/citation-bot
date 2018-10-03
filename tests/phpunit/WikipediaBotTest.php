f3a1d67caf7970ec9786a327303ad5bd0a8b221d
GlazerMann <kurt@ioiowa.com>
2018-10-03 06:43:07 -0700

    Update WikipediaBotTest.php

--- tests/phpunit/WikipediaBotTest.php
+++ tests/phpunit/WikipediaBotTest.php
@@ -4,7 +4,7 @@
  * Tests for WikipediaBot.php
  */
 
-require_once __DIR__ . '/../../testBaseClass.php';
+require_once __DIR__ . '/testBaseClass.php';
  
 if (getenv('TRAVIS_PULL_REQUEST')) {
   echo (" - Skipping WikipediaBotTest.php: Testing bot in Travis pull requests would endanger secure tokens.\n");
