beefa2ea45fed6c0c53a7c136d9199c427b56ba1
GlazerMann <kurt@ioiowa.com>
2018-10-03 15:50:24 -0700

    Update bad_data.php

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -6,7 +6,6 @@ const BAD_ACCEPTED_MANUSCRIPT_TITLES = array("oup accepted manuscript", "placeho
                                              "placeholder", "symbolic placeholder", "[placeholder]", 
                                              "placeholder for arabic language transliteration");
 const BAD_AUTHORS = array("unknown", "missing");
-
 // Catch 'authors' such as "hearst magazines", "time inc", "nielsen business media, inc"
 // Ordered alphabetically.
 const PUBLISHER_ENDINGS = ["books", "corporation", 'centre', 'center', 'company', "inc.", "inc", "magazines",
@@ -24,4 +23,4 @@ const BAD_ZOTERO_TITLES = ['Browse publications', 'Central Authentication Servic
                                  'ZbMATH - the first resource for mathematics', 'MR: Matches for:',
                                  ' Log In', 'Log In ', 'Bookmarkable URL intermediate page', 'Shibboleth Authentication Request',
                                  'domain for sale', 'website for sale', 'domain is for sale', 'website is for sale',
-                                 'lease this domain', 'domain available'];
+                                 'lease this domain', 'domain available', 'metaTags', '}}', '{{'];
