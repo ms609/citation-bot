533e4b21fa63ae05970813e805f3da1e44008dc2
GlazerMann <kurt@ioiowa.com>
2018-10-04 12:42:50 -0700

    Clean up test suite

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -5,10 +5,6 @@
  */
 
 require_once __DIR__ . '/../testBaseClass.php';
-
-// Initialize bot configuration
-if (!defined('VERBOSE')) define('VERBOSE', TRUE);
-$SLOW_MODE = TRUE;
  
 final class PageTest extends testBaseClass {
 
