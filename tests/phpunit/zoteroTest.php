e059bed6c68edb39f0d37892fd362e63d7f8a49e
GlazerMann <kurt@ioiowa.com>
2018-10-04 16:57:21 -0700

    Update zoteroTest.php

--- tests/phpunit/zoteroTest.php
+++ tests/phpunit/zoteroTest.php
@@ -7,13 +7,12 @@
 require_once __DIR__ . '/../testBaseClass.php';
  
 class ZoteroTest extends testBaseClass {
-
-// TODO    
-//public function testZoteroExpansionRG() {
-//    $text = '{{Cite journal|url =https://www.researchgate.net/publication/23445361}}';
-//    $expanded = $this->expand_via_zotero($text);
-//    $this->assertEquals('10.1136/jnnp.2008.144360', $expanded->get('doi'));
-//  }
+  
+public function testZoteroExpansionRG() {
+    $text = '{{Cite journal|url =https://www.researchgate.net/publication/23445361}}';
+    $expanded = $this->expand_via_zotero($text);
+    $this->assertEquals('10.1136/jnnp.2008.144360', $expanded->get('doi'));
+  }
       
   public function testZoteroExpansionPII() {
     $text = '{{Cite journal|url = https://www.sciencedirect.com/science/article/pii/S0024379512004405}}';
