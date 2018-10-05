cc12c1c82464247ebbc04f1da6920715b7715ac8
GlazerMann <kurt@ioiowa.com>
2018-10-04 20:35:53 -0700

    Update zotero.php

--- api_handlers/zotero.php
+++ api_handlers/zotero.php
@@ -128,8 +128,9 @@ function expand_by_zotero(&$template, $url = NULL) {
       case 'newspaperArticle': 
         if (isset($result->libraryCatalog) && in_array($result->libraryCatalog, WEB_NEWSPAPERS)) break;
         $template->change_name_to('cite newspaper'); 
-        if ($template->get('publisher') === 'Associated Press') {
-          $template->rename('publisher', 'agency');
+        if ($template->get('publisher') === 'Associated Press' &&
+            stripos($url, 'ap.org') === FALSE  ) {
+          $template->rename('publisher', 'agency'); // special template parameter just for them
           if ($template->get('author') === 'Associated Press') $template->forget('author'); // all too common
         }
         break;
