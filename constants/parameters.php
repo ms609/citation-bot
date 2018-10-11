f01c9a3940a80768ffcaf30c9601f188006cc4a3
GlazerMann <kurt@ioiowa.com>
2018-10-11 09:19:59 -0700

    encyclopedia & newspaper are work aliases (#914)
    * encyclopedia is another work alias
    
    * Update Template.php
    
    * Update Template.php

--- constants/parameters.php
+++ constants/parameters.php
@@ -224,7 +224,7 @@ const EDITOR1_ALIASES = ["editor1", "editor", "editor-last", "editor1-last"];
 const ISSUE_ALIASES = ['issue', 'number'];
 const PAGE_ALIASES = ['page', 'pages', 'pp', 'p', 'at'];
 const TIME_ALIASES = ['date', 'year'];
-const WORK_ALIASES = ['work', 'journal', 'newspaper', 'magazine', 'periodical', 'website']; 
+const WORK_ALIASES = ['work', 'journal', 'newspaper', 'magazine', 'periodical', 'website', 'encyclopedia']; 
 
 const ALL_ALIASES = [TITLE_LINK_ALIASES, ARXIV_ALIASES, COAUTHOR_ALIASES, CHAPTER_ALIASES,
               DISPLAY_AUTHORS, DISPLAY_EDITORS, DOI_BROKEN_ALIASES, AUTHOR1_ALIASES,
