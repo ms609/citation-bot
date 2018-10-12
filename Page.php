db53994d6463779487ca8c97205be4e669d797df
GlazerMann <kurt@ioiowa.com>
2018-10-12 08:32:02 -0700

    Update Page.php

--- Page.php
+++ Page.php
@@ -53,7 +53,7 @@ class Page {
 
     $this->text = @file_get_contents(WIKI_ROOT . '?' . http_build_query(['title' => $title, 'action' =>'raw']));
     $this->start_text = $this->text;
-    $this->make_modifications;
+    $this->make_modifications();
 
     if (stripos($this->text, '#redirect') !== FALSE) {
       echo "Page is a redirect.";
