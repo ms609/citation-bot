77a60ed1434deb324bdc06ded3a7594bce7cae8d
GlazerMann <kurt@ioiowa.com>
2018-10-13 16:57:47 -0700

    Test Bram_van_Leer

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -8,6 +8,14 @@ require_once __DIR__ . '/../testBaseClass.php';
  
 final class TemplateTest extends testBaseClass {
 
+ 
+ h
+public function testHugePage() {
+     $text = file_get_contents('https://en.wikipedia.org/w/index.php?title=Bram_van_Leer&action=raw');
+     $page = new TestPage();
+     $page->parse_text($text);
+     $page->expand_text();
+   }
   public function testParameterWithNoParameters() {
     $text = "{{Cite web | text without equals sign  }}";
     $expanded = $this->process_citation($text);
