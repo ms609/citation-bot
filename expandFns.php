638c16cbf72a1ec55f63203e63acc3c357a24774
GlazerMann <kurt@ioiowa.com>
2018-10-14 06:25:17 -0700

    Unicode dates
    Grrrrrr

--- expandFns.php
+++ expandFns.php
@@ -338,7 +338,7 @@ function tidy_date($string) {
     if (stripos($string, 'Invalid') !== FALSE) return '';
     return $string;
   }
-  if (preg_match('~^(.*?\d{4}\-\d?\d(?:\-?\d\d?))\S*~', $string, $matches)) return $matches[1];
+  if (preg_match('~^(.*?\d{4}\-\d?\d(?:\-?\d\d?))\S*~u', $string, $matches)) return $matches[1];
   return $string;
 }
 
