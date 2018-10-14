eb7a7266505bc0e5ded62c25a37cae2548beb2a4
GlazerMann <kurt@ioiowa.com>
2018-10-13 17:06:21 -0700

    Update wikiFunctionsTest.php

--- tests/phpunit/wikiFunctionsTest.php
+++ tests/phpunit/wikiFunctionsTest.php
@@ -4,8 +4,6 @@ require_once __DIR__ . '/../testBaseClass.php';
  
 final class wikiFunctionsTest extends testBaseClass {
   
-
- h
 public function testHugePage() {
      $text = file_get_contents('https://en.wikipedia.org/w/index.php?title=Bram_van_Leer&action=raw');
      $page = new TestPage();
