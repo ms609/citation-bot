26900f3d7f5d299b36a0084309f16f102eeaa732
GlazerMann <kurt@ioiowa.com>
2018-10-17 06:56:16 -0700

    drop canonical URLs for DOIs (#935)

--- api_handlers/zotero.php
+++ api_handlers/zotero.php
@@ -116,6 +116,12 @@ function expand_by_zotero(&$template, $url = NULL) {
   
   if ( isset($result->DOI) && $template->blank('doi')) {
     $template->add_if_new('doi', $result->DOI);
+    expand_by_doi($template);
+    if (!$template->incomplete() && doi_active($template->get('doi')) && 
+        (str_ireplace(CANONICAL_PUBLISHER_URLS, '', $url) != $url)) { // This is the use a replace to see if a substring is present trick
+      report_forget("Existing canonical URL resulting in equivalent DOI; dropping URL");
+      $template->forget('url');
+    }
     return TRUE; // We can just use this.  If this is wrong, then we should not trust anything else anyway
   }
 
