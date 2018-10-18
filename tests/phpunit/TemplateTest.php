c760a4d5c6d62b5b31f683eef7be4d8c3b614b1e
GlazerMann <kurt@ioiowa.com>
2018-10-17 19:51:07 -0700

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -134,7 +134,7 @@ final class TemplateTest extends testBaseClass {
   }
 
   public function testVauthors() {
-      $text = '{{cite journal |vauthors= |title=Guidelines for the management of adults with hospital-acquired, ventilator-associated, and healthcare-associated pneumonia |journal=Am. J. Respir. Crit. Care Med. |volume=171 |issue=4 |pages=388–416 |year=2005 |pmid=15699079 |doi=10.1164/rccm.200405-644ST |url=}}'
+      $text = '{{cite journal |vauthors= |title=Guidelines for the management of adults with hospital-acquired, ventilator-associated, and healthcare-associated pneumonia |journal=Am. J. Respir. Crit. Care Med. |volume=171 |issue=4 |pages=388–416 |year=2005 |pmid=15699079 |doi=10.1164/rccm.200405-644ST |url=}}';
       $expanded = $this->process_citation($text);
       $this->assertNull($expanded->get('author2'));
   }
