7f23aed6a3e57823da2a7bcc0893520c7a2c687d
GlazerMann <kurt@ioiowa.com>
2018-10-19 15:23:35 -0700

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -83,6 +83,6 @@ final class PageTest extends testBaseClass {
       $this->assertTrue((boolean) strpos($page->parsed_text(), '12-05-2016'));
       $text = '{{Use dmy dates}}{{cite web|url=https://www.nasa.gov/content/profile-of-john-glenn}}';
       $page = $this->process_page($text);
-      $this->assertTrue((boolean) strpos($page->parsed_text(), '04-12-2016'));
+      $this->assertTrue((boolean) strpos($page->parsed_text(), '05-12-2016'));
   }
 }
