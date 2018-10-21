649d7f2f76bbf54f1c57b621904345e9db91a584
GlazerMann <kurt@ioiowa.com>
2018-10-21 06:33:01 -0700

    Finalized suggested changes

--- expandFns.php
+++ expandFns.php
@@ -335,8 +335,7 @@ function tidy_date($string) {
   }
   $string = preg_replace("~[^\x01-\x7F]~","-", $string); // Convert any non-ASCII Characters to dashes
   $string = preg_replace('~[\s\-]*\-[\s\-]*~', '-',$string); // Combine dash with any following or preceeding white space and other dash
-  $string = preg_replace('~^\-*(.+?)\-*$~', '\1', $string);  // Remove trailing dash
-  $string = preg_replace('~^\-~', '',$string);  // Remove leading dash
+  $string = preg_replace('~^\-*(.+?)\-*$~', '\1', $string);  // Remove trailing/leading dashes
   $string = trim($string);
   // End of character clean-up
   if (is_numeric($string) && is_int(1*$string)) {
