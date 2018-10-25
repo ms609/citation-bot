96b5de802ddb018a905ec2b1166abe8b8039e215
GlazerMann <kurt@ioiowa.com>
2018-10-25 13:44:07 -0700

    Update expandFns.php

--- expandFns.php
+++ expandFns.php
@@ -31,8 +31,6 @@ const CROSSREFUSERNAME = 'martins@gmail.com';
 mb_internal_encoding('UTF-8'); // Avoid ??s
 
 //Optimisation
-ob_implicit_flush();
-ob_start();
 ini_set("memory_limit", "256M");
 
 define("FAST_MODE", isset($_REQUEST["fast"]) ? $_REQUEST["fast"] : FALSE);
