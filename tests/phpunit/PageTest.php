37b67a519c4b445e06813633204e72473c804340
GlazerMann <kurt@ioiowa.com>
2018-10-25 11:05:46 -0700

    Save this test so we can have it standing by

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -85,4 +85,14 @@ final class PageTest extends testBaseClass {
       $page = $this->process_page($text);
       $this->assertTrue((boolean) strpos($page->parsed_text(), '05-12-2016'));
   }
+ 
+  public function testBadPage() {  // Use this when debugging pages that crash the bot
+    $this->assertTrue(TRUE);
+    return;
+    $text = file_get_contents('https://en.wikipedia.org/w/index.php?title=Vietnam_War&action=raw');
+    $page = new TestPage();
+    $page->parse_text($text);
+    $page->expand_text();
+    $this->assertTrue(TRUE);
+  }
 }
