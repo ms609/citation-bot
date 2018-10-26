cfd4f701ac4272abb9a512e8899fe710ee707e64
GlazerMann <kurt@ioiowa.com>
2018-10-25 18:48:41 -0700

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -10,6 +10,7 @@ final class PageTest extends testBaseClass {
 
    public function testBadPage() {  // Use this when debugging pages that crash the bot
     $text = file_get_contents('https://en.wikipedia.org/w/index.php?title=Inductive_programming&action=raw');
+    $text = "{{cite web|url=https://pdfs.semanticscholar.org/48f2/2821220555f8e327c2aa9614fb28c98f9542.pdf}}";
     $page = new TestPage();
     $page->parse_text($text);
     $page->expand_text();
