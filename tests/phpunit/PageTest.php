2594f34fd37f88c0289d633ba3a26d042628d656
GlazerMann <kurt@ioiowa.com>
2018-10-26 20:07:44 -0700

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -10,7 +10,7 @@ final class PageTest extends testBaseClass {
 
 
   public function testRespectDates() {
-      $text = '{{cite journal |doi=10.1002/1097-0142(19920315)69:6+<1578::AID-CNCR2820691312>3.0.CO;2-K |title=Molecular marker test standardization |year=1992 |last1=Koepke |first1=John A. |journal=Cancer |volume=69 |pages=1578–81 |pmid=1540898 |issue=6 Suppl}}';
+      $text = '{{cite journal |doi=10.1002/1097-0142(19920315)69:6+<1578::AID-CNCR2820691312>3.0.CO;2-K}}';
       $page = $this->process_page($text);
       $this->assertNull($page->parsed_text());
   }
