dd56667bdb7110d31cd28a8d25d927ca56f556bb
GlazerMann <kurt@ioiowa.com>
2018-10-30 09:09:08 -0700

    jstor too

--- Template.php
+++ Template.php
@@ -2750,9 +2750,11 @@ final class Template {
           }
           return;
           
-        case 'via':
-          if ($this->blank(['url', 'chapter-url', 'chapterurl', 'contribution-url', 'contributionurl']))
-          if (stripos($this->get('via'), 'PubMed') !== FALSE && ($this->has('pmc') || $this->has('pmid'))) { // Should just remove all 'via' with no url, but do not want to make people angry
+        case 'via':   // Should just remove all 'via' with no url, but do not want to make people angry
+          if ($this->blank(['url', 'chapter-url', 'chapterurl', 'contribution-url', 'contributionurl'])) 
+          if (stripos($this->get('via'), 'PubMed') !== FALSE && ($this->has('pmc') || $this->has('pmid'))) {
+            $this->forget('via');
+          } elseif (stripos($this->get('via'), 'JSTOR') !== FALSE && $this->has('jstor')) {
             $this->forget('via');
           }
           return;
