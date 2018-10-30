fb2852a7f081301a480e0c99ffc201eb2e93ba78
Martin R. Smith <martins@gmail.com>
2018-10-30 14:30:22 -0700

    Update Template.php
    Co-Authored-By: GlazerMann <kurt@ioiowa.com>

--- Template.php
+++ Template.php
@@ -2714,7 +2714,7 @@ final class Template {
         case 'url':
           if (preg_match("~^https?://(?:www.|)researchgate\.net/[^\s]*publication/([0-9]+)_*~i", $this->get($param), $matches)) {
               $this->set($param, 'https://www.researchgate.net/publication/' . $matches[1]);
-          } elseif (preg_match("~^https?://(?:www.|)academia\.edu/([0-9]+)/*~i", $this->get($param), $matches)) {
+          } elseif (preg_match("~^https?://(?:www\.|)academia\.edu/([0-9]+)/*~i", $this->get($param), $matches)) {
               $this->set($param, 'https://www.academia.edu/' . $matches[1]);
           } elseif (preg_match("~^https?://(?:www\.|)zenodo\.org/record/([0-9]+)(?:#|/files/)~i", $this->get($param), $matches)) {
               $this->set($param, 'https://zenodo.org/record/' . $matches[1]);
