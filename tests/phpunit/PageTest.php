3292c02e5a5c7a6fb3f00488739e0db7b1180b13
GlazerMann <kurt@ioiowa.com>
2018-10-30 14:33:27 -0700

    Save this test so we can have it standing by (#986)

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -87,4 +87,16 @@ final class PageTest extends testBaseClass {
       $page = $this->process_page($text);
       $this->assertTrue((boolean) strpos($page->parsed_text(), '05-12-2016'));
   }
+ 
+  public function testBadPage() {  // Use this when debugging pages that crash the bot
+    $bad_page = ""; //  Replace with something like "Vietnam_War" when debugging
+    if ($bad_page !== "") {
+      $text = file_get_contents('https://en.wikipedia.org/w/index.php?title=' . $bad_page . '&action=raw');
+      $page = new TestPage();
+      $page->parse_text($text);
+      $page->expand_text();
+      $this->assertTrue(FALSE); // prevent us from git committing with a website included
+    }
+    $this->assertTrue(TRUE);
+  }
 }
