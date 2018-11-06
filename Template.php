bd7557e72e22d08b4a0d11722b67ba73386c9316
Jonatan Svensson Glad <josve05a@users.noreply.github.com>
2018-11-06 19:22:17 +0100

    zenodo.org has been blacklisted (#1029)

--- Template.php
+++ Template.php
@@ -1728,6 +1728,7 @@ final class Template {
         }
         if (strpos($oa_url, 'bioone.org/doi') !== FALSE) return TRUE;
         if (strpos($oa_url, 'gateway.isiknowledge.com') !== FALSE) return TRUE;
+        if (strpos($oa_url, 'zenodo.org') !== FALSE) return TRUE;   //is currently blacklisted due to copyright concerns https://en.wikipedia.org/w/index.php?oldid=867438103#zenodo.org
         // Check if best location is already linked -- avoid double linki
         if (preg_match("~^https?://europepmc\.org/articles/pmc(\d+)~", $oa_url, $match) || preg_match("~^https?://www\.pubmedcentral\.nih\.gov/articlerender.fcgi\?.*\bartid=(\d+)"
                       . "|^https?://www\.ncbi\.nlm\.nih\.gov/pmc/articles/PMC(\d+)~", $oa_url, $match)) {
