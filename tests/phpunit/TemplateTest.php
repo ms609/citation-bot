4caa41edd552436650f753b019257d4f5e63f7b0
GlazerMann <kurt@ioiowa.com>
2018-11-06 14:56:11 -0800

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/../testBaseClass.php';
  
 final class TemplateTest extends testBaseClass {
 
-  public function testParameterWithNoParameters() {b
+  public function testParameterWithNoParameters() {
     $text = "{{Cite web | text without equals sign  }}";
     $expanded = $this->process_citation($text);
     $this->assertEquals($text, $expanded->parsed_text());
