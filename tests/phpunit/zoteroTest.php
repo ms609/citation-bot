72c6a25bc8cf18408332683647c547e3aeb107aa
GlazerMann <kurt@ioiowa.com>
2018-11-08 09:48:08 -0800

    Update zoteroTest.php

--- tests/phpunit/zoteroTest.php
+++ tests/phpunit/zoteroTest.php
@@ -8,12 +8,12 @@ require_once __DIR__ . '/../testBaseClass.php';
  
 class ZoteroTest extends testBaseClass {
 
-// TODO - seems to want a login or cookie   
-//public function testZoteroExpansionRG() {
-//    $text = '{{Cite journal|url =https://www.researchgate.net/publication/23445361}}';
-//    $expanded = $this->expand_via_zotero($text);
-//    $this->assertEquals('10.1136/jnnp.2008.144360', $expanded->get('doi'));
-//  }
+// Not actually Zotero anymore
+public function testZoteroExpansionRG() {
+    $text = '{{Cite journal|url =https://www.researchgate.net/publication/23445361}}';
+    $expanded = $this->expand_via_zotero($text);
+    $this->assertEquals('10.1136/jnnp.2008.144360', $expanded->get('doi'));
+  }
       
   public function testZoteroExpansionPII() {
     $text = '{{Cite journal|url = https://www.sciencedirect.com/science/article/pii/S0024379512004405}}';
