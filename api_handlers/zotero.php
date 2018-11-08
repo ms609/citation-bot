3a5b0e97d84f7924087f02a7a5c30948ae28b8ca
GlazerMann <kurt@ioiowa.com>
2018-11-08 06:03:18 -0800

    Update zotero.php

--- api_handlers/zotero.php
+++ api_handlers/zotero.php
@@ -62,7 +62,7 @@ function expand_by_zotero(&$template, $url = NULL) {
 
   if(preg_match("~^https?://(?:www\.|)twitter\.~", $url)) return FALSE;  // This should be {{cite tweet}}.  Stay away!!!
   if(preg_match("~^https?://(?:www\.|)youtube\.~", $url)) return FALSE;  // This should be {{cite AV media}}.
-  if (
+  if(preg_match("~^https?://(?:www\.|)youtu\.be~", $url)) return FALSE; 
   if(preg_match("~^https?://books\.google\.~", $url)) return FALSE;  // We have special google gooks code
   if(stristr($url, 'CITATION_BOT_PLACEHOLDER') !== FALSE) return FALSE; // That's a bad url
   if(preg_match('~^https?://(?:www.|)jstor.org/stable/(.*)$~', $url, $match)) return FALSE; // We do this ourself
