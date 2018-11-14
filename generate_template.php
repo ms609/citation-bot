90735f16cb493fb66cef5e80b222a6ade5776a3c
GlazerMann <kurt@ioiowa.com>
2018-11-14 08:12:45 -0800

    Update generate_template.php

--- generate_template.php
+++ generate_template.php
@@ -9,11 +9,11 @@ include('expandFns.php');
 $t = new Template();
 $t->parse_text('{{cite web}}');
 if (count($_GET) > 10) die('Excessive parameters passed');
+if (count($_GET) === 0) die('No parameters passed');
 foreach ($_GET as $param=>$value) {
   if (strlen($param . $value) > 256) die('Excessively long parameters passed');
   $t->set($param, $value);
 }
-if ($count === 0) die;
 $t->process(); // better than calling expand_by_doi because it also sets the wikiname
 echo "\n";
 echo "\n";
