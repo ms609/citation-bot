10635d5d8d46e57884361da2b0095d856065909f
GlazerMann <kurt@ioiowa.com>
2018-11-14 13:31:52 -0800

    Update expandFns.php

--- expandFns.php
+++ expandFns.php
@@ -425,7 +425,9 @@ function str_almost_the_same($str1, $str2) {  // Move forgiving than ===
   $str2 = mb_strtolower($str2);
   $str1 = str_replace(["[", "]"], ["", ""], $str1); // Ignore wiki-links
   $str2 = str_replace(["[", "]"], ["", ""], $str2);
-  $str1 = preg_replace("~^the ~", "", $str1);  // Ignore leading "the" so "New York Times" == "The New York Times"
-  $str2 = preg_replace("~^the ~", "", $str2);
-  return ($str1 === $str2);
+  $str1 = trim($str1);  // Remove spaces
+  $str2 = trim($str2);
+  $str1 = preg_replace("~^the\s+~", "", $str1);  // Ignore leading "the" so "New York Times" == "The New York Times"
+  $str2 = preg_replace("~^the\s+~", "", $str2);
+  return $str1 === $str2;
 }
