f4eb5955f955601d48387953c5fe80c4cd18d941
GlazerMann <kurt@ioiowa.com>
2018-11-15 15:08:29 -0800

    add another testEmptyCitations() test

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -1362,6 +1362,9 @@ ER -  }}';
     $text = 'bad things like {{cite journal}}{{cite book|||}} should not crash bot'; // bot removed pipes
     $expanded = $this->process_page($text);
     $this->assertEquals('bad things like {{cite journal}}{{cite book}} should not crash bot', $expanded->parsed_text());
+    $t = new Template();
+    $t->parse_text('{{cite web}}');
+    $t->process();
   }
  
   public function testBadBibcodeARXIVPages() {
