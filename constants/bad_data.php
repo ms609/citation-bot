670b66bb3e06d3fbe0bf854266ff1324b9dee5cc
GlazerMann <kurt@ioiowa.com>
2018-11-17 07:01:14 -0800

    also add Dissertations Available from Proquest

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -12,7 +12,7 @@ const BAD_AUTHORS = array("unknown", "missing");
 const PUBLISHER_ENDINGS = ["books", "corporation", 'centre', 'center', 'company', "inc.", "inc", "magazines",
                            'museum', "press", "publishers", "publishing", 'science'];
 const BAD_TITLES = array("unknown", "missing", "arxiv e-prints", "arxiv mathematics e-prints", 
-                         "ssrn electronic journal");
+                         "ssrn electronic journal", "dissertations available from proquest");
 const IN_PRESS_ALIASES = array("in press", "inpress", "pending", "published", 
                                "published online", "no-no", "n/a", "online ahead of print", 
                                "unpublished", "unknown", "tba", "forthcoming", "in the press", 
