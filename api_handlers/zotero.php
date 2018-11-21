c23d1a07fb6af11c9f2d56607a36b69ab6d136aa
GlazerMann <kurt@ioiowa.com>
2018-11-21 13:52:52 -0800

    Do it all in one place

--- api_handlers/zotero.php
+++ api_handlers/zotero.php
@@ -157,21 +157,20 @@ function expand_by_zotero(&$template, $url = NULL) {
   }
   
   if (isset($result->bookTitle)) {
-    $result->bookTitle = preg_replace('~\s*\(pdf\)$~iu', '', $result->bookTitle);
-    $result->bookTitle = preg_replace('~^\(pdf\)\s*~iu', '', $result->bookTitle);
+    $result->bookTitle = preg_replace('~\s*\(pdf\)$~i', '', $result->bookTitle);
+    $result->bookTitle = preg_replace('~^\(pdf\)\s*~i', '', $result->bookTitle);
+  }
+  if (isset($result->title)) {
+    $result->title = preg_replace('~\s*\(pdf\)$~i', '', $result->title);
+    $result->title = preg_replace('~^\(pdf\)\s*~i', '', $result->title);
+  }
+  
+  if (isset($result->bookTitle)) {
     $template->add_if_new('title', $result->bookTitle);
-    if (isset($result->title)) {
-      $result->title = preg_replace('~\s*\(pdf\)$~iu', '', $result->title);
-      $result->title = preg_replace('~^\(pdf\)\s*~iu', '', $result->title);
-      $template->add_if_new('chapter',   $result->title);
-    }
+    if (isset($result->title)) $template->add_if_new('chapter',   $result->title);
     if (isset($result->publisher))  $template->add_if_new('publisher', $result->publisher);
   } else {
-    if (isset($result->title)) {
-      $result->title = preg_replace('~\s*\(pdf\)$~iu', '', $result->title);
-      $result->title = preg_replace('~^\(pdf\)\s*~iu', '', $result->title);
-      $template->add_if_new('title'  , $result->title);
-    }
+    if (isset($result->title)) $template->add_if_new('title'  , $result->title);
     if (isset($result->itemType) && ($result->itemType === 'book')) {
        if (isset($result->publisher))  $template->add_if_new('publisher', $result->publisher); 
     }
