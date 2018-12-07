8f03593349137d8e009193db4df2b2a752500935
GlazerMann <kurt@ioiowa.com>
2018-12-07 10:14:03 -0800

    NON_HUMAN_AUTHORS

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -6,6 +6,7 @@ const BAD_ACCEPTED_MANUSCRIPT_TITLES = array("oup accepted manuscript", "placeho
                                              "placeholder", "symbolic placeholder", "[placeholder]", 
                                              "placeholder for arabic language transliteration");
 const BAD_AUTHORS = array("unknown", "missing", "- -.", "- -");
+const NON_HUMAN_AUTHORS = array('collaborat', 'reporter', 'journalist', 'correspondent', 'anchor');
 
 // Catch 'authors' such as "hearst magazines", "time inc", "nielsen business media, inc"
 // Ordered alphabetically.
