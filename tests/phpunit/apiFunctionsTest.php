a8774e3865e19a8751d60dd0f738032b24722adb
GlazerMann <kurt@ioiowa.com>
2018-12-15 15:03:14 -0800

    Update apiFunctionsTest.php

--- tests/phpunit/apiFunctionsTest.php
+++ tests/phpunit/apiFunctionsTest.php
@@ -5,14 +5,9 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class apiFunctionsTest extends testBaseClass {
   
   public function testArxivDateUpgradeSeesDate() {
-      $text = '{{Cite journal|date=September 2010|doi=10.1016/j.physletb.2010.08.018|arxiv=1006.4000}}';
+      $text = '{{cite journal|doi=10.17816/uroved513-6}}';
       $expanded = $this->process_citation($text);
-      $this->assertEquals('September 2010', $expanded->get('date'));
-      $this->assertNull($expanded->get('year'));
-      
-      $text = '{{Cite journal|date=September 2009|doi=10.1016/j.physletb.2010.08.018|arxiv=1006.4000}}';
-      $expanded = $this->process_citation($text);
-      $this->assertNull($expanded->get('date'));
-      $this->assertEquals('2010', $expanded->get('year'));
+     // $this->assertEquals('', $expanded->get(''));
+     $this->assertNull($expanded->get('last1')); 
   } 
 }
