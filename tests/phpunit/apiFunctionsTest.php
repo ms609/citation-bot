18e3bb84da6110c2536b7c104dfdf7964d9ee8c3
GlazerMann <kurt@ioiowa.com>
2018-12-15 16:42:43 -0800

    Update apiFunctionsTest.php

--- tests/phpunit/apiFunctionsTest.php
+++ tests/phpunit/apiFunctionsTest.php
@@ -7,6 +7,9 @@ final class apiFunctionsTest extends testBaseClass {
   public function testArxivDateUpgradeSeesDate() {
       $text = '{{cite journal|url=https://www.gbif.org/dataset/0938172b-2086-439c-a1dd-c21cb0109ed5}}';
       $expanded = $this->process_citation($text);
-      $this->assertNull($expanded->parsed_text()); 
+      $text  = $expanded->parsed_text();
+      $expanded = $this->process_citation($text);
+      $text  = $expanded->parsed_text();
+      $this->assertNull($text); 
   }
 }
