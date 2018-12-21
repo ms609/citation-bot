af781d06169b991f419213549287a6b368b7c8ab
GlazerMann <kurt@ioiowa.com>
2018-12-20 23:39:24 -0800

    Do not change existing linked data (#1129)

--- Template.php
+++ Template.php
@@ -597,6 +597,8 @@ final class Template {
           if ($this->has('website')) { // alias for journal
              if (str_equivalent($this->get('website'), $value)) {
                $this->rename('website', $param_name);
+             } elseif (preg_match('~^[.+]$~', $this->get('website'))) {
+               $this->rename('website', $param_name); // existing data is linked
              } else {
                $this->rename('website', $param_name, $value);
              }
