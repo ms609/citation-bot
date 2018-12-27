12dda1d5939ac97af1a3c40b29969aab6230eb2d
GlazerMann <kurt@ioiowa.com>
2018-12-26 16:10:53 -0800

    Reject ‘null’

--- Template.php
+++ Template.php
@@ -345,8 +345,13 @@ final class Template {
    *
    */
   public function add_if_new($param_name, $value, $api = NULL) {
-    if (trim($value) == '') {
+    $value = trim($value);
+    if ($value == '') {
       return FALSE;
+    }
+    
+    if (strcasecmp($value, 'null') === O) {
+      return FALSE; // We hope that name is not actually NULL
     }
     
     if (mb_stripos($this->get($param_name), 'CITATION_BOT_PLACEHOLDER_COMMENT') !== FALSE) {
