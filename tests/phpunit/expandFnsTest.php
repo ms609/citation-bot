7a3ae6c5251a5f3b79e73ff79baee7a58ea0a089
GlazerMann <kurt@ioiowa.com>
2019-01-01 14:55:49 -0800

    Update expandFnsTest.php

--- tests/phpunit/expandFnsTest.php
+++ tests/phpunit/expandFnsTest.php
@@ -10,8 +10,8 @@ final class expandFnsTest extends testBaseClass {
 
   public function testCapitalization() {
     $text='{{cite web|url=https://www.nature.com/articles/546031a#bk4}}';
-    $this->process_citation($text);
-    $this->assertNull($this->parsed_text());
+    $expanded = $this->process_citation($text);
+    $expanded->assertNull($this->parsed_text());
   }
     
 }
