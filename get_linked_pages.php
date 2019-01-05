2554c950c6a8ae26cf30c3aa677cc8a04ad53e0d
GlazerMann <kurt@ioiowa.com>
2019-01-04 18:21:40 -0800

    Remove links to lots of standard things

--- get_linked_pages.php
+++ get_linked_pages.php
@@ -14,10 +14,22 @@ $json = file_get_contents($url);
 $array = json_decode($json, true);
 $links = $array['parse']['links'];
 
+// List of things to not print links to, since they occur all the time
+const AVOIDED_LINKS = array('', 'Digital_object_identifier', 'JSTOR', 'Website', 'International_Standard_Book_Number',
+                            'Library_of_Congress_Control_Number', 'Handle_System', 'PubMed_Central', 'PubMed',
+                            'PubMed_Identifier', 'Bibcode', 'International_Standard_Serial_Number', 'bioRxiv',
+                            'CiteSeerX', 'Zentralblatt_MATH', 'Jahrbuch_über_die_Fortschritte_der_Mathematik',
+                            'Mathematical_Reviews', 'Office_of_Scientific_and_Technical_Information',
+                            'Request_for_Comments', 'Social_Science_Research_Network', 'Zentralblatt_MATH',
+                            'Open_Library', 'ArXiv', 'OCLC')
+
 echo "<pre>\n";
 foreach($links as $link) {
-    if (isset($link['exists']) && ($link['ns'] == 0 || $link['ns'] == 118)) {
-        echo str_replace(' ', '_', $link['*']) . "\n";
+    if (isset($link['exists']) && ($link['ns'] == 0 || $link['ns'] == 118))) {  // normal and draft aritcles only
+        $linked_page = str_replace(' ', '_', $link['*']);
+        if(!in_array($linked_page, AVOIDED_LINKS)) {
+            echo $linked_page . "\n";
+        }
     }
 }
 echo  "</pre>\n";
