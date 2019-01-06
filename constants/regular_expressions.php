153739fd57dbbaea429a575ef8a8b7ca6d0834c8
GlazerMann <kurt@ioiowa.com>
2019-01-06 11:52:16 -0800

    add REGEXP_DOI_ISSN_ONLY

--- constants/regular_expressions.php
+++ constants/regular_expressions.php
@@ -11,6 +11,7 @@ const REGEXP_SICI = "~(\d{4}-\d{3}[\dxX])" . // ISSN
                     "\((\d{4})(\d{2})?/?(\d{2})?\)" . // Chronology, YY MM DD
                     "(\d+):?([\+\d]*)" . // Enumeration: Volume / issue
                     "[<\[]" . "(\d+)::?\w+" . "[>\]]" . "2\.0\.CO;2\-?[A-z0-9]?~";
+const REGEXP_DOI_ISSN_ONLY = '~^10.[^/]+/\(ISSN\)\d{4}-\d{3}[xX0-9]$~';
 
 // See https://mathiasbynens.be/demo/url-regex/  This regex is more exact than validator.  We only spend time on this after quick and dirty check is passed
 const REGEXP_IS_URL = '~^(?:(?:https?|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?!10(?:\\.\\d{1,3}){3})(?!127(?:\\.\\d{1,3}){3})(?!169\\.254(?:\\.\\d{1,3}){2})(?!192\\.168(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,})))(?::\\d{2,5})?(?:/[^\\s]*)?$~iuS';
