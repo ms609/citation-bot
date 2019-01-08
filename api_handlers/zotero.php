73d4b001723be4a3426361eba2f0d462cd3194ea
GlazerMann <kurt@ioiowa.com>
2019-01-07 22:58:59 -0800

    Add Documentation (#1183)

--- api_handlers/zotero.php
+++ api_handlers/zotero.php
@@ -250,7 +250,7 @@ function expand_by_zotero(&$template, $url = NULL) {
           trigger_error("Unhandled itemType: " . $result->itemType);
         } else {
           report_warning("Unhandled itemType: " . $result->itemType);
-        }
+        } // see https://www.mediawiki.org/wiki/Citoid/itemTypes
     }
     
     $i = 0; $author_i = 0; $editor_i = 0; $translator_i = 0;
