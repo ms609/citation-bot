a6f1d0a9bb0db2e00a6ed72638c69664669a7199
GlazerMann <kurt@ioiowa.com>
2019-01-09 12:26:17 -0800

    Loop over Regex array

--- Page.php
+++ Page.php
@@ -302,22 +302,24 @@ class Page {
   public function extract_object($class) {
     $i = 0;
     $text = $this->text;
-    $regexp = $class::REGEXP;
+    $regexp_list = $class::REGEXP;
     $placeholder_text = $class::PLACEHOLDER_TEXT;
     $treat_identical_separately = $class::TREAT_IDENTICAL_SEPARATELY;
     $objects = array();
     
-    $preg_ok = TRUE;
-    while ($preg_ok = preg_match($regexp, $text, $match)) {
-      $obj = new $class();
-      $obj->parse_text($match[0]);
-      $exploded = $treat_identical_separately ? explode($match[0], $text, 2) : explode($match[0], $text);
-      $text = implode(sprintf($placeholder_text, $i++), $exploded);
-      $objects[] = $obj;
-    }
-    if ($preg_ok === FALSE) {
-       // PHP 5 segmentation faults in preg_match when it fails.  PHP 7 returns FALSE.
-       trigger_error('Regular expression failure in ' . htmlspecialchars($this->title) . ' when extracting ' . $class . 's', E_USER_ERROR);
+    foreach ($regexp_list as $regexp) {
+      $preg_ok = TRUE;
+      while ($preg_ok = preg_match($regexp, $text, $match)) {
+        $obj = new $class();
+        $obj->parse_text($match[0]);
+        $exploded = $treat_identical_separately ? explode($match[0], $text, 2) : explode($match[0], $text);
+        $text = implode(sprintf($placeholder_text, $i++), $exploded);
+        $objects[] = $obj;
+      }
+      if ($preg_ok === FALSE) {
+         // PHP 5 segmentation faults in preg_match when it fails.  PHP 7 returns FALSE.
+         trigger_error('Regular expression failure in ' . htmlspecialchars($this->title) . ' when extracting ' . $class . 's', E_USER_ERROR);
+      }
     }
     $this->text = $text;
     return $objects;
