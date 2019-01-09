8f77bc61e6dd57be03cfadd9f4b872848e63dc87
GlazerMann <kurt@ioiowa.com>
2019-01-09 12:11:04 -0800

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -9,23 +9,8 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class PageTest extends testBaseClass {
 
 
-  public function testUrlReferencesThatFail() {
-      $text = 'testUrlReferencesThatFail <ref name="bob">http://this.fails/nothing< / ref> testUrlReferencesThatFail <ref >  http://this.fails/nothing </ref> testUrlReferencesThatFail <ref>10.1234/ABCDEFGHIJ.faker</ref>';
-      $page = $this->process_page($text);
-      $this->assertEquals($text, $page->parsed_text());
-  }
- 
-  public function testRespectDates() {
-      $text = '{{Use mdy dates}}{{cite web|url=https://www.nasa.gov/content/profile-of-john-glenn}}';
-      $page = $this->process_page($text);
-      $this->assertTrue((boolean) strpos($page->parsed_text(), '12-05-2016'));
-      $text = '{{Use dmy dates}}{{cite web|url=https://www.nasa.gov/content/profile-of-john-glenn}}';
-      $page = $this->process_page($text);
-      $this->assertTrue((boolean) strpos($page->parsed_text(), '05-12-2016'));
-  }
- 
   public function testBadPage() {  // Use this when debugging pages that crash the bot
-    $bad_page = ""; //  Replace with something like "Vietnam_War" when debugging
+    $bad_page = "Deim_Zubeir"; //  Replace with something like "Vietnam_War" when debugging
     if ($bad_page !== "") {
       $text = file_get_contents('https://en.wikipedia.org/w/index.php?title=' . $bad_page . '&action=raw');
       $page = new TestPage();
