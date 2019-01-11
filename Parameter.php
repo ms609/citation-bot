e87331b82ca1dfa0eb5136ad18648c57f8e30762
GlazerMann <kurt@ioiowa.com>
2019-01-11 07:12:43 -0800

    Update Parameter.php

--- Parameter.php
+++ Parameter.php
@@ -58,6 +58,7 @@ final class Parameter {
   protected function set_value($value) {
     switch ($this->param) {
       case 'pages':
+        // When changing this do not forget similar code in Template.php
         if (stripos($value, 'http') === FALSE && mb_stripos($value, 'CITATION_BOT_PLACEHOLDER_COMMENT') === FALSE &&
             mb_strpos($value, '[//') === FALSE) {
           $value = mb_ereg_replace(REGEXP_TO_EN_DASH, REGEXP_EN_DASH, $value);
