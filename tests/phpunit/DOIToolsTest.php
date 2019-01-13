723e30f3269ec18dd1a0016e08b21356d8f51eb4
GlazerMann <kurt@ioiowa.com>
2019-01-12 19:46:10 -0800

    Update DOIToolsTest.php

--- tests/phpunit/DOIToolsTest.php
+++ tests/phpunit/DOIToolsTest.php
@@ -8,10 +8,10 @@ require_once __DIR__ . '/../testBaseClass.php';
 
 final class doiToolsTest extends testBaseClass {
   public function testFormat1() {
-    $this->assertEquals('& Johnson, A.', format_surname('& A. Johnson'));
+    $this->assertEquals('& a. Johnson', format_surname('& A. Johnson'));
   }
   public function testFormat2() {
-    $this->assertEquals('Johnson', format_surname('Johnson; Smith'));
+    $this->assertEquals('Johnson; Smith', format_surname('Johnson; Smith'));
   }
   public function testFormat3() {
     $this->assertEquals(FALSE, format_author(''));
@@ -26,6 +26,6 @@ final class doiToolsTest extends testBaseClass {
     $this->assertEquals('Johnson, A. B. C. D. E. F. G', format_author('A. B. C. D. E. F. G. Johnson'));
    }
   public function testFormat7() {
-    $this->assertEquals(['John, .','Bob, .','Kim, .','Billy, .'], format_multiple_authors('John;Bob;Kim;Billy', TRUE));
+    $this->assertEquals(['John, .','Bob, .','Kim, .','Billy,'], format_multiple_authors('John;Bob;Kim;Billy', TRUE));
   } 
 }
