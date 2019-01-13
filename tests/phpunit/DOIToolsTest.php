a2e5b48a7780493ab52d3a1582dd6c416edcb670
GlazerMann <kurt@ioiowa.com>
2019-01-12 20:07:59 -0800

    Update DOIToolsTest.php

--- tests/phpunit/DOIToolsTest.php
+++ tests/phpunit/DOIToolsTest.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/../testBaseClass.php';
 
 final class doiToolsTest extends testBaseClass {
   public function testFormat1() {
-    $this->assertEquals('Johnson', wikify_external_text('Johsnon&nbsp;'));
+    $this->assertEquals('Johsnon ', wikify_external_text('Johsnon&nbsp;'));
     $this->assertEquals('[Johsnon And me]', title_capitalization('[Johsnon And me]'));
     $this->assertEquals('ABC', remove_comments('A<!-- -->B# # # CITATION_BOT_PLACEHOLDER_COMMENT 33 # # #C'));
     $this->assertEquals('', tidy_date('22/22/2010'));  // That is not valid date code
