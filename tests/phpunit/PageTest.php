11f707192969e35a53e1dbea1933a1255dadcd40
GlazerMann <kurt@ioiowa.com>
2019-01-16 07:27:33 -0800

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -15,8 +15,14 @@ final class PageTest extends testBaseClass {
       $page = new TestPage();
       $page->parse_text($text);
       $page->expand_text();
-      $this->assertTrue(FALSE); // prevent us from git committing with a website included
     }
-    $this->assertTrue(TRUE);
+    $bad_page = "List_of_Flashpoint_episodes"; //  Replace with something like "Vietnam_War" when debugging
+    if ($bad_page !== "") {
+      $text = file_get_contents('https://en.wikipedia.org/w/index.php?title=' . $bad_page . '&action=raw');
+      $page = new TestPage();
+      $page->parse_text($text);
+      $page->expand_text();
+    }
+    $this->assertTrue(FASLE);
   }
 }
