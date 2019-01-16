3a70f095e542d207428530e615118763fb65e698
GlazerMann <kurt@ioiowa.com>
2019-01-16 09:54:46 -0800

    Update bad_data.php

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -62,7 +62,7 @@ const NON_JOURNAL_WEBSITES = array('cnn.com/', 'foxnews.com/', 'msnbc.com/', 'nb
                                    'gsfc.nasa.gov/', 'solarsystem.nasa.gov/', 'latimes.com/',
                                    'usatoday.com/', 'wsj.com/', 'haaretz.com/', 'buzzfeed.com/',
                                    'aljazeera.com/', 'vox.com/', 'reuters.com/', 'dailynews.com/', 
-                                   'newsweek.com/', 'monitor.com/', 'observer.com/', '.pbs.org/', '.bbm.ca/' '/bbm.ca/',
+                                   'newsweek.com/', 'monitor.com/', 'observer.com/', '.pbs.org/', '.bbm.ca/', '/bbm.ca/',
                                    'mediaincanada.com/', 'cbspressexpress.com/', 'deadline.com/', 'zap2it.com/',
                                    'yourentertainmentnow.com/', 'shows.ctv.ca/' ,'toronto.com/'); 
                                    // Just a list of ones that are obvious.  Add ones that time-out as we find them
