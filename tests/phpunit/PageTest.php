108ab1fe5d2c0e2718943522fddf4650d586b683
GlazerMann <kurt@ioiowa.com>
2019-01-16 08:47:55 -0800

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -10,13 +10,6 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class PageTest extends testBaseClass {
 
   public function testBadPage() {  // Use this when debugging pages that crash the bot
-    $bad_page = "2016_Turkish_coup_d%27état_attempt"; //  Replace with something like "Vietnam_War" when debugging
-    if ($bad_page !== "") {
-      $text = file_get_contents('https://en.wikipedia.org/w/index.php?title=' . $bad_page . '&action=raw');
-      $page = new TestPage();
-      $page->parse_text($text);
-      $page->expand_text();
-    }
     $bad_page = "List_of_Flashpoint_episodes"; //  Replace with something like "Vietnam_War" when debugging
     if ($bad_page !== "") {
       $text = file_get_contents('https://en.wikipedia.org/w/index.php?title=' . $bad_page . '&action=raw');
