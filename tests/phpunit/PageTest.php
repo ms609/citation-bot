640218e0e862b33a0da707d4d7d96d383fd297ee
GlazerMann <kurt@ioiowa.com>
2019-01-16 11:49:45 -0800

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -10,13 +10,10 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class PageTest extends testBaseClass {
 
   public function testBadPage() {  // Use this when debugging pages that crash the bot
-    $bad_page = "List_of_Flashpoint_episodes"; //  Replace with something like "Vietnam_War" when debugging
-    if ($bad_page !== "") {
-      $text = file_get_contents('https://en.wikipedia.org/w/index.php?title=' . $bad_page . '&action=raw');
+      $text = '{{Cite journal|last=Šťovíček|first=Jan|date=2013-01-01|title=Deconstructibility and the Hill Lemma in Grothendieck categories|journal=Forum Mathematicum|language=en|volume=25|issue=1|pages=|arxiv=1005.3251|doi=10.1515/FORM.2011.113}}';
       $page = new TestPage();
       $page->parse_text($text);
       $page->expand_text();
-    }
-    $this->assertTrue(FASLE);
+    $this->assertNull($page->parsed_text());
   }
 }
