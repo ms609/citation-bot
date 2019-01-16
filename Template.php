d89daa4fc184df652493ccd14a0396e04d0701f8
GlazerMann <kurt@ioiowa.com>
2019-01-16 12:11:19 -0800

    Update Template.php

--- Template.php
+++ Template.php
@@ -1675,7 +1675,8 @@ final class Template {
       $header = substr($return, 0, $header_length);
       $body = substr($return, $header_length);
       $decoded = @json_decode($body);
-      
+      echo "\n\n HEADER  " . $header . "\n\n";
+      print_r($decoded);
       if (is_object($decoded) && isset($decoded->error)) {
         if (is_object($decoded) && isset($decoded->error)) {
           throw new Exception(
