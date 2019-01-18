ba2e69f0b8d023ce5abcf72b20b4c984a4d471ef
GlazerMann <kurt@ioiowa.com>
2019-01-17 16:32:31 -0800

    Update Template.php

--- Template.php
+++ Template.php
@@ -1478,7 +1478,7 @@ final class Template {
         $result = $this->query_adsabs("title:" . urlencode('"' .  trim(str_replace('"', ' ', $this->get_without_comments_and_placeholders("title"))) . '"'));
         if ($result->numFound == 0) return FALSE;
         $record = $result->docs[0];
-        if (titles_are_dissimilar($record->title[0], $this->get('title')) {
+        if (titles_are_dissimilar($record->title[0], $this->get('title'))) {
           report_info("Similar title not found in database");
           return FALSE;
         }
