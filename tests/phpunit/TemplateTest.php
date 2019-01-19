800b4eb00f4fe9d73930213d751fdd77ea34d026
GlazerMann <kurt@ioiowa.com>
2019-01-19 13:11:05 -0800

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -592,7 +592,7 @@ final class TemplateTest extends testBaseClass {
   }
   
   public function testBibcodesBooks() {
-    $this->requires_secrets(function() {
+   requires_secrets(function() {
       $text = "{{Cite book|bibcode=1982mcts.book.....H}}";
       $expanded = $this->process_citation($text);
       $this->assertEquals('1982', $expanded->get('year'));
@@ -1164,7 +1164,7 @@ ER -  }}';
   }
   
   public function testBibcodeDotEnding() {
-    $this->requires_secrets(function() {
+    requires_secrets(function() {
       $text='{{cite journal|title=Electric Equipment of the Dolomites Railway|journal=Nature|date=2 January 1932|volume=129|issue=3244|page=18|doi=10.1038/129018a0}}';
       $expanded = $this->process_citation($text);
       $this->assertEquals('1932Natur.129Q..18.', $expanded->get('bibcode'));
@@ -1405,7 +1405,7 @@ ER -  }}';
   }
     
   public function testJustAnOCLC() {
-    $this->requires_secrets(function() {
+    requires_secrets(function() {
       $text = '{{cite web | url=http://www.worldcat.org/oclc/9334453}}';
       $expanded = $this->process_citation($text);
       $this->assertEquals('cite book', $expanded->wikiname());
@@ -1416,7 +1416,7 @@ ER -  }}';
   }
 
   public function testJustAnLCCN() {
-    $this->requires_secrets(function() {
+    requires_secrets(function() {
       $text = '{{cite book | lccn=2009925036}}';
       $expanded = $this->process_citation($text);
       $this->assertEquals('Alternative Energy for Dummies', $expanded->get('title'));
