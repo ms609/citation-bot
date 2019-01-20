e2286f75ba5db1a8bebe06bf86dbe13826c151ae
GlazerMann <kurt@ioiowa.com>
2019-01-20 10:30:26 -0800

    Update apiFunctionsTest.php

--- tests/phpunit/apiFunctionsTest.php
+++ tests/phpunit/apiFunctionsTest.php
@@ -29,23 +29,4 @@ final class apiFunctionsTest extends testBaseClass {
     });
   }
   
-  public function testArxivDateUpgradeSeesDate() {
-      $text = '{{Cite journal|date=September 2010|doi=10.1016/j.physletb.2010.08.018|arxiv=1006.4000}}';
-      $expanded = $this->process_citation($text);
-      $this->assertEquals('September 2010', $expanded->get('date'));
-      $this->assertNull($expanded->get('year'));
-      
-      $text = '{{Cite journal|date=September 2009|doi=10.1016/j.physletb.2010.08.018|arxiv=1006.4000}}';
-      $expanded = $this->process_citation($text);
-      $this->assertNull($expanded->get('date'));
-      $this->assertEquals('2010', $expanded->get('year'));
-  }
-  
-  public function testExpansion_doi_not_from_crossref() {
-     $text = '{{Cite journal| doi= 10.13140/RG.2.1.1002.9609}}';
-     $expanded = $this->process_citation($text);
-     $this->assertNull(NULL); // Stopped working
-     // $this->assertEquals('Lesson Study as a form of in-School Professional Development', $expanded->get('title'));
-     // $this->assertEquals('2015', $expanded->get('year'));
-   }
 }
