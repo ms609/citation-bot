c91fed402b7f5b1570dae4f26be8a762d5b84882
GlazerMann <kurt@ioiowa.com>
2019-01-25 13:22:22 -0800

    documentatino

--- apiFunctions.php
+++ apiFunctions.php
@@ -445,9 +445,10 @@ function query_crossref($doi) {
 }
 
 function expand_doi_with_dx($template, $doi) {
-     // See https://crosscite.org/docs.html for discussion of API we are using -- not all agencies resolve this way
+     // See https://crosscite.org/docs.html for discussion of API we are using -- not all agencies resolve the same way
      // https://api.crossref.org/works/$doi can be used to find out the agency
      // https://www.doi.org/registration_agencies.html  https://www.doi.org/RA_Coverage.html List of all ten doi granting agencies - many do not do journals
+     // Examples of DOI usage   https://www.doi.org/demos.html
      $try_to_add_it = function($name, $data) use($template) {
        if (is_null($data)) return;
        while (is_array($data)) {
