90f4ce7ed543222bc6705bd6f123ff900d3d70de
GlazerMann <kurt@ioiowa.com>
2019-01-29 09:04:00 -0800

    Update apiFunctions.php

--- apiFunctions.php
+++ apiFunctions.php
@@ -350,6 +350,7 @@ function expand_by_doi($template, $force = FALSE) {
   
   if (!$template->verify_doi()) return FALSE;
   $doi = $template->get_without_comments_and_placeholders('doi');
+  if (preg_match(REGEXP_DOI_ISSN_ONLY, $doi)) return FALSE; // We do not use DOI's that are just an ISSN.
   if ($doi && preg_match('~^10\.2307/(\d+)$~', $doi)) {
       $template->add_if_new('jstor', substr($doi, 8));
   }
