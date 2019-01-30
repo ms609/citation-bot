fdda4098a9642752df3b511942113c1d2ff8f573
GlazerMann <kurt@ioiowa.com>
2019-01-30 07:30:11 -0800

    Add more publishers (#1271)

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -38,7 +38,8 @@ const CANONICAL_PUBLISHER_URLS = array ('elsevier.com', 'springer.com', 'science
                                 'scielo.org', 'scielo.br', 'degruyter.com', 'hindawi.com', 'inderscience.com',
                                 'cambridge.org', '.oup.com', 'nature.com', 'macmillan.com', 'ieeexplore.ieee.org',
                                 'worldscientific.com', 'iospress.com', 'iospress.nl', 'pnas.org', 'journals.ametsoc.org',
-                                'pubs.geoscienceworld.org',
+                                'pubs.geoscienceworld.org', 'pubs.rsc.org', 'journals.uchicago.edu',
+                                'annualreviews.org', 'aip.scitation.org',
                                 //  Below are sites that are simply DOI resolvers, like dx.doi.org
                                 'doi.library.ubc.ca');
 
