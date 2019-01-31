fc284cac0e2d42d4ddebd186d27c5a328a30a8b0
GlazerMann <kurt@ioiowa.com>
2019-01-31 08:22:03 -0800

    fix messages

--- api_handlers/zotero.php
+++ api_handlers/zotero.php
@@ -142,7 +142,7 @@ function expand_by_zotero(&$template, $url = NULL) {
   if (isset($result->bookTitle)) {
    foreach (array_merge(BAD_ACCEPTED_MANUSCRIPT_TITLES, IN_PRESS_ALIASES) as $bad_title ) {
       if (strcasecmp($result->bookTitle, $bad_title) === 0) {
-        report_info("Received invalid book title data for URL ". $url . ": $test_data");
+        report_info("Received invalid book title data for URL ". $url . ": $result->bookTitle");
         return FALSE;
       }
    }
@@ -150,7 +150,7 @@ function expand_by_zotero(&$template, $url = NULL) {
   if (isset($result->title)) {
    foreach (array_merge(BAD_ACCEPTED_MANUSCRIPT_TITLES, IN_PRESS_ALIASES) as $bad_title ) {
       if (strcasecmp($result->title, $bad_title) === 0) {
-        report_info("Received invalid title data for URL ". $url . ": $test_data");
+        report_info("Received invalid title data for URL ". $url . ": $result->title");
         return FALSE;
       }
    }
@@ -158,7 +158,7 @@ function expand_by_zotero(&$template, $url = NULL) {
   if (isset($result->publicationTitle)) {
    foreach (array_merge(BAD_ACCEPTED_MANUSCRIPT_TITLES, IN_PRESS_ALIASES) as $bad_title ) {
       if (strcasecmp($result->publicationTitle, $bad_title) === 0) {
-        report_info("Received invalid publication title data for URL ". $url . ": $test_data");
+        report_info("Received invalid publication title data for URL ". $url . ": $result->publicationTitle");
         return FALSE;
       }
    }
