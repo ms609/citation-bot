2a74c336f3b76776644e907db3b2ab30c3de1593
GlazerMann <kurt@ioiowa.com>
2019-02-03 14:39:26 -0800

    Update userMessagesTest.php

--- tests/phpunit/userMessagesTest.php
+++ tests/phpunit/userMessagesTest.php
@@ -1,29 +1,58 @@
+<?php
+/*
+ * Tests for user_messages.php.  We use a string that would be encoded, not a realistic one
+ */
+require_once __DIR__ . '/../testBaseClass.php';
 
-function safely_echo ($string) {
-  echo echoable($string);
-}
-function echoable($string) {
-  return HTML_OUTPUT ? htmlspecialchars($string) : $string;
-}
-function pubmed_link($identifier, $pm) {
-  return HTML_OUTPUT 
-       ? '<a href="https://www.ncbi.nlm.nih.gov/pubmed/' . urlencode($pm) . '" target="_blank">'
-         . strtoupper($identifier) . ' ' . $pm . "</a>"
-       : strtoupper($identifier) . ' ' . $pm;
-}
-function bibcode_link($id) {
-  return HTML_OUTPUT
-    ? '<a href="http://adsabs.harvard.edu/abs/' . urlencode($id) . '" target="_blank">'
-      . $id . '</a>'
-    : $id;
-}
-function doi_link($doi) {
-  return HTML_OUTPUT
-    ? '<a href="http://dx.doi.org/' . urlencode($doi) . '" target="_blank">' . $doi . '</a>'
-    : $doi;
-}
-function jstor_link($id) {
-  return HTML_OUTPUT
-    ? '<a href="https://www.jstor.org/citation/ris/' . urlencode($id) . '" target="_blank">JSTOR ' . $id . '</a>'
-    : "JSTOR $id";
+final class userMessagesTest extends testBaseClass {
+  public function testEchoable() {
+    $HTML = HTML_OUTPUT;
+    $text = '<[Hello]>';
+    define("HTML_OUTPUT", TRUE);
+    $this->assertEquals('', echoable($text));
+    define("HTML_OUTPUT", FALSE);
+    $this->assertEquals('', echoable($text));
+    define("HTML_OUTPUT", $HTML);
+  }
+
+  public function testPubMed() {
+    $HTML = HTML_OUTPUT;
+    $text = '<[Hello]>';
+    define("HTML_OUTPUT", TRUE);
+    $this->assertEquals('', pubmed_link($text, 'pmid'));
+    define("HTML_OUTPUT", FALSE);
+    $this->assertEquals('', pubmed_link($text, 'pmid'));
+    define("HTML_OUTPUT", $HTML);
+  }
+ 
+  public function testBibCode() {
+    $HTML = HTML_OUTPUT;
+    $text = '<[Hello]>';
+    define("HTML_OUTPUT", TRUE);
+    $this->assertEquals('', bibcode_link($text));
+    define("HTML_OUTPUT", FALSE);
+    $this->assertEquals('', bibcode_link($text));
+    define("HTML_OUTPUT", $HTML);
+  }
+
+  public function testDoiLink() {
+    $HTML = HTML_OUTPUT;
+    $text = '<[Hello]>';
+    define("HTML_OUTPUT", TRUE);
+    $this->assertEquals('', doi_link($text));
+    define("HTML_OUTPUT", FALSE);
+    $this->assertEquals('', doi_link($text));
+    define("HTML_OUTPUT", $HTML);
+  }
+
+  public function testJstorLink() {
+    $HTML = HTML_OUTPUT;
+    $text = '<[Hello]>';
+    define("HTML_OUTPUT", TRUE);
+    $this->assertEquals('', jstor_link($text));
+    define("HTML_OUTPUT", FALSE);
+    $this->assertEquals('', jstor_link($text));
+    define("HTML_OUTPUT", $HTML);
+  }
+
 }
