6c5873132d19b18eb98d5779879001b49c44b7e8
GlazerMann <kurt@ioiowa.com>
2019-02-04 15:38:48 -0800

    Update Template.php

--- Template.php
+++ Template.php
@@ -1286,6 +1286,7 @@ final class Template {
     ];
     
     $novel_data = FALSE;
+    foreach ($data as $key => $value) if ($value) {		
       if ($this->api_has_not_used('crossref', equivalent_parameters($key))) $novel_data = TRUE;
       $this->record_api_usage('crossref', $key);    
     }
