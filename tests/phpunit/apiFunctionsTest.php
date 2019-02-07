342b39aac9d85f41fdf0379e24d18ad3863ceba2
GlazerMann <kurt@ioiowa.com>
2019-02-07 14:20:12 -0800

    add test

--- tests/phpunit/apiFunctionsTest.php
+++ tests/phpunit/apiFunctionsTest.php
@@ -29,6 +29,14 @@ final class apiFunctionsTest extends testBaseClass {
     });
   }
   
+  public function testComplexCrossRef() {
+    $text = '{{citation | title = Deciding the Winner of an Arbitrary Finite Poset Game is PSPACE-Complete| arxiv = 1209.1750| bibcode = 2012arXiv1209.1750G}}';
+    $expanded = $this->process_citation($text);
+    $this->assertEquals('Deciding the Winner of an Arbitrary Finite Poset Game is PSPACE-Complete', $expanded->get('chapter');
+    $this->assertEquals('Lecture Notes in Computer Science', $expanded->get('series');
+    $this->assertEquals('Automata, Languages, and Programming', $expanded->get('title');
+  }
+
   public function testArxivDateUpgradeSeesDate() {
       $text = '{{Cite journal|date=September 2010|doi=10.1016/j.physletb.2010.08.018|arxiv=1006.4000}}';
       $expanded = $this->process_citation($text);
