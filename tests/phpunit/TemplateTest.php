3480742b858c66e4bc40fb9791391847ebb682a4
GlazerMann <kurt@ioiowa.com>
2019-02-08 15:10:42 -0800

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -1425,6 +1425,9 @@ ER -  }}';
     $text = '{{Cite journal| doi = 10.1017/jpa.2018.43|title = New well-preserved scleritomes of Chancelloriida from early Cambrian Guanshan Biota, eastern Yunnan, China|journal = Journal of Paleontology|volume = 92|issue = 6|pages = 1–17|year = 2018|last1 = Zhao|first1 = Jun|last2 = Li|first2 = Guo-Biao|last3 = Selden|first3 = Paul A}}';
     $expanded = $this->process_citation($text);
     $this->assertEquals('955–971', $expanded->get('pages')); // Converted should use long dashes
+    $text = '{{Cite journal| doi = 10.1017/jpa.2018.43|title = New well-preserved scleritomes of Chancelloriida from early Cambrian Guanshan Biota, eastern Yunnan, China|journal = Journal of Paleontology|volume = 92|issue = 6|pages = 960|year = 2018|last1 = Zhao|first1 = Jun|last2 = Li|first2 = Guo-Biao|last3 = Selden|first3 = Paul A}}';
+    $expanded = $this->process_citation($text);
+    $this->assertEquals('960', $expanded->get('at')); // Existing page number was within existing range
   }
     
   public function testCollapseRanges() {
