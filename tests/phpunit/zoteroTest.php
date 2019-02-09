ms609/citation-bot

commit 36c462860a6b003fd424bc3b6a7b98b1c962f740

Update zotero.php

api_handlers/zotero.php
@@ -57,11 +57,8 @@ function query_url_api($ids, $templates) {
             $url_short         = strtok($url,               '?#');
             if (stripos($redirectedUrl_doi, 'cookie') !== FALSE) break;
             if (stripos($redirectedUrl_doi, 'denied') !== FALSE) break;
-            if ( preg_match('~^https?://linkinghub\.elsevier\.com/retrieve/pii/(S\d{4}[0-9]+)~i', $redirectedUrl_doi, $matches ) === 1 ) {
-                 $redirectedUrl_doi = $matches[1] ;
-            }
-            if ( preg_match('~^https?://(?:www\.|)sciencedirect\.com/science/article/pii/(S\d{4}[0-9]+)~i', $redirectedUrl_doi, $matches ) === 1 ) {
-                 $redirectedUrl_doi = $matches[1] ;
+            if ( preg_match('~^https?://*+/pii/(S\d{4}[0-9]+)~i', $redirectedUrl_doi, $matches ) === 1 ) {
+                 $redirectedUrl_doi = $matches[1] ;  // Grab PII numbers
             }
             $url_short = str_ireplace('https', 'http', $url_short);
             $redirectedUrl_doi = str_ireplace('https', 'http', $redirectedUrl_doi);

commit bd70d88989e333226f7675ee226cce329ea1bd05

Actually check DOI resolvers and URL redirects
* Do not require URL to match some narrow regexes before resolving redirects.
* Fix regex typo introduced in 36c462860a6b003fd424bc3b6a7b98b1c962f740.

api_handlers/zotero.php
@@ -46,8 +46,6 @@ function query_url_api($ids, $templates) {
         $url &&
         !$template->incomplete() &&
         !preg_match(REGEXP_DOI_ISSN_ONLY, $doi) &&
-        str_ireplace(CANONICAL_PUBLISHER_URLS, '', $url) != $url &&
-        str_ireplace(['pdf', 'image', 'plate', 'figure', 'picture'], '', $url) == $url &&
         $template->blank(DOI_BROKEN_ALIASES))
     {
           curl_setopt($ch, CURLOPT_URL, "https://dx.doi.org/" . urlencode($doi));
@@ -57,7 +55,7 @@ function query_url_api($ids, $templates) {
             $url_short         = strtok($url,               '?#');
             if (stripos($redirectedUrl_doi, 'cookie') !== FALSE) break;
             if (stripos($redirectedUrl_doi, 'denied') !== FALSE) break;
-            if ( preg_match('~^https?://*+/pii/(S\d{4}[0-9]+)~i', $redirectedUrl_doi, $matches ) === 1 ) {
+            if ( preg_match('~^https?://[^/]+/pii/(S\d{4}[0-9]+)~i', $redirectedUrl_doi, $matches ) === 1 ) {
                  $redirectedUrl_doi = $matches[1] ;  // Grab PII numbers
             }
             $url_short = str_ireplace('https', 'http', $url_short);

tests/phpunit/zoteroTest.php
@@ -16,7 +16,7 @@ class ZoteroTest extends testBaseClass {
 //  }
       
   public function testZoteroExpansionPII() {
-    $text = '{{Cite journal|url = https://www.sciencedirect.com/science/article/pii/S0024379512004405}}';
+    $text = '{{Cite journal|url = http://www.sciencedirect.com/science/article/pii/S0024379512004405}}';
     $expanded = $this->expand_via_zotero($text);
     $this->assertEquals('10.1016/j.laa.2012.05.036', $expanded->get('doi'));
     $this->assertNull($expanded->get('url')); // Recognize canonical publisher URL as duplicate of valid doi
@@ -133,4 +133,15 @@ class ZoteroTest extends testBaseClass {
      $expanded = $this->process_citation($text);
      $this->assertNull($expanded->get('url'));
   }
+  public function testDropUrlCode2() { // URL redirects to URL with the same DOI
+     $text = '{{cite journal | last = De Vivo | first = B. | title = New constraints on the pyroclastic eruptive history of the Campanian volcanic Plain (Italy) | url = http://www.springerlink.com/content/8r046aa9t4lmjwxj/ | doi = 10.1007/s007100170010 }}';
+     $expanded = $this->process_citation($text);
+     $this->assertNull($expanded->get('url'));
+  }
+  public function testDropUrlCode3() { // url is same as one doi points to, except for http vs. https
+     $text = "{{cite journal | first = Luca | last = D'Auria | year = 2015 | title = Magma injection beneath the urban area of Naples | url = http://www.nature.com/articles/srep13100 | doi=10.1038/srep13100 }}";
+     $expanded = $this->process_citation($text);
+     $this->assertNull($expanded->get('url'));
+  }
+
 }