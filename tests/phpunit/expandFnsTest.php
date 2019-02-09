5bf8ebf0e030b7107932200c57417b89fd6537aa
GlazerMann <kurt@ioiowa.com>
2019-02-08 16:25:25 -0800

    Update expandFnsTest.php

--- tests/phpunit/expandFnsTest.php
+++ tests/phpunit/expandFnsTest.php
@@ -9,8 +9,9 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class expandFnsTest extends testBaseClass {
 
   public function testBogusPageRanges() {  // At some point this test will age out (perhaps add special TRAVIS code to template.php
-    $text = '{{Cite journal| doi = 10.1017/jpa.2018.43|title = New well-preserved scleritomes of Chancelloriida from early Cambrian Guanshan Biota, eastern Yunnan, China|journal = Journal of Paleontology|volume = 92|issue = 6|pages = 960|year = 2018|last1 = Zhao|first1 = Jun|last2 = Li|first2 = Guo-Biao|last3 = Selden|first3 = Paul A}}';
+    $text = '{{Cite journal| doi = 10.1017/jpa.2018.43|pages = 960}}';
     $expanded = $this->process_citation($text);
+    echo $expanded->parsed_text();
     $this->assertEquals('960', $expanded->get('at')); // Existing page number was within existing range
   }
 }    
