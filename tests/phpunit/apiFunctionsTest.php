c9c19534cdb5b0ad90a16869a73c70c3358d0b07
GlazerMann <kurt@ioiowa.com>
2019-02-11 11:37:04 -0800

    Update apiFunctionsTest.php

--- tests/phpunit/apiFunctionsTest.php
+++ tests/phpunit/apiFunctionsTest.php
@@ -6,9 +6,10 @@ final class apiFunctionsTest extends testBaseClass {
   
 
   public function testArxivDateUpgradeSeesDate() {
-      $text = '{{cite web |url= https://www.flightglobal.com/news/articles/pacific-aerospace-poised-to-open-final-assembly-line-429988/description |title= Pacific Aerospace Final Production Line in China |publisher= Flight Global}}';
+      $text = '{{cite web |url= https://www.flightglobal.com/news/articles/pacific-aerospace-poised-to-open-final-assembly-line-429988/description}}';
       $expanded = $this->process_citation($text);
-      $this->assertNull($expanded->get('date'));
+      $this->assertNotNull($expanded->get('title'));
+      $this->assertEquals('Should be something', $expanded->get('date'));
       
   }
   
