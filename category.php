a2518bfdedcbe638f30e696e2bca04d9bc4945e4
GlazerMann <kurt@ioiowa.com>
2019-02-14 15:56:40 -0800

    Update category.php

--- category.php
+++ category.php
@@ -9,7 +9,7 @@
 $user = isset($_REQUEST["user"]) ? $_REQUEST["user"] : 'YOUR_USER_ID';
 $slow = isset($_REQUEST["slow"]) ? $_REQUEST["slow"] : '0';
 $cat  = isset($_REQUEST["cat"])  ? $_REQUEST["cat"]  : 'THE_CATEGOY_YOU_WANT';
-echo "Access categories API like this  https://tools.wmflabs.org/citations/process_page.php?edit=YES&slow=" . $slow . "&user=" . $user . "&cat=" . $cat;
+echo "Access categories API like this  https://tools.wmflabs.org/citations/process_page.php?slow=" . $slow . "&user=" . $user . "&cat=" . $cat;
 ?>
   </p>
 </body>
