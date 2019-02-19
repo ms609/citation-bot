e9f23c79714a715a656f3a4919461eb27253a49f
GlazerMann <kurt@ioiowa.com>
2019-02-18 17:07:42 -0800

    Update process_page.php

--- process_page.php
+++ process_page.php
@@ -40,6 +40,7 @@ if (is_valid_user($user)) {
 $pages = (isset($argv) && isset($argv[1])) // argv set on command line
        ? $argv[1] : trim(ucfirst(strip_tags($_REQUEST["page"])));
 if (!isset($ON)) $ON = isset($argv[2]);
+
 foreach (explode('|', $pages) as $title) {
 
 
