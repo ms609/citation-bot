25efbbc82ccc3758f5ea88d8e7c10c1baf749361
GlazerMann <kurt@ioiowa.com>
2019-02-20 20:54:31 -0800

    white space

--- WikipediaBot.php
+++ WikipediaBot.php
@@ -1,14 +1,14 @@
 <?php
 // To use the oauthclient library, run:
 // composer require mediawiki/oauthclient
- use MediaWiki\OAuthClient\Consumer;
- use MediaWiki\OAuthClient\Token;
- use MediaWiki\OAuthClient\Request;
- use MediaWiki\OAuthClient\SignatureMethod\HmacSha1;
- use MediaWiki\OAuthClient\ClientConfig;
- use MediaWiki\OAuthClient\Client;
+use MediaWiki\OAuthClient\Consumer;
+use MediaWiki\OAuthClient\Token;
+use MediaWiki\OAuthClient\Request;
+use MediaWiki\OAuthClient\SignatureMethod\HmacSha1;
+use MediaWiki\OAuthClient\ClientConfig;
+use MediaWiki\OAuthClient\Client;
 
-final class WikipediaBot {
+class WikipediaBot {
   
   protected $consumer, $token, $ch;
 
