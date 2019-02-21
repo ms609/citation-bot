b8ff5c907c32635547582b2c6b7883de87733e10
GlazerMann <kurt@ioiowa.com>
2019-02-20 19:55:26 -0800

    minimize changes

--- process_page.php
+++ process_page.php
@@ -68,6 +68,7 @@ foreach (explode('|', $pages) as $title) {
   ?>
   <form method="post" action="process_page.php">
     <input type="hidden" name="page" value="<?php echo $title;?>" />
+    <input type="hidden" name="user" value="<?php echo $user;?>" />
     <input type="hidden" name="edit" value="on" />
     <input type="hidden" name="slow" value="<?php echo $SLOW_MODE;?>" />
     <input type="submit" value="Submit edits" />
