b908fdcd585b6ed5744dc9ed37fd629a8377e026
GlazerMann <kurt@ioiowa.com>
2019-02-21 07:44:27 -0800

    Update WikipediaBot.php

--- WikipediaBot.php
+++ WikipediaBot.php
@@ -368,6 +368,7 @@ class WikipediaBot {
         "redirects" => "1",
         "titles" => $page,
         ), 'POST');
+    print_r($res);
     if (!isset($res->query->redirects->to)) {
         report_warning("Failed to get redirect target");
         return FALSE;
