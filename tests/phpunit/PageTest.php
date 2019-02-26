78a1b90c58b31872bbb4de10af123047230c4ad9
GlazerMann <kurt@ioiowa.com>
2019-02-25 16:11:49 -0800

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -9,8 +9,9 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class PageTest extends testBaseClass {
 
   public function testPageChangeSummary() {
-      $page = $this->process_citation('{{cite journal|chapter=chapter name|title=book name}}');
-      $this->assertEquals('Alterhere',$page->get('ss'));
+   $text = "{{cite journal | first = Peter | last = Frohling | title = Bonaparte's Gull Feeding on Walnut Meat | journal = The Wilson Bulletin | volume = 79 | issue = 3 | page = 341 | date = September 1967 | jstor = 4159631 }}";
+      $page = $this->process_citation($text);
+      $this->assertEquals('341',$page->get('page'));
   }
  
  
