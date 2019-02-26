e21e085a08b1de1d95050da2e4acf0b1fac6aa7e
GlazerMann <kurt@ioiowa.com>
2019-02-25 16:12:35 -0800

    Update DOItools.php

--- DOItools.php
+++ DOItools.php
@@ -77,6 +77,7 @@ function format_initials($str) {
 }
 
 function is_initials($str){
+        $str = trim($str);
         if (!$str) return FALSE;
         if (strlen(str_replace(array("-", ".", ";"), "", $str)) >3) return FALSE;
         if (strlen(str_replace(array("-", ".", ";"), "", $str)) ==1) return TRUE;
@@ -151,14 +152,22 @@ function format_author($author){
           }
           unset($auth[0]);
           foreach ($auth as $bit){
-            if (is_initials($bit)) $i[] = format_initials($bit);
+            if (is_initials($bit)) {
+              $i[] = format_initials($bit) . '.';
+            } else {
+              $i[] = $bit;
+            }
           }
         } else {
-          foreach ($auth as $A){
-            if (is_initials($A)) $i[] = format_initials($A);
+          foreach ($auth as $A) {
+            if (is_initials($A)) {
+                $i[] = format_initials($A) . '.';
+            } else {
+                $i[] = $A;
+            }
           }
         }
-        $fore = mb_strtoupper(implode(".", $i));
+        $fore = mb_strtoupper(implode(" ", $i));
       } else {
         // it ends with the surname
         $surname = $auth[$countAuth-1];
@@ -176,7 +185,18 @@ function format_author($author){
       $fore = implode(" ", $i);
     }
   }
-  return str_replace("..", ".", format_surname($surname) . ", " . format_forename($fore)); // Sometimes add period after period
+  // Special cases when code cannot fully determine things, or if the name is only Smith
+  if (trim($surname) == '') { // get this with A. B. C.
+    $full_name = format_forename($fore);
+  } elseif (trim($fore) == '') {  // Get this with just Smith
+    $full_name = format_surname($surname);
+  } else {
+    $full_name = format_surname($surname) . ", " . format_forename($fore);
+  }
+  $full_name = str_replace("..", ".", $full_name);  // Sometimes add period after period
+  $full_name = str_replace(".", ". ", $full_name);  // Add spaces after all periods
+  $full_name = str_replace(["   ", "  "], [" ", " "], $full_name); // Remove extra spaces
+  return trim($full_name);
 }
 
 function format_multiple_authors($authors, $returnAsArray = FALSE){
