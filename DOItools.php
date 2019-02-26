e8c206242b012310565b213bbe425bcd3743339c
GlazerMann <kurt@ioiowa.com>
2019-02-25 19:16:15 -0800

    Update DOItools.php

--- DOItools.php
+++ DOItools.php
@@ -30,7 +30,7 @@ function truncate_publisher($p){
 
 function format_surname($surname) {
   if ($surname == '-') return '';
-  if (preg_match('~^\S\.?$~', $surname)) return mb_strtoupper($surname); // Just a single initial, with or without period
+  if (preg_match('~^\S\.?$~u', $surname)) return mb_strtoupper($surname); // Just a single initial, with or without period
   $surname = mb_convert_case(trim(mb_ereg_replace("-", " - ", $surname)), MB_CASE_LOWER);
   if (mb_substr($surname, 0, 2) == "o'") {
         return "O'" . format_surname_2(mb_substr($surname, 2));
