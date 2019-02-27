1b0f82401b454ac7034b6d7ba4f0ccaad2e10747
GlazerMann <kurt@ioiowa.com>
2019-02-27 15:41:10 -0800

    Do not use echo.  Use native HTML

--- category.php
+++ category.php
@@ -9,19 +9,23 @@ if ($cat) {
   $url = "https://tools.wmflabs.org/citations/process_page.php?edit=category" . $slow . $user . $cat;
   header("Location: " . $url);
 } else {
-  echo '<!DOCTYPE html>';
-  echo '<html lang="en" dir="ltr">';
-  echo '<head>';
-  echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
-  echo '<meta content="Smith609" name="author">';
-  echo '<meta name="keywords" content="User:DOI bot,Citation, citation bot,Digital object identifier,wikipedia,cite journal" />';
-  echo '<link rel="apple-touch-icon" href="https://en.wikipedia.org/apple-touch-icon.png" />';
-  echo '<link rel="copyright" href="https://www.gnu.org/copyleft/fdl.html" />';
-  echo '<title>Citation bot: Category landing page</title>';
-  echo '<link rel="stylesheet" type="text/css" href="css/results.css" />';
-  echo '</head>';
-  echo '<body>';
-  echo 'You must specify a category.  Try appending ?cat=Blah+blah to the URL';
-  echo '</body></html>';
+?>
+<!DOCTYPE html>
+ <html lang="en" dir="ltr">
+  <head>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   <meta content="Smith609" name="author" />
+   <meta name="keywords" content="User:DOI bot,Citation, citation bot,Digital object identifier,wikipedia,cite journal" />
+   <meta name="robots" content="noindex, nofollow" />
+   <link rel="apple-touch-icon" href="https://en.wikipedia.org/apple-touch-icon.png" />
+   <link rel="copyright" href="https://www.gnu.org/copyleft/fdl.html" />
+   <link rel="stylesheet" type="text/css" href="css/results.css" />
+   <title>Citation bot: Category landing page</title>
+  </head>
+  <body>
+   You must specify a category.  Try appending ?cat=Blah+blah to the URL
+  </body>
+ </html>
+<?php
 }
 exit(0);
