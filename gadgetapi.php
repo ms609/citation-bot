ff81af18c5cd1aad961e0661c5ae993cc46f4c55
GlazerMann <kurt@ioiowa.com>
2019-02-28 13:19:17 -0800

    Update gadgetapi.php

--- gadgetapi.php
+++ gadgetapi.php
@@ -17,6 +17,7 @@ $page = new Page();
 $page->parse_text($originalText);
 $page->expand_text();
 $newText = $page->parsed_text();
+if ($newText == "") $newText = $originalText;
 
 //Modify edit summary to identify bot-assisted edits
 if ($newText !== $originalText) {
