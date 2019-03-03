58738dfac920a40af0b8d29d5e0722d108dacd93
GlazerMann <kurt@ioiowa.com>
2019-03-03 11:45:24 -0800

    Update user_messages.php

--- user_messages.php
+++ user_messages.php
@@ -1,11 +1,11 @@
 <?php
   
 function html_echo($text, $alternate_text='') {
-  if (!getenv('TRAVIS')) echo HTML_OUTPUT ? $text : $alternate_text;
+  if (TRUE) echo HTML_OUTPUT ? $text : $alternate_text;
 }
 
 function user_notice($symbol, $class, $text) {
-  if (!getenv('TRAVIS')) {
+  if (TRUE)) {
     echo "\n " . (HTML_OUTPUT ? "<span class='$class'>" : "")
      . "$symbol $text" . (HTML_OUTPUT ? "</span>" : "");
   }
@@ -20,7 +20,7 @@ function report_warning($text) { user_notice("  !", "warning", $text); }
 function report_modification($text) { user_notice("  ~", "changed", $text); }
 function report_add($text) { user_notice("  +", "added", $text); }
 function report_forget($text) { user_notice("  -", "removed", $text); }
-function report_inline($text) { if (!getenv('TRAVIS')) echo " $text"; }
+function report_inline($text) { if (TRUE) echo " $text"; }
 function report_error($text) { report_warning($text); trigger_error($text, E_USER_ERROR); } // call report_warning to give users a message before we die
 function report_minor_error($text) {  // For things we want to error on TRAVIS, but continue on Wikipedia
   report_warning($text);
