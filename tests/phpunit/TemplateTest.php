16ebd16132a9ef6899d8d8878a6f260acdeaa8f7
GlazerMann <kurt@ioiowa.com>
2019-03-03 12:07:39 -0800

    string functions want strings in 7.3

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -1532,14 +1532,14 @@ ER -  }}';
   public function testBadBibcodeARXIVPages() {
     $text = '{{cite journal|bibcode=2017arXiv171102260L}}'; // Some bibcodes have pages set to arXiv:1711.02260
     $expanded = $this->process_citation($text);
-    $pages = $expanded->get('pages');
-    $volume = $expanded->get('volume');
+    $pages = (string) $expanded->get('pages');
+    $volume = (string) $expanded->get('volume');
     $this->assertEquals(FALSE, stripos($pages, 'arxiv'));
     $this->assertEquals(FALSE, stripos('1711', $volume));
     $this->assertNull($expanded->get('journal'));  // if we get a journal, the data is updated and test probably no longer gets bad data
     $text = "{{cite journal|bibcode=1995astro.ph..8159B|pages=8159}}"; // Pages from bibcode have slash in it astro-ph/8159B
     $expanded = $this->process_citation($text);
-    $pages = $expanded->get('pages');
+    $pages = (string) $expanded->get('pages');
     $this->assertEquals(FALSE, stripos($pages, 'astro'));
     $this->assertNull($expanded->get('journal'));  // if we get a journal, the data is updated and test probably no longer gets bad data
   }
