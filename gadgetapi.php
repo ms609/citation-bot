9327759d6885a07a69aa0fbc07a99fc7013e5791
GlazerMann <kurt@ioiowa.com>
2019-03-07 07:26:40 -0800

    Update gadgetapi.php

--- gadgetapi.php
+++ gadgetapi.php
@@ -5,7 +5,10 @@ header("Content-Type: text/json");
 // This is needed because the Gadget API expects only JSON back, therefore ALL output from the citation bot is thrown away
 ob_start();
 define("FLUSHING_OKAY", FALSE);
-  
+global $SLOW_MODE;
+$SLOW_MODE = FALSE;
+if (isset($_REQUEST["slow"])) $SLOW_MODE = TRUE;
+
 //Set up tool requirements
 require_once('expandFns.php');
 
