36967682fa3b008529a24fb26dc038008691f3a5
GlazerMann <kurt@ioiowa.com>
2019-03-08 13:55:11 -0800

    Update expandFns.php

--- expandFns.php
+++ expandFns.php
@@ -480,7 +480,7 @@ function check_doi_for_jstor($doi, &$template) {
 
 function check_10_1093_doi($url) { // We assume dois are bad, unless on good list
   if(!preg_match('~10.1093/([^/]+)/~u', $url, $match)) return TRUE;
-  $test = strtolower($mathch[0]);
+  $test = strtolower($match[0]);
   // March 2019 Good list
   if (in_array($test, GOOD_10_1093_DOIS)) return TRUE;
   return FALSE;
