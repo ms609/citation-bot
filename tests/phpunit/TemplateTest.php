f624e29d84acc38323b10955c036785af37e4093
GlazerMann <kurt@ioiowa.com>
2019-03-09 14:15:20 -0800

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -696,7 +696,7 @@ final class TemplateTest extends testBaseClass {
       
       $text = '{{cite book|pages=1–2|id={{arxiv|astr.ph|1234.5678}}}}{{cite book|pages=1–3|id={{arxiv|astr.ph|1234.5678}}}}'; // Two of the same sub-template, but in different tempalates
       $expanded = $this->process_page($text);
-      $this->assertEquals('{{cite book|pages=1–2|arxiv=astr.ph/1234.5678}}{{cite book|pages=1–3|arxiv=astr.ph/1234.5678}}', $expanded->parsed_text());
+      $this->assertEquals('{{cite book|pages=1–2|arxiv= astr.ph/1234.5678}}{{cite book|pages=1–3|arxiv= astr.ph/1234.5678}}', $expanded->parsed_text());
   }
   
   public function testNestedTemplates() {
