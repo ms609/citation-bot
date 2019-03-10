4cc8bef9e0044c80e583a9edfe242887cb58dd3b
GlazerMann <kurt@ioiowa.com>
2019-03-09 20:07:29 -0800

    Update zotero.php

--- api_handlers/zotero.php
+++ api_handlers/zotero.php
@@ -252,6 +252,7 @@ function expand_by_zotero(&$template, $url = NULL) {
   } 
   
   if ( isset($result->DOI) && $template->blank('doi')) {
+    
     $template->add_if_new('doi', $result->DOI);
     expand_by_doi($template);
     if (stripos($url, 'jstor')) check_doi_for_jstor($template->get('doi'), $template);
