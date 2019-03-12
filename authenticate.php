2b65208baef997fca8a595d45bc0a521c7bce9cb
GlazerMann <kurt@ioiowa.com>
2019-03-12 08:21:38 -0700

    Create authenticate.php

---
+++ authenticate.php
@@ -0,0 +1,9 @@
+<?php
+// To use the oauthclient library, run:
+// composer require mediawiki/oauthclient
+use MediaWiki\OAuthClient\Consumer;
+use MediaWiki\OAuthClient\Token;
+use MediaWiki\OAuthClient\Request;
+use MediaWiki\OAuthClient\SignatureMethod\HmacSha1;
+use MediaWiki\OAuthClient\ClientConfig;
+use MediaWiki\OAuthClient\Client;
