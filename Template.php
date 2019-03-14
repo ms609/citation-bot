0d7911b233457258794ae43a453ab2e4ca9219c3
GlazerMann <kurt@ioiowa.com>
2019-03-14 07:10:56 -0700

    remove | request pdf from all titles

--- Template.php
+++ Template.php
@@ -2910,6 +2910,9 @@ final class Template {
           $this->set($param, $title);
           if ($title && str_equivalent($this->get($param), $this->get('work'))) $this->forget('work');
           if ($title && str_equivalent($this->get($param), $this->get('encyclopedia'))) $this->forget('$param');
+          if (preg_match('~^(.+)\{\{!\}\} Request PDF$~i', trim($this->get($param)), $match)) {
+                 $this->set('title', trim($match[1]));
+          }
           return;
      
         case 'chapter-url':
@@ -2921,8 +2924,7 @@ final class Template {
         case 'url':
           if (preg_match("~^https?://(?:www\.|)researchgate\.net/[^\s]*publication/([0-9]+)_*~i", $this->get($param), $matches)) {
               $this->set($param, 'https://www.researchgate.net/publication/' . $matches[1]);
-              if (preg_match('~^(.+)\{\{!\}\} Request PDF$~i', trim($this->get('title')), $match) ||
-                  preg_match('~^\(PDF\)(.+)$~i', trim($this->get('title')), $match)) {
+              if (preg_match('~^\(PDF\)(.+)$~i', trim($this->get('title')), $match)) {
                  $this->set('title', trim($match[1]));
               }
           } elseif (preg_match("~^https?://(?:www\.|)academia\.edu/([0-9]+)/*~i", $this->get($param), $matches)) {
