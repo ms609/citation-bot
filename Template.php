9dbb95c891ff5fa731d10cd5bc1831b8ccfe496f
GlazerMann <kurt@ioiowa.com>
2019-03-16 18:18:04 -0700

    Update Template.php

--- Template.php
+++ Template.php
@@ -174,7 +174,7 @@ final class Template {
       ));
     }
     // And now everything else
-    if ($this->blank(['pages', 'page'[) ||
+    if ($this->blank(['pages', 'page']) ||
         preg_match('~no.+no|n/a|in press|none~', $this->get('pages') . $this->get('page')) ||
         (preg_match('~^1[^0-9]~', $this->get('pages') . $this->get('page') . '-') && ($this->blank('year') || 2 > (date("Y") - $this->get('year')))) // It claims to be on page one
        ) {
