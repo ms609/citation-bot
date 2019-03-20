837c99fa9e4fc0cf34fb27b12110dce85bbc2c7a
GlazerMann <kurt@ioiowa.com>
2019-03-20 07:30:18 -0700

    Update Template.php

--- Template.php
+++ Template.php
@@ -10,7 +10,7 @@
  * A range of functions will search CrossRef/adsabs/Google Books/other online databases
  * to find information that can be added to existing citations.
  */
-
+require_once('constants.php');
 require_once("Page.php");
 require_once("Parameter.php");
 
