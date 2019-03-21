6be712d213afac8eb06f8826f42c60df1723282f
GlazerMann <kurt@ioiowa.com>
2019-03-21 07:15:56 -0700

    Update DOItools.php

--- DOItools.php
+++ DOItools.php
@@ -1,5 +1,4 @@
 <?php
-require_once('constants.php');
 /* junior_test - tests a name for a Junior appellation
  *  Input: $name - the name to be tested
  * Output: array ($name without Jr, if $name ends in Jr, Jr)
