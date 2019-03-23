8bf4e37b20467f0b4c8af880a6e92d3d965df517
GlazerMann <kurt@ioiowa.com>
2019-03-23 07:58:37 -0700

    simpler code

--- tests/testBaseClass.php
+++ tests/testBaseClass.php
@@ -20,7 +20,7 @@ abstract class testBaseClass extends PHPUnit\Framework\TestCase {
   }
 
   protected function requires_secrets($function) {
-    if (getenv('TRAVIS_PULL_REQUEST') && (getenv('TRAVIS_PULL_REQUEST') !== 'false' )) {
+    if ((bool) getenv('TRAVIS_PULL_REQUEST')) {
       echo 'S'; // Skipping test: Risks exposing secret keys
       $this->assertNull(NULL); // Make Travis think we tested something
     } else {
