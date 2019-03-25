7fb3e5d6920f85114c999e1209be14a15a76a6e4
GlazerMann <kurt@ioiowa.com>
2019-03-25 09:26:39 -0700

    Update gadgetapi.php

--- gadgetapi.php
+++ gadgetapi.php
@@ -43,7 +43,7 @@ $result = array(
 );
 
 // Throw away all output
-ob_end _clean();
+ob_end  _clean();
 @ob_end_clean(); @ob_end_clean();  // Other parts of the code might open a buffer
 
 echo @json_encode($result);  // On error returns "FALSE", which makes echo print nothing.  Thus we do not have to check for FALSE
