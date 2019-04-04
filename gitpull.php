ae16196d0f96e116ce58a818e5730378c0de275b
GlazerMann <kurt@ioiowa.com>
2019-04-04 09:10:20 -0700

    Update gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -4,24 +4,22 @@ exec ("git checkout constants/capitalization.php", $output);
 exec ("git pull", $output, $return_var);
 #exec ("git fetch --all", $output, $return_var); // Doesn't seem to do much...
 ?><pre>
-<?php foreach($output as $line) echo "$line \n<br />"; ?>
-</pre>
+<?php foreach($output as $line) echo "$line \n"; ?>
 <?php if ($return_var) {
-  echo "Returned error code $return_var \n<br />";
+  echo "Returned error code $return_var \n";
   if ($return_var == 1) {
-    echo "\n<br /> Check that there are no uncommitted changes on the server.\n<br />";
+    echo "\nCheck that there are no uncommitted changes on the server.\n";
     unset($output);
     exec("git status -uno", $output, $return_var);
-    foreach ($output as $line) echo "$line \n<br />";
+    foreach ($output as $line) echo "$line \n";
   }
 } else {
-  echo "Successfully updated from Git repository.\n<br />";
+  echo "Successfully updated from Git repository.\n";
 }
 ?>
-<pre>
 <?php
 unset($output);
 exec("git show --oneline -s", $output, $return_var);
-foreach ($output as $line) echo "$line \n<br />";
+foreach ($output as $line) echo "$line \n";
 ?>
 </pre>
