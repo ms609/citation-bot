dc5762216cf8fead7091accb65e0c0b1cec5a291
GlazerMann <kurt@ioiowa.com>
2019-04-04 09:55:28 -0700

    support ...nih.gov/pmc/articles/pmid/27036656/ url (#1576)

--- Template.php
+++ Template.php
@@ -1093,7 +1093,12 @@ final class Template {
         }
         if ($this->wikiname() === 'cite web') $this->change_name_to('cite arxiv');
         
-      } elseif (preg_match("~https?://(?:www\.|)ncbi\.nlm\.nih\.gov/(?:m/)?(?:pubmed/|entrez/eutils/elink\.fcgi\S+dbfrom=pubmed\S+/|entrez/query\.fcgi\S+db=pubmed\S+).*?=?(\d+)~i", $url, $match)) {
+      } elseif (preg_match("~https?://(?:www\.|)ncbi\.nlm\.nih\.gov/(?:m/)?" 
+      . "(?:pubmed/|" 
+      . "/eutils/elink\.fcgi\S+dbfrom=pubmed\S+/|"
+      . "entrez/query\.fcgi\S+db=pubmed\S+|"
+      . "pmc/articles/pmid/)"
+      . ".*?=?(\d+)~i", $url, $match)) {
         if (preg_match("~https?://(?:www\.|)ncbi\.nlm\.nih\.gov/(?:m/)?/pubmed/\?term~i", $url)) return FALSE; // A search such as https://www.ncbi.nlm.nih.gov/pubmed/?term=Sainis%20KB%5BAuthor%5D&cauthor=true&cauthor_uid=19447493
         quietly('report_modification', "Converting URL to PMID parameter");
         if (is_null($url_sent)) {
