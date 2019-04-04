d6df10c2cb721cd806a02266fc0eddc18c5ecdbc
GlazerMann <kurt@ioiowa.com>
2019-04-04 08:50:30 -0700

    conflict

--- gitpull.php
+++ gitpull.php
@@ -10,6 +10,7 @@ exec ("git pull", $output, $return_var);
   echo "Returned error code $return_var";
   if ($return_var == 1) {
     echo "\n<br /> Check that there are no uncommitted changes on the server.";
+    
   }
 } else {
   echo "Successfully updated from Git repository.";
