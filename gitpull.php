c24464e4f69b4b640899e69daec2efc667353e19
GlazerMann <kurt@ioiowa.com>
2019-04-05 08:08:13 -0700

    Update gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -1,8 +1,7 @@
 <?php
 // Local changes seem to accumulate in capitalization.php, preventing a pull
-exec ("git checkout constants/capitalization.php", $output); 
+exec ("git checkout constants/capitalization.php", $output);
 exec ("git pull", $output, $return_var);
-#exec ("git fetch --all", $output, $return_var); // Doesn't seem to do much...
 ?><pre>
 <?php foreach($output as $line) echo "$line \n"; ?>
 <?php if ($return_var) {
