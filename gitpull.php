6a4b894b58b0636cbca458a01b2a6bb62884cf94
Martin R. Smith <martins@gmail.com>
2019-04-08 09:54:05 +0100

    Maximum concision for easy reading at command line

--- gitpull.php
+++ gitpull.php
@@ -1,26 +1,6 @@
 <?php
-echo "<!DOCTYPE html><html><head><title>Update</title></head><body><pre>";
-// Local changes seem to accumulate in capitalization.php, preventing a pull
-echo shell_exec ("/usr/bin/git checkout constants/capitalization.php");
-echo shell_exec ("/usr/bin/git pull  2>&1"); 
-/*exec ("git pull", $output, $return_var);
-foreach($output as $line) echo "$line \n";
-
-if ($return_var) {
-  if ($return_var == 1) {
-    echo "\n Failure.  Check that there are no uncommitted changes on the server.\n\n";
-    unset($output);
-    echo shell_exec("git status");
-  } else {
-    echo "\n Returned error code $return_var \n\n";
-  }
-} else {
-  echo "Successfully updated from Git repository.\n";
-}
-
-unset($output);
-exec("git show --oneline -s", $output, $return_var);
-foreach ($output as $line) echo "$line \n";
-*/
+echo "<!DOCTYPE html><html><body><pre>";
+echo shell_exec ("/usr/bin/git checkout constants/capitalization.php 2>&1");
+echo shell_exec ("/usr/bin/git pull  2>&1");
 echo "</pre></body></html>"
 ?>
