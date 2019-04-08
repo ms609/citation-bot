c669f10506c52ffc8d6c0a69124539ee9835742e
Martin R. Smith <martins@gmail.com>
2019-04-08 09:35:24 +0100

    Proper use of `shell_exec`

--- gitpull.php
+++ gitpull.php
@@ -1,17 +1,15 @@
 <?php
 echo "<!DOCTYPE html><html><head><title>Update</title></head><body><pre>";
 // Local changes seem to accumulate in capitalization.php, preventing a pull
-echo shell_exec ("git checkout constants/capitalization.php", $output);
-echo shell_exec ("git pull", $output, $return_var);
+echo shell_exec ("git checkout constants/capitalization.php");
+exec ("git pull", $output, $return_var);
 foreach($output as $line) echo "$line \n";
 
 if ($return_var) {
   if ($return_var == 1) {
     echo "\n Failure.  Check that there are no uncommitted changes on the server.\n\n";
     unset($output);
-    exec("git status", $output, $return_var);
-    foreach ($output as $line) echo "$line \n";
-    echo "\n";
+    echo shell_exec("git status");
   } else {
     echo "\n Returned error code $return_var \n\n";
   }
