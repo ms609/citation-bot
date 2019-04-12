b0d8490a8dfeb9d198b3bc8812f18aa4fcc645b9
GlazerMann <kurt@ioiowa.com>
2019-04-11 21:01:08 -0700

    Update gadgetapi.php

--- gadgetapi.php
+++ gadgetapi.php
@@ -3,7 +3,7 @@
 @header("Content-Type: text/json");
 
 // This is needed because the Gadget API expects only JSON back, therefore ALL output from the citation bot is thrown away
-if (!getenv("TRAVIS")) define("FLUSHING_OKAY", FALSE);
+$FLUSHING_OKAY = FALSE;
 
 $SLOW_MODE = FALSE;
 if (isset($_REQUEST["slow"])) $SLOW_MODE = TRUE;
