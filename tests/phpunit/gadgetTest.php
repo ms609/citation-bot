1b8587561b1b9b29a4a204e5bd48c2f49ff726bf
GlazerMann <kurt@ioiowa.com>
2019-04-11 20:56:56 -0700

    Update gadgetTest.php

--- tests/phpunit/gadgetTest.php
+++ tests/phpunit/gadgetTest.php
@@ -7,12 +7,11 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class gadgetTest extends testBaseClass {
   public function testGadget() {
       ob_start();
-      $result = 'before';
       $_POST['text'] = '{{cite|pmid=34213}}';
       $_POST['summary'] = 'Something Nice';
       require_once __DIR__ . '/../../gadgetapi.php';
+      $json = ob_get_contents();
       ob_end_clean();
-      print_r($result);
-      $this->assertNULL($result);
+      $this->assertNULL($json);
   }
 }
