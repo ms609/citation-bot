c2e66cb872baccf9eb511ec1a6d0017d4a6c697d
GlazerMann <kurt@ioiowa.com>
2019-04-11 20:25:57 -0700

    Create testGadget.php

---
+++ tests/phpunit/testGadget.php
@@ -0,0 +1,16 @@
+/*
+ * Tests for gadgetapi.php
+ */
+require_once __DIR__ . '/../testBaseClass.php';
+ 
+final class gadgetTest extends testBaseClass {
+  public function testGadget() {
+      ob_start();
+      $result = 'before';
+      $_POST['text'] = '{{cite|pmid=34213}}';
+      $_POST['summary'] = 'Something Nice';
+      require_once __DIR__ . '/../gadgetapi.php';
+      ob_end_clean();
+      print_r($result);
+      $this->assertNULL($result);
+  }
