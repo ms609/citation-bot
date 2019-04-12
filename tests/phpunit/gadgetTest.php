105f63f11ff877f7e273812022d11c6bea2bcd33
GlazerMann <kurt@ioiowa.com>
2019-04-11 21:18:00 -0700

    proper output

--- tests/phpunit/gadgetTest.php
+++ tests/phpunit/gadgetTest.php
@@ -10,8 +10,11 @@ final class gadgetTest extends testBaseClass {
       $_POST['text'] = '{{cite|pmid=34213}}';
       $_POST['summary'] = 'Something Nice';
       require_once __DIR__ . '/../../gadgetapi.php';
-      $json = ob_get_contents();
+      $json_text = ob_get_contents();
       ob_end_clean();
-      $this->assertNULL($json);
+      $json = json_decode($json_text);
+      $this->assertEquals("{{citation|pmid=34213|year=1979|last1=Weber|first1=F.|title=Antimicrobial resistance and serotypes of Streptococcus pneumoniae in Switzerland|journal=Schweizerische Medizinische Wochenschrift|volume=109|issue=11|pages=395\u20139|last2=Kayser|first2=F. H.}}", $json->expandedtext);
+      $this->assertEquals("Something Nice | Alter: template type. Add: pages, issue, volume, journal, title, year, author pars. 1-2. Formatted [[WP:ENDASH|dashes]]. | You can [[WP:UCB|use this tool]] yourself. [[WP:DBUG|Report bugs here]]. ", $json->editsummary);
+      $this->assertEquals("", $json->debug);
   }
 }
