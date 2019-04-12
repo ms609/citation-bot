90bdb1019c0338cf300784def2e814de10fd18e5
GlazerMann <kurt@ioiowa.com>
2019-04-12 06:48:49 -0700

    Update gadgetTest.php

--- tests/phpunit/gadgetTest.php
+++ tests/phpunit/gadgetTest.php
@@ -12,7 +12,7 @@ final class gadgetTest extends testBaseClass {
       require_once __DIR__ . '/../../gadgetapi.php';
       $json_text = ob_get_contents();
       ob_end_clean();
-      @ob_end_flush(); // flush and close setup.php buffer
+      @ob_end_flush(); @ob_end_flush(); // flush and close setup.php buffer
       $json = json_decode($json_text);
       $this->assertEquals("{{citation|pmid=34213|year=1979|last1=Weber|first1=F.|title=Antimicrobial resistance and serotypes of Streptococcus pneumoniae in Switzerland|journal=Schweizerische Medizinische Wochenschrift|volume=109|issue=11|pages=395–9|last2=Kayser|first2=F. H.}}", $json->expandedtext);
       $this->assertEquals("Something Nice | Alter: template type. Add: pages, issue, volume, journal, title, year, author pars. 1-2. Formatted [[WP:ENDASH|dashes]]. | You can [[WP:UCB|use this tool]] yourself. [[WP:DBUG|Report bugs here]]. ", $json->editsummary);
