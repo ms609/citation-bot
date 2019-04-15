2c8a6561426fd0b93e0d8201f435361b18f7650c
GlazerMann <kurt@ioiowa.com>
2019-04-15 07:25:37 -0700

    Implement file locking in git pull

--- gitpull.php
+++ gitpull.php
@@ -1,6 +1,12 @@
 <?php
 echo "<!DOCTYPE html><html><body><pre>";
-echo shell_exec ("/usr/bin/git checkout constants/capitalization.php 2>&1");
-echo shell_exec ("/usr/bin/git pull  2>&1");
+if (mkdir('git_pull.lock', 0700)) {
+  echo shell_exec ("/usr/bin/git fetch  2>&1"); // This only updates .git, so it is very safe. This is first half of pull
+  echo shell_exec ("/usr/bin/git checkout constants/capitalization.php 2>&1");
+  echo shell_exec ("/usr/bin/git pull  2>&1");
+  rmdir('git_pull.lock') ;
+} else {
+  echo 'lock file exists -- aborting ';
+}
 echo "</pre></body></html>"
 ?>
