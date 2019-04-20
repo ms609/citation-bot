2be5158625b95b7e1a9136e6f4c751705e577458
GlazerMann <kurt@ioiowa.com>
2019-04-20 15:21:57 -0700

    Update apiFunctionsTest.php

--- tests/phpunit/apiFunctionsTest.php
+++ tests/phpunit/apiFunctionsTest.php
@@ -9,7 +9,7 @@ final class apiFunctionsTest extends testBaseClass {
   public function testArxivDateUpgradeSeesDate() {
       $text = '{{cite journal|author = Yasushi Komori |author2 = Kohji Matsumoto  |author3 = Hirofumi Tsumura|title = Hyperbolic-sine analogues of Eisenstein series, generalized Hurwitz numbers, and q-zeta functions|year = 2010}}';
       $expanded = $this->process_citation($text);
-      $this->assertEquals('not null', $expanded->get('doi'));
+      $this->assertEquals('not null', $expanded->parsed_text());
   }
 
 }
