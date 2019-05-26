4a1fd6ffbdf412177d616a583737afaf94871e0c
GlazerMann <kurt@ioiowa.com>
2019-05-26 08:45:32 -0700

    Update Template.php

--- Template.php
+++ Template.php
@@ -816,6 +816,7 @@ final class Template {
       
       case 'publisher':
         if (stripos($value, 'Springer') === 0) $value = 'Springer'; // they add locations often
+        Do something with BAD_PUBLISHERS here;
         if (stripos($value, '[s.n.]') !== FALSE) return FALSE; 
         if ($this->has('journal') && ($this->wikiname() === 'cite journal')) return FALSE;
         $value = truncate_publisher($value);
