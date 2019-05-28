419c8815704412bd22403ad0409b498b6d0a5977
GlazerMann <kurt@ioiowa.com>
2019-05-27 20:47:37 -0700

    Update bad_data.php

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -13,8 +13,8 @@ const NON_HUMAN_AUTHORS = array('collaborat', 'reporter', 'journalist', 'corresp
                                 'corporation', 'incorporated', 'editorial');
 const BAD_PUBLISHERS = array('London');
 
-const PUBLISHERS_ARE_WORKS = array('The San Diego Union-Tribune', 'Forbes', 'Salon', 'San Jose Mercury News', 'New York Times',
-                                   'The New York Times');
+const PUBLISHERS_ARE_WORKS = array('the san diego union-tribune', 'forbes', 'salon', 'san jose mercury news', 'new york times',
+                                   'the new york times'); // LOWER CASE!
 
 // Catch 'authors' such as "hearst magazines", "time inc", "nielsen business media, inc"
 // Ordered alphabetically.
