71d9d7f35060d2440a99712567e4b18f9e944e97
GlazerMann <kurt@ioiowa.com>
2019-06-02 12:45:54 -0700

    CANONICAL_PUBLISHER_URLS

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -52,6 +52,7 @@ const CANONICAL_PUBLISHER_URLS = array ('elsevier.com', 'springer.com', 'science
                                 'pubs.geoscienceworld.org', 'pubs.rsc.org', 'journals.uchicago.edu',
                                 'annualreviews.org', 'aip.scitation.org', 'psyche.entclub.org', 'thelancet.com',
                                 'amjbot.org', 'gsapubs.org', 'jwildlifedis.org', 'msptm.org', 'nrcresearchpress.',
+                                'fundacionmenteclara.org.ar',
                                 //  Below are journal search engines
                                 '.serialssolutions.com', '.ebscohost.com',
                                 //  Below are proxys
