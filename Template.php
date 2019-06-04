d65e2b3318186592681297824443ec892eedf941
GlazerMann <kurt@ioiowa.com>
2019-06-03 22:09:45 -0700

    Remove brackets and years from publishers (#1712)
    * Remove [publisher] brackets from new data
    
    * Update Template.php
    
    * Update Template.php
    
    * Update bad_data.php
    
    * Remove years from publisher

--- Template.php
+++ Template.php
@@ -820,8 +820,10 @@ final class Template {
       
       case 'publisher':
         if (stripos($value, 'Springer') === 0) $value = 'Springer'; // they add locations often
-        if (in_array($value, BAD_PUBLISHERS)) return FALSE;
         if (stripos($value, '[s.n.]') !== FALSE) return FALSE;
+        if (preg_match('~^\[([^\|\[\]]*)\]$~', $value, $match)) $value = $match[1]; // usually zotero problem of [data]
+        if (preg_match('~^(.+), \d{4}$~', $value, $match)) $value = $match[1]; // remove years from zotero 
+        if (in_array(strtolower($value), BAD_PUBLISHERS)) return FALSE;
         if (str_equivalent($this->get('location'), $value)) return FALSE; // Catch some bad archive.org data
         if ($this->has('journal') && ($this->wikiname() === 'cite journal')) return FALSE;
         $value = truncate_publisher($value);
