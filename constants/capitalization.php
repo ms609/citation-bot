bb6109424b49a389bb57c7136405e1e70aaec6f7
GlazerMann <kurt@ioiowa.com>
2019-06-03 19:34:24 -0700

    USA AND US (#1708)

--- constants/capitalization.php
+++ constants/capitalization.php
@@ -58,7 +58,7 @@ const JOURNAL_ACRONYMS = array(/* The following will be automatically updated to
           ' PLOS Medicine ', ' PLOS Neglected Tropical Diseases ', 
           ' PLOS ONE ', ' PLOS ONE ',' PLOS ONE ',' PLOS ONE ', ' PNAS ', ' Published in: ', ' RNA ', ' S.A.P.I.EN.S ',
           ' Srp Arh Celok Lek ', ' Star Trek: The Official Monthly Magazine ', 
-          ' Tellus A ', ' The EMBO Journal ', ' Time Out London ', ' tot de ', ' uHealth ', ' uHealth ', ' v Astronomicheskii Zhurna ',
+          ' Tellus A ', ' The EMBO Journal ', ' Time Out London ', ' tot de ', ' U.S. ', ' U.S.A. ', ' U.S.A. ', ' uHealth ', ' uHealth ', ' v Astronomicheskii Zhurna ',
           ' z/Journal ', ' z/Journal ', ' Zeitschrift für Geologische Wissenschaften ', ' Zeitschrift für Physik A Hadrons and Nuclei ', 
           ' Zeitschrift für Physik A: Hadrons and Nuclei ', ' ZooKeys '
           /* The above will be automatically updated to alphabetical order */ 
@@ -87,7 +87,7 @@ const UCFIRST_JOURNAL_ACRONYMS = array(/* The following will be automatically up
           ' Plos Medicine ', ' Plos Neglected Tropical Diseases ', 
           ' Plos One ', ' PloS One ', ' PLoS One ', ' PLOS One ', ' Pnas ', ' Published In: ' , ' Rna ', ' S.a.p.i.en.s ',
           ' SRP Arh Celok Lek ', ' Star Trek: The Official Monthly Magazine ', 
-          ' Tellus a ', ' The Embo Journal ', ' Time Out London ', ' Tot de ', ' Uhealth ', ' UHealth ', ' V Astronomicheskii Zhurna ',
+          ' Tellus a ', ' The Embo Journal ', ' Time Out London ', ' Tot de ', ' U.s. ', ' U.S.a. ', ' U.s.a ', ' Uhealth ', ' UHealth ', ' V Astronomicheskii Zhurna ',
           ' Z/journal ', ' Z/Journal ', ' Zeitschrift Für Geologische Wissenschaften ', ' Zeitschrift für Physik a Hadrons and Nuclei ', 
           ' Zeitschrift Für Physik a: Hadrons And Nuclei ', ' Zookeys '
           /* The above will be automatically updated to alphabetical order */ 
