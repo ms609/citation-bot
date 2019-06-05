fc367e7c6d4f0da912e828f8ce3ad0b2bd8a08ed
GlazerMann <kurt@ioiowa.com>
2019-06-04 19:19:10 -0700

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class TemplateTest extends testBaseClass {
 
   public function testEmptyCitations() {
-    $text = "{{cite book||||||\n||||}}"; // bot removed pipes
+    $text = "{{cite book|||\n|||\n||||}}"; // bot removed pipes
     $expanded = $this->process_page($text);
     $this->assertEquals('HUH', $expanded->parsed_text());
   }
