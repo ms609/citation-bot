4979dce2f9b0d8fde52a7810b3e4dcb2afcc4a00
Martin R. Smith <martins@gmail.com>
2019-06-13 11:56:16 +0100

    Use QA.adsabs for testing (#1768)
    Errors in Travis builds do not seem to correspond to this pull.

--- Template.php
+++ Template.php
@@ -1747,7 +1747,8 @@ final class Template {
       curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . getenv('PHP_ADSABSAPIKEY')));
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
       curl_setopt($ch, CURLOPT_HEADER, TRUE);
-      $adsabs_url = "https://api.adsabs.harvard.edu/v1/search/query"
+      $adsabs_url = "https://" . (getenv('TRAVIS') ? 'qa' : 'api')
+                  . ".adsabs.harvard.edu/v1/search/query"
                   . "?q=$options&fl=arxiv_class,author,bibcode,doi,doctype,identifier,"
                   . "issue,page,pub,pubdate,title,volume,year";
       curl_setopt($ch, CURLOPT_URL, $adsabs_url);
