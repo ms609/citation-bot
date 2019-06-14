d25dc254d4f35793d3977bae40a36c7cf10dc249
GlazerMann <kurt@ioiowa.com>
2019-06-13 21:02:12 -0700

    Update category.php

--- category.php
+++ category.php
@@ -47,16 +47,7 @@ if (HTML_OUTPUT) {
 <?php
 }
 
-$user = isset($_REQUEST["user"]) ? $_REQUEST["user"] : NULL;
-if (WikipediaBot::is_valid_user($user)) {
-  echo " Activated by $user.\n\n";
-  $edit_summary_end = " | [[User:$user|$user]]; [[Category:$category]].";
-} else {
-  echo " Anonymous user.  Add &user=MyUserName to URL to sign the bot's edits\n\n";
-  $edit_summary_end = " | [[WP:UCB|User-activated]]; [[Category:$category]].";
-  $user = NULL;
-  sleep(10);
-}
+$edit_summary_end = " | [[WP:UCB|User-activated]]; [[Category:$category]].";
 
 if ($category) {
   $attempts = 0;
