37e740f37338e8f1eaa15463208f8bb2398471b8
GlazerMann <kurt@ioiowa.com>
2019-06-13 21:06:15 -0700

    Update category.php

--- category.php
+++ category.php
@@ -47,7 +47,7 @@ if (HTML_OUTPUT) {
 <?php
 }
 
-$edit_summary_end = " | [[WP:UCB|User-activated]]; [[Category:$category]].";
+$edit_summary_end = " | [[Category:$category]].";
 
 if ($category) {
   $attempts = 0;
