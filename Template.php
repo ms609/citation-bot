a0a6ec4e5d6a2cad1d931805a645ab297ab035bb
GlazerMann <kurt@ioiowa.com>
2019-06-18 14:12:05 -0700

    Remove postscript parameters that are a comment

--- Template.php
+++ Template.php
@@ -3330,6 +3330,12 @@ final class Template {
           if ($this->wikiname() !== 'citation') {
             if ($this->get($param) === '.') $this->forget($param); // Default action does not need specified
             if ($this->blank($param)) $this->forget($param);  // Misleading -- blank means period!!!!
+            if (preg_match('~^# # # CITATION_BOT_PLACEHOLDER_COMMENT \d+ # # #$~i', $this->get($param)) {
+              unset($this->param[$this->get_param_key('postscript')]);
+              report_forget("Dropping postscript that is only a comment');
+              // Misleading -- comments of "NONE" etc mean nothing!
+              // Cannot call forget, since it will not remove items with comments in it
+            }
           }
           return;
           
