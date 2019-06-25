74b36786e05a5f366228962984719b0af6c858f2
GlazerMann <kurt@ioiowa.com>
2019-06-25 11:06:56 -0700

    Update apiFunctionsTest.php

--- tests/phpunit/apiFunctionsTest.php
+++ tests/phpunit/apiFunctionsTest.php
@@ -5,12 +5,10 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class apiFunctionsTest extends testBaseClass {
   
   public function testAdsabsApi() {
-      $text = '{{Cite journal | bibcode = 2018MNRAS.tmp.2192I}}';
-      $page = new TestPage();
-      $page->parse_text($text);
-      $templates = $page->extract_object('Template');
-      $page->expand_templates_from_identifier('bibcode', $templates);
-      $this->assertNull($this->parsed_text());
+    $template = new Template();
+    $template->parse_text('{{Cite journal | bibcode = 2018MNRAS.tmp.2192I}}');
+    $template->expand_by_adsabs();
+    $this->assertNull($template->parsed_text());
   }
   
  
