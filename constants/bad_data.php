dbe94e4c1a976226041e175091bb9fed33867e18
GlazerMann <kurt@ioiowa.com>
2019-06-26 09:37:04 -0700

    Temporarily Unavailable

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -48,7 +48,7 @@ const BAD_ZOTERO_TITLES = ['Browse publications', 'Central Authentication Servic
                                  'Subscribe to read', 'Wiley Online Library', 'pagina is niet gevonden',
                                  'Zoeken in over NA', 'na een 404', '404 error', 'Account Suspended',
                                  'Error 404', 'EZProxy', 'EBSCOhost Login', '404 - Not Found',
-                                 'SSRN Temporarily Unavailable'];
+                                 'Temporarily Unavailable'];
 
 const CANONICAL_PUBLISHER_URLS = array ('elsevier.com', 'springer.com', 'sciencedirect.com', 'tandfonline.com',
                                 'taylorandfrancis.com', 'wiley.com', 'sagepub.com', 'sagepublications.com',
