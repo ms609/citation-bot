555160b46fff944ea75071b0d9d1163b9e3a527a
GlazerMann <kurt@ioiowa.com>
2019-06-26 08:25:14 -0700

    fix

--- Template.php
+++ Template.php
@@ -1577,15 +1577,13 @@ final class Template {
     report_action("Checking AdsAbs database");
     if ($this->has('bibcode')) {
       $result = $this->query_adsabs("bibcode:" . urlencode('"' . $this->get("bibcode") . '"'));
-    } elseif ($this->blank(['eprint', 'arxiv'] && $this->has('doi') 
+    } elseif ($this->blank(['eprint', 'arxiv']) && $this->has('doi') 
               && preg_match(REGEXP_DOI, $this->get_without_comments_and_placeholders('doi'), $doi)) {
       $result = $this->query_adsabs("doi:" . urlencode('"' . $doi[0] . '"'));
-    } elseif ($this->blank('doi') && !$this->blank(['eprint', 'arxiv'])) {
-      if ($this->has('eprint')) {
+    } elseif ($this->blank('doi') && $this->has('eprint')) {
         $result = $this->query_adsabs("arXiv:" . urlencode('"' .$this->get('eprint') . '"'));
-      } elseif ($this->has('arxiv')) {
+    } elseif ($this->blank('doi') && $this->has('arxiv')) {
         $result = $this->query_adsabs("arXiv:" . urlencode('"' .$this->get('arxiv') . '"'));
-      }
     } else { // Does this work???
       $identifiers = array_filter(array(
       ($this->has('doi') 
