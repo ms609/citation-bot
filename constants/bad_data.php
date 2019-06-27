23b5f7670fc1dee82e2cd733e585bf6ac765cea7
GlazerMann <kurt@ioiowa.com>
2019-06-27 13:58:37 -0700

    Update bad_data.php

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -152,3 +152,23 @@ const AVOIDED_LINKS = array('', 'Digital_object_identifier', 'JSTOR', 'Website',
                             'Mathematical_Reviews', 'Office_of_Scientific_and_Technical_Information',
                             'Request_for_Comments', 'Social_Science_Research_Network', 'Zentralblatt_MATH',
                             'Open_Library', 'ArXiv', 'OCLC', 'Cf.');
+
+// This list is going to grow and grow based upon user requests
+// ALWAYS verify that they registered the examples you are given
+// http://oasis.postech.ac.kr/handle/2014.oak/9965 is not registered for example
+const HANDLES_HOSTS = array('hdl.handle.net', 'kb.osu.edu', 'repositorio.aemet.es', 'fieldresearch.msf.org',
+                            'dspace.lib.cranfield.ac.uk', 'dspace.lboro.ac.uk', 'scholarship.rice.edu',
+                            'deepblue.lib.umich.edu', 'arrow.latrobe.edu.au', 'kb.osu.edu', 'scholarworks.csun.edu',
+                            'www.era.lib.ed.ac.uk', 'research-repository.st-andrews.ac.uk',
+                            'dataverse.scholarsportal.info', 'idus.us.es', 'dspace.library.uu.nl',
+                            'dspace.mit.edu', 'digital.csic.es', 'apps.who.int', 'repository.si.edu',
+                            'lume.ufrgs.br', 'digitallibrary.amnh.org'
+                           );
+// Order here could matter
+const HANDLES_PATHS = array('/dspace/bitstream/handle/', '/msf/handle/', '/vital/access/HandleResolver/',
+                            '/handle/', '/dspace-jspui/handle/', '/dataset.xhtml?persistentId=hdl:',
+                            '/dspace/handle/', '/handle2/', '/xmlui/handle/', '/iris/handle/',
+                            '/repository/bitstream/handle/', '/repository/handle/', 
+                            '/tools/dspace/load/?file=/repository/bitstream/handle/',
+                            '/xmlui/bitstream/handle/');
+
