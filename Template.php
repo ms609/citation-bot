22c78012f206b72010eed4d945bcc692aafa03c9
GlazerMann <kurt@ioiowa.com>
2019-06-30 11:54:55 -0700

    regex escape

--- Template.php
+++ Template.php
@@ -3358,7 +3358,7 @@ final class Template {
             }
           }
           if (stripos($this->get($param), 'proquest') !== FALSE) {
-            if (preg_match("~^(?:http.+/login\?url=|)https?://(?:0-|)search.proquest.com.+/docview/(.+)$~", $this->get($param), $matches)) {
+            if (preg_match("~^(?:http.+/login\?url=|)https?://(?:0\-|)search.proquest.com.+/docview/(.+)$~", $this->get($param), $matches)) {
                  $this->set($param, 'https://search.proquest.com/docview/' . $matches[1]);
                  report_info("Remove proxy from ProQuest URL");
                  if ($this->has('via') && stripos($this->get('via'), 'library') !== FALSE) $this->forget('via');
