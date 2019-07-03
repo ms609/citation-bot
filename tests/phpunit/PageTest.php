b0e09269c25ce2b0f6ebcbdbad3badb13f2406b4
GlazerMann <kurt@ioiowa.com>
2019-07-03 10:38:09 -0700

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -11,7 +11,7 @@ final class PageTest extends testBaseClass {
 
    public function testUrlReferencesWithText0() {
       $text = "{{Cite journal | url=http://iopscience.iop.org/1367-2630/11/6/063043/ | doi=10.1088/1367-2630/11/6/063043| title=High error-rate quantum key distribution for long-distance communication| journal=New Journal of Physics| volume=11| issue=6| pages=063043| year=2009| last1=Mubashir Khan| first1=Muhammad| last2=Murphy| first2=Michael| last3=Beige| first3=Almut| bibcode=2009NJPh...11f3043M| arxiv=0901.3909}}";
-      $page = $this->prepare_citation($text);
+      $page = $this->process_citation($text);
       $this->assertNull($page->parsed_text());
   }
 
