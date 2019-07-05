1b528eed36677770aba5d93b917cb034048aa558
GlazerMann <kurt@ioiowa.com>
2019-07-05 09:41:24 -0700

    Truncate CR titles at roman numerals before check (#1898)

--- apiFunctions.php
+++ apiFunctions.php
@@ -373,10 +373,33 @@ function expand_by_doi($template, $force = FALSE) {
       if (in_array(strtolower($crossRef->article_title), BAD_ACCEPTED_MANUSCRIPT_TITLES)) return FALSE ;
       if ($template->has('title') && trim(@$crossRef->article_title)) { // Verify title of DOI matches existing data somewhat
         $bad_data = TRUE;
+        $new = $crossRef->article_title;
+        if (preg_match('~^(.................+)\.\s+([IVX]+)\.\s.+$~i', $new, $matches)) {
+           $new = $matches[1];
+           $new_roman = $matches[2];
+        } else {
+           $new_roman = FALSE;
+        }
         foreach (['chapter', 'title', 'series'] as $possible) {
-          if ($template->has($possible) && titles_are_similar($template->get($possible), $crossRef->article_title)) {
-            $bad_data = FALSE;
-            break;
+          if ($template->has($possible)) {
+            $old = $template->get($possible);
+            if (preg_match('~^(.................+)\.\s+([IVX]+)\.\s.+$~i', $old, $matches)) {
+               $old = $matches[1];
+               $old_roman = $matches[2];
+            } else {
+               $old_roman = FALSE;
+            }
+            if (titles_are_similar($old, $new)) {
+              if ($old_roman && $new_roman) {
+                if ($old_roman == $new_roman) { // If they got roman numeral truncted, then must match
+                  $bad_data = FALSE;
+                  break;
+                }
+              } else {
+                $bad_data = FALSE;
+                break;
+              }
+            }
           }
         }
         if (isset($crossRef->series_title)) {
