f0242ecad9e932d54d6fcca87d873f2fd643eeee
GlazerMann <kurt@ioiowa.com>
2019-07-10 18:27:39 -0700

    Turn pubmed zotero back on (#1741)

--- tests/phpunit/zoteroTest.php
+++ tests/phpunit/zoteroTest.php
@@ -24,7 +24,6 @@ class ZoteroTest extends testBaseClass {
 
   public function testZoteroExpansionNBK() {
     $text = '{{Cite journal|url=https://www.ncbi.nlm.nih.gov/books/NBK24662/|access-date=2099-12-12}}';  // Date is before access-date so will expand
- return;
     $expanded = $this->expand_via_zotero($text);
     $this->assertSame('Continuing Efforts to More Efficiently Use Laboratory Animals', $expanded->get('title'));
     $this->assertSame('2004', $expanded->get('year'));
