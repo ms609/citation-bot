ce0b48071085cf4cb055c28548346f6da39c8b01
GlazerMann <kurt@ioiowa.com>
2019-07-19 08:33:18 -0700

    typo

--- category.php
+++ category.php
@@ -95,12 +95,12 @@ if ($category) {
         " | <a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . "&action=history>history</a> ] " . "<a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . ">" . echoable($page_title) . "</a>";
       } else {
          report_warning("Write failed.");
-         $final_edit_overview .= "\n Write failed.      " . . "<a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . ">" . echoable($page_title) . "</a>";
+         $final_edit_overview .= "\n Write failed.      " . "<a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . ">" . echoable($page_title) . "</a>";
       }
     } else {
       report_phase($page->parsed_text() ? 'No changes required.' : 'Blank page');
       echo "\n\n    # # # ";
-      $final_edit_overview .= "\n No changes needed. " . . "<a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . ">" . echoable($page_title) . "</a>";
+      $final_edit_overview .= "\n No changes needed. " . "<a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . ">" . echoable($page_title) . "</a>";
     }
   }
   echo ("\n Done all " . count($pages_in_category) . " pages in Category:$category. \n");
