3b1717a6a6e911d6de5ce20fd37a553fa386c466
GlazerMann <kurt@ioiowa.com>
2019-07-21 13:43:57 -0500

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class TemplateTest extends testBaseClass {
 
   public function testParameterWithNoParameters() {
-    $text = "{{Cite web | text without equals sign  }}";
+    $text = "{{Cite web|url=https://pubs.acs.org/page/ancham/submission/prior.html|title=Policy Summary on Prior Publication|website=pubs.acs.org}}";
     $expanded = $this->process_citation($text);
     $this->assertNull($expanded->parsed_text());
   }
