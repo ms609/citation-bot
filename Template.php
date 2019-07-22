355c5ed8228f8172f8d772381e3602153c93e9ce
GlazerMann <kurt@ioiowa.com>
2019-07-22 18:50:30 -0500

    Give up on AdsAbs after the first failure

--- Template.php
+++ Template.php
@@ -1836,9 +1836,11 @@ final class Template {
   // $options should be a series of field names, colons (optionally urlencoded), and
   // URL-ENCODED search strings, separated by (unencoded) ampersands.
   // Surround search terms in (url-encoded) ""s, i.e. doi:"10.1038/bla(bla)bla"
-  protected function query_adsabs($options) {  
+  protected function query_adsabs($options) {
+    static $ADSABS_GIVE_UP = FALSE;
     // API docs at https://github.com/adsabs/adsabs-dev-api/blob/master/Search_API.ipynb
     if (getenv('TRAVIS_PULL_REQUEST') && (getenv('TRAVIS_PULL_REQUEST') !== 'false')) return (object) array('numFound' => 0);
+    if ($ADSABS_GIVE_UP === TRUE) return (object) array('numFound' => 0);
     if (!getenv('PHP_ADSABSAPIKEY')) {
       report_warning("PHP_ADSABSAPIKEY environment variable not set. Cannot query AdsAbs.");
       return (object) array('numFound' => 0);
@@ -1921,6 +1923,10 @@ final class Template {
       } else {
         report_warning(sprintf("Error %d in query_adsabs: %s",
                       $e->getCode(), $e->getMessage()));
+        if ($e->getCode() == 999 && $e->getMessage() == 'Too many requests') {
+          $ADSABS_GIVE_UP = TRUE;
+          report_warning('Giving up on AdsAbs');
+        }
       }
       return (object) array('numFound' => 0);
     }
