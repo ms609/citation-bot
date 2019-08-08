cf7c20529e63defb21e4e303c0ff176c2445d52d
GlazerMann <kurt@ioiowa.com>
2019-08-08 18:14:06 -0500

    Update Template.php

--- Template.php
+++ Template.php
@@ -1456,7 +1456,7 @@ final class Template {
              $this->forget($url_type);
           }
           return $this->add_if_new('ol', $match[1]); 
-      } elseif (preg_match("~^https?://search\.proquest\.com/document/(\d{4,})$~i, $url, $match) && $this->blank('id')) {
+      } elseif (preg_match("~^https?://search\.proquest\.com/document/(\d{4,})$~i", $url, $match) && $this->blank('id')) {
           quietly('report_modification', 'Converting URL to ProQuest parameter');
           if (is_null($url_sent)) {
              $this->forget($url_type);
