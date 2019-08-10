50fd7dc744b7ad5a183a0c6cb55c91498cb77414
GlazerMann <kurt@ioiowa.com>
2019-08-10 17:01:32 -0500

    another failur

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -19,4 +19,15 @@ final class PageTest extends testBaseClass {
     }
     $this->assertTrue(TRUE);
   }
+  public function testBadPage2() {  // Use this when debugging pages that crash the bot
+    $bad_page = "List_of_awards_and_nominations_received_by_Deepika_Padukone"; //  Replace with something like "Vietnam_War" when debugging
+    if ($bad_page !== "") {
+      $text = file_get_contents('https://en.wikipedia.org/w/index.php?title=' . $bad_page . '&action=raw');
+      $page = new TestPage();
+      $page->parse_text($text);
+      $page->expand_text();
+      $this->assertTrue(FALSE); // prevent us from git committing with a website included
+    }
+    $this->assertTrue(TRUE);
+  }
 }
