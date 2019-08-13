1843c7aaadf3f292ce711ad0131e7237e7766d24
GlazerMann <kurt@ioiowa.com>
2019-08-13 18:49:17 -0500

    Create restart.php (#2081)

---
+++ restart.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html><html><body><pre>
+<?php
+if (password_verify($_REQUEST['p'], '$2y$10$UOmZtkKs1X17vE/mmbfVgOiy0ZAkXnxa9UxFO97cFX8ioiJZpZ96S')) {
+  echo shell_exec ("webservice restart 2>&1");
+}
+?>
+PHP done
+</pre></body></html>
