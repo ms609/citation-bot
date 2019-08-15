dab368f5b3e02902dc7857c74ae5e6ca34d1a7f8
GlazerMann <kurt@ioiowa.com>
2019-08-15 13:55:20 -0500

    Fix crash (#2087)

--- restart.php
+++ restart.php
@@ -1,8 +1,7 @@
-<!DOCTYPE html><html><body><pre>
 <?php
+echo '<!DOCTYPE html><html><body><pre>';
 if (password_verify($_REQUEST['p'], '$2y$10$UOmZtkKs1X17vE/mmbfVgOiy0ZAkXnxa9UxFO97cFX8ioiJZpZ96S')) {
   echo shell_exec ("webservice restart 2>&1");
 }
+echo 'PHP done </pre></body></html>';
 ?>
-PHP done
-</pre></body></html>
