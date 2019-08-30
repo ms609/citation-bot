86d33b9e50d7903b09212d7de978c51ebeac705b
GlazerMann <kurt@ioiowa.com>
2019-08-30 15:33:34 -0500

    typo

--- Template.php
+++ Template.php
@@ -4125,7 +4125,7 @@ final class Template {
         if(empty($headers_test['Location'])) {
            if ($this->blank('doi-broken-date')) {
              $this->add_if_new('doi-broken-date', date("Y-m-d"));
-           } elseif (mb_stripos($this->get('doi-broken-date'), 'CITATION_BOT_PLACEHOLDER_COMMENT') === FALSE)) {
+           } elseif (mb_stripos($this->get('doi-broken-date'), 'CITATION_BOT_PLACEHOLDER_COMMENT') === FALSE) {
              report_inline("DOI still broken: " . echoable($doi));
              $this->set('doi-broken-date', date("Y-m-d")); // Update date to today
            }
