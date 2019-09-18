1afd22e4e5af6004f1c2da609ae741deb5ac0aa1
GlazerMann <kurt@ioiowa.com>
2019-09-17 21:30:47 -0500

    Update Template.php

--- Template.php
+++ Template.php
@@ -1985,7 +1985,7 @@ final class Template {
   protected function query_adsabs($options) {
     global $ADSABS_GIVE_UP;
     // API docs at https://github.com/adsabs/adsabs-dev-api/blob/master/Search_API.ipynb
-    if (getenv('TRAVIS_PULL_REQUEST') && (getenv('TRAVIS_PULL_REQUEST') !== 'false')) return (object) array('numFound' => 0);
+    // if (getenv('TRAVIS_PULL_REQUEST') && (getenv('TRAVIS_PULL_REQUEST') !== 'false')) return (object) array('numFound' => 0);
     if (@$ADSABS_GIVE_UP) return (object) array('numFound' => 0);
     if (!getenv('PHP_ADSABSAPIKEY')) {
       report_warning("PHP_ADSABSAPIKEY environment variable not set. Cannot query AdsAbs.");
