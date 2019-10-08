6a78d8b62b76df44333554a05ef1af9a2b70bdd8
GlazerMann <kurt@ioiowa.com>
2019-10-08 06:34:09 -0500

    Update gadgetTest.php

--- tests/phpunit/gadgetTest.php
+++ tests/phpunit/gadgetTest.php
@@ -23,7 +23,7 @@ final class gadgetTest extends testBaseClass {
       // Output checking time
       $json = json_decode($json_text);
       $this->assertSame("{{citation|pmid=34213|year=1979|last1=Weber|first1=F.|title=Antimicrobial resistance and serotypes of Streptococcus pneumoniae in Switzerland|journal=Schweizerische Medizinische Wochenschrift|volume=109|issue=11|pages=395–9|last2=Kayser|first2=F. H.}}", $json->expandedtext);
-      $this->assertSame("Something Nice | Alter: template type. Add: pages, issue, volume, journal, title, year, author pars. 1-2. Formatted [[WP:ENDASH|dashes]]. | You can [[WP:UCB|use this tool]] yourself. [[WP:DBUG|Report bugs here]]. | via #UCB_Gadget. ", $json->editsummary);
+      $this->assertSame("Something Nice | Alter: template type. Add: pages, issue, volume, journal, title, year, author pars. 1-2. Formatted [[WP:ENDASH|dashes]]. | You can [[WP:UCB|use this tool]] yourself. [[WP:DBUG|Report bugs here]]. | via #UCB_Gadget ", $json->editsummary);
       $this->assertSame("", $json->debug);
   }
 }
