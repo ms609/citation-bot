3cbc8f18a79403ee6e002baabbed554fe0d4cd2a
GlazerMann <kurt@ioiowa.com>
2019-10-11 15:55:53 -0500

    update test (#2195)

--- tests/phpunit/WikipediaBotTest.php
+++ tests/phpunit/WikipediaBotTest.php
@@ -18,7 +18,7 @@ require_once __DIR__ . '/../testBaseClass.php';
     public function testCategoryMembers() {
      $this->requires_secrets(function() {
       $api = new WikipediaBot();
-      $this->assertTrue(count($api->category_members('GA-Class cricket articles')) > 10);
+      $this->assertTrue(count($api->category_members('GA-Class cricket articles of Low-importance')) > 10);
       $this->assertSame(0, count($api->category_members('A category we expect to be empty')));
      });
     }
