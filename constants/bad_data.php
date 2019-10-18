636ada540960dc2d8384a6af111226fa8e62e12f
GlazerMann <kurt@ioiowa.com>
2019-10-18 10:59:51 -0500

    Methods in Molecular Biology (Clifton, N.J.) (#2212)

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -171,4 +171,4 @@ const AVOIDED_LINKS = array('', 'Digital_object_identifier', 'JSTOR', 'Website',
                             'Request_for_Comments', 'Social_Science_Research_Network', 'Zentralblatt_MATH',
                             'Open_Library', 'ArXiv', 'OCLC', 'Cf.');
 
-const JOURNAL_IS_BOOK_SERIES = array('methods of molecular miology' , 'methods mol biol');
+const JOURNAL_IS_BOOK_SERIES = array('methods of molecular biology' , 'methods mol biol', 'methods in molecular biology (clifton, n.j.)');
