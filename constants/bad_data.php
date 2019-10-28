f07eb41ffc804a6506d5fc9551d7c6f7dc3fb387
GlazerMann <kurt@ioiowa.com>
2019-10-28 17:44:28 -0500

    junk

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -96,7 +96,7 @@ const ZOTERO_AVOID_REGEX = array("twitter\.",               // This should be {{
                                  "jstor\.org/stable/",      // We have special jstor code
                                  "ned\.ipac\.caltech\.edu", // Gives no real title
                                  "pep\-web\.org",           // Does not parse very well at all
-                                 "ezproxy", "arkive\.org");                // Junk
+                                 "ezproxy", "arkive\.org", "bloomberg\.com/tosv2.html");                // Junk
 const NON_JOURNAL_WEBSITES = array('cnn.com/', 'foxnews.com/', 'msnbc.com/', 'nbcnews.com/', 'abcnews.com/', 'cbs.com/', 
                                    'cbsnews.com/', 'abc.com/', 'bbc.com/', 'bbc.co.uk/', 'apnews.com/',
                                    '.ap.org/', 'nytimes.com/', 'theguardian.com/', 'washingtonpost.com/',
