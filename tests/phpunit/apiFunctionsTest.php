a6edb4ec3fae5225d70c38b286050d568fc55656
GlazerMann <kurt@ioiowa.com>
2019-10-29 07:00:56 -0500

    Update apiFunctionsTest.php

--- tests/phpunit/apiFunctionsTest.php
+++ tests/phpunit/apiFunctionsTest.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class apiFunctionsTest extends testBaseClass {  
 
  public function testUrlReferences3() {
-       $page = $this->process_page("{{cite web|doi=10.1007/s12668-011-0022-5}}");
+       $page = $this->process_page("{{cite journal|doi=10.1007/s12668-011-0022-5}}");
        $this->assertSame('huh', $page->parsed_text());
   }
 
