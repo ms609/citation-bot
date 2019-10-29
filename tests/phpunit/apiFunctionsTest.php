6285d70a0cc40e305e4b20110684995d65774ee7
GlazerMann <kurt@ioiowa.com>
2019-10-28 20:43:51 -0500

    Update apiFunctionsTest.php

--- tests/phpunit/apiFunctionsTest.php
+++ tests/phpunit/apiFunctionsTest.php
@@ -9,14 +9,13 @@ final class apiFunctionsTest extends testBaseClass {
      $text = '{{Cite journal| doi= 10.13140/RG.2.1.1002.9609}}';
      $expanded = $this->process_citation($text);
      $this->assertSame('Lesson Study as a form of in-School Professional Development', $expanded->get('title'));
-     $this->assertSame('2015', $expanded->get('year'));
-     $this->assertSame('Aoibhinn Ni Shuilleabhain', $expanded->get('author1'));
   }
   
 
   public function testUrlReferences() {		   public function testUrlReferences() {
        $page = $this->process_page("<ref>http://doi.org/10.1007/s12668-011-0022-5< / ref>");
-   }
+       $this-->assertNull($page->parsed_text());
+  }
 
     
 }
