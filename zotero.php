38bdbc53ccdf38feb0c0a1286aaa5595004ed11b
GlazerMann <kurt@ioiowa.com>
2019-11-13 20:34:57 -0600

    Disbarred MR

--- zotero.php
+++ zotero.php
@@ -59,10 +59,11 @@ function query_url_api($ids, $templates) {
        //  Has a CAPCHA --  if ($template->has('zbl'))       expand_by_zotero($template, 'https://zbmath.org/?format=complete&q=an:' . $template->get('zbl'));
        //  Has "MR: Matches for: MR=154360" title -- if ($template->has('mr'))        expand_by_zotero($template, 'https://mathscinet.ams.org/mathscinet-getitem?mr=' . $template->get('mr'));
        if ($template->has('mr') && $template->blank('doi')) {
-         $mr_data = @file_get_contents('https://mathscinet.ams.org/mathscinet-getitem?mr=' . $template->get('mr'));
-         if (preg_match('~<a class="link" target="_blank" href="/leavingmsn\?url=https://doi\.org/(10\.[^\s"]+)">Article</a>~i', $mr_data, $matches)) {
-           $template->add_if_new('doi', $matches[1]);
-           expand_by_doi($template, TRUE);
+    // TODO figure out how to not get doi for article reviewed
+    //     $mr_data = @file_get_contents('https://mathscinet.ams.org/mathscinet-getitem?mr=' . $template->get('mr'));
+    //     if (preg_match('~<a class="link" target="_blank" href="/leavingmsn\?url=https://doi\.org/(10\.[^\s"]+)">Article</a>~i', $mr_data, $matches)) {
+    //       $template->add_if_new('doi', $matches[1]);
+    //       expand_by_doi($template, TRUE);
          }
        }
        if ($template->has('osti'))      expand_by_zotero($template, 'https://www.osti.gov/biblio/' . $template->get('osti'));
