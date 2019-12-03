6b737eeeace14473f7bb7b3d242621518746cd95
GlazerMann <kurt@ioiowa.com>
2019-12-03 14:07:51 -0600

    Update WikipediaBotTest.php

--- tests/phpunit/WikipediaBotTest.php
+++ tests/phpunit/WikipediaBotTest.php
@@ -45,9 +45,9 @@ require_once __DIR__ . '/../testBaseClass.php';
     public function testRedirects() {
      $this->requires_secrets(function() {
       $api = new WikipediaBot();
-      $this->assertSame(-1, $api->is_redirect('NoSuchPage:ThereCan-tBe'));
-      $this->assertSame( 0, $api->is_redirect('User:Citation_bot'));
-      $this->assertSame( 1, $api->is_redirect('WP:UCB'));
+      $this->assertSame(-1, WikipediaBot::is_redirect('NoSuchPage:ThereCan-tBe', $api));
+      $this->assertSame( 0, WikipediaBot::is_redirect('User:Citation_bot', $api);
+      $this->assertSame( 1, WikipediaBot::is_redirect('WP:UCB', $api));
       $this->assertSame('User:Citation bot/use', $api->redirect_target('WP:UCB'));
      });
     }
