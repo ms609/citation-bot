b0eb0388a4054e9ad6940de24ea69999524e49f4
GlazerMann <kurt@ioiowa.com>
2019-12-21 13:12:34 -0600

    DEBUG

--- tests/phpunit/WikipediaBotTest.php
+++ tests/phpunit/WikipediaBotTest.php
@@ -16,11 +16,13 @@ require_once __DIR__ . '/../testBaseClass.php';
     }
       
     public function testCategoryMembers() {
-     $this->requires_secrets(function() {
+
       $api = new WikipediaBot();
+      print_r($api->category_members('GA-Class cricket articles of Low-importance'));
+      print_r($api->category_members('A category we expect to be empty'));
       $this->assertTrue(count($api->category_members('GA-Class cricket articles of Low-importance')) > 10);
       $this->assertSame(0, count($api->category_members('A category we expect to be empty')));
-     });
+
     }
     
     public function testWhatTranscludes() {
