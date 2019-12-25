72308822d35d7f5697449f7496f20ebc129fa77e
GlazerMann <kurt@ioiowa.com>
2019-12-25 11:15:58 -0600

    More series

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -178,4 +178,5 @@ const JOURNAL_IS_BOOK_SERIES = array('methods of molecular biology' , 'methods m
                                      'methods in molecular biology (clifton, n.j.)',
                                      'advances in pharmacology (san diego, calif.)',
                                      'advances in pharmacology', 'inorganic syntheses',
-                                     'advances in enzymology and related areas of molecular biology');
+                                     'advances in enzymology and related areas of molecular biology',
+                                     'studies in bilingualism', 'antibiotics and chemotherapy');
