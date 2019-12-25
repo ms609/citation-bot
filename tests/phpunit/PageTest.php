1c78355ba4feff015858ec6ca64a2bf51f3a1d23
GlazerMann <kurt@ioiowa.com>
2019-12-25 16:55:48 -0600

    Better code coverage (#2348)

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -47,13 +47,14 @@ final class PageTest extends testBaseClass {
       $trialCitation = '{{Cite journal | title Bot Testing | ' .
         'doi_broken_date=1986-01-01 | doi = 10.1038/nature09068}}';
       $page->overwrite_text($trialCitation);
+      $page_result = $page->write($api, "Testing bot write function");
       if (getenv('TRAVIS')) {
         $this->assertTrue(TRUE); // make CI happy
         echo 'T';  // ! API call failed: '''Your IP address is in a range which has been blocked on all wikis.''' The block was made by [//meta.wikimedia.org/wiki/User:Jon_Kolbert Jon Kolbert] (meta.wikimedia.org). The reason given is ''[[m:NOP|Open Proxy]]: Colocation webhost - Contact [[m:Special:Contact/stewards|stewards]] if you are affected ''. * Start of block: 02:23, 27 October 2019 * Expiration of block: 02:23, 27 October 2021
         return;
+      } else {
+        $this->assertTrue($page_result);
       }
-      $this->assertTrue($page->write($api, "Testing bot write function"));
-      
       $page->get_text_from($writeTestPage, $api);
       $this->assertSame($trialCitation, $page->parsed_text());
       $page->expand_text();
