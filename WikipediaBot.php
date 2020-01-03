f707775ca44031dd7cfb32f21e6466287587b8d3
GlazerMann <kurt@ioiowa.com>
2020-01-03 11:49:43 -0600

    Fix bug that should not ever happend

--- WikipediaBot.php
+++ WikipediaBot.php
@@ -148,7 +148,8 @@ class WikipediaBot {
           }
           return ($this->ret_okay($ret)) ? $ret : FALSE;
           
-        report_error("Unrecognized method in Fetch."); // @codeCoverageIgnore - will only be hit if error in our code
+        default:
+          report_error("Unrecognized method in Fetch."); // @codeCoverageIgnore - will only be hit if error in our code
       }
     } catch(Exception $E) {
       report_warning("Exception caught!\n");
