102839cdc9118bea58e2df46b7a0d5a1ac8fde57
GlazerMann <kurt@ioiowa.com>
2020-01-03 10:22:01 -0600

    Update Page.php

--- Page.php
+++ Page.php
@@ -262,8 +262,8 @@ class Page {
       if ($this_template->blank('bibcode')) {
         $no_arxiv = $this_template->blank('arxiv');
         $this_template->expand_by_adsabs(); // Try to get a bibcode
-        if (!$this_template->blank('arxiv') && $no_arxiv) {
-          expand_arxiv_templates([$this_template]); // Added an arXiv.  Stuff to learn and sometimes even find a DOI
+        if (!$this_template->blank('arxiv') && $no_arxiv) {  // Added an arXiv.  Stuff to learn and sometimes even find a DOI -- VERY RARE
+          expand_arxiv_templates([$this_template]);     // @codeCoverageIgnore
         }
       }
       $this_template->get_open_access_url();
