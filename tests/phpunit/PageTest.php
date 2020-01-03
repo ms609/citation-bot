134e445ee397ff6fed77da0943a85d2bba0ddeab
GlazerMann <kurt@ioiowa.com>
2020-01-03 09:24:24 -0600

    code coverage

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -217,6 +217,13 @@ final class PageTest extends testBaseClass {
       $this->assertSame('<ref>{{cite web}}</ref><ref>{{cite web}}</ref><ref>{{Cite journal |doi = 10.1145/358589.358596|title = Improving computer program readability to aid modification|journal = Communications of the ACM|volume = 25|issue = 8|pages = 512–521|year = 1982|last1 = Elshoff|first1 = James L.|last2 = Marcotty|first2 = Michael}}</ref>', $page->parsed_text());
   }
  
+   public function testUrlReferencesWithText15() {
+      $text = "<ref>[http://doi.acm.org/10.1145/358589.358596 http://doi.acm.org/10.1145/358589.3585964444]</ref>";
+      $text = $text . $text;
+      $page = $this->process_page($text);
+      $this->assertSame($text, $page->parsed_text());
+  }
+ 
   public function testRespectDatesZotero() {
       $text = '{{Use mdy dates}}{{cite web|url=https://www.nasa.gov/content/profile-of-john-glenn}}';
       $page = $this->process_page($text);
