1c788ac8cf552e30c3bfdab0915c2f0f7cbcfed2
GlazerMann <kurt@ioiowa.com>
2020-01-04 14:20:46 -0600

    Update Template.php

--- Template.php
+++ Template.php
@@ -2040,8 +2040,8 @@ final class Template {
     if (getenv('TRAVIS_PULL_REQUEST') && (getenv('TRAVIS_PULL_REQUEST') !== 'false')) return (object) array('numFound' => 0);
     if (@$ADSABS_GIVE_UP) return (object) array('numFound' => 0);
     if (!getenv('PHP_ADSABSAPIKEY')) {
-      report_warning("PHP_ADSABSAPIKEY environment variable not set. Cannot query AdsAbs.");
-      return (object) array('numFound' => 0);
+      report_warning("PHP_ADSABSAPIKEY environment variable not set. Cannot query AdsAbs.");  // @codeCoverageIgnore
+      return (object) array('numFound' => 0);                                                 // @codeCoverageIgnore
     }
     
     try {
