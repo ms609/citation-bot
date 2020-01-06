6fe16a44e157cadfa39e2390e4703359e08ee716
GlazerMann <kurt@ioiowa.com>
2020-01-06 12:51:03 -0600

    add bloomberg

--- Template.php
+++ Template.php
@@ -3829,6 +3829,8 @@ final class Template {
               $this->set($param, $this->simplify_google_search($this->get($param)));
           } elseif (preg_match("~^(https?://(?:www\.|)sciencedirect\.com/\S+)\?via(?:%3d|=)\S*$~i", $this->get($param), $matches)) {
               $this->set($param, $matches[1]);
+          } elseif (preg_match("~^(https?://(?:www\.|)bloomberg\.com/\S+)\?(?:utm_|cmpId=)\S*$~i", $this->get($param), $matches)) {
+              $this->set($param, $matches[1]);
           } elseif (preg_match("~^https?://watermark\.silverchair\.com/~", $this->get($param))
                  || preg_match("~^https?://s3\.amazonaws\.com/academia\.edu~", $this->get($param))
                  || preg_match("~^https?://onlinelibrarystatic\.wiley\.com/store/~", $this->get($param))) {
