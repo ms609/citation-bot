709c956d157ddb7d98b73c6ba91537c5e051e4f9
GlazerMann <kurt@ioiowa.com>
2020-01-07 18:32:13 -0600

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -8,8 +8,8 @@ require_once __DIR__ . '/../testBaseClass.php';
  
 final class TemplateTest extends testBaseClass {
 
-   public function testNewspaperJournal101() {
-    $text = "{{cite journal|website=xyz}}";
+  public function testNewspaperJournal101() {
+    $text = "{{cite web|website=xyz}}";
     $template = $this->make_citation($text);
     $this->assertTrue($template->add_if_new('newspaper', 'news.bbc.co.uk'));
     $this->assertSame('what is it?', $template->parsed_text());
