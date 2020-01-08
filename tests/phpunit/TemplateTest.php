72461f5d953113df4526e6f5b0ea4e2269aff0d4
GlazerMann <kurt@ioiowa.com>
2020-01-07 18:27:07 -0600

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -12,6 +12,6 @@ final class TemplateTest extends testBaseClass {
     $text = "{{cite journal|website=xyz}}";
     $template = $this->make_citation($text);
     $this->assertTrue($template->add_if_new('newspaper', 'news.bbc.co.uk'));
-    $this->assertSame('what is it?', $template->get_parsed());
+    $this->assertSame('what is it?', $template->parsed_text());
   }       
 }
