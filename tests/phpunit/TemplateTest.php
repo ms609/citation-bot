b316fc291827fb7bd71d4ed23df119f61f515198
GlazerMann <kurt@ioiowa.com>
2020-01-07 18:23:19 -0600

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -8,21 +8,10 @@ require_once __DIR__ . '/../testBaseClass.php';
  
 final class TemplateTest extends testBaseClass {
 
- 
-  public function testTidy70() {
-    $text = "{{cite journal|issue=1|volume=2|doi=3}}";
+   public function testNewspaperJournal101() {
+    $text = "{{cite journal|website=xyz}}";
     $template = $this->make_citation($text);
-    print_r($template->param);
-    $template->rename('doi', 'issue');
-    print_r($template->param);
-    $template->rename('volume', 'issue');
-    print_r($template->param);
-    $template->forget('issue');
-    print_r($template->param);
-    $template->forget('issue');
-    print_r($template->param);
-    $template->forget('issue');
-    print_r($template->param);
-  }
-          
+    $this->assertTrue($template->add_if_new('newspaper', 'news.bbc.co.uk'));
+    $this->assertSame('what is it?', $template->get_parsec());
+  }       
 }
