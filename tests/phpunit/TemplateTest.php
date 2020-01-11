42cb22aa6351ebe24d1902923b847e3e654f1d00
GlazerMann <kurt@ioiowa.com>
2020-01-11 07:14:37 -0600

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -11,12 +11,14 @@ final class TemplateTest extends testBaseClass {
   public function testIncomplete() {
     $text = "{{cite book|url=http://perma-archives.org/pqd1234|isbn=Xxxx|title=xxx|issue=a|volume=x}}"; // Non-date website
     $template = $this->make_citation($text);
-    $this->assertFalse($template->profoundly_incomplete());
+    $this->assertTrue($template->profoundly_incomplete());
+    $this->assertTrue($template->profoundly_incomplete('http://perma-archives.org/pqd1234'));
   }
      public function testIncomplete_part2() {
     $text = "{{cite book|url=http://a_perfectly_acceptable_website/pqd1234|isbn=Xxxx|issue=hh|volume=rrfff|title=xxx}}";
     $template = $this->make_citation($text);
     $this->assertTrue($template->profoundly_incomplete());
+    $this->assertTrue($template->profoundly_incomplete('http://a_perfectly_acceptable_website/pqd1234'));
   }
  
   
