9cbaec6550b712cecc7f742d5dbf23619cdf43e6
GlazerMann <kurt@ioiowa.com>
2020-01-11 07:16:17 -0600

    fix tests

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -2702,11 +2702,13 @@ ER -  }}';
   public function testIncomplete() {
     $text = "{{cite book|url=http://perma-archives.org/pqd1234|isbn=Xxxx|title=xxx|issue=a|volume=x}}"; // Non-date website
     $template = $this->make_citation($text);
-    return ; // TODO
-    $this->assertFalse($template->profoundly_incomplete());
+    $this->assertTrue($template->profoundly_incomplete());
+    $this->assertFalse($template->profoundly_incomplete('http://perma-archives.org/pqd1234'));
     $text = "{{cite book|url=http://a_perfectly_acceptable_website/pqd1234|isbn=Xxxx|issue=hh|volume=rrfff|title=xxx}}";
     $template = $this->make_citation($text);
     $this->assertTrue($template->profoundly_incomplete());
+    $this->assertTrue($template->profoundly_incomplete('http://a_perfectly_acceptable_website/pqd1234'));
+    $this->assertFalse($template->profoundly_incomplete('http://perma-archives.org/pqd1234'));
   }
  
   public function testAddEditor() {
