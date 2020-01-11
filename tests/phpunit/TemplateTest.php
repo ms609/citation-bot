1cfb0f5896f997e1e5f08d890fa606d55453beca
GlazerMann <kurt@ioiowa.com>
2020-01-11 08:59:14 -0600

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -2703,12 +2703,17 @@ ER -  }}';
     $text = "{{cite book|url=http://perma-archives.org/pqd1234|isbn=Xxxx|title=xxx|issue=a|volume=x}}"; // Non-date website
     $template = $this->make_citation($text);
     $this->assertTrue($template->profoundly_incomplete());
-    $this->assertFalse($template->profoundly_incomplete('http://perma-archives.org/pqd1234'));
+    $this->assertTrue($template->profoundly_incomplete('http://perma-archives.org/pqd1234'));
     $text = "{{cite book|url=http://a_perfectly_acceptable_website/pqd1234|isbn=Xxxx|issue=hh|volume=rrfff|title=xxx}}";
     $template = $this->make_citation($text);
     $this->assertTrue($template->profoundly_incomplete());
     $this->assertTrue($template->profoundly_incomplete('http://a_perfectly_acceptable_website/pqd1234'));
+    $this->assertTrue($template->profoundly_incomplete('http://perma-archives.org/pqd1234'));
+    $text = "{{cite book|url=http://perma-archives.org/pqd1234|isbn=Xxxx|title=xxx|issue=a|volume=x|author1=Yes}}"; // Non-date website
+    $template = $this->make_citation($text);
+    $this->assertTrue($template->profoundly_incomplete());
     $this->assertFalse($template->profoundly_incomplete('http://perma-archives.org/pqd1234'));
+
   }
  
   public function testAddEditor() {
