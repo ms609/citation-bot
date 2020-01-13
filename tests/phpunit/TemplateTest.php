183b7aea170fc6c65fcd71231935cba8dc46a72d
GlazerMann <kurt@ioiowa.com>
2020-01-13 12:27:49 -0600

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -16,7 +16,23 @@ public function testvalidate_and_add() {
      $text = "{{cite web}}";
      $template = $this->make_citation($text);
      $template->validate_and_add('author1', 'George @Hashtags Billy@hotmail.com', 'Sam @Hashtags Billy@hotmail.com', '', FALSE);
-     $this->assertSame('', $template->parsed_text());
+     echo $template->parsed_text();
+
+     $text = "{{cite web}}";
+     $template = $this->make_citation($text);
+     $template->validate_and_add('author1', 'George @Hashtags', '', '', FALSE);
+     echo $template->parsed_text();
+
+     $text = "{{cite web}}";
+     $template = $this->make_citation($text);
+     $template->validate_and_add('author1', 'George Billy@hotmail.com', 'Sam @Hashtag', '', FALSE);
+     echo $template->parsed_text();
+
+     $text = "{{cite web}}";
+     $template = $this->make_citation($text);
+     $template->validate_and_add('author1', 'com', 'Sam', '', FALSE);
+     echo $template->parsed_text();
+
    }
 
   
