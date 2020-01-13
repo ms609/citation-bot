8d83e227d5e194073ffb7aae3c2d17893cc1e74f
GlazerMann <kurt@ioiowa.com>
2020-01-13 12:17:49 -0600

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -12,6 +12,13 @@ final class TemplateTest extends testBaseClass {
     $this->assertSame('MERGE', 'NO');
   }
 
+public function testvalidate_and_add() {
+     $text = "{{cite web}}";
+     $template = $this->make_citation($text);
+     $template->validate_and_add('author1', 'George @Hashtags Billy@hotmail.com', 'Sam @Hashtags Billy@hotmail.com', '', FALSE);
+     $this->assertSame('', $template->parsed_text());
+   }
+
   
  
 }
