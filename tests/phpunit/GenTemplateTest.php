65f38ecfdb4b8f9660bd324c83ca301b6e337fef
GlazerMann <kurt@ioiowa.com>
2020-01-16 19:03:35 -0600

    Update GenTemplateTest.php

--- tests/phpunit/GenTemplateTest.php
+++ tests/phpunit/GenTemplateTest.php
@@ -23,6 +23,7 @@ final class GenTemplateTest extends testBaseClass {
       while (ob_get_level()) { ob_end_flush(); };
       ob_start(); // PHPUnit turns on a level of buffering itself -- Give it back to avoid "Risky Test"
       // Output checking time
-      $this->assertTrue(preg_match("~^.*DOC.*pre.*ref.*\{\{.*10.2307/373737.*\}\}.*ref.*pre.*$~", $template_text));
+echo $template_text;
+      $this->assertTrue(preg_match("~^.*DOC.*pre.*ref.*\{\{.*10.2307.*\}\}.*ref.*pre.*$~", $template_text));
   }
 }
