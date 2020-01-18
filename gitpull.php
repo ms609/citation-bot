ddf14df54e399839e532abff047d1c79c653862a
GlazerMann <kurt@ioiowa.com>
2020-01-18 06:34:52 -0600

    lock during GIT updates

--- gitpull.php
+++ gitpull.php
@@ -2,7 +2,7 @@
 echo "<!DOCTYPE html><html><body><pre>";
 if (mkdir('git_pull.lock', 0700)) {
   echo shell_exec ("/usr/bin/git fetch  2>&1"); // This only updates .git, so it is very safe. This is first half of pull
-  echo shell_exec ("/usr/bin/git checkout constants/capitalization.php 2>&1");
+  @sleep(3);
   echo shell_exec ("/usr/bin/git pull  2>&1");
   rmdir('git_pull.lock') ;
 } else {
