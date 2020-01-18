aa97d62be2c4ef98fdc8adde3a4929b938d2dcd0
GlazerMann <kurt@ioiowa.com>
2020-01-18 07:18:28 -0600

    Update unlock_git.php

--- unlock_git.php
+++ unlock_git.php
@@ -1,19 +1,19 @@
 <?php
 echo "<!DOCTYPE html><html><body><pre>\n";
 // Make the person wait.  I know how I can be.
-@echo "Wait 6</p>\n";
-@sleep(1);
-@echo "Wait 5</p>\n";
-@sleep(1);
-@echo "Wait 4</p>\n";
-@sleep(1);
-@echo "Wait 3</p>\n";
-@sleep(1);
-@echo "Wait 2</p>\n";
-@sleep(1);
-@echo "Wait 1</p>\n";
-@sleep(1);
-@rmdir('git_pull.lock') ;
-@echo "Script done</p>\n";
-@echo "</pre></body></html>\n"
+echo "Wait 6</p>\n";
+sleep(1);
+echo "Wait 5</p>\n";
+sleep(1);
+echo "Wait 4</p>\n";
+sleep(1);
+echo "Wait 3</p>\n";
+sleep(1);
+echo "Wait 2</p>\n";
+sleep(1);
+echo "Wait 1</p>\n";
+sleep(1);
+rmdir('git_pull.lock') ;
+echo "Script done</p>\n";
+echo "</pre></body></html>\n"
 ?>
