0f35f51db6147ba395dd98dd0bed3e88fdea8242
GlazerMann <kurt@ioiowa.com>
2020-01-19 15:02:42 -0600

    more code coverage tests

--- tests/phpunit/zoteroTest.php
+++ tests/phpunit/zoteroTest.php
@@ -633,5 +633,22 @@ class ZoteroTest extends testBaseClass {
     $this->assertSame('Billy', $template->get('title'));
     $this->assertSame('Joe', $template->get('author1'));
   }
+ 
+   public function testZoteroResponse30() {
+    $text = '{{cite web|id=}}';
+    $template = $this->make_citation($text);
+    $access_date = FALSE;
+    $url = '';
+    $url_kind = NULL;
+    $author = array(0 => 'Smith', 1 => 'Johnson', 2 => 'Jackson');
+    $author[0] = $author;
+    $zotero_data[0] = (object) array('title' => 'Billy', 'itemType' => 'webpage', 'author' => $author);
+    $zotero_response = json_encode($zotero_data);
+    $this->assertTrue(process_zotero_response($zotero_response, $template, $url, $url_kind, $access_date));
+    $this->assertSame('Billy', $template->get('title'));
+    $this->assertSame('Smith', $template->get('author1'));
+    $this->assertSame('Johnson', $template->get('author2'));
+    $this->assertSame('Jackson', $template->get('author3'));
+  }
 
 }
