8f3faaea17a67e2f0055985a963b9e117f2eec37
GlazerMann <kurt@ioiowa.com>
2020-01-19 15:14:03 -0600

    Update testBaseClass.php

--- tests/testBaseClass.php
+++ tests/testBaseClass.php
@@ -41,7 +41,7 @@ abstract class testBaseClass extends PHPUnit\Framework\TestCase {
   }
 
   protected function requires_zotero($function) {
-    $skip_zotero = FALSE; // Set to TRUE to commit skipping to GIT.  FALSE to no skip.  Something else to skip tests while debugging
+    $skip_zotero = 'testing' ; // Set to TRUE to commit skipping to GIT.  FALSE to no skip.  Something else to skip tests while debugging
     if ($skip_zotero !== FALSE && $skip_zotero !== TRUE) {
       $this->assertNull('skip_zotero is not set right in requires_zotero()');
     }
