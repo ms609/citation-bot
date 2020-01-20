ac16903f1788b05af5f7d36ecd650f35495ea9a2
GlazerMann <kurt@ioiowa.com>
2020-01-19 18:49:28 -0600

    Update zoteroTest.php

--- tests/phpunit/zoteroTest.php
+++ tests/phpunit/zoteroTest.php
@@ -655,6 +655,6 @@ class ZoteroTest extends testBaseClass {
     $text = '{{cite journal|doi=10.1021/acs.analchem.8b04567|url=http://shortdoi.org/gf7sqt|pmid=30741529|pmc=6526953|title=ISiCLE: A Quantum Chemistry Pipeline for Establishing in Silico Collision Cross Section Libraries|journal=Analytical Chemistry|volume=91|issue=7|pages=4346–4356|year=2019|last1=Colby|first1=Sean M.|last2=Thomas|first2=Dennis G.|last3=Nuñez|first3=Jamie R.|last4=Baxter|first4=Douglas J.|last5=Glaesemann|first5=Kurt R.|last6=Brown|first6=Joseph M.|last7=Pirrung|first7=Meg A.|last8=Govind|first8=Niranjan|last9=Teeguarden|first9=Justin G.|last10=Metz|first10=Thomas O.|last11=Renslow|first11=Ryan S.}}';
     $template = $this->make_citation($text);
     query_url_api(array('http://shortdoi.org/gf7sqt'), array($template));
-    $this->assertNull($expanded->get('url'));
+    $this->assertNull($template->get('url'));
   }
 }
