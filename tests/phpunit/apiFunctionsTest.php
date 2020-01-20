bc5d20a132f299864e035e3a2bb9f1ce8506584d
GlazerMann <kurt@ioiowa.com>
2020-01-20 09:33:52 -0600

    Update apiFunctionsTest.php

--- tests/phpunit/apiFunctionsTest.php
+++ tests/phpunit/apiFunctionsTest.php
@@ -11,14 +11,20 @@ final class apiFunctionsTest extends testBaseClass {
    public function testJstor() {
      $text = "{{cite journal|url=https://jstor.org/stable/832414?seq=1234}}";
      $template = $this->make_citation($text);
-     expand_by_jstor($template);
-     $this->assertSame('832414', $template->get('jstor'));
-  }
-  
-   public function testJstor2() {
+     $this->assertTrue(expand_by_jstor($template));
+     $this->assertNull($template->get('jstor')); // We don't do that here
+
      $text = "{{cite journal|jstor=832414?seq=1234}}";
      $template = $this->make_citation($text);
-     expand_by_jstor($template);
+     $this->assertTrue(expand_by_jstor($template));
      $this->assertNull($template->get('url'));
+
+     $text = "{{cite journal|jstor=123 123}}";
+     $template = $this->make_citation($text);
+     $this->assertFalse(expand_by_jstor($template));
+
+     $text = "{{cite journal|jstor=i832414}}";
+     $template = $this->make_citation($text);
+     $this->assertFalse(expand_by_jstor($template));
   }
 }
