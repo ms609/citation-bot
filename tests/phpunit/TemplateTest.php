a8f8f94fe01bf91a0ff1e96fa2b1edeb5d00401f
GlazerMann <kurt@ioiowa.com>
2020-01-21 19:29:06 -0600

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -11,9 +11,9 @@ final class TemplateTest extends testBaseClass {
    public function testIDconvert8() {
      $text='{{Cite journal | id = {{ASIN|0226845494|country=eu}} }}';
      $template = $this->process_citation($text);
-     echo "\n" . template->parsed_text() . "\n";
+     echo "\n" . $template->parsed_text() . "\n";
      $template = $this->make_citation($template->parsed_text()); // Turn sub-templates into text
-     echo "\n" . template->parsed_text() . "\n";
+     echo "\n" . $template->parsed_text() . "\n";
      $this->assertSame('{{ASIN|0226845494|country=eu}}', $template->get('id'));
      $this->assertSame('0226845494', $template->get('isbn'));
    }
@@ -21,9 +21,9 @@ final class TemplateTest extends testBaseClass {
    public function testIDconvert9() {
      $text = '{{Cite journal | id = {{howdy|0226845494}} }}';
      $template = $this->process_citation($text);
-     echo "\n" . template->parsed_text() . "\n";
+     echo "\n" . $template->parsed_text() . "\n";
      $template = $this->make_citation($template->parsed_text()); // Turn sub-templates into text
-     echo "\n" . template->parsed_text() . "\n";
+     echo "\n" . $template->parsed_text() . "\n";
      $this->assertSame('{{howdy|0226845494}}', $template->get('id'));
      $this->assertSame('0226845494', $template->get('isbn'));
     }
