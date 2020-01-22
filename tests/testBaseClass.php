a20944550e55c730b8a0fea038fa615d833c9f47
GlazerMann <kurt@ioiowa.com>
2020-01-22 10:32:58 -0600

    secrets

--- tests/testBaseClass.php
+++ tests/testBaseClass.php
@@ -33,16 +33,11 @@ abstract class testBaseClass extends PHPUnit\Framework\TestCase {
     }
   }
 
-  protected function wastes_secrets($function) {
+  protected function bibcode_secrets($function) {
     global $BLOCK_BIBCODE_SEARCH;
-    if (FALSE && getenv('TRAVIS_PULL_REQUEST') && (getenv('TRAVIS_PULL_REQUEST') !== 'false' )) {
-      echo 'W'; // Skipping test: uses up a security key up
-      $this->assertNull(NULL); // Make Travis think we tested something
-    } else {
-      $BLOCK_BIBCODE_SEARCH = FALSE;
-      $function();
-      $BLOCK_BIBCODE_SEARCH = TRUE;
-    }
+    $BLOCK_BIBCODE_SEARCH = FALSE;
+    $function();
+    $BLOCK_BIBCODE_SEARCH = TRUE;
   }
 
   protected function requires_zotero($function) {
