24e2838671d75faf5b58678fc0d2c5a93deb9dfc
GlazerMann <kurt@ioiowa.com>
2020-01-22 21:28:48 -0600

    secrets bibcodes (#2467)

--- tests/testBaseClass.php
+++ tests/testBaseClass.php
@@ -26,18 +26,19 @@ abstract class testBaseClass extends PHPUnit\Framework\TestCase {
 
   protected function requires_secrets($function) {
     if (!getenv('PHP_OAUTH_CONSUMER_TOKEN')) {
-      echo 'S'; // Skipping test: Risks exposing secret keys
-      $this->assertNull(NULL); // Make Travis think we tested something
+      echo 'S';
+      $this->assertNull(NULL);
     } else {
       $function();
     }
   }
 
-  protected function wastes_secrets($function) {
+  // Only routines that absolutely need bibcode access since we are limited 
+  protected function bibcode_secrets($function) {
     global $BLOCK_BIBCODE_SEARCH;
-    if (FALSE && getenv('TRAVIS_PULL_REQUEST') && (getenv('TRAVIS_PULL_REQUEST') !== 'false' )) {
-      echo 'W'; // Skipping test: uses up a security key up
-      $this->assertNull(NULL); // Make Travis think we tested something
+    if (!getenv('PHP_ADSABSAPIKEY')) {
+      echo 'B';
+      $this->assertNull(NULL);
     } else {
       $BLOCK_BIBCODE_SEARCH = FALSE;
       $function();
@@ -45,19 +46,19 @@ abstract class testBaseClass extends PHPUnit\Framework\TestCase {
     }
   }
 
+  // allows us to turn off zoreto tests
   protected function requires_zotero($function) {
-    $skip_zotero = FALSE; // Set to TRUE to commit skipping to GIT.  FALSE to no skip.  Something else to skip tests while debugging
+    $skip_zotero = TRUE; // TODO Set to TRUE to commit skipping to GIT.  FALSE to not skip.  Something else to skip tests while debugging
     if ($skip_zotero !== FALSE && $skip_zotero !== TRUE) {
-      $this->assertNull('skip_zotero is not set right in requires_zotero()');
+      $this->assertNull('skip_zotero');
     }
-    if ($skip_zotero && getenv('TRAVIS_PULL_REQUEST') && (getenv('TRAVIS_PULL_REQUEST') !== 'false' )) {
-      echo 'Z'; // Skipping test: Zoteros is rubbish right now
-      $this->assertNull(NULL); // Make Travis think we tested something
+    if ($skip_zotero && getenv('TRAVIS_PULL_REQUEST') && (getenv('TRAVIS_PULL_REQUEST') !== 'false' )) { // Main build NEVER skips anything
+      echo 'Z';
+      $this->assertNull(NULL);
     } else {
       $function();
     }
-  }
-  
+  } 
   
   protected function make_citation($text) {
     $this->assertSame('{{', mb_substr($text, 0, 2));
