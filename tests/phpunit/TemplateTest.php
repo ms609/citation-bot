dc9b7181e551119319109d36447deb67f190c284
GlazerMann <kurt@ioiowa.com>
2020-01-24 16:23:46 -0600

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -889,7 +889,6 @@ final class TemplateTest extends testBaseClass {
       $this->assertSame('Houk', $expanded->get('last1'));
       $this->assertSame('N.', $expanded->get('first1'));
       $this->assertNotNull($expanded->get('title'));
-      $this->assertFalse($expanded->expand_by_adsabs()); // Repeat should cause false
     });
   }
  
