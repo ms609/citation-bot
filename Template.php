cb65d4f88cbf718aa68db80a7c7d396d24b5f268
GlazerMann <kurt@ioiowa.com>
2020-01-27 09:37:21 -0600

    clean up book url detection code

--- Template.php
+++ Template.php
@@ -2738,17 +2738,21 @@ final class Template {
       }
     }
     
-    foreach ($this->param as $param_key => $p) {
-      if (!empty($p->param)) {
-        if ($this->blank('url') && preg_match('~^\s*(https?://|www\.)\S+~', $p->param)) { # URL ending ~ xxx.com/?para=val
-          $this->param[$param_key]->val = $p->param . '=' . $p->val;
-          $this->param[$param_key]->param = 'url';
-          if (stripos($p->val, 'books.google.') !== FALSE) {
-            $this->change_name_to('cite book');
+    if ($this->blank('url')) {
+      foreach ($this->param as $param_key => $p) {
+        if (!empty($p->param) && $this->blank('url')) { // Check url again to avoid doing more than once!
+          if (preg_match('~^\s*(https?://|www\.)\S+~', $p->param)) { # URL ending ~ xxx.com/?para=val
+            $this->param[$param_key]->val = $p->param . '=' . $p->val;
+            $this->param[$param_key]->param = 'url';
+            if (stripos($p->val, 'books.google.') !== FALSE) {
+              $this->change_name_to('cite book');
+            }
           }
         }
-        continue;
       }
+    }
+    foreach ($this->param as $param_key => $p) {
+      if (empty($p->param)) continue;
       $dat = $p->val;
       $endnote_test = explode("\n%", "\n" . $dat);
       if (isset($endnote_test[1])) {
