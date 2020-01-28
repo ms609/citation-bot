a6c7faedb281acf07d472e29e0acdf09af7cc8c6
GlazerMann <kurt@ioiowa.com>
2020-01-27 19:23:17 -0600

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -937,7 +937,7 @@ final class TemplateTest extends testBaseClass {
   public function testBibcodesBooks() {
     $this->bibcode_secrets(function() {
       $text = "{{Cite book|bibcode=1982mcts.book.....H}}";
-      $this->assertTrue($expanded->expand_by_adsabs());
+      $expanded = $this->process_citation($text);
       $this->assertSame('1982', $expanded->get('year'));
       $this->assertSame('Houk', $expanded->get('last1'));
       $this->assertSame('N.', $expanded->get('first1'));
