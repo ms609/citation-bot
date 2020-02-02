91c8904f34e7d2544c3de52bbb4c80eecfaa1165
GlazerMann <kurt@ioiowa.com>
2020-02-01 18:52:52 -0600

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -4338,17 +4338,17 @@ T1 - This is the Title }}';
      $text = '{{cite journal|doi=1111/j.1471-0528.1995.tb09132.x}}';
      $template = $this->make_citation($text);
      $template->verify_doi();
-     $this->assertSame('10.111/j.1471-0528.1995.tb09132.x', $template->get('doi'));
+     $this->assertSame('10.1111/j.1471-0528.1995.tb09132.x', $template->get('doi'));
    
      $text = '{{cite journal|doi=10.1111/j.1471-0528.1995.tb09132.}}';
      $template = $this->make_citation($text);
      $template->verify_doi();
-     $this->assertSame('10.111/j.1471-0528.1995.tb09132.x', $template->get('doi'));
+     $this->assertSame('10.1111/j.1471-0528.1995.tb09132.x', $template->get('doi'));
    
      $text = '{{cite journal|doi=10.1111/j.1471-0528.1995.tb09132.x;}}';
      $template = $this->make_citation($text);
      $template->verify_doi();
-     $this->assertSame('10.111/j.1471-0528.1995.tb09132.x', $template->get('doi'));
+     $this->assertSame('10.1111/j.1471-0528.1995.tb09132.x', $template->get('doi'));
    
      $text = '{{cite journal|doi=10.1175/1525-7541(2003)004&gt;1147:TVGPCP&lt;2.0.CO;2}}';
      $template = $this->make_citation($text);
