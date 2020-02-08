90482941b1379db93ca383793f969a0fc8437e15
GlazerMann <kurt@ioiowa.com>
2020-02-08 07:59:24 -0600

    Do not add url if title=none

--- Template.php
+++ Template.php
@@ -859,6 +859,7 @@ final class Template {
         // look for identifiers in URL - might be better to add a PMC parameter, say
         if ($this->get_identifiers_from_url($value)) return FALSE;
         if (!$this->blank(array_merge([$param_name], TITLE_LINK_ALIASES))) return FALSE;
+        if ($this->get('title') === 'none') return FALSE;
         $value = sanitize_string($value);
         foreach (['url', 'article-url', 'chapter-url', 'chapterurl', 'conference-url', 'conferenceurl',
                   'contribution-url', 'contributionurl', 'entry-url', 'event-url', 'eventurl', 'lay-url',
