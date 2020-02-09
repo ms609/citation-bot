67f05f981fc9d43b71d4625094fbc1e5bc06cb94
GlazerMann <kurt@ioiowa.com>
2020-02-09 10:54:12 -0600

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -13,10 +13,9 @@ final class PageTest extends testBaseClass {
   }
  
   public function testNobots2() {
-      $text = '{{cite thesis|url=https://mathscinet.ams.org/mathscinet-getitem?mr=1234}}{{bots|allow=not_you}}';
-      $page = $this->process_page($text);
+      $text = '{{cite journal|url=https://search.proquest.com/docview/12341234|id=CITATION_BOT_PLACEHOLDER_COMMENT}}';
+      $page = $this->process_citation($text);
       $this->assertSame($text, $page->parsed_text());
-      $this->assertSame(FALSE, $page->write(NULL, NULL));
   }
 
 }
