1f82d8889ade1e7a99c81efcfb153504aad5abf8
GlazerMann <kurt@ioiowa.com>
2020-02-10 09:11:38 -0600

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -12,7 +12,8 @@ final class PageTest extends testBaseClass {
  
    public function testGetDOIWithBadLastPageAndAuthor() {
       $text = '{{cite journal|title=ISiCLE: A Quantum Chemistry Pipeline for Establishing in Silico Collision Cross Section Libraries|journal=Analytical Chemistry|volume=91|issue=7|pages=4346–99999 |year=2019|last1=WRONG|pmc=<!-- nope -->|pmid=<!-- nope -->|doi=10.1021/acs.analchem.8b04567}}';
-      $template = $this->process_citation($text);
+      $template = $this->make_citation($text);
+      $template->get_doi_from_crossref();
       $this->assertSame('10.1021/acs.analchem.8b04567', $template->get('doi'));
   }
 
