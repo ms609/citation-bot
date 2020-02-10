67ea3886acb04c0b351f056135dc583f7b65f624
GlazerMann <kurt@ioiowa.com>
2020-02-10 16:18:58 -0600

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -1020,7 +1020,7 @@ final class TemplateTest extends testBaseClass {
     $this->assertSame('10393/35779', $expanded->get('hdl'));
   }
  
-  public function testOpenAccessLookup5() {  
+  public function testOpenAccessLookup6() {  
     $text = '{{Cite journal | doi = 10.5260/chara.18.3.53|hdl=10393/35779}}'; 
     $expanded = $this->process_citation($text);
     $this->assertSame('10393/35779', $expanded->get('hdl')); // This basically runs through a bunch of code to return 'have free'
