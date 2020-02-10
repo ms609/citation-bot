2e4d9777a97a0a0870feb0d8541045ca7e1466eb
GlazerMann <kurt@ioiowa.com>
2020-02-10 10:45:34 -0600

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -11,7 +11,7 @@ final class PageTest extends testBaseClass {
 
  
    public function testGetPMIDwitNoDOIorJournal() {
-      $text = '{{cite journal|title=ISiCLE: A Quantum Chemistry Pipeline for Establishing in Silico Collision Cross Section Libraries|volume=91|issue=7|pages=4346 |year=2019|last1=Colby}}';
+      $text = '{{cite journal|title=ISiCLE: A Quantum Chemistry Pipeline for Establishing in Silico Collision Cross Section Libraries|volume=91|issue=7|pages=4346 |year=2019|author1=Colby}}';
       $template = $this->make_citation($text);
       $template->find_pmid();
       $this->assertSame('30741529', $template->get('pmid'));
