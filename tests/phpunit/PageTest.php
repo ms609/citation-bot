c68bcf89cd1acd66953dc6c9396c2ae64d1d00bc
GlazerMann <kurt@ioiowa.com>
2020-02-10 10:10:28 -0600

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -11,12 +11,12 @@ final class PageTest extends testBaseClass {
 
  
    public function testGetDOIWithBadLastPageAndAuthor() {
-      $text = '{{cite journal|title=ISiCLE: A Quantum Chemistry Pipeline for Establishing in Silico Collision Cross Section Libraries|journal=Analytical Chemistry|volume=91|issue=7|pages=4346–99999 |year=2019|last1=WRONG}}';
+      $text = '{{cite journal|title=ISiCLE: A Quantum Chemistry Pipeline for Establishing in Silico Collision Cross Section Libraries|journal=Analytical Chemistry|volume=91|pages=4346–99999999 |year=2019|last1=WRONG OH SO WRONG WANG SMITH JONES}}';
       $template = $this->make_citation($text);
       $template->get_doi_from_crossref();
       $this->assertSame('10.1021/acs.analchem.8b04567', $template->get('doi'));
-      $this->assertNull(FALSE, $template->get('pmc'));
-      $this->assertNull(FALSE, $template->get('pmid'));
+      $this->assertNull($template->get('pmc'));
+      $this->assertNull($template->get('pmid'));
   }
 
   public function testBadPage() {  // Use this when debugging pages that crash the bot
