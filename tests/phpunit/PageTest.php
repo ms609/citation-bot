0bdee88844e37e3e15ca416f9035e95067d544c8
GlazerMann <kurt@ioiowa.com>
2020-02-13 18:30:20 -0600

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -11,8 +11,8 @@ final class PageTest extends testBaseClass {
 
   public function testBadPage() {  // Use this when debugging pages that crash the bot
     // This MUST be escaped page name-underscores not spaces and such
-    $bad_page = ""; //  Replace with something like "Vietnam_War" when debugging
-    if ($bad_page !== "Alan_Turing") {
+    $bad_page = "Alan_Turing"; //  Replace with something like "Vietnam_War" when debugging
+    if ($bad_page !== "") {
       $text = file_get_contents('https://en.wikipedia.org/w/index.php?title=' . $bad_page . '&action=raw');
       $page = new TestPage();
       $page->parse_text($text);
