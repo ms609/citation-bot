adda41542f532664171356b6743fec7fa3b3ed69
GlazerMann <kurt@ioiowa.com>
2020-02-14 15:33:14 -0600

    code coverage I hope

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -261,8 +261,8 @@ final class TemplateTest extends testBaseClass {
     $this->assertSame('1941451', $expanded->get('pmid'));
   }
  
-  public function testGetPMIDwitNoDOIorJournal() {  // Also has evil colon in the name
-      $text = '{{cite journal|title=ISiCLE: A Quantum Chemistry Pipeline for Establishing in Silico Collision Cross Section Libraries|volume=91|issue=7|pages=4346 |year=2019|last1=Colby}}';
+  public function testGetPMIDwitNoDOIorJournal() {  // Also has evil colon in the name.  Use wikilinks for code coverage reason
+      $text = '{{cite journal|title=ISiCLE: A Quantum Chemistry Pipeline for Establishing in Silico Collision Cross Section Libraries|volume=[[91]]|issue=[[7|7]]|pages=4346|year=2019|last1=Colby}}';
       $template = $this->make_citation($text);
       $template->find_pmid();
       $this->assertSame('30741529', $template->get('pmid'));
