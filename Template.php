67ce6eb0d70f809db81e026c6250052159c3f604
GlazerMann <kurt@ioiowa.com>
2020-02-13 20:53:30 -0600

    only first page counts

--- Template.php
+++ Template.php
@@ -1848,9 +1848,7 @@ final class Template {
         $data = $this->get_without_comments_and_placeholders('title');
         $data = str_replace([';', ',', ':', '.', '?', '!', '&', '/', '(', ')', '[', ']', '{', '}', '"', "'", '|', '\\'],
                             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], $data);
-        $data = strtr(utf8_decode($data), 
-           'ŠŒŽšœžŸ¥µÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýÿ',
-           'SOZsozYYuAAAAAAACEEEEIIIIDNOOOOOOUUUUYsaaaaaaaceeeeiiiionoooooouuuuyy');
+        $data = strip_diacritics($data);
         $data_array = explode(" ", $data);
         foreach ($data_array as $val) {
           if (!in_array(strtolower($val), array('the', 'and', 'a', 'for', 'in', 'on', 's', 're', 't',
@@ -1863,14 +1861,13 @@ final class Template {
       } else {
         $key = $key_index[mb_strtolower($term)];
         if ($key && $term && $val = $this->get_without_comments_and_placeholders($term)) {
+          if ($key = 'Pagination') $val = $this->page_range()[1];
           if (preg_match(REGEXP_PLAIN_WIKILINK, $val, $matches)) {
               $val = $matches[1];
           } elseif (preg_match(REGEXP_PIPED_WIKILINK, $val, $matches)) {
               $val = $matches[2];
           }
-          $val = strtr(utf8_decode($val), 
-            'ŠŒŽšœžŸ¥µÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýÿ',
-            'SOZsozYYuAAAAAAACEEEEIIIIDNOOOOOOUUUUYsaaaaaaaceeeeiiiionoooooouuuuyy');
+          $val = strip_diacritics($val);
           if ($key === "AID") {
              $query .= " AND (" . "\"" . str_replace(array("%E2%80%93", ';'), array("-", '%3B'), $val) . "\"" . "[$key])"; // PMID does not like escaped /s in DOIs, but other characters seem problematic.
           } else {
