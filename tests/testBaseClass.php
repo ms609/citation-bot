d2bcf1ba0c3f04b2f41a5a9b2b0e37a54f63952c
GlazerMann <kurt@ioiowa.com>
2020-02-15 14:07:12 -0600

    fix bug (#2657)

--- tests/testBaseClass.php
+++ tests/testBaseClass.php
@@ -7,9 +7,9 @@ if (!defined('VERBOSE')) define('VERBOSE', TRUE);
 
 // Change these to temporarily disable sets of tests======================
 $testing_skip_zotero = TRUE; // TODO                                    //
-$testing_skip_bibcode= FALSE;                                           //
-$testing_skip_google = FALSE;                                           //
-$testing_skip_wiki   = FALSE;                                           //
+$testing_skip_bibcode= TRUE; // TODO                                    //
+$testing_skip_google = TRUE; // TODO                                    //
+$testing_skip_wiki   = TRUE; // TODO                                    //
 $testing_skip_dx     = TRUE; // TODO                                    //
 // =======================================================================
 
