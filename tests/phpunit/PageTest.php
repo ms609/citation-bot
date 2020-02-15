e50950437d9b29d59e5f02e089416d8043c9fed5
GlazerMann <kurt@ioiowa.com>
2020-02-15 07:40:18 -0600

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -10,9 +10,9 @@ final class PageTest extends testBaseClass {
 
  
    public function testNobots3() {
-      $text = '{{cite thesis}}';
-      $page = $this->process_citation($text);
-      $this->assertSame('{{cite thesis|mr = 1234}}{{bots|allow=Citation Bot}}', $page->parsed_text());
+      $text = '{{cite web |url=https://doi.org/10.1093/gmo/9781561592630.article.J441700 |title=Tatum, Art(hur, Jr.) (jazz) |last=Howlett |first=Felicity | website=Grove Music Online |publisher=Oxford University Press |date=2002 |access-date=November 20, 2018 |url-access=subscription|via=Grove Music Online}}';
+      $template = $this->process_citation($text);
+      $this->assertSame('What?', $template->parsed_text());
   }
  
   
