c076be0d2a05f18111694b264be1991dc5e9518a
GlazerMann <kurt@ioiowa.com>
2020-02-17 17:25:55 -0600

    Update zoteroTest.php

--- tests/phpunit/zoteroTest.php
+++ tests/phpunit/zoteroTest.php
@@ -102,7 +102,7 @@ class ZoteroTest extends testBaseClass {
  
   public function testIEEEdoi() {
     $url = "https://ieeexplore.ieee.org/document/42423" . (string) rand(10,99);  // Mix it up to avoid blocks
-    $template->process_citation('{{cite journal | url = ' . $url . ' }}');
+    $template = $this->process_citation('{{cite journal | url = ' . $url . ' }}');
     $this->assertNotNull($template->get('doi'));
   }
 
