85d894cd3ba75d793d78e679155ec1c4fa4b18d9
GlazerMann <kurt@ioiowa.com>
2020-02-29 16:03:41 -0600

    Existing IEEE no longer works - dropping URL

--- zotero.php
+++ zotero.php
@@ -103,6 +103,15 @@ function query_ieee_webpages($templates) {
           }
        }
     }
+    if ($template->has('doi') && preg_match("~^https://ieeexplore\.ieee\.org/document/(\d{5,})$~", $template->get($kind), $matches_url) && doi_works($template->get('doi'))) {
+       usleep(100000); // 0.10 seconds
+       curl_setopt($ch_ieee, CURLOPT_URL, $template->get($kind));
+       $return = @curl_exec($ch_ieee);
+       if ($return && strpos("'<title> -  </title>'", $return) !== FALSE) {
+         report_forget("Existing IEEE no longer works - dropping URL");
+         $template->forget($kind);
+       }
+    }
    }
   }
   curl_close($ch_ieee);
