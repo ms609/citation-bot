4586161204b491252fa0e3c9ca21ad38c6df8b05
GlazerMann <kurt@ioiowa.com>
2020-03-29 09:10:54 -0500

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -10,9 +10,9 @@ final class TemplateTest extends testBaseClass {
  
 
   public function testParameterWithNoParameters() {
-    $text = "{{Cite web | text without equals sign  }}";
+    $text = "{{cite book |last1=Baldwin |first1=AL |chapter=Mast cell activation by stress |title=Methods in Molecular Biology |date=2006 |volume=315 |pages=349–60 |doi=10.1385/1-59259-967-2:349 |pmid=16110169 |isbn=1-59259-967-2 }}";
     $expanded = $this->process_citation($text);
-    $this->assertSame($text, $expanded->parsed_text());
+    $this->assertNull($expanded->parsed_text());
   }
 
  
