0d47a9e76a96c24a5f44ae984706d65b39d15084
GlazerMann <kurt@ioiowa.com>
2020-04-05 06:33:09 -0500

    COINS data now works for full title links

--- Template.php
+++ Template.php
@@ -3901,11 +3901,13 @@ final class Template {
              if (preg_match(REGEXP_PLAIN_WIKILINK, $title, $matches)) {
                $title = str_replace(array("[[", "]]"), "", $title);
                if (strlen($matches[1]) > (0.6 * strlen($title))) {  // Only add as title-link if a large part of title text
-                 $this->add_if_new('title-link', $matches[1]);
+                 // $this->add_if_new('title-link', $matches[1]);
+                 $title = '[[' . $title . ']]';
                }
              } elseif (preg_match(REGEXP_PIPED_WIKILINK, $title, $matches)) {
-               $this->add_if_new('title-link', $matches[1]);
-               $title = preg_replace(REGEXP_PIPED_WIKILINK, "$2", $title);
+            // Do not do anymore.  COINS data now works for full title links
+            //   $this->add_if_new('title-link', $matches[1]);
+            //   $title = preg_replace(REGEXP_PIPED_WIKILINK, "$2", $title);
              }
           }
           $this->set($param, $title);
