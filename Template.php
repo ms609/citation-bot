89f60cee8ef10255447473b3f44d4a90b3df4c5f
GlazerMann <kurt@ioiowa.com>
2020-04-13 08:10:21 -0500

    Better google books shortener code (#2814)

--- Template.php
+++ Template.php
@@ -2718,6 +2718,7 @@ final class Template {
     }
     // Now we parse a Google Books URL
     if ($url && preg_match("~books\.google\.[\w\.]+/.*\bid=([\w\d\-]+)~", $url, $gid)) {
+      $orig_book_url = $url;
       $removed_redundant = 0;
       $hash = '';
       $removed_parts ='';
@@ -2752,11 +2753,20 @@ final class Template {
         }
       }
       if (strpos($hash, 'v=onepage') !== FALSE) {
+        if (!str_i_same($hash, '#v=onepage')) {
+          $removed_redundant++;
+          $removed_parts .= substr(str_ireplace('v=onepage', '', $hash), 1);
+        }
         $hash = '#v=onepage';
       }
-      if ($removed_redundant > 1) { // http:// is counted as 1 parameter
-        report_forget(echoable($removed_parts));
-        $this->set($url_type, $url . $hash);
+      $url = $url . $hash;
+      if ($url != $orig_book_url) {
+        if ($removed_redundant > 1) { // http:// is counted as 1 parameter
+          report_forget(echoable($removed_parts));
+        } else {
+          report_forget('Simplified Google Books URL');
+        }
+        $this->set($url_type, $url);
       }
       $this->google_book_details($gid[1]);
       return TRUE;
