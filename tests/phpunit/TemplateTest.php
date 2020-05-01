ce942eb00230fa4c8ea0beaabbc7a83fc30d26ad
GlazerMann <kurt@ioiowa.com>
2020-05-01 15:08:04 -0500

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -16,7 +16,7 @@ final class TemplateTest extends testBaseClass {
   }
 
   public function testWierdNONBookMetaData() {
-    $text = "{{Cite journal|doi=10.5061/dryad.cf493|url=http://datadryad.org/resource/doi:10.5061/dryad.cf493|hdl=10255/dryad.92916 |last1=Smith|first1=Martin R.|title=Data from: A palaeoscolecid worm from the Burgess Shale. Dryad Digital Repository|year=2015}}
+    $text = "{{Cite journal|doi=10.5061/dryad.cf493|url=http://datadryad.org/resource/doi:10.5061/dryad.cf493|hdl=10255/dryad.92916 |last1=Smith|first1=Martin R.|title=Data from: A palaeoscolecid worm from the Burgess Shale. Dryad Digital Repository|year=2015}}";
     $expanded = $this->process_citation($text);
     $this->assertNull$expanded->parsed_text());  
   }
