8ea59175eaff2914e3ff5288d09ba58f9cb73b87
GlazerMann <kurt@ioiowa.com>
2020-05-03 10:10:59 -0500

    cite templates now work right

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -2609,11 +2609,10 @@ T1 - This is the Title }}';
     $this->assertNull($template->get('url'));
   }
  
-  public function testHandles5() { // Do not shorten when already hdl
+  public function testHandles5() {
     $template = $this->make_citation('{{Cite journal|url=http://hdl.handle.net/2027/loc.ark:/13960/t6349vh5n?urlappend=%3Bseq=672}}');
     $template->get_identifiers_from_url();
-    return; // TODO-HDL parameter does not work right-it encodes the question mark
-    $this->assertSame('2027/loc.ark:/13960/t6349vh5n?urlappend=;seq=672', $template->get('hdl'));
+    $this->assertSame('2027/loc.ark:/13960/t6349vh5n?urlappend=%3Bseq=672', $template->get('hdl'));
     $this->assertNull($template->get('url'));
   }
  
