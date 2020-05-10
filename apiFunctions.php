3c0b786bcb921ceed5f355782bb1c1fed418d072
GlazerMann <kurt@ioiowa.com>
2020-05-10 14:48:53 -0500

    Update apiFunctions.php

--- apiFunctions.php
+++ apiFunctions.php
@@ -703,7 +703,13 @@ function is_doi_active($doi) {
 
 function is_doi_works($doi) {
   if (strpos($doi, '10.1111/j.1572-0241') === 0 ) return FALSE; // Nature dropped the ball for now TODO - 10.1111/j.1572-0241.2006.00844.x
-  $headers_test = @get_headers("https://dx.doi.org/" . urlencode($doi), 1);
+  $context = stream_context_create(
+    'ssl' => [
+        'verify_peer' => FALSE,
+        'verify_peer_name' => FALSE,
+        'allow_self_signed' => TRUE
+    ]); // Allow crudy cheap journals
+  $headers_test = @get_headers("https://dx.doi.org/" . urlencode($doi), 1, $context);
   if ($headers_test === FALSE) return NULL; // most likely bad, but will recheck again an again
   $response = $headers_test[0];
   if (empty($headers_test['Location'])) return FALSE; // leads nowhere
