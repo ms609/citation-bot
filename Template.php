14e19e2958a9b3c4779f0b412f43dcffdcb424c1
GlazerMann <kurt@ioiowa.com>
2020-05-18 18:26:58 -0500

    Wikipedia Proxy

--- Template.php
+++ Template.php
@@ -4129,6 +4129,18 @@ final class Template {
                  }
               }
           }
+          // Wikipedia Proxy
+          if (stripos($this->get($param), 'wikipedialibrary') !== FALSE) {
+              if (preg_match("~^https://([^\.\-\/]+)-([^\.\-\/]+)-([^\.\-\/]+)\.wikipedialibrary\.idm\.oclc\.org/(.+)$~", $this->get($param), $matches)) {
+                 $this->set($param, $matches[1] . '.' $matches[2] . '.' . $matches[3] . '/' . $matches[4]);
+                 report_info("Remove proxy from Wikipeadia URL");
+                 if ($this->has('via') && stripos($this->get('via'), 'wiki') !== FALSE) $this->forget('via');
+              } elseif (preg_match("~^https://([^\.\-\/]+)-([^\.\-\/]+)\.wikipedialibrary\.idm\.oclc\.org/(.+)$~", $this->get($param), $matches)) {
+                 $this->set($param, $matches[1] . '.' $matches[2] . '/' . $matches[3]);
+                 report_info("Remove proxy from Wikipeadia URL");
+                 if ($this->has('via') && stripos($this->get('via'), 'wiki') !== FALSE) $this->forget('via');
+              }
+          }
           if (preg_match('~^https?://([^/]+)/~', $this->get($param), $matches)) { 
              $the_host = $matches[1];
           } else {
