17197460b3d419db60dd4b867b1eb7bf56f80b24
GlazerMann <kurt@ioiowa.com>
2020-05-18 20:28:16 -0500

    Wikipedia Proxy (#2877)

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -3457,6 +3457,34 @@ T1 - This is the Title }}';
     $this->assertNull($template->get('website'));
    }
  
+   public function testTidy80() {
+    $text = "{{cite web|url=https://www-rocksbackpages-com.wikipedialibrary.idm.oclc.org/Library/Article/camel-over-the-moon }}";
+    $template = $this->make_citation($text);
+    $template->tidy_parameter('url');
+    $this->assertSame('https://www.rocksbackpages.com/Library/Article/camel-over-the-moon', $template->get('url'));
+   }
+ 
+   public function testTidy81() {
+    $text = "{{cite web|url=https://rocksbackpages-com.wikipedialibrary.idm.oclc.org/Library/Article/camel-over-the-moon }}";
+    $template = $this->make_citation($text);
+    $template->tidy_parameter('url');
+    $this->assertSame('https://rocksbackpages.com/Library/Article/camel-over-the-moon', $template->get('url'));
+   }
+ 
+   public function testTidy82() {
+    $text = "{{cite web|url=https://butte.idm.oclc.org/login?qurl=http://search.ebscohost.com/X}}";
+    $template = $this->make_citation($text);
+    $template->tidy_parameter('url');
+    $this->assertSame('http://search.ebscohost.com/X', $template->get('url'));
+   }
+ 
+   public function testTidy83() {
+    $text = "{{cite web|url=https://butte.idm.oclc.org/login?url=http://search.ebscohost.com/X}}";
+    $template = $this->make_citation($text);
+    $template->tidy_parameter('url');
+    $this->assertSame('http://search.ebscohost.com/X', $template->get('url'));
+   }
+ 
   public function testIncomplete() {
     $text = "{{cite book|url=http://perma-archives.org/pqd1234|isbn=Xxxx|title=xxx|issue=a|volume=x}}"; // Non-date website
     $template = $this->make_citation($text);
