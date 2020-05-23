e2cee0050f309dc56752910193a4b98bec8d39fc
GlazerMann <kurt@ioiowa.com>
2020-05-23 14:24:04 -0500

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -227,31 +227,31 @@ final class TemplateTest extends testBaseClass {
   public function testGroveMusic1() {
     $text = '{{cite web |url=https://doi.org/10.1093/gmo/9781561592630.article.J441700 |title=Tatum, Art(hur, Jr.) (jazz) |last=Howlett |first=Felicity | website=Grove Music Online |publisher=Oxford University Press |date=2002 |access-date=November 20, 2018 |url-access=subscription|via=Grove Music Online}}';
     $template = $this->process_citation($text);
-    $this->assertSame('{{cite document |title=Tatum, Art(hur, Jr.) (jazz) |last=Howlett |first=Felicity | work=Grove Music Online |publisher=Oxford University Press |date=2002 |doi=10.1093/gmo/9781561592630.article.J441700 }}', $template->parsed_text());
+    $this->assertSame('{{cite document |title=Tatum, Art(hur, Jr.) (jazz) |last1=Howlett |first1=Felicity | work=Grove Music Online |publisher=Oxford University Press |date=2002 |doi=10.1093/gmo/9781561592630.article.J441700 }}', $template->parsed_text());
   }
  
   public function testGroveMusic2() {
     $text = '{{cite web |url=https://doi.org/10.1093/gmo/9781561592630.article.J441700 |title=Tatum, Art(hur, Jr.) (jazz) |last=Howlett |first=Felicity | website=Grove Music Online |publisher=Oxford University Press |date=2002|via=Grove Music Online}}';
     $template = $this->process_citation($text);
-    $this->assertSame('{{cite document |title=Tatum, Art(hur, Jr.) (jazz) |last=Howlett |first=Felicity | work=Grove Music Online |publisher=Oxford University Press |date=2002|doi=10.1093/gmo/9781561592630.article.J441700 }}', $template->parsed_text());
+    $this->assertSame('{{cite document |title=Tatum, Art(hur, Jr.) (jazz) |last1=Howlett |first1=Felicity | work=Grove Music Online |publisher=Oxford University Press |date=2002|doi=10.1093/gmo/9781561592630.article.J441700 }}', $template->parsed_text());
   }
  
   public function testGroveMusic3() {
     $text = '{{cite web |url=https://doi.org/10.1093/gmo/9781561592630.article.J441700 |title=Tatum, Art(hur, Jr.) (jazz) |last=Howlett |first=Felicity |publisher=Oxford University Press |date=2002|via=Grove Music Online}}';
     $template = $this->process_citation($text);
-    $this->assertSame('{{cite document |title=Tatum, Art(hur, Jr.) (jazz) |last=Howlett |first=Felicity |publisher=Oxford University Press |date=2002|doi=10.1093/gmo/9781561592630.article.J441700 |work=Grove Music Online}}', $template->parsed_text());
+    $this->assertSame('{{cite document |title=Tatum, Art(hur, Jr.) (jazz) |last1=Howlett |first1=Felicity |publisher=Oxford University Press |date=2002|doi=10.1093/gmo/9781561592630.article.J441700 |work=Grove Music Online}}', $template->parsed_text());
   }
  
   public function testGroveMusic4() {
     $text = '{{cite web |url=https://doi.org/=10.1093/gmo/9781561592630.article.J441700 |title=Tatum, Art(hur, Jr.) (jazz) |last=Howlett |first=Felicity |publisher=Oxford University Press |date=2002|website=Grove Music Online}}';
     $template = $this->process_citation($text);
-    $this->assertSame('{{cite document |title=Tatum, Art(hur, Jr.) (jazz) |last=Howlett |first=Felicity |publisher=Oxford University Press |date=2002|work=Grove Music Online|doi=10.1093/gmo/9781561592630.article.J441700 }}', $template->parsed_text());
+    $this->assertSame('{{cite document |title=Tatum, Art(hur, Jr.) (jazz) |last1=Howlett |first1=Felicity |publisher=Oxford University Press |date=2002|work=Grove Music Online|doi=10.1093/gmo/9781561592630.article.J441700 }}', $template->parsed_text());
   }
  
   public function testGroveMusic5() {
     $text = '{{cite web |url=https://doi.org/10.1093/gmo/9781561592630.article.J441700 |title=Tatum, Art(hur, Jr.) (jazz) |last=Howlett |first=Felicity |publisher=Oxford University Press |date=2002|website=Grove Music Online|via=The Dog Farm}}';
     $template = $this->process_citation($text);
-    $this->assertSame('{{cite document |title=Tatum, Art(hur, Jr.) (jazz) |last=Howlett |first=Felicity |publisher=Oxford University Press |date=2002|work=Grove Music Online via The Dog Farm|doi=10.1093/gmo/9781561592630.article.J441700 }}', $template->parsed_text());
+    $this->assertSame('{{cite document |title=Tatum, Art(hur, Jr.) (jazz) |last1=Howlett |first1=Felicity |publisher=Oxford University Press |date=2002|work=Grove Music Online via The Dog Farm|doi=10.1093/gmo/9781561592630.article.J441700 }}', $template->parsed_text());
   }
   
   public function testBrokenDoiUrlRetention1() {
@@ -366,7 +366,7 @@ final class TemplateTest extends testBaseClass {
   public function testNoLoseUrl() {
      $text = '{{cite book |last=Söderström |first=Ulrika |date=2015 |title=Sandby Borg: Unveiling the Sandby Borg Massacre |url= |location= |publisher=Kalmar lāns museum |isbn=9789198236620 |language=Swedish }}';
      $expanded = $this->process_page($text);
-     $this->assertSame($text, $expanded->parsed_text());
+     $this->assertSame(str_replace(['last=', 'first='],['last1=', 'first1='],$text), $expanded->parsed_text());
   }
  
   public function testDotsAndVia() {
@@ -2391,7 +2391,7 @@ T1 - This is the Title }}';
   }
  
   public function testHTMLNotLost() {
-    $text = '{{cite journal|last=&ndash;|first=&ndash;|title=&ndash;|journal=&ndash;|edition=&ndash;|pages=&ndash;}}';
+    $text = '{{cite journal|last1=&ndash;|first1=&ndash;|title=&ndash;|journal=&ndash;|edition=&ndash;|pages=&ndash;}}';
     $prepared = $this->prepare_citation($text);
     $this->assertSame($text, $prepared->parsed_text());
   }
