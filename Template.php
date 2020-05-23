c79f5b911da095f987de0f88153c68215caaf944
GlazerMann <kurt@ioiowa.com>
2020-05-23 14:44:57 -0500

    better author clean up (#2897)

--- Template.php
+++ Template.php
@@ -3569,6 +3569,10 @@ final class Template {
                   }
               }
             }
+            if (!$pmatch[2] && $pmatch[1] === 'last' && !$this->blank(['first', 'first1'])) {
+              $this->rename('last', 'last1');
+              if ($this->blank('first1')) $this->rename('first', 'first1');
+            }
             return;
 
         case 'bibcode':
