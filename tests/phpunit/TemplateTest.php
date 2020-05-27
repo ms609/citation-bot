be8b9e225044c3c7e89737e34f78586394f88b2e
GlazerMann <kurt@ioiowa.com>
2020-05-27 15:57:30 -0500

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -12,6 +12,7 @@ final class TemplateTest extends testBaseClass {
   public function testGetDoiFromCrossref() {
      $text = '{{cite journal | title = Interaction of casein kinase 1 delta (CK1 delta) with the light chain LC2 of microtubule associated protein 1A (MAP1A)| bibcode = <!-- NO -->  | pmid = <!-- NO --> | doi = 10.1016/j.bbamcr.2005.05.004 }}';
      $expanded = $this->process_citation($text);
+   $this->assertTrue(FALSE);
   }
   
   
