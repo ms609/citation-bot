ec4a279690778df856318cd5f5cf8eed7352a84a
GlazerMann <kurt@ioiowa.com>
2020-05-27 18:51:29 -0500

    Better text matching (#2920)

--- expandFns.php
+++ expandFns.php
@@ -135,11 +135,11 @@ function wikify_external_text($title) {
   }
 
   $title = sanitize_string($title);
-  
+
   for ($i = 0; $i < count($replacement); $i++) {
     $title = str_replace($placeholder[$i], $replacement[$i], $title);
   }
-  return($title); 
+  return $title; 
 }
 
 function restore_italics ($text) {
@@ -224,6 +224,13 @@ function titles_are_dissimilar($inTitle, $dbTitle) {
         $inTitle = str_replace(" ","", $inTitle);
         $inTitle2 = str_replace(" ","", $inTitle2);
         $dbTitle = str_replace(" ","", $dbTitle);
+        $inTitle = str_replace("&nbsp","", $inTitle);
+        $inTitle2 = str_replace("&nbsp","", $inTitle2);
+        $dbTitle = str_replace("&nbsp","", $dbTitle);
+  // This will convert &delta into delta
+        $inTitle = str_replace("&","", $inTitle);
+        $inTitle2 = str_replace("&","", $inTitle2);
+        $dbTitle = str_replace("&","", $dbTitle);
         return ((strlen($inTitle) > 254 || strlen($dbTitle) > 254)
               ? (strlen($inTitle) != strlen($dbTitle)
                 || similar_text($inTitle, $dbTitle) / strlen($inTitle) < 0.98)
