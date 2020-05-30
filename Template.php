e8e7148763511b4d68f1dee3e60b0af836cfac23
GlazerMann <kurt@ioiowa.com>
2020-05-30 11:30:12 -0500

    Update Template.php

--- Template.php
+++ Template.php
@@ -5503,7 +5503,7 @@ final class Template {
              if (str_i_same($part_start[1], 'utf-8')) break;  // UTF-8 is the default
              $url .=  $part . "&" ;
              break;
-          case "hl": case "safe": case "q": case "tbm"
+          case "hl": case "safe": case "q": case "tbm":
              $url .=  $part . "&" ;
              break;
           default:
