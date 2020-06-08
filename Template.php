f65bfd5625711596e2c1e5aac3e04f10c2cb87ca
GlazerMann <kurt@ioiowa.com>
2020-06-08 10:07:35 -0500

    simpler code

--- Template.php
+++ Template.php
@@ -2569,17 +2569,19 @@ final class Template {
     $doi = $this->get_without_comments_and_placeholders('doi');
     if (!$doi) return;
     $return = $this->get_unpaywall_url($doi);
-    if ($return === 'publisher' || $return === 'duplicate' || $return === 'have free') return;
     return; // Not yet
-    $this->get_semanticscholar_url($doi);
+    $this->get_semanticscholar_url($doi, $return);
   }
 
-  public function get_semanticscholar_url($doi) {
+  public function get_semanticscholar_url($doi, $unpay) {
    if(      $this->has('pmc') ||
             ($this->has('doi') && $this->get('doi-access') === 'free') ||
             ($this->has('jstor') && $this->get('jstor-access') === 'free') ||
             ($this->has('osti') && $this->get('osti-access') === 'free') ||
-            ($this->has('ol') && $this->get('ol-access') === 'free')
+            ($this->has('ol') && $this->get('ol-access') === 'free') ||
+            ($unpay === 'publisher') ||
+            ($unpay === 'duplicate') ||
+            ($unpay === 'have free')) return;  
            ) return; // do not add url if have OA already.  Do indlude preprints in list
     if ($this->has('s2cid') || $this->has('S2CID')) return;
     $context = stream_context_create(array(
