abcda1c8f9474f90dc7be1b8427df0bc1e9c8cc7
GlazerMann <kurt@ioiowa.com>
2020-06-10 09:33:19 -0500

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -4,7 +4,7 @@
  * Tests for Template.php
  */
 
-require_once __DIR__ . '/../testBaseClass.php';
+require_once(__DIR__ . '/../testBaseClass.php');
  
 final class TemplateTest extends testBaseClass {
  
