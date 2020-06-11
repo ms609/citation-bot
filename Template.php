2f92858b268785107a7839e9db67f5b13ff7549b
GlazerMann <kurt@ioiowa.com>
2020-06-11 16:28:56 -0500

    Update Template.php

--- Template.php
+++ Template.php
@@ -5395,12 +5395,12 @@ final class Template {
     return $isbn13;
   }
   
-  protected function inline_doi_information() {
-    if ($this->name !== "doi-inline") return FALSE;
-    if (count($this->param) !==2) return FALSE;
+  protected function inline_doi_information() : ?array {
+    if ($this->name !== "doi-inline") return NULL;
+    if (count($this->param) !==2) return NULL;
     $vals   = array();
-    $vals[] = $this->param[0]->parsed_text();
-    $vals[] = $this->param[1]->parsed_text();
+    $vals[0] = $this->param[0]->parsed_text();
+    $vals[1] = $this->param[1]->parsed_text();
     return $vals;
   }
   
