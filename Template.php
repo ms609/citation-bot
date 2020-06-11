645092f60da9cabdac0f1987b72f47ab95340d58
GlazerMann <kurt@ioiowa.com>
2020-06-11 14:13:29 -0500

    Update Template.php

--- Template.php
+++ Template.php
@@ -5129,13 +5129,13 @@ final class Template {
     return $this->param_with_index($i)->val;
   }
   
-  public function get_without_comments_and_placeholders(string $name) {
+  public function get_without_comments_and_placeholders(string $name) : ?string {
     $ret = $this->get($name);
     $ret = preg_replace('~<!--.*?-->~su', '', $ret); // Comments
     $ret = preg_replace('~# # # CITATION_BOT_PLACEHOLDER.*?# # #~sui', '', $ret); // Other place holders already escaped.  Case insensitive
     $ret = str_replace("\xc2\xa0", ' ', $ret); // Replace non-breaking with breaking spaces, which are trimmable
     $ret = trim($ret);
-    return ($ret ? $ret : FALSE);
+    return ($ret ? $ret : NULL);
   }
 
   protected function get_param_key ($needle) {
