b823441ff11de029f986a4b62b6504733c296c50
GlazerMann <kurt@ioiowa.com>
2020-06-12 09:38:06 -0500

    Update Page.php

--- Page.php
+++ Page.php
@@ -111,7 +111,7 @@ class Page {
     }
     for ($i = 0; $i < count($templates); $i++) {
       if (in_array($templates[$i]->wikiname(), TEMPLATES_WE_PROCESS)) {
-        if ($this->has($identifier)
+        if ($templates[$i]->has($identifier)
         && !$templates[$i]->api_has_used($api, equivalent_parameters($identifier))) {
           $ids[$i] = $templates[$i]->get_without_comments_and_placeholders($identifier);
         }
