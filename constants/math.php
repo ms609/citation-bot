2b08f356eae2a039ff5bccefbe32c3c0a14efc10
GlazerMann <kurt@ioiowa.com>
2020-06-12 16:21:39 -0500

    Update math.php

--- constants/math.php
+++ constants/math.php
@@ -1,6 +1,8 @@
-<?php 
+<?php
+declare(strict_types=1);
+
 // Source: http://www.tilman.de/programme/mathparser/substitutions.txt
-// This list includes some items twice.  That is okay, but some static analyis tools will complain
+// This list previously includes some items twice.  That was okay, but some static analyis tools will complained, so non-used ones have been removed
  const MML_TAGS = array (
   '<msqrt>'  => '\sqrt{',
   '<mi>'     => '',
