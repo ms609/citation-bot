83eb2300812b8f415473960cd7703e31715a6d54
GlazerMann <kurt@ioiowa.com>
2020-06-13 07:45:08 -0500

    This test does not work if doi is found

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -1495,9 +1495,8 @@ final class TemplateTest extends testBaseClass {
   
   public function testLongAuthorLists() : void {
   $this->requires_arxiv(function() : void {
-    $text = '{{cite web | https://arxiv.org/PS_cache/arxiv/pdf/1003/1003.3124v2.pdf}}';
-    $expanded = $this->process_citation($text);
-    return; // TODO
+    $text = '{{cite web | https://arxiv.org/PS_cache/arxiv/pdf/1003/1003.3124v2.pdf|doi=<!--Do not add-->}}';
+    $expanded = $this->process_citation($text)
     $this->assertSame('Aad, G.', $expanded->first_author());
     $this->assertNull($expanded->get2('class'));
    });
