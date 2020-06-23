a4ffdfd8e03f423e34acd805aea5e480df2c41a3
GlazerMann <kurt@ioiowa.com>
2020-06-23 08:13:29 -0500

    standardize on double quotes

--- js/index.js
+++ js/index.js
@@ -2,20 +2,20 @@ function ValidateForm() {
   var botPage = document.getElementById("botPage");
   var botCat = document.getElementById("botCat");
   var submitButton; // From StackOverflow user3126867
-  if (typeof event.explicitOriginalTarget != 'undefined') {  
+  if (typeof event.explicitOriginalTarget != "undefined") {  
     submitButton = event.explicitOriginalTarget;
-  } else if(typeof document.activeElement.value != 'undefined'){  // IE
+  } else if(typeof document.activeElement.value != "undefined"){  // IE
     submitButton = document.activeElement;
   }
   console.log(submitButton.id);
-  if (submitButton.id == 'PageSubmit') {
+  if (submitButton.id == "PageSubmit") {
     if (botPage.value.trim() == "") {
       botPage.classList.add("error");
       submitButton.disabled = "disabled";
       return false;
     }
     document.getElementById("PageSpinner").style.display = "inline-block";
-  } else if (submitButton.id == 'CatSubmit') {
+  } else if (submitButton.id == "CatSubmit") {
     if (botCat.value.trim() == "") {
       botCat.classList.add("error");
       submitButton.disabled = "disabled";
@@ -29,8 +29,8 @@ function ValidateForm() {
 }
 
 function ValidatePageName() {
-  document.getElementById('PageSubmit').innerHTML = 'Process page' + 
-    ((document.getElementById('botPage').value.indexOf('|') > -1) ? 's' : '');
+  document.getElementById("PageSubmit").innerHTML = "Process page" + 
+    ((document.getElementById("botPage").value.indexOf("|") > -1) ? "s" : "");
     
   if (this.value.trim() == "") {
     this.classList.add("error");
