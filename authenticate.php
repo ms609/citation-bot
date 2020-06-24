d2a30832546b70478663bb58591679fd438c85f6
GlazerMann <kurt@ioiowa.com>
2020-06-24 17:08:04 -0500

    Typo (#3171)

--- authenticate.php
+++ authenticate.php
@@ -85,14 +85,19 @@ try {
       $host = $_SERVER['HTTP_HOST'];
       $path = $_SERVER['REQUEST_URI'];
       $callback = $proto . '://' . $host . $path;
-      $callback = str_replace('citations.toolforge.org', 'https://tools.wmflabs.org/citations', $callback); // Temporary hack try
+      $callback = str_replace('citations.toolforge.org', 'tools.wmflabs.org/citations', $callback); // Temporary hack try
       $client->setCallback($callback);
+}
+catch (Throwable $e) {
+      death_time("Unable to set callback");
+}
+try {
       list( $authUrl, $token ) = $client->initiate();
       $_SESSION['request_key'] = $token->key; // We will retrieve these from session when the user is sent back
       $_SESSION['request_secret'] = $token->secret;
       return_to_sender($authUrl);
 }
 catch (Throwable $e) { ; }
-death_time("Error authenticating.  Resetting.  Please try again.");
+death_time("Unable to initiate OAuth.");
 
 
