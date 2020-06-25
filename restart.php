3733c51aae21928d936c60f82a035c7ddc974272
GlazerMann <kurt@ioiowa.com>
2020-06-25 13:29:46 -0500

    Update restart.php

--- restart.php
+++ restart.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html><html><body><pre>
+<!DOCTYPE html><html><head><title>Restart Bot</title></head><body><pre>
 <?php
 if (password_verify($_REQUEST['p'], '$2y$10$UOmZtkKs1X17vE/mmbfVgOiy0ZAkXnxa9UxFO97cFX8ioiJZpZ96S')) {
   echo shell_exec ("webservice restart 2>&1");
