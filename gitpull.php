baee6f65f6253a60ec9845448d7d540dc5a4586c
GlazerMann <kurt@ioiowa.com>
2020-06-25 13:33:31 -0500

    Update gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -1,5 +1,5 @@
 <?php
-echo "<!DOCTYPE html><html><head><title>Git Pull</title></head><body><pre>";
+echo "<!DOCTYPE html><html lang=\"en\"><head><title>Git Pull</title></head><body><pre>";
 if (mkdir('git_pull.lock', 0700)) {
   echo shell_exec ("/usr/bin/git fetch  2>&1"); // This only updates .git, so it is very safe. This is first half of pull
   @sleep(3);
