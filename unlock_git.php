1ed64bf10e5a423a769d3f7de0d07a2b9ec443a9
GlazerMann <kurt@ioiowa.com>
2020-06-25 13:28:21 -0500

    Update unlock_git.php

--- unlock_git.php
+++ unlock_git.php
@@ -1,6 +1,6 @@
 <?php
 ob_implicit_flush();
-echo "<!DOCTYPE html><html><body><pre>" . phpversion(). "\n";
+echo "<!DOCTYPE html><html><head><title>Unlocker</title></head><body><pre>" . phpversion(). "\n";
 if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></body></html>\n");
 // Make the person wait.  I know how I can be.
 echo "Wait 6</p>\n";
