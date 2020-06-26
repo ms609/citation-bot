2562b792c301c35719ad60f24f98cf109a70f557
GlazerMann <kurt@ioiowa.com>
2020-06-26 10:25:55 -0500

    Fuller HTML format (#3179)

--- generate_template.php
+++ generate_template.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 
 @header("Access-Control-Allow-Origin: *"); //This is ok because the API is not authenticated
 
-echo "<!DOCTYPE html><html lang=\"en\"><head><title>Make a Template</title></head><body><pre>\n";
+echo "<!DOCTYPE html><html lang=\"en\" dir=\"ltr\"><head><title>Make a Template</title></head><body><pre>\n";
 $SLOW_MODE = TRUE;
 require_once('setup.php');
 $t = new Template();
--- gitpull.php
+++ gitpull.php
@@ -1,5 +1,5 @@
+<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Git Pull</title></head><body><pre>
 <?php
-echo "<!DOCTYPE html><html lang=\"en\"><head><title>Git Pull</title></head><body><pre>";
 if (mkdir('git_pull.lock', 0700)) {
   echo shell_exec ("/usr/bin/git fetch  2>&1"); // This only updates .git, so it is very safe. This is first half of pull
   @sleep(3);
@@ -8,5 +8,5 @@ if (mkdir('git_pull.lock', 0700)) {
 } else {
   echo 'lock file exists -- aborting ';
 }
-echo "</pre></body></html>"
 ?>
+</pre></body></html>
