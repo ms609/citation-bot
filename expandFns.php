e863e1355b7f1f16ca69262741786634b3a38601
GlazerMann <kurt@ioiowa.com>
2020-06-28 15:48:56 -0500

    Update expandFns.php

--- expandFns.php
+++ expandFns.php
@@ -701,6 +701,7 @@ function curl_get_url($url) : string {
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_HEADER, 0);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_USERAGENT, "Citation_bot");  
    }
    curl_setopt($ch, CURLOPT_URL, $url);
    return (string) @curl_exec($ch);
