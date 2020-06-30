a48896edd83112fa0a7402eaf7be3131c896e457
GlazerMann <kurt@ioiowa.com>
2020-06-30 08:58:29 -0500

    Better blocked message

--- category.php
+++ category.php
@@ -64,7 +64,10 @@ if (HTML_OUTPUT) {
 <?php
 }
 
-if (BOT_BLOCKED) report_error("The Citation Bot is currently blocked because of disagreement over its usage.  Please join in the discussion");
+if (BOT_BLOCKED) {
+  echo('</pre><h1>The Citation Bot is currently blocked because of disagreement over its usage. <a href="https://en.wikipedia.org/wiki/User_talk:Citation_bot" title="Join the discussion" target="_blank">Please join in the discussion</a></h1></body></html>', "\n");
+  exit(0);
+}
 
 $edit_summary_end = "| Suggested by " . $api->get_the_user() . " | [[Category:$category]] | via #UCB_Category ";
 $final_edit_overview = "";
