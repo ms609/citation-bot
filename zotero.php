fbfa9cf5965aaa3075ba2ad222708b03ddb607b5
GlazerMann <kurt@ioiowa.com>
2020-07-16 15:42:46 -0500

    Update zotero.php

--- zotero.php
+++ zotero.php
@@ -8,7 +8,7 @@ require_once("constants.php");
 
 function make_ch_zotero() : void { // This is never closed
   global $ch_zotero;
-  if (isset($ch_zotero)) return;
+  if (isset($ch_zotero) && $ch_zotero != NULL) return;
   $ch_zotero = curl_init(ZOTERO_ROOT);
   curl_setopt($ch_zotero, CURLOPT_CUSTOMREQUEST, "POST");
   curl_setopt($ch_zotero, CURLOPT_HTTPHEADER, ['Content-Type: text/plain']);
