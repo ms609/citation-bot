d6c49381b098f382bd3944d38ed0394e6db180b6
GlazerMann <kurt@ioiowa.com>
2020-07-20 18:36:30 -0500

    clean up the file order

--- setup.php
+++ setup.php
@@ -1,5 +1,14 @@
 <?php
 declare(strict_types=1);
+if (file_exists('git_pull.lock')) exit('GIT pull in progress');
+
+/*
+ * setup.php sets up the environment
+ * Most of the page expansion depends on everything else
+ */
+
+ini_set("user_agent", "Citation_bot; citations@tools.wmflabs.org");
+include_once('./vendor/autoload.php');
 
 if ((bool) getenv('TRAVIS') || isset($argv)) {
   error_reporting(E_ALL);
@@ -8,14 +17,6 @@ if ((bool) getenv('TRAVIS') || isset($argv)) {
   error_reporting(E_ALL^E_NOTICE);
   define("HTML_OUTPUT", TRUE);
 }
-
-/*
- * setup.php sets up the environment
- * Most of the page expansion depends on everything else
-*/
-
-ini_set("user_agent", "Citation_bot; citations@tools.wmflabs.org");
-include_once('./vendor/autoload.php');
 
 // This is needed because the Gadget API expects only JSON back, therefore ALL output from the citation bot is thrown away
 if (strpos((string) @$_SERVER['PHP_SELF'], '/gadgetapi.php') === FALSE) {
@@ -34,9 +35,6 @@ if (!getenv('TRAVIS')) {
     ob_start();
 }
 
-require_once('user_messages.php');
-if (file_exists('git_pull.lock')) report_error('GIT pull in progress');
-
 if (!getenv('PHP_OAUTH_CONSUMER_TOKEN') && file_exists('env.php')) {
   // An opportunity to set the PHP_OAUTH_ environment variables used in this function,
   // if they are not set already. Remember to set permissions (not readable!)
@@ -54,6 +52,24 @@ if (!getenv('PHP_OAUTH_CONSUMER_TOKEN') && file_exists('env.php')) {
     ob_end_clean();
   }
 }
+
+mb_internal_encoding('UTF-8');
+ini_set("memory_limit", "256M");
+
+$ADSABS_GIVE_UP = FALSE;
+
+if (isset($_REQUEST["slow"]) || getenv('TRAVIS') || (@$argv[2] === '--slow')) {
+  $SLOW_MODE = TRUE;
+} elseif (isset($argv[2])) {
+  exit("Unexpected text on the command.  Only --slow is valid second argument.  Found: " . $argv[2]);
+} else {
+  $SLOW_MODE = FALSE;
+}
+
+function check_blocked() : void {
+  if (!getenv('TRAVIS') && ! WikipediaBot::is_valid_user('Citation_bot')) exit('</pre><div style="text-align:center"><h1>The Citation Bot is currently blocked because of disagreement over its usage.</h1><br/><h2><a href="https://en.wikipedia.org/wiki/User_talk:Citation_bot" title="Join the discussion" target="_blank">Please join in the discussion</a></h2></div><footer><a href="./" title="Use Citation Bot again">Another&nbsp;page</a>?</footer></body></html>');
+}
+
 require_once('constants.php');
 require_once('NameTools.php');
 require_once('expandFns.php');
@@ -64,22 +80,4 @@ require_once('WikipediaBot.php');
 require_once('apiFunctions.php');
 require_once('Template.php');
 require_once('Page.php');
-
-mb_internal_encoding('UTF-8');
-ini_set("memory_limit", "256M");
-
-$ADSABS_GIVE_UP = FALSE;
-
-if (file_exists('git_pull.lock')) report_error('GIT pull in progress');
-
-if (isset($_REQUEST["slow"]) || getenv('TRAVIS') || (@$argv[2] === '--slow')) {
-  $SLOW_MODE = TRUE;
-} elseif (isset($argv[2])) {
-  report_error("Unexpected text on the command.  Only --slow is valid second argument.  Found: " . $argv[2]);
-} else {
-  $SLOW_MODE = FALSE;
-}
-
-function check_blocked() : void {
-  if (!getenv('TRAVIS') && ! WikipediaBot::is_valid_user('Citation_bot')) exit('</pre><div style="text-align:center"><h1>The Citation Bot is currently blocked because of disagreement over its usage.</h1><br/><h2><a href="https://en.wikipedia.org/wiki/User_talk:Citation_bot" title="Join the discussion" target="_blank">Please join in the discussion</a></h2></div><footer><a href="./" title="Use Citation Bot again">Another&nbsp;page</a>?</footer></body></html>');
-}
+require_once('user_messages.php');
