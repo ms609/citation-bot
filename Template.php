66d43048638101589cda66cd593c5a920f80e4db
GlazerMann <kurt@ioiowa.com>
2020-07-21 09:27:16 -0500

    Update Template.php

--- Template.php
+++ Template.php
@@ -1626,13 +1626,11 @@ final class Template {
         if ($this->wikiname() === 'cite web') $this->change_name_to('cite journal');
         return $this->add_if_new('pmid', $match[1]);
         
-      } elseif (($this->has('doi') || $this->has('pmc')) && preg_match('~^ncbi\.nlm\.nih\.gov/entrez/eutils/elink.fcgi\?.+tool=sumsearch\.org.+id=(\d+)$~', $url, $match)) {  // This is a redirect to the publisher, not pubmed
-        if ($url_sent) return FALSE;
-        if ($match[1] == $this->get('pmc')) {
+      } elseif (preg_match('~^ncbi\.nlm\.nih\.gov/entrez/eutils/elink.fcgi\?.+tool=sumsearch\.org.+id=(\d+)$~', $url, $match)) {
+        if ($url_sent) return FALSE;   // Many do not work
+        if ($this->blank(['doi', 'pmc'])) return FALSE;  // This is a redirect to the publisher, not pubmed
+        if ($match[1] == $this->get('pmc') || $match[1] == $this->get('pmid')) {
            $this->forget($url_type);
-        }
-        if ($match[1] == $this->get('pmid')) {
-          $this->forget($url_type);
         }
         return FALSE;
         
