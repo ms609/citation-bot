0514f8887c95dc04de7c4a6f7452a05644daf2d8
GlazerMann <kurt@ioiowa.com>
2020-07-22 21:36:14 -0500

    No need to wait (#3325)

--- gitpull.php
+++ gitpull.php
@@ -2,7 +2,6 @@
 <?php
 if (mkdir('git_pull.lock', 0700)) {
   echo shell_exec ("/usr/bin/git fetch  2>&1"); // This only updates .git, so it is very safe. This is first half of pull
-  @sleep(3);
   echo shell_exec ("/usr/bin/git pull  2>&1");
   rmdir('git_pull.lock') ;
 } else {
