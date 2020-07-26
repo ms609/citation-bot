458e4dcf0f8a35bd776e32942b95fc74d1164b70
GlazerMann <kurt@ioiowa.com>
2020-07-26 12:35:12 -0500

    Update Zotero.php

--- Zotero.php
+++ Zotero.php
@@ -213,7 +213,7 @@ public static function drop_urls_that_match_dois(array $templates) : void {
                curl_setopt($ch, CURLOPT_URL, $url);
                if (@curl_exec($ch)) {
                   $redirectedUrl_url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-                  $redirectedUrl_url = url_simplify($redirectedUrl_url);
+                  $redirectedUrl_url = self::url_simplify($redirectedUrl_url);
                   if (stripos($redirectedUrl_url, $redirectedUrl_doi) !== FALSE ||
                       stripos($redirectedUrl_doi, $redirectedUrl_url) !== FALSE) {
                     report_forget("Existing canonical URL resulting from equivalent DOI; dropping URL");
