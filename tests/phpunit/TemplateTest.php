a1fb43a64c9395d9b251338b0ac1b206dee729d4
GlazerMann <kurt@ioiowa.com>
2020-08-12 18:27:08 -0500

    Update TemplateTest.php

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -5159,7 +5159,7 @@ T1 - This is the Title }}';
      $this->assertSame('53378830', $template->get2('s2cid')); 
      $this->assertNull($template->get2('s2cid-access'));
      $this->assertSame('cite journal', $template->wikiname());
-     $this->assertSame('10.1093/ser/mwp005', $template->get2('doi'));
+     $this->assertSame('10.1093/ser/mwp005', strtolower($template->get2('doi')));
      $this->assertSame('http://www.lisdatacenter.org/wps/liswps/480.pdf', $template->get2('url')); // OA URL
   }
  
