4dfb9d994f4435e4ba189162863c683e4e236353
GlazerMann <kurt@ioiowa.com>
2020-08-14 20:53:39 -0500

    more explicit code

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@ if (strpos((string) @$_SERVER['PHP_SELF'], '/gadgetapi.php') === FALSE) {
   define("FLUSHING_OKAY", FALSE);
 }
 
-if (isset($_REQUEST["slow"]) || TRAVIS || (@$argv[2] === '--slow')) {
+if (isset($_REQUEST["slow"]) || TRAVIS || (isset($argv[2]) && $argv[2] === '--slow')) {
   define("SLOW_MODE", TRUE);
 } elseif (isset($argv[2])) {
   exit("Unexpected text on the command.  Only --slow is valid second argument.  Found: " . $argv[2]);
