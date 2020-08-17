3002ea39e4fd5d0d80d5a75ab9b40a8c52f8425b
GlazerMann <kurt@ioiowa.com>
2020-08-17 08:34:16 -0500

    Update linked_pages.php

--- linked_pages.php
+++ linked_pages.php
@@ -44,6 +44,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Citation_bot; citations@tools.wmflabs.org');
 $json = (string) @curl_exec($ch);
 curl_close($ch);
 if ($json == '') {
