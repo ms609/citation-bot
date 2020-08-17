dbb10ee67d6297358ad7d93e8fe2dcd3848c2724
GlazerMann <kurt@ioiowa.com>
2020-08-17 12:35:19 -0500

    Update apiFunctions.php

--- apiFunctions.php
+++ apiFunctions.php
@@ -500,10 +500,9 @@ function expand_by_doi(Template $template, bool $force = FALSE) : bool {
       }
       report_action("Querying CrossRef: doi:" . doi_link($doi));
 
-      if ($crossRef->volume_title && $template->blank('journal')) {
+      if ($crossRef->volume_title && ($template->blank(WORK_ALIASES) || $template->wikiname() === 'cite book')) {
         if (strtolower($template->get('title')) == strtolower((string) $crossRef->article_title)) {
            $template->rename('title', 'chapter');
-           $template->change_name_to('cite book');
          } else {
            $template->add_if_new('chapter', restore_italics((string) $crossRef->article_title), 'crossref'); // add_if_new formats this value as a title
         }
