5e902b601e0974f38d5aced0aca3e515250becea
GlazerMann <kurt@ioiowa.com>
2020-08-23 11:14:52 -0500

    simpler code and remove debug

--- WikipediaBot.php
+++ WikipediaBot.php
@@ -69,7 +69,6 @@ final class WikipediaBot {
   }
   
   private function ret_okay(?object $response) : bool {
-    if (self::NonStandardMode()) print_r($response); // DEBUG
     if (is_null($response)) {
       report_minor_error('Wikipedia responce was not decoded.');  // @codeCoverageIgnore
       return FALSE;                                               // @codeCoverageIgnore
@@ -444,9 +443,7 @@ final class WikipediaBot {
   }
   
   static public function NonStandardMode() : bool {
-    if (TRAVIS) return FALSE;
-    if (!isset(self::$last_WikipediaBot)) return FALSE;
-    return (self::$last_WikipediaBot->get_the_user() === 'AManWithNoPlan');
+    return !TRAVIS && isset(self::$last_WikipediaBot) && self::$last_WikipediaBot->get_the_user() === 'AManWithNoPlan';
   }
   
 /**
