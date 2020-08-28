9ffad6ff3464c16f03a884a916635d70b6ad57d6
GlazerMann <kurt@ioiowa.com>
2020-08-27 19:29:56 -0500

    Update category.php

--- category.php
+++ category.php
@@ -85,7 +85,7 @@ if ($category) {
       $final_edit_overview .= "\n No changes needed. " . "<a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . ">" . echoable($page_title) . "</a>";
     }
   }
-  echo ("\n Done all " . count($pages_in_category) . " pages in Category:" . echoable($category) . ". \n");
+  echo ("\n Done all " . (string) count($pages_in_category) . " pages in Category:" . echoable($category) . ". \n");
   $final_edit_overview .= "\n\n" . ' To get the best results, see our helpful <a href="https://en.wikipedia.org/wiki/User:Citation_bot/use">user guides</a>' . "\n\n";
   html_echo($final_edit_overview, '');
 } else {
