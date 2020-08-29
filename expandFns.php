e3736779ef0e75433af48a32b59741f1139e2a4b
GlazerMann <kurt@ioiowa.com>
2020-08-29 09:06:39 -0500

    int vs float & Curl optimization (#3437)

--- expandFns.php
+++ expandFns.php
@@ -657,8 +657,9 @@ function check_doi_for_jstor(string $doi, Template $template) : void {
   }
   $test_url = "https://www.jstor.org/citation/ris/" . $doi;
   $ch = curl_init($test_url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-  curl_setopt($ch, CURLOPT_USERAGENT, 'Citation_bot; citations@tools.wmflabs.org');
+  curl_setopt_array($ch,
+          [CURLOPT_RETURNTRANSFER => TRUE,
+           CURLOPT_USERAGENT => 'Citation_bot; citations@tools.wmflabs.org']);
   $ris = (string) @curl_exec($ch);
   $httpCode = (int) @curl_getinfo($ch, CURLINFO_HTTP_CODE);
   curl_close($ch);
