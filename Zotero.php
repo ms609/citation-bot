3c3a5951c3d2824b9861807e085acda932e93c17
GlazerMann <kurt@ioiowa.com>
2020-08-30 16:29:07 -0500

    Update Zotero.php

--- Zotero.php
+++ Zotero.php
@@ -12,9 +12,7 @@ final class Zotero {
   private const ZOTERO_SKIPS = 100;
   private const ERROR_DONE = 'ERROR_DONE'; 
   protected static int $zotero_announced;
-  /**
-  * @var resource $zotero_ch
-  */
+  /** @var resource $zotero_ch */
   protected static $zotero_ch;
   protected static int $zotero_failures_count = 0;
 
