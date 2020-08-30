a982acee0daf7507790835681e9c5fd81162ebb5
GlazerMann <kurt@ioiowa.com>
2020-08-30 09:32:00 -0500

    Update user_messages.php

--- user_messages.php
+++ user_messages.php
@@ -52,7 +52,9 @@ function quietly(callable $function, string $text) : void { // Stuff suppressed 
 }
 
 function echoable(?string $string) : string {
-  return HTML_OUTPUT ? htmlspecialchars((string) $string) : (string) $string;
+  $string = (string) $string;
+  // @psalm-taint-escape html
+  return HTML_OUTPUT ? htmlspecialchars($string) : escapeshellcmd($string);
 }
 
 function pubmed_link(string $identifier, string $pm) : string {
