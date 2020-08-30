48c168aea76f34368d9aea9e3884827a549d1006
GlazerMann <kurt@ioiowa.com>
2020-08-30 14:51:02 -0500

    Update linked_pages.php

--- linked_pages.php
+++ linked_pages.php
@@ -55,8 +55,7 @@ $array = @json_decode($json, TRUE);
 if ($array === FALSE || !isset($array['parse']['links']) || !is_array($array['parse']['links'])) {
   report_error(' Error interpreting page list - perhaps page requested does not even exist');
 }
-/* @suppress PhanTypeArraySuspiciousNullable */
-$links = $array['parse']['links'];
+$links = $array['parse']['links']; // @phan-suppress-current-line PhanTypeArraySuspiciousNullable
 $pages_in_category = [];
 foreach($links as $link) {
     if (isset($link['exists']) && ($link['ns'] == 0 || $link['ns'] == 118)) {  // normal and draft articles only
