88ddbec5bdaf77fdaf0ab1ceacaca43ed5adc3b1
GlazerMann <kurt@ioiowa.com>
2020-08-31 11:32:03 -0500

    Update expandFns.php

--- expandFns.php
+++ expandFns.php
@@ -428,7 +428,8 @@ function title_capitalization(string $in, bool $caps_after_punctuation) : string
   if ($its_in === $its_out && $its_in != 0) {
     $matches_in = $matches_in[0];
     $matches_out = $matches_out[0];
-    foreach ($matches_in as $key => $value) {
+    /** @psalm-suppress UnusedVariable */
+    foreach ($matches_in as $key => $value) { // $value will not be used directly
       if ($matches_in[$key][0] != $matches_out[$key][0]  &&
           $matches_in[$key][1] == $matches_out[$key][1]) {
         $new_case = mb_substr_replace($new_case, trim($matches_in[$key][0]), $matches_out[$key][1], 3);
