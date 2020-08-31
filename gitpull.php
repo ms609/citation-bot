5846577c987a61e629ab1cc8dbd131c1a46fbeed
GlazerMann <kurt@ioiowa.com>
2020-08-31 07:06:13 -0500

    Update gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html><html lang="en" dir="ltr"><head><title>Git Pull</title></head><body><pre>
 <?php
 if (mkdir('git_pull.lock', 0700)) {
-  // @suppress ForbiddenCode
+   /** @psalm-suppress ForbiddenCode */
   echo shell_exec ("/usr/bin/git fetch  2>&1; /usr/bin/git pull  2>&1 ");
   rmdir('git_pull.lock') ;
 } else {
