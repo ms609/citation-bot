24f8e24b43c9f5bde0da30ec19a3570d0cc7f59e
GlazerMann <kurt@ioiowa.com>
2020-08-31 07:00:10 -0500

    Flag warnings as okay

--- gitpull.php
+++ gitpull.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html><html lang="en" dir="ltr"><head><title>Git Pull</title></head><body><pre>
 <?php
 if (mkdir('git_pull.lock', 0700)) {
-  echo shell_exec ("/usr/bin/git fetch  2>&1"); // This only updates .git, so it is very safe. This is first half of pull
-  echo shell_exec ("/usr/bin/git pull  2>&1");
+  // @suppress ForbiddenCode
+  echo shell_exec ("/usr/bin/git fetch  2>&1; /usr/bin/git pull  2>&1 ");
   rmdir('git_pull.lock') ;
 } else {
   echo 'lock file exists -- aborting ';
