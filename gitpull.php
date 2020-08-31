029308d25bbef0e0842996c72b76a266290c949e
GlazerMann <kurt@ioiowa.com>
2020-08-31 07:09:54 -0500

    Update gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -2,7 +2,7 @@
 <?php
 if (mkdir('git_pull.lock', 0700)) {
   /** @psalm-suppress ForbiddenCode */
-  echo shell_exec ("/usr/bin/git fetch  2>&1");
+  echo shell_exec ("/usr/bin/git fetch  2>&1"); // This only updates .git, so it is very safe. This is first half of pull
   /** @psalm-suppress ForbiddenCode */
   echo shell_exec ("/usr/bin/git pull  2>&1");
   rmdir('git_pull.lock') ;
