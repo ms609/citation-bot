903c311d9a0dde4400394acde431d861df736276
GlazerMann <kurt@ioiowa.com>
2020-09-03 13:26:11 -0500

    Do not upgrade dash for page=1-3 like we do pages

--- Template.php
+++ Template.php
@@ -4698,7 +4698,7 @@ final class Template {
             $this->set($param, $value);
           }
           if (!preg_match("~^[A-Za-z ]+\-~", $value) && mb_ereg(REGEXP_TO_EN_DASH, $value)
-              && can_safely_modify_dashes($value)) {
+              && can_safely_modify_dashes($value) && ($pmatch[1] !== 'page')) {
             $this->mod_dashes = TRUE;
             report_modification("Upgrading to en-dash in " . echoable($param) .
                   " parameter");
@@ -4706,7 +4706,7 @@ final class Template {
             $this->set($param, $value);
           }
           if (   (mb_substr_count($value, "–") === 1) // Exactly one EN_DASH.  
-              && can_safely_modify_dashes($value)) { 
+              && can_safely_modify_dashes($value) && ($pmatch[1] !== 'page')) { 
             $the_dash = (int) mb_strpos($value, "–"); // ALL must be mb_ functions because of long dash
             $part1 = trim(mb_substr($value, 0, $the_dash));
             $part2 = trim(mb_substr($value, $the_dash + 1));
