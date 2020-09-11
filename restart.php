bb74d11c1aeb6908a87ecaa349a01385d1514288
GlazerMann <kurt@ioiowa.com>
2020-09-11 15:27:11 -0500

    Update restart.php

--- restart.php
+++ restart.php
@@ -1,3 +1,6 @@
+<?php
+@header("Access-Control-Allow-Origin: *"); //This is ok because the API is not authenticated
+?>
 <!DOCTYPE html><html lang="en" dir="ltr"><head><title>Restart Bot</title></head><body><pre>
 <?php
 if (password_verify($_REQUEST['p'], '$2y$10$UOmZtkKs1X17vE/mmbfVgOiy0ZAkXnxa9UxFO97cFX8ioiJZpZ96S')) {
