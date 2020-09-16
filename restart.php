d434a4bc2b72d29275c510b4f17aa8d93522b304
GlazerMann <kurt@ioiowa.com>
2020-09-16 10:34:43 -0500

    Update restart.php

--- restart.php
+++ restart.php
@@ -1,12 +1,9 @@
-<?php
-@header("Access-Control-Allow-Origin: *"); //This is ok because the API is not authenticated
-?>
 <!DOCTYPE html><html lang="en" dir="ltr"><head><title>Restart Bot</title></head><body><pre>
 <?php
 ob_implicit_flush();
 if (password_verify($_REQUEST['p'], '$2y$10$UOmZtkKs1X17vE/mmbfVgOiy0ZAkXnxa9UxFO97cFX8ioiJZpZ96S')) {
   /** @psalm-suppress ForbiddenCode */
-  echo shell_exec ("webservice restart 2>&1");
+  echo htmlspecialchars(shell_exec ("webservice restart 2>&1"));
 }
 ?>
 PHP done </pre></body></html>
