d6e9c9922bb2cbbcfe9dbbd42c5195786bc153da
GlazerMann <kurt@ioiowa.com>
2020-09-16 11:34:13 -0500

    avoid echoing back user supplied text

--- setup.php
+++ setup.php
@@ -30,7 +30,7 @@ if (strpos((string) @$_SERVER['PHP_SELF'], '/gadgetapi.php') === FALSE) {
 if (isset($_REQUEST["slow"]) || TRAVIS || (isset($argv[2]) && $argv[2] === '--slow')) {
   define("SLOW_MODE", TRUE);
 } elseif (isset($argv[2])) {
-  exit("Unexpected text on the command.  Only --slow is valid second argument.  Found: " . $argv[2]);
+  exit("Unexpected text on the command.  Only --slow is valid second argument.);
 } else {
   define("SLOW_MODE", FALSE);
 }
