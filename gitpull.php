6cc75a51f1a208a58146f0ca10752bd5219a02cf
GlazerMann <kurt@ioiowa.com>
2020-09-16 10:51:58 -0500

    Update gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -3,9 +3,9 @@
 ob_implicit_flush();
 if (mkdir('git_pull.lock', 0700)) {
   /** @psalm-suppress ForbiddenCode */
-  echo htmlspecialchars(shell_exec ("/usr/bin/git fetch  2>&1")); // This only updates .git, so it is very safe. This is first half of pull
+  echo htmlspecialchars((string) shell_exec("/usr/bin/git fetch  2>&1")); // This only updates .git, so it is very safe. This is first half of pull
   /** @psalm-suppress ForbiddenCode */
-  echo htmlspecialchars(shell_exec ("/usr/bin/git pull  2>&1"));
+  echo htmlspecialchars((string) shell_exec("/usr/bin/git pull  2>&1"));
   rmdir('git_pull.lock') ;
 } else {
   echo 'lock file exists -- aborting ';
