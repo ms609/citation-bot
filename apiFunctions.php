131f9a08d48c18023cc737cf551856275282999e
GlazerMann <kurt@ioiowa.com>
2020-09-17 10:52:56 -0500

    Update apiFunctions.php

--- apiFunctions.php
+++ apiFunctions.php
@@ -129,8 +129,8 @@ function expand_arxiv_templates (array $templates) : bool {
     }
     $eprint = str_ireplace("arXiv:", "", $this_template->get('eprint') . $this_template->get('arxiv'));
     if ($eprint) {
-      array_push($ids, $eprint);
-      array_push($arxiv_templates, $this_template);
+      $ids[] = $eprint;
+      $arxiv_templates[] = & $this_template);  // Pointer to save memory
     }
   }
   return arxiv_api($ids, $arxiv_templates);
