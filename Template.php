8dbe1368c6c36c4050b30c1d13d031a277b6d070
GlazerMann <kurt@ioiowa.com>
2020-09-17 07:51:45 -0500

    Update Template.php

--- Template.php
+++ Template.php
@@ -2636,12 +2636,13 @@ final class Template {
  
   public function expand_by_pubmed(bool $force = FALSE) : void {
     if (!$force && !$this->incomplete()) return;
+    $this_array = [$this];
     if ($pm = $this->get('pmid')) {
       report_action('Checking ' . pubmed_link('pmid', $pm) . ' for more details');
-      query_pmid_api(array($pm), array($this));
+      query_pmid_api(array($pm), $this_array);
     } elseif ($pm = $this->get('pmc')) {
       report_action('Checking ' . pubmed_link('pmc', $pm) . ' for more details');
-      query_pmc_api(array($pm), array($this));
+      query_pmc_api(array($pm), $this_array);
     }
   }
 
