faa6223eb3ad82ada5a04792cd78f736fe8e55fd
GlazerMann <kurt@ioiowa.com>
2020-09-18 21:41:55 -0500

    Update setup.php

--- setup.php
+++ setup.php
@@ -83,7 +83,6 @@ define("PHP_S2APIKEY", (string) getenv("PHP_S2APIKEY"));
 
 function check_blocked() : void {
   if (!TRAVIS && ! WikipediaBot::is_valid_user('Citation_bot')) exit('</pre><div style="text-align:center"><h1>The Citation Bot is currently blocked because of disagreement over its usage.</h1><br/><h2><a href="https://en.wikipedia.org/wiki/User_talk:Citation_bot" title="Join the discussion" target="_blank">Please join in the discussion</a></h2></div><footer><a href="./" title="Use Citation Bot again">Another&nbsp;page</a>?</footer></body></html>');
-  echo_html("\n" . str_pad("", 8096) . "\n", ''); // send 8K to the browser to try to get it to display something 
 }
 
 define("MAX_TRIES", 2);
