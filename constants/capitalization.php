6b514ea52715279f9c88ebccc57afa6580180803
GlazerMann <kurt@ioiowa.com>
2020-09-19 13:04:06 -0500

    Update capitalization.php

--- constants/capitalization.php
+++ constants/capitalization.php
@@ -157,7 +157,7 @@ const OBVIOUS_FOREIGN_WORDS = array(" Abhandlungen ", " Actes ", " Annales ", " 
            " Etudes ", " irlandaises  ", " Studia ", " humaniora ", " Estonica ",
            " Archiwa ", " Biblioteki  ", " Muzea ", " Kościelne ", " Zbornik ",			   
            " Radova ", " Filozofskog  ", " Fakulteta ", " Prištini ", " Mém. ", " Elektriceskaja ",
-	   " Teplovoznaja ", " Tjaga ", " Aarbøger ",  " Oldkyndighed ", " Histori ");
+	   " Teplovoznaja ", " Tjaga ", " Aarbøger ",  " Oldkyndighed ", " Histori ", " Les Publications ");
  
 const MAP_DIACRITICS = array("À"=>"A", "Á"=>"A", "Â"=>"A", "Ã"=>"A",
 	"Ä"=>"A", "Å"=>"A", "Æ"=>"AE", "Ç"=>"C", "È"=>"E", "É"=>"E",
