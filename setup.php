cc061c0e7274d278ab815d7d40e162f541463790
GlazerMann <kurt@ioiowa.com>
2020-09-18 19:54:43 -0500

    Update setup.php

--- setup.php
+++ setup.php
@@ -36,7 +36,7 @@ if (isset($_REQUEST["slow"]) || TRAVIS || (isset($argv[2]) && $argv[2] === '--sl
 }
 
 //Optimisation
-ini_set('output_buffering', false);
+ini_set('output_buffering', '0');
 ob_implicit_flush();
 if (!TRAVIS) {
     if (FLUSHING_OKAY) {
