543e497065baf577ba4fb2174920b2de46d5fc2b
GlazerMann <kurt@ioiowa.com>
2020-09-20 06:37:13 -0500

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 @session_start();
 @header( 'Content-type: text/html; charset=utf-8' );
 @header("Content-Encoding: None", TRUE);
+@header('Cache-Control: no-cache, no-store, must-revalidate');
+@header('Pragma: no-cache');
+@header('Expires: 0');
 
 require_once('setup.php');
 
