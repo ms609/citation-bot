bfa7b5a28fb4124e6627bf0af92226de041d5b79
GlazerMann <kurt@ioiowa.com>
2020-09-20 11:52:28 -0500

    Update debug_output.php (#3554)

--- debug_output.php
+++ debug_output.php
@@ -2,16 +2,19 @@
 declare(strict_types=1);
 @session_start();
 error_reporting(E_ALL^E_NOTICE);
+@header( 'Content-type: text/html; charset=utf-8' );
+@header("Content-Encoding: None", TRUE);
 @header('Cache-Control: no-cache, no-store, must-revalidate');
 @header('Pragma: no-cache');
 @header('Expires: 0');
 ?>
-<!DOCTYPE html><html lang="en" dir="ltr"><head><title>DEBUG CACHE</title></head><body><pre>
+<!DOCTYPE html><html lang="en" dir="ltr"><head><title>DEBUG CACHE</title></head><body><main>X</main><div>Y</div><pre>
 <?php
 ob_implicit_flush();
 while (ob_get_level()) {
  ob_end_flush();
 }
+echo("\n" . str_pad("", 8096) . "\n");
 
 echo "Wait 6</p>\n";
 sleep(2);
@@ -25,7 +28,7 @@ sleep(2);
 /** @psalm-suppress ForbiddenCode */
 @shell_exec("/usr/bin/sync 2>&1 /dev/null");
 
-echo "Wait 3</p>\n";
+echo "</pre><pre>Wait 3</p>\n";
 sleep(2);
 
 echo "Wait 2</p>\n";
