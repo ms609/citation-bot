26fce1b06dcc4c50155d6774cd7ed9a42a7ebcb1
GlazerMann <kurt@ioiowa.com>
2020-09-19 20:16:54 -0500

    Update setup.php

--- setup.php
+++ setup.php
@@ -71,7 +71,7 @@ if (file_exists('env.php')) {
 }
 
 mb_internal_encoding('UTF-8');
-ini_set("memory_limit", "256M");
+ini_set("memory_limit", "196M");
 date_default_timezone_set('UTC');
 
 /** @psalm-suppress UnusedFunctionCall */
