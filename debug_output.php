bbaad1d5ef7e7ccd017bc63dd58e307eca220f64
GlazerMann <kurt@ioiowa.com>
2020-09-20 09:45:50 -0500

    Update debug_output.php

--- debug_output.php
+++ debug_output.php
@@ -1,10 +1,12 @@
 <?php
+declare(strict_types=1);
 @session_start();
+error_reporting(E_ALL^E_NOTICE);
 @header('Cache-Control: no-cache, no-store, must-revalidate');
 @header('Pragma: no-cache');
 @header('Expires: 0');
 ?>
-<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Unlocker</title></head><body><pre>
+<!DOCTYPE html><html lang="en" dir="ltr"><head><title>DEBUG CACHE</title></head><body><pre>
 <?php
 ob_implicit_flush();
 while (ob_get_level()) {
