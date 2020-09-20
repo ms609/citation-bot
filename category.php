ab107157eb1fd70befd5d8ac5c4f75e8259f0496
GlazerMann <kurt@ioiowa.com>
2020-09-20 12:35:44 -0500

    flush does not work

--- category.php
+++ category.php
@@ -43,12 +43,6 @@ if (HTML_OUTPUT) {
 <?php
 }
 
-html_echo("\n" . str_pad("", 8096) . "\n", ''); // send 8K to the browser to try to get it to display something 
-// Dropping out of PHP helps force PHP to flush ALL buffers
-?>
-</pre><pre id="botOutput">
-<?php
-
 check_blocked();
 
 $edit_summary_end = "| Suggested by " . $api->get_the_user() . " | [[Category:$category]] | via #UCB_Category ";
@@ -95,7 +89,7 @@ if ($category) {
       echo "\n\n    # # # ";
       $final_edit_overview .= "\n No changes needed. " . "<a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . ">" . echoable($page_title) . "</a>";
     }
-    html_echo("\n" . '</pre><pre id="botOutput">' . "\n", "\n");
+    echo "\n";
   }
   echo ("\n Done all " . (string) count($pages_in_category) . " pages in Category:" . echoable($category) . ". \n");
   $final_edit_overview .= "\n\n" . ' To get the best results, see our helpful <a href="https://en.wikipedia.org/wiki/User:Citation_bot/use">user guides</a>' . "\n\n";
