c3975077f9717b9cfb151576ecf986ee19a93bae
GlazerMann <kurt@ioiowa.com>
2020-09-20 07:44:57 -0500

    Update category.php

--- category.php
+++ category.php
@@ -44,6 +44,10 @@ if (HTML_OUTPUT) {
 }
 
 html_echo("\n" . str_pad("", 8096) . "\n", ''); // send 8K to the browser to try to get it to display something 
+// Dropping out of PHP helps force PHP to flush ALL buffers
+?>
+</pre><pre id="botOutput">
+<?php
 
 check_blocked();
 
