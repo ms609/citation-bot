260bd46b5500f24998b7ffc3e90d23daedf44cbf
GlazerMann <kurt@ioiowa.com>
2020-09-25 09:20:20 -0500

    Update gadgetTest.php

--- tests/phpunit/gadgetTest.php
+++ tests/phpunit/gadgetTest.php
@@ -8,6 +8,7 @@ require_once(__DIR__ . '/../testBaseClass.php');
  
 final class gadgetTest extends testBaseClass {
   public function testGadget() : void {
+      $this->check_memory();
       ob_start();
       $_POST['text'] = '{{cite|pmid=34213}}';
       $_POST['summary'] = 'Something Nice';
@@ -24,5 +25,12 @@ final class gadgetTest extends testBaseClass {
       $json = json_decode($json_text);
       $this->assertSame('{{citation|pmid=34213|year=1979|last1=Weber|first1=F.|last2=Kayser|first2=F. H.|title=Antimicrobial resistance and serotypes of Streptococcus pneumoniae in Switzerland|journal=Schweizerische Medizinische Wochenschrift|volume=109|issue=11|pages=395–9}}', $json->expandedtext);
       $this->assertSame("Something Nice | Alter: template type. Add: pages, issue, volume, journal, title, year, author pars. 1-2. Formatted [[WP:ENDASH|dashes]]. | You can [[WP:UCB|use this tool]] yourself. [[WP:DBUG|Report bugs here]]. | via #UCB_Gadget ", $json->editsummary);
+     $this->check_memory();
   }
+
+  public function testGadgetMem() : void {
+        $this->check_memory();
+         $this->assertSame("X","X")
+  }
+
 }
