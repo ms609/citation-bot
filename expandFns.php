18e9e19023cb1793c95638b9fd89f7597dae6450
GlazerMann <kurt@ioiowa.com>
2020-10-02 08:26:52 -0500

    Update expandFns.php

--- expandFns.php
+++ expandFns.php
@@ -317,7 +317,7 @@ function straighten_quotes(string $str) : string { // (?<!\') and (?!\') means t
      (mb_strpos($str, '‹')        !== FALSE && mb_strpos($str, '›')        !== FALSE)) { // Only replace single angle quotes if some of both
      $str2 = preg_replace('~&[lr]saquo;|[\x{2039}\x{203A}]|[‹›]~u', "'", $str);           // Websites tiles: Jobs ›› Iowa ›› Cows ›› Ames
      if ($str2 !== NULL) $str = $str2;
-  }	
+  }
   $str2 = preg_replace('~&#822[013];|[\x{201C}-\x{201F}]|&[rlb][d]?quo;~u', '"', $str);
   if ($str2 !== NULL) $str = $str2;
   if((mb_strpos($str, '&raquo;')  !== FALSE && mb_strpos($str, '&laquo;')  !== FALSE) ||
