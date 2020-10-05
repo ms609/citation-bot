7588b9da6605da205c13ba4f0e72455f986564c3
GlazerMann <kurt@ioiowa.com>
2020-10-05 13:15:29 -0500

    faster git pulls

--- gitpull.php
+++ gitpull.php
@@ -7,10 +7,9 @@
 <?php
 ob_implicit_flush();
 if (mkdir('git_pull.lock', 0700)) {
+  // Fetch only updates .git, so it is very safe. That is the first half of pull. So do it as own command
   /** @psalm-suppress ForbiddenCode */
-  echo htmlspecialchars((string) shell_exec("/usr/bin/git fetch  2>&1")); // This only updates .git, so it is very safe. This is first half of pull
-  /** @psalm-suppress ForbiddenCode */
-  echo htmlspecialchars((string) shell_exec("/usr/bin/git pull  2>&1"));
+  echo htmlspecialchars((string) shell_exec("/usr/bin/git fetch 2>&1 ; /usr/bin/git pull 2>&1"));
   rmdir('git_pull.lock') ;
 } else {
   echo 'lock file exists -- aborting ';
