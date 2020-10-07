6925923077ab7e97d99e3c4c9c7f121a7e0bc8e8
GlazerMann <kurt@ioiowa.com>
2020-10-07 11:00:19 -0500

    Update Template.php

--- Template.php
+++ Template.php
@@ -3027,7 +3027,7 @@ final class Template {
         if ($part_start[0] === 'page')     $part_start[0] = 'pg';
         switch ($part_start[0]) {
           case "dq": case "pg": case "lpg": case "q": case "printsec": case "cd": case "vq": case "jtp": case "sitesec":
-            if ($part_start[1] == '') {
+            if (!isset($part_start[1]) || $part_start[1] == '') {
                 $removed_redundant++;
                 $removed_parts .= $part;
             } else {
