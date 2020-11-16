708a239b5b7c265d38b3e471a5fd7548101d8361
GlazerMann <kurt@ioiowa.com>
2020-11-15 19:53:35 -0600

    Update apiFunctions.php

--- apiFunctions.php
+++ apiFunctions.php
@@ -39,10 +39,15 @@ function entrez_api(array $ids, array &$templates, string $db) : bool {   // Poi
   
   $xml = @simplexml_load_file($url);
   if (!is_object($xml)) {
+    echo "\n ENTREZ ERROR : ";
+    print_r($xml);
+    echo "\n ";
     report_warning("Error in PubMed search: No response from Entrez server");    // @codeCoverageIgnore
     return FALSE;                                                                // @codeCoverageIgnore
   }
-
+    echo "\n ENTREZ KIND of WORKED : ";
+    print_r($xml);
+    echo "\n ";
   if (isset($xml->DocSum->Item) && count($xml->DocSum->Item) > 0) foreach($xml->DocSum as $document) {
     report_info("Found match for $db identifier " . $document->Id);
     $template_key = array_search($document->Id, $ids);
