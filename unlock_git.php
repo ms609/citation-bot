43a5d786117f7bb41193519f484bd951db9899b6
GlazerMann <kurt@ioiowa.com>
2020-12-14 17:20:50 -0600

    html5 tags

--- unlock_git.php
+++ unlock_git.php
@@ -7,27 +7,27 @@
 <?php
 ob_implicit_flush();
 echo phpversion() . "\n";
-if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></body></html>");
+if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></main></body></html>");
 // Make the person wait.  I know how I can be.
 echo "Wait 6</p>\n";
 sleep(1);
-if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></body></html>");
+if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></main></body></html>");
 echo "Wait 5</p>\n";
 sleep(1);
-if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></body></html>");
+if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></main></body></html>");
 echo "Wait 4</p>\n";
 sleep(1);
-if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></body></html>");
+if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></main></body></html>");
 echo "Wait 3</p>\n";
 sleep(1);
-if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></body></html>");
+if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></main></body></html>");
 echo "Wait 2</p>\n";
 sleep(1);
-if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></body></html>");
+if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></main></body></html>");
 echo "Wait 1</p>\n";
 sleep(1);
-if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></body></html>");
+if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></main></body></html>");
 rmdir('git_pull.lock') ;
-if (file_exists('git_pull.lock'))  exit("Lock file not gone</pre></body></html>");
+if (file_exists('git_pull.lock'))  exit("Lock file not gone</pre></main></body></html>");
 echo "Script done</p></pre></main></body></html>"
 ?>
