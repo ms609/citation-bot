3dc66075a568a01e29f7f3a715ee57cb1c2b881e
GlazerMann <kurt@ioiowa.com>
2021-02-23 07:18:06 -0600

    up limit to 2000

--- category.php
+++ category.php
@@ -59,7 +59,7 @@ if ($category) {
     exit(0);
   }
   $pages_in_category = array_unique($pages_in_category); // Paranoid
-  if (count($pages_in_category) > 1000 && !WikipediaBot::NonStandardMode()) {
+  if (count($pages_in_category) > 2000 ) {
     echo('Category is huge.  Cancelling run. Pick a smaller category.  Listen to Obi-Wan Kenobi:  You want to go home and rethink your life.');
     html_echo(' </pre></body></html>', "\n");
     exit(0);
