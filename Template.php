0b66a78db9b5a74184fd7b33caa7b10f09169ee3
GlazerMann <kurt@ioiowa.com>
2021-03-27 09:47:04 -0500

    Change DOI broken to Six months of difference

--- Template.php
+++ Template.php
@@ -1319,7 +1319,7 @@ final class Template {
         }
         $existing = strtotime($this->get('doi-broken-date'));
         $the_new  = strtotime($value);
-        if (($existing === FALSE) || ($existing + (4*2592000) < $the_new) || ((2592000*4) + $the_new < $existing)) { // Four month difference
+        if (($existing === FALSE) || ($existing + (6*2592000) < $the_new) || ((2592000*6) + $the_new < $existing)) { // Six months of difference
            return $this->add($param_name, $value);
         }
         return FALSE;
