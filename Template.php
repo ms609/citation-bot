a42b948ae5eaba635d3821cf42479d421057e66d
GlazerMann <kurt@ioiowa.com>
2021-04-07 12:40:39 -0500

    oxfordartonline fix

--- Template.php
+++ Template.php
@@ -5089,7 +5089,8 @@ final class Template {
                     $this->add_if_new('doi', $new_doi);
                 }
               }
-          } elseif (preg_match('~^https?://www\.oxfordartonline\.com/(?:groveart/|)(?:view|abstract)/10\.1093/gao/9781884446054\.001\.0001/oao\-9781884446054\-e\-7000(\d+)$~', $this->get($param), $matches)) {
+          }
+          if (preg_match('~^https?://www\.oxfordartonline\.com/(?:groveart/|)(?:view|abstract)/10\.1093/gao/9781884446054\.001\.0001/oao\-9781884446054\-e\-7000(\d+)$~', $this->get($param), $matches)) {
               $new_doi = '10.1093/gao/9781884446054.article.T' . $matches[1];
               if (doi_works($new_doi)) {
                 if ($this->has('doi') && $this->has('doi-broken-date')) {
@@ -5100,7 +5101,7 @@ final class Template {
                     $this->add_if_new('doi', $new_doi);
                 }
               }
-          } elseif (preg_match('~^https?://www\.oxfordartonline\.com/(?:groveart/|)(?:view|abstract)/10\.1093/gao/9781884446054\.001\.0001/oao\-9781884446054\-e\-70(20\d+)$~', $this->get($param), $matches)) {
+          } elseif (preg_match('~^https?://www\.oxfordartonline\.com/(?:groveart/|)(?:view|abstract)/10\.1093/gao/9781884446054\.001\.0001/oao\-9781884446054\-e\-700(20\d+)$~', $this->get($param), $matches)) {
               $new_doi = '10.1093/gao/9781884446054.article.T' . $matches[1];
               if (doi_works($new_doi)) {
                 if ($this->has('doi') && $this->has('doi-broken-date')) {
