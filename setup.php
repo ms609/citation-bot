7e85d4d896ccb9664d0951506e2b0ffe24a07c95
GlazerMann <kurt@ioiowa.com>
2021-05-03 10:13:36 -0500

    Update setup.php

--- setup.php
+++ setup.php
@@ -87,21 +87,21 @@ function check_blocked() : void {
 }
 
 function unlock_user_and_exit() : void {
- if (TRUE || defined('BIG_JOB_MODE')) {
-   @session_start();
+ if (defined('BIG_JOB_MODE')) {
+   session_start();
    unset($_SESSION['big_and_busy']);     
-   @session_write_close();
+   session_write_close();
  }
  exit(0);
 }
 
 function check_overused() : void {
  if (TRAVIS) return;
- if (isset($_SESSION['big_and_busy']) && $_SESSION['big_and_busy'] === 'BLOCK3') {
+ if (isset($_SESSION['big_and_busy']) && $_SESSION['big_and_busy'] === 'BLOCK2') {
    exit('</pre><div style="text-align:center"><h1>Run blocked by your existing big run.</h1></div></footer></body></html>');
  }
  @session_start();
- $_SESSION['big_and_busy'] = 'BLOCK3';
+ $_SESSION['big_and_busy'] = 'BLOCK2';
  define('BIG_JOB_MODE', 'YES');
  @session_write_close();
 }
