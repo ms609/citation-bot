ab594021e2774e16779180d4915698f94c54f168
GlazerMann <kurt@ioiowa.com>
2021-05-03 09:48:55 -0500

    Only flush every five seconds

--- user_messages.php
+++ user_messages.php
@@ -16,7 +16,7 @@ function user_notice(string $symbol, string $class, string $text) : void {
     echo "\n " . (HTML_OUTPUT ? "<span class='$class'>" : "") . $symbol . $text . (HTML_OUTPUT ? "</span>" : "");
     if (FLUSHING_OKAY && ob_get_level()) {
       $now = microtime(TRUE);
-      if (in_array($class, array('phase', 'subitem', 'warning')) || 5 < ($now - $last_time)) {
+      if (5 < ($now - $last_time)) {
         $last_time = $now;
         ob_flush();
       }
