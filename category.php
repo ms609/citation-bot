915eef19cfadd3bb4f68b545ba78304bf5c0bedc
GlazerMann <kurt@ioiowa.com>
2021-05-06 14:18:08 -0500

    slow down

--- category.php
+++ category.php
@@ -75,6 +75,7 @@ if ($category) {
   $total = count($pages_in_category);
   gc_collect_cycles();
   foreach ($pages_in_category as $page_title) {
+    sleep(1);
     $done++;
     // $page->expand_text will take care of this notice if we are in HTML mode.
     html_echo('', "\n\n\n*** Processing page '" . echoable($page_title) . "' : " . date("H:i:s") . "\n");
