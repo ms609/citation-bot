6b00bb0804130696a0b65349d8a7d6a621fd0364
GlazerMann <kurt@ioiowa.com>
2021-05-14 13:58:10 -0500

    More forbes stuff

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -38,7 +38,8 @@ const PUBLISHERS_ARE_WORKS = array('the san diego union-tribune', 'forbes', 'sal
                                    'the star (malaysia)', 'utusan malaysia', 'daily news, sri lanka', 'daily news & analysis',
                                    'new york daily news', 'new york daily news', 'daily news (new york)', 
                                    'anchorage daily news', 'palm beach daily news', 'daily news egypt', 'the daily news egypt',
-                                   'daily news latino', 'forbes méxico', 'forbes mexico'
+                                   'daily news latino', 'forbes méxico', 'forbes mexico', 'forbes india', 'forbesmiddleeast',
+                                   'forbes middle east', 'forbes russia', 'forbes.ru', 'forbes afrique', 'forbes magazine'
                                    // WP:CITALICSRFC and MOS:ITALICWEBCITE  ?????     'abc news', 'nbc news', 'cbs news', 'bbc news'
                                   ); // LOWER CASE!  WWW not there too! 
 
