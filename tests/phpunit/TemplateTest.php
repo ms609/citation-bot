869a1025fa8d1d53a089ceeb38eae833f4d1f877
Federico Leva <federicoleva@tiscali.it>
2021-05-15 14:47:17 +0300

    Remove test for repository URL which no longer exists

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -3121,14 +3121,7 @@ T1 - This is the Title }}';
     $this->assertSame('http://hdl.handle.net/handle/10125/dfsjladsflhdsfaewfsdfjhasjdfhldsaflkdshkafjhsdjkfhdaskljfhdsjklfahsdafjkldashafldsfhjdsa_TEST_DATA_FOR_BOT_TO_FAIL_ON', $template->get2('url'));
     $this->assertNull($template->get2('hdl'));
   }
- 
-  public function testHandles4() : void {
-    $template = $this->make_citation('{{Cite journal|url=http://digitallibrary.amnh.org/dataset.xhtml?persistentId=hdl:10125/20269;jsessionid=EE3BA49390611FCE0AAAEBB819E777BC?sequence=1}}');
-    $template->get_identifiers_from_url();
-    $this->assertSame('10125/20269', $template->get2('hdl'));
-    $this->assertNotNull($template->get2('url'));
-  }
- 
+
   public function testHandles5() : void {
     $template = $this->make_citation('{{Cite journal|url=http://hdl.handle.net/2027/loc.ark:/13960/t6349vh5n?urlappend=%3Bseq=672}}');
     $template->get_identifiers_from_url();
