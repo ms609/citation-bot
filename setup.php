11910d30a34a4cf2704ab5966a612dedf189dde5
GlazerMann <kurt@ioiowa.com>
2021-05-15 09:42:15 -0500

    go big or go home

--- setup.php
+++ setup.php
@@ -118,7 +118,7 @@ require_once('Template.php');
 require_once('Page.php');
 require_once('user_messages.php');
 
-define("MAX_PAGES", 1634);
+define("MAX_PAGES", 2100);
 define("BIG_RUN", 3);
 
 Zotero::block_zotero();  // TODO - block until fixed
