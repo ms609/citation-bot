88aa62a133627f866c2324a57cf8d366b60ddc9f
GlazerMann <kurt@ioiowa.com>
2021-05-15 11:51:36 -0500

    suppress falsse positives

--- Zotero.php
+++ Zotero.php
@@ -17,7 +17,7 @@ final class Zotero {
   protected static int $zotero_failures_count = 0;
 
 private static function set_default_ch_zotero() : void {
-  /** @phan-suppress-next-line PhanRedundantCondition */
+  /** @phan-suppress-next-line PhanRedundantCondition */ /** @phpstan-ignore-next-line */
   if ( USE_CITOID ) {
         /** @psalm-suppress PossiblyNullArgument */ 
         curl_setopt_array(self::$zotero_ch,
@@ -279,7 +279,7 @@ private static function zotero_request(string $url) : string {
      self::$zotero_ch = curl_init();   // @codeCoverageIgnore
      self::set_default_ch_zotero();    // @codeCoverageIgnore
   }
-  /** @phan-suppress-next-line PhanRedundantCondition */
+  /** @phan-suppress-next-line PhanRedundantCondition */  /** @phpstan-ignore-next-line */
   if ( USE_CITOID ) {
      curl_setopt(self::$zotero_ch, CURLOPT_URL, CITOID_ZOTERO . urlencode($url));
   } else {
