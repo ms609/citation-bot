3e792a0a338ca9ff2a4698031f11604f473720e3
GlazerMann <kurt@ioiowa.com>
2021-05-15 16:44:16 -0500

    Update WikipediaBot.php

--- WikipediaBot.php
+++ WikipediaBot.php
@@ -17,8 +17,7 @@ final class WikipediaBot {
 
   private Consumer $consumer;
   private Token $token;
-  /** @var resource $ch */
-  private $ch;
+  private CurlHandle $ch;
   private string $the_user = '';
   private static ?self $last_WikipediaBot;  // This leads what looks like a circular memory-leak in the test suite, but not in real-life
 
