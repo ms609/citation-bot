5e3de2ac96d5416df3d8486776df42d743525d7b
GlazerMann <kurt@ioiowa.com>
2021-05-15 20:20:36 -0500

    isset vs is_null

--- Zotero.php
+++ Zotero.php
@@ -51,7 +51,7 @@ public static function unblock_zotero() : void {
 
 public static function query_url_api_class(array $ids, array &$templates) : void { // Pointer to save memory
   if (!SLOW_MODE) return; // Zotero takes time
-  if (is_null(self::$zotero_ch)) {
+  if (!isset(self::$zotero_ch)) {
      self::$zotero_ch = curl_init();
      self::set_default_ch_zotero();
   }
@@ -274,7 +274,7 @@ private static function zotero_request(string $url) : string {
     if (self::ZOTERO_GIVE_UP == self::$zotero_failures_count) self::$zotero_failures_count = 0; // @codeCoverageIgnore
   }
 
-  if (is_null(self::$zotero_ch)) {
+  if (!isset(self::$zotero_ch)) {
      self::$zotero_ch = curl_init();   // @codeCoverageIgnore
      self::set_default_ch_zotero();    // @codeCoverageIgnore
   }
