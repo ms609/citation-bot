8b91565a96764ecb2a3a5b7783208fd8b6176e0b
GlazerMann <kurt@ioiowa.com>
2021-05-20 08:30:17 -0500

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -9,6 +9,12 @@ require_once(__DIR__ . '/../testBaseClass.php');
  
 final class PageTest extends testBaseClass {
 
+  protected function setUp(): void {
+   if (BAD_PAGE_HTTP !== '' || BAD_PAGE_API !== '') {
+     $this->markTestSkipped();
+   }
+  }
+
   public function testPageChangeSummary1() : void {
       $page = $this->process_page('{{cite journal|chapter=chapter name|title=book name}}'); // Change to book from journal
       $this->assertSame('Alter: template type. | [[WP:UCB|Use this bot]]. [[WP:DBUG|Report bugs]]. ', $page->edit_summary());
