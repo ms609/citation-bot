050d56be3c7e971a78d05e48afd74c6a76b768cd
GlazerMann <kurt@ioiowa.com>
2021-05-20 08:22:31 -0500

    wrong name =- oops

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
 
 require_once(__DIR__ . '/../testBaseClass.php');
  
-final class PageTest extends testBaseClass {
+final class CrashTest extends testBaseClass {
 
   public function testBadPage() : void {  // Use this when debugging pages that crash the bot
     $bad_page = BAD_PAGE_HTTP;
