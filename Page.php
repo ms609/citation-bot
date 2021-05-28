92371541f73891f935969621b7bf7ebd116f545d
GlazerMann <kurt@ioiowa.com>
2021-05-28 08:50:14 -0500

    caps

--- Page.php
+++ Page.php
@@ -421,8 +421,8 @@ class Page {
     // we often just fix Journal caps, so must be case sensitive compare
     // Avoid minor edits - gadget API will make these changes, since it does not check return code
     $caps_ok = array('lccn', 'isbn', 'doi');
-    $last_first_in  = array(' last=',  ' last =',  '|last=',  '|last =',  ' first=',  ' first =',  '|first=',  '|first =',  'accessdate',  'archivedate',  'archiveurl',  'origyear', 'authorlink', 'cite newspaper');
-    $last_first_out = array(' last1=', ' last1 =', '|last1=', '|last1 =', ' first1=', ' first1 =', '|first1=', '|first1 =', 'access-date', 'archive-date', 'archive-url', 'orig-year','author-link','cite news');
+    $last_first_in  = array(' last=',  ' last =',  '|last=',  '|last =',  ' first=',  ' first =',  '|first=',  '|first =',  'accessdate',  'archivedate',  'archiveurl',  'origyear', 'authorlink', 'cite newspaper', 'Cite newspaper');
+    $last_first_out = array(' last1=', ' last1 =', '|last1=', '|last1 =', ' first1=', ' first1 =', '|first1=', '|first1 =', 'access-date', 'archive-date', 'archive-url', 'orig-year','author-link','cite news',      'Cite news');
     return strcmp(str_replace($last_first_in, $last_first_out, str_ireplace($caps_ok, $caps_ok, $this->text)),
                   str_replace($last_first_in, $last_first_out, str_ireplace($caps_ok, $caps_ok, $this->start_text))) != 0;
   }
