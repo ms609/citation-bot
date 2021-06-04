f7eb832d3688fa8a5d012bcfc133a9f7a7cf67d8
GlazerMann <kurt@ioiowa.com>
2021-06-04 12:57:17 -0500

    conflict

--- Template.php
+++ Template.php
@@ -5836,7 +5836,7 @@ final class Template {
               }
           }
           
-          if (preg_match('~^https?://(?:|politics\.)oxfordre\.com/(?:|politics/)view/10\.1093/acrefore/9780190228637\.001\.0001/acrefore\-9780190228637\-e\-(\d+)$~', $this->get($param), $matches)) {
+          if (preg_match('~^https?://(?:|CONFLIS\.)oxfordre\.com/(?:|politics/)view/10\.1093/acrefore/9780190228637\.001\.0001/acrefore\-9780190228637\-e\-(\d+)$~', $this->get($param), $matches)) {
               $new_doi = '10.1093/acrefore/9780190228637.013.' . $matches[1];
               if (doi_works($new_doi)) {
                 $this->add_if_new('isbn', '978-0-19-022863-7');
