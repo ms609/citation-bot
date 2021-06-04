4fe14fabc103c332feb37ef390d9445a62d8a761
GlazerMann <kurt@ioiowa.com>
2021-06-03 20:48:01 -0500

    oops

--- setup.php
+++ setup.php
@@ -16,7 +16,7 @@ include_once('./vendor/autoload.php');
 define("TRAVIS", (bool) getenv('CI')); // Not just TRAVIS, but GitHub actions set this to true
 define("USE_CITOID", TRUE); // Define which Zotero to use
 if (isset($_GET["page"]) && (string) $_GET["page"] === "User:AManWithNoPlan/sandbox3") {
-  define('EDIT_AS_USER', TRUE)
+  define('EDIT_AS_USER', TRUE);
 }
 
 if (TRAVIS || isset($argv)) {
