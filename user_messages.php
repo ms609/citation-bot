e64145a0ffb9689eab1dc30dcd870498b913f18b
GlazerMann <kurt@ioiowa.com>
2021-06-25 10:01:19 -0500

    Update user_messages.php

--- user_messages.php
+++ user_messages.php
@@ -61,8 +61,7 @@ function quietly(callable $function, string $text) : void { // Stuff suppressed 
 // special flags to mark this function as making all untrustworthy input magically safe to output
 function echoable(?string $string) : string {
   /** @psalm-taint-escape html */
-  $string = (string) $string;
-  /** @psalm-taint-escape quotes */
+  /** @psalm-taint-escape has_quotes */
   $string = (string) $string;
   /** @psalm-suppress TypeDoesNotContainType */ /* PSALM thinks HTML_OUTPUT cannot be false */
   return HTML_OUTPUT ? htmlspecialchars($string) : $string;
