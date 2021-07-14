1d946ba547dbfce6bfc190040145f4126da12af7
GlazerMann <kurt@ioiowa.com>
2021-07-14 08:21:43 -0500

    try prepare_citation

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -19,7 +19,7 @@ final class TemplateTest extends testBaseClass {
  
   public function testLotsMagazines() : void {
     $text_in = "{{cite journal |last=Rauhut |first=O.W. |year=2004 |title=The interrelationships and evolution of basal theropod dinosaurs |journal=Special Papers in Palaeontology |volume=69 |pages=213}}";
-    $prepared = $this->process_citation($text_in);
+    $prepared = $this->prepare_citation($text_in);
     $this->assertSame($text_in, $prepared->parsed_text());
   }
 
