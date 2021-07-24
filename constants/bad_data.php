661a410164a4489c27bb64379aaee80e810f4a1d
GlazerMann <kurt@ioiowa.com>
2021-07-23 19:33:02 -0500

    More bad authors

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -10,13 +10,15 @@ const BAD_ACCEPTED_MANUSCRIPT_TITLES = array("oup accepted manuscript", "placeho
                                              "placeholder", "symbolic placeholder", "[placeholder]", 
                                              "placeholder for arabic language transliteration", "article not found");
 const BAD_AUTHORS = array("unknown", "missing", "- -.", "- -", "no authorship indicated", "no authorship", "no author",
-                           "no authors", "no author indicated", "no authorship indicated", "dk eyewitness", "united states", "great britain");
+                           "no authors", "no author indicated", "no authorship indicated", "dk eyewitness", "united states",
+                           "great britain", "indiatoday");
 const NON_HUMAN_AUTHORS = array('collaborat', 'reporter', 'journalist', 'correspondent', 'anchor', 'staff', 'foreign',
                                 'national', 'endowment', ' for the ', 'humanities', 'committee', 'group',
                                 'society', ' of america', 'association', ' at the ', 'board of ', 'communications',
                                 'corporation', 'incorporated', 'editorial', 'university', 'dept. of', 'department',
                                 'dept of ', 'college', 'center for', 'office of', 'editor', 
-                                'world news', 'national news', 'eyewitness', 'information', 'business', 'bureau', 'us census');
+                                'world news', 'national news', 'eyewitness', 'information', 'business', 'bureau',
+                                'us census', 'indiatoday');
 const BAD_PUBLISHERS = array('london', 'edinburgi', 'edinburgh', 'no publisher', 'no publisher given',
                              'no publisher specified', 'unknown', 'publisher not identified', 'report');
 
