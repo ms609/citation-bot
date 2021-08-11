efab5e0e65be0430f3b3388bbd708f492caf5fe6
GlazerMann <kurt@ioiowa.com>
2021-08-11 15:41:18 -0500

    CONFLICT

--- Zotero.php
+++ Zotero.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once("constants.php");  // @codeCoverageIgnore 
+require_once("constants.php");  // @codeCoverageIgnore  CONFLICT
 
 function query_url_api(array $ids, array &$templates) : void {  // Pointer to save memory
    Zotero::query_url_api_class($ids, $templates);
