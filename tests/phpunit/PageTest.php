553b04ae41bb6c880dc91ae5ace18825dc8f12f3
GlazerMann <kurt@ioiowa.com>
2021-09-13 09:09:48 -0500

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -19,7 +19,8 @@ final class PageTest extends testBaseClass {
  
   public function testPageChangeSummary14() : void {
    $this->requires_zotero(function() : void {
-      $page = $this->process_page("<ref>{{cite web|url=https://www.pbs.org/wnet/nature/episodes/walking-with-giants-the-grizzlies-of-siberia/introduction/3027/|title=Walking with Giants: The Grizzlies of Siberia|work=Nature|publisher=PBS|accessdate=9 December 2013}}</ref> and ''Bear Man of Kamchatka'' ([[BBC]], 2006).<ref>{{cite web|url=http://www.bbc.co.uk/programmes/b00793rz|title=Bear Man of Kamchatka|work=Natural World|publisher=BBC|accessdate=9 December 2013}}</ref> He died after complications from a surgery at a hospital in Calgary, Alberta on May 7, 2018.<ref>[https://www.nytimes.com/2018/05/10/obituaries/charlie-russell-who-befriended-bears-dies-at-76.html]</ref>");
+      $page = $this->process_page("<ref>[https://www.nytimes.com/2018/05/10/obituaries/charlie-russell-who-befriended-bears-dies-at-76.html]</ref>");
+      echo "\n" . $page->parsed_text() . "\n" . $page->edit_summary() .  "\n" ;
       $this->assertSame('WHAT?', $page->edit_summary());
    });
   }
