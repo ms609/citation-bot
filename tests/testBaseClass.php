b518cb5c8f7a280f11d30dc79bbb0f9c9f85e4b3
GlazerMann <kurt@ioiowa.com>
2021-09-15 18:44:40 -0500

    Update testBaseClass.php

--- tests/testBaseClass.php
+++ tests/testBaseClass.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 require_once(__DIR__ . '/../setup.php');
 
-define("BAD_PAGE_HTTP", "");
+define("BAD_PAGE_HTTP", "Brainfuck");
 define("BAD_PAGE_API", "");
 
 abstract class testBaseClass extends PHPUnit\Framework\TestCase {
