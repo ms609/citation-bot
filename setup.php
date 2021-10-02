e462868c8443a8d1a982ae8dc6f26c9aa953cd31
GlazerMann <kurt@ioiowa.com>
2021-10-02 10:48:20 -0500

    conflict

--- setup.php
+++ setup.php
@@ -124,7 +124,7 @@ function check_killed() : void {
 
 define("MAX_TRIES", 2);
 require_once('constants.php');
-require_once('NameTools.php');
+require_once('NameTools.php');CONCLICT
 require_once('expandFns.php');
 require_once('Zotero.php');
 require_once('Parameter.php');
