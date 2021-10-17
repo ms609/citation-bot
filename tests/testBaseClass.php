dd7d2b1ed8000eff32cc63a7297b6b15e2c3f20c
GlazerMann <kurt@ioiowa.com>
2021-10-17 08:31:31 -0500

    List of organisms named after famous people (born before 1900)

--- tests/testBaseClass.php
+++ tests/testBaseClass.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../setup.php';
 
-define("BAD_PAGE_HTTP", "");
+define("BAD_PAGE_HTTP", "List of organisms named after famous people (born before 1900)");
 define("BAD_PAGE_API", "");
 
 abstract class testBaseClass extends PHPUnit\Framework\TestCase {
