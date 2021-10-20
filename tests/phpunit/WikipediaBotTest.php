198b3e53868b20ec965789b189fc48353410745d
GlazerMann <kurt@ioiowa.com>
2021-10-20 11:17:17 -0500

    These are now static class members

--- tests/phpunit/WikipediaBotTest.php
+++ tests/phpunit/WikipediaBotTest.php
@@ -41,7 +41,7 @@ require_once __DIR__ . '/../testBaseClass.php';
      $this->requires_secrets(function() : void {
       $api = new WikipediaBot();
       $namespace = $api->get_namespace('Template:Cite journal');
-      $this->assertSame($api->namespace_id('Template'), $namespace);
+      $this->assertSame(WikipediaBot::namespace_id('Template'), $namespace);
       $results = $api->get_prefix_index('Cite jo', $namespace); // too many results if we just use 'Cite'
       $this->assertTrue(array_search('Template:Cite journal', $results) !== FALSE);
       $results = $api->get_prefix_index("If we retrieve anything here, it's an error", $namespace);
@@ -78,8 +78,8 @@ require_once __DIR__ . '/../testBaseClass.php';
       foreach ($namespaces->query->namespaces as $ns) {
         $ns_name = isset($ns->canonical)? $ns->canonical : '';
         $ns_id = $ns->id;
-        $this->assertSame($ns_id, $api->namespace_id($ns_name));
-        $this->assertSame($ns_name, $api->namespace_name($ns_id));
+        $this->assertSame($ns_id, WikipediaBot::namespace_id($ns_name));
+        $this->assertSame($ns_name, WikipediaBot::namespace_name($ns_id));
       }
       
       /*
