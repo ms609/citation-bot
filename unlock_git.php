892d0b9d8b95ae05d142ef47551b357037c50be7
GlazerMann <kurt@ioiowa.com>
2022-02-01 13:37:51 -0600

    clearstatcache must be called to make sure file_exists actually works

--- unlock_git.php
+++ unlock_git.php
@@ -11,23 +11,30 @@ if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></main></body></htm
 // Make the person wait.  I know how I can be.
 echo "Wait 6</p>\n";
 sleep(1);
+clearstatcache();
 if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></main></body></html>");
 echo "Wait 5</p>\n";
 sleep(1);
+clearstatcache();
 if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></main></body></html>");
 echo "Wait 4</p>\n";
 sleep(1);
+clearstatcache();
 if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></main></body></html>");
 echo "Wait 3</p>\n";
 sleep(1);
+clearstatcache();
 if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></main></body></html>");
 echo "Wait 2</p>\n";
 sleep(1);
+clearstatcache();
 if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></main></body></html>");
 echo "Wait 1</p>\n";
 sleep(1);
+clearstatcache();
 if (!file_exists('git_pull.lock')) exit("Lock file gone</pre></main></body></html>");
 rmdir('git_pull.lock') ;
+clearstatcache();
 if (file_exists('git_pull.lock'))  exit("Lock file not gone</pre></main></body></html>");
 echo "Script done</p></pre></main></body></html>"
 ?>
