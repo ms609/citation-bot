6719c4799ff719f6989b65319eef1fe522ab9874
GlazerMann <kurt@ioiowa.com>
2022-02-04 14:04:41 -0600

    Semantic Scholar is a bogus hournal name

--- Template.php
+++ Template.php
@@ -5104,7 +5104,7 @@ final class Template {
         case 'journal':
         case 'periodical':
           if ($this->blank($param)) return;
-          if ($this->get($param) === 'Undefined') {
+          if ($this->get($param) === 'Undefined' || $this->get($param) === 'Semantic Scholar' || $this->get($param) === '[[Semantic Scholar]]') {
              $this->forget($param);
             return;
           }
