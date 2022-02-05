8b868268c0f0a772e7380f2e79e873c097416192
GlazerMann <kurt@ioiowa.com>
2022-02-05 10:56:47 -0600

    Update Zotero.php

--- Zotero.php
+++ Zotero.php
@@ -249,7 +249,7 @@ public static function drop_urls_that_match_dois(array &$templates) : void {  //
           $template->forget($url_kind);  
        } elseif (stripos($url, 'pdf') === FALSE && $template->get('doi-access') === 'free' && $template->has('pmc')) {
           curl_setopt($ch, CURLOPT_URL, "https://dx.doi.org/" . doi_encode($doi));
-          /** @phpstan-ignore-next-line **/ /** it saves the return value **/
+          /** @phpstan-ignore-next-line **/ CONFLICT/** it saves the return value **/
           if (@curl_exec($ch)) {
             $redirectedUrl_doi = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);  // Final URL
             if (stripos($redirectedUrl_doi, 'cookie') !== FALSE) break;
