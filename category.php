9566f172b64ae3351b8d70dca993552b6d9e9e99
GlazerMann <kurt@ioiowa.com>
2022-02-20 10:04:34 -0600

    Remove recent edits - they are en.wikipedia specific (#3872)

--- category.php
+++ category.php
@@ -34,7 +34,6 @@ if (HTML_OUTPUT) {
     <p>Follow Citation bots progress below.</p>
     <p>
       <a href="https://en.wikipedia.org/wiki/User:Citation_bot/use" target="_blank" title="Using Citation Bot">How&nbsp;to&nbsp;Use&nbsp;/&nbsp;Tips&nbsp;and&nbsp;Tricks</a> |
-      <a href="https://en.wikipedia.org/wiki/Special:Contributions/Citation_bot" target="_blank" title="Recent contributions">Bot&rsquo;s&nbsp;recent&nbsp;edits</a> |
       <a href="https://en.wikipedia.org/wiki/User_talk:Citation_bot" title="Report bugs at Wikipedia" target="_blank">Report&nbsp;bugs</a> |
       <a href="https://github.com/ms609/citation-bot" target="_blank" title="GitHub repository">Source&nbsp;code</a>
     </p>
