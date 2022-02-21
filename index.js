e7f18afaec1862ae1b461a1acd3219dffde56259
GlazerMann <kurt@ioiowa.com>
2022-02-21 12:55:53 -0600

    initialize more

--- index.js
+++ index.js
@@ -74,13 +74,16 @@ function InitializeForm() {
   document.getElementById("botPage").oninput  = ValidatePageName;
   document.getElementById("botCat").oninput   = ValidateCategory;
   document.getElementById("botLinked").oninput= ValidateLinked;
-  this.classList.remove("error");
   document.getElementById("CatSubmit").disabled = false;
   document.getElementById("PageSubmit").disabled = false;
   document.getElementById("linkedSubmit").disabled = false;
   document.getElementById("PageSpinner").style.display = "none";
   document.getElementById("CatSpinner").style.display = "none";
   document.getElementById("LinkSpinner").style.display = "none";
+  document.getElementById("botPage").value = "";
+  document.getElementById("botCat").value = "";
+  document.getElementById("botLinked".value = "";
+  this.classList.remove("error");
 }
 
 window.onload = InitializeForm;
