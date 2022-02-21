e235ea5a1853a7d62c06fa05294e643d12d0b685
GlazerMann <kurt@ioiowa.com>
2022-02-20 18:24:07 -0600

    That is in headers too

--- kill_big_job.php
+++ kill_big_job.php
@@ -9,12 +9,12 @@ declare(strict_types=1);
 
 if (@$_SESSION['big_and_busy'] !== 'BLOCK3') {
  @session_write_close();
- echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Killing the big job</title><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head><body><main><pre>No exiting large job found</pre></main></body></html>';
+ echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Killing the big job</title></head><body><main><pre>No exiting large job found</pre></main></body></html>';
  exit();
 }
 
 $_SESSION['kill_the_big_job'] = "YES";  
 @session_write_close();
-echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Killing the big job</title><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head><body><main><pre>Existing large job flagged for stopping</pre></main></body></html>';
+echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Killing the big job</title></head><body><main><pre>Existing large job flagged for stopping</pre></main></body></html>';
 exit();
 ?>
