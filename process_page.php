62312840735604a1d4593b34e0a63ff4b5b1a34a
GlazerMann <kurt@ioiowa.com>
2022-02-20 18:30:56 -0600

    Remove Content-Encoding that did not work (#3874)

--- process_page.php
+++ process_page.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 @session_start();
 @header('Content-type: text/html; charset=utf-8');
-@header('Content-Encoding: None');
 @header('Cache-Control: no-cache, no-store, must-revalidate');
 @header('Pragma: no-cache');
 @header('Expires: 0');
