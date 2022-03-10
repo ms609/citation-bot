c7dd580fe27195a5ebb226b4836e594f09ad3fd0
GlazerMann <kurt@ioiowa.com>
2022-03-10 14:28:03 -0600

    pure php

--- generate_template.php
+++ generate_template.php
@@ -6,10 +6,9 @@ declare(strict_types=1);
 @header('Cache-Control: no-cache, no-store, must-revalidate');
 @header('Pragma: no-cache');
 @header('Expires: 0');
-?>
 
-<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Make a Template</title></head><body><main><pre>
-<?php
+echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Make a Template</title></head><body><main><pre>';
+
 require_once 'setup.php';
 
 $t = new Template();
