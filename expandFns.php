d438e1973ec56a0bcfdf6a7e727f60c1de9ec08a
GlazerMann <kurt@ioiowa.com>
2022-03-10 14:14:14 -0600

    Remove pages with blank as the name, if present

--- expandFns.php
+++ expandFns.php
@@ -986,6 +986,10 @@ function doi_encode (string $doi) : string {
 
 function edit_a_list_of_pages(array $pages_in_category, WikipediaBot $api, string $edit_summary_end) {
   $final_edit_overview = "";
+  // Remove pages with blank as the name, if present
+  if (($key = array_search("", $pages_in_category)) !== false) {
+    unset($pages_in_category[$key]);
+  }
   if (empty($pages_in_category)) {
     report_warning('No links to expand found');
     echo '</pre><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>';
