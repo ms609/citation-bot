18159c26214720a4256b7ff2d75fc00098353e2d
GlazerMann <kurt@ioiowa.com>
2022-03-10 14:22:55 -0600

    does this work?

--- generate_template.php
+++ generate_template.php
@@ -16,7 +16,7 @@ require_once 'setup.php';
 $t = new Template();
 $t->parse_text('{{Cite web}}');
 if (count($_GET) > 10) exit('Excessive number of parameters passed</pre></body></html>');
-if (count($_GET) === 0) exit('No parameters passed</pre></body></html>');
+if (count($_GET) === 0) exit('No parameters passed');
 foreach ($_GET as $param=>$value) {
   if (strlen($param . $value) > 256) exit('Excessively long parameter passed</pre></body></html>');
   if (strlen($param) == 0) exit('Parameter error</pre></body></html>');
@@ -31,3 +31,4 @@ $page->expand_text();
 echo "\n\n<ref>" . echoable($page->parsed_text()) . "</ref>\n\n</pre></main></body></html>";
 
 ?>
+</pre></body></html>
