30df6a287f0a1d82fc827e53695f7e768758fd0d
GlazerMann <kurt@ioiowa.com>
2022-03-29 15:24:43 -0500

    better output for one page

--- expandFns.php
+++ expandFns.php
@@ -1039,7 +1039,11 @@ function edit_a_list_of_pages(array $pages_in_category, WikipediaBot $api, strin
     }
     echo "\n";
   }
-  echo "\n Done all " . (string) $total . " pages. \n  # # # \n" . $final_edit_overview  . ' </pre><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>';
+  if ($total > 1) {
+    echo "\n Done all " . (string) $total . " pages. \n  # # # \n" . $final_edit_overview  . ' </pre><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>';
+  } else {
+    echo "\n Done with page.</pre><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>';
+  }
 }
 
 function bot_html_header() : void {
