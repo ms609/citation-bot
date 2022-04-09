ff015a753f097c4bd41901856678724db90d8caf
GlazerMann <kurt@ioiowa.com>
2022-04-09 10:34:57 -0500

    support both get and post methods for this special test case of editing as a user

--- setup.php
+++ setup.php
@@ -29,7 +29,7 @@ include_once './vendor/autoload.php';
 
 define("TRAVIS", (bool) getenv('CI')); // Not just TRAVIS, but GitHub actions set this to true
 define("USE_CITOID", TRUE); // Define which Zotero to use
-if (isset($_GET["page"]) && (string) $_GET["page"] === "User:AManWithNoPlan/sandbox3") {
+if (isset($_REQUEST["page"]) && (string) $_REQUEST["page"] === "User:AManWithNoPlan/sandbox3") {
   define('EDIT_AS_USER', TRUE);
 }
 
