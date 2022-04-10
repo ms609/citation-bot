49e7dc5010e2336d7760474203509acf3a9c0fa5
GlazerMann <kurt@ioiowa.com>
2022-04-10 16:44:01 -0500

    Remove command line option to category page

--- category.php
+++ category.php
@@ -10,19 +10,16 @@ set_time_limit(120);
 require_once 'setup.php';
 
 $api = new WikipediaBot();
-$category = isset($_POST["cat"]) ? (string) $_POST["cat"] : (string) @$argv[1];
-$category = trim($category);
+$category = '';
+if (isset($_POST["cat"])) $category = trim((string) $_POST["cat"]);
+if (strtolower(substr($category, 0, 9)) === 'category:') $category = trim(substr($category, 9));
 if ($category === '' && isset($_GET["cat"])) {
    $maybe = (string) $_GET["cat"];
    if (in_array($maybe, ['CS1 errors: DOI' , 'CS1 maint: PMC format', 'CS1 maint: MR format', 'Articles with missing Cite arXiv inputs',
                          'CS1 maint: PMC embargo expired', 'CS1 maint: ref=harv'])) $category = $maybe;
 }
 
-if (strtolower(substr($category, 0, 9)) == 'category:') $category = trim(substr($category, 9));
-/** @psalm-suppress RedundantCondition */ /* PSALM thinks HTML_OUTPUT cannot be FALSE */
-if (HTML_OUTPUT) {
-  bot_html_header();
-}
+bot_html_header();
 
 check_blocked();
 
@@ -32,7 +29,7 @@ if ($category) {
   $pages_in_category = $api->category_members($category);
   if (empty($pages_in_category)) {
     report_warning('Category appears to be empty');
-    html_echo(' </pre><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>', "\n");
+    echo ' </pre><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>';
     exit();
   }
   $pages_in_category = array_unique($pages_in_category); // Paranoid
@@ -43,24 +40,22 @@ if ($category) {
     report_warning( 'Category is huge (' . (string) $total . ')  Cancelling run. Pick a smaller category (maximum size is ' . (string) intval(MAX_PAGES / 4) . ').  Listen to Obi-Wan Kenobi:  You want to go home and rethink your life.');
     echo "\n\n";
     foreach ($pages_in_category as $page_title) {
-       html_echo((string) $page_title . "\n");
+       echo (string) $page_title . "\n";
     }
     echo "\n\n";
-    html_echo(' </pre><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>', "\n");
+    echo ' </pre><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>';
     exit();
   }
   edit_a_list_of_pages($pages_in_category, $api, $edit_summary_end);
 } else {
-  if (isset($argv[1])) {
-    echo "You must specify a category on the command line.";
-  } elseif (isset($_POST["cat"])) {
-    report_warning("You must specify a valid category on the webform.");
+  if (isset($_POST["cat"])) {
+    report_warning("Invalid category on the webform.");
   } elseif (isset($_GET["cat"])) {
-    report_warning("You must specify a category on the webform.  We do not support using as a parameter to the php file anymore");
+    report_warning("You must specify the category using the webform.");
   } else {
-    report_warning("You must specify a category using the API -- OR -- category got lost during initial authorization ");
+    report_warning("Category got lost during initial authorization.");
   }
-  html_echo(' </pre><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>', "\n");
+  echo ' </pre><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>;
 }
 exit();
 ?>
