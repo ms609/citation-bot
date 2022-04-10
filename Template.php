47c41cec54f51efbea4780483fd34e26cbd3517e
GlazerMann <kurt@ioiowa.com>
2022-04-10 15:45:32 -0500

    Typo last commit

--- Template.php
+++ Template.php
@@ -624,7 +624,7 @@ final class Template {
             report_action("Found and used SICI");
           }
       }
-      if (!$this->blank(['pmc', 'pmid', 'doi', 'jstor']) || (!$this-blank(['arxiv', 'eprint']) && stripos($this->get('journal'), 'arXiv') !== FALSE)) { // Have some good data
+      if (!$this->blank(['pmc', 'pmid', 'doi', 'jstor']) || (!$this->blank(['arxiv', 'eprint']) && stripos($this->get('journal'), 'arXiv') !== FALSE)) { // Have some good data
           $the_title   = $this->get('title');
           $the_journal = $this->get('journal');
           $the_chapter = $this->get('chapter');
