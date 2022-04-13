f8dc4cdefb805206135ee8d24684da0086998d6c
GlazerMann <kurt@ioiowa.com>
2022-04-13 13:42:59 -0500

    Update php_info.php

--- php_info.php
+++ php_info.php
@@ -1,9 +1,6 @@
 <?php
 set_time_limit(120);
-@header('Cache-Control: no-cache, no-store, must-revalidate');
-@header('Pragma: no-cache');
-@header('Expires: 0');
-@header('Content-type: text/html; charset=utf-8');
+require_once 'html_headers.php';
 
 ob_implicit_flush();
 echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>PHP Info</title></head><body><pre>';
