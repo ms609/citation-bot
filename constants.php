678f7c44b81a609cc4163e4b49bfd25347354a18
GlazerMann <kurt@ioiowa.com>
2022-04-14 12:50:03 -0500

    Move BOT_USER_AGENT into constants

--- constants.php
+++ constants.php
@@ -35,3 +35,5 @@ const CROSSREFUSERNAME = 'martins@gmail.com';
 const PUBMEDUSERNAME   = 'martins+pubmed@gmail.com';
 
 const GET_THE_HEADERS = 1; // expects 1 in early PHP, and TRUE in newer versions
+
+const BOT_USER_AGENT = "Citation_bot; citations@tools.wmflabs.org";
