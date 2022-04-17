4b8f1fc06bc7d328c5dc6f93f1812433591f9f2e
GlazerMann <kurt@ioiowa.com>
2022-04-17 14:22:56 -0500

    Update linked_pages.php

--- linked_pages.php
+++ linked_pages.php
@@ -35,7 +35,7 @@ if (strlen($page_name) > 256)  {
 }
 $edit_summary_end = "| Suggested by " . $api->get_the_user() . " | Linked from $page_name | #UCB_webform_linked ";
 
-$json = WikipediaBot(['action' => 'parse', 'prop' => 'links', 'page' => $page_name]);
+$json = WikipediaBot::QueryAPI(['action' => 'parse', 'prop' => 'links', 'page' => $page_name]);
 
 if ($json == '') {
   report_warning(' Error getting page list');
