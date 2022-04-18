a9f13964c916916d70d84eb8931042a835d161ec
GlazerMann <kurt@ioiowa.com>
2022-04-18 09:39:16 -0500

    Update testBaseClass.php

--- tests/testBaseClass.php
+++ tests/testBaseClass.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../setup.php';
 
-define("BAD_PAGE_API", "");
+define("BAD_PAGE_API", "User:AManWithNoPlan/sandbox2");
 
 abstract class testBaseClass extends PHPUnit\Framework\TestCase {
   // Change these to temporarily disable sets of tests======================
