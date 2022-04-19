06d9d4ff8811bb6ff3c93983e1456af19b4d9e2c
GlazerMann <kurt@ioiowa.com>
2022-04-19 13:28:19 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -10,19 +10,6 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class CrashTest extends testBaseClass {
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
-    $bad_page = BAD_PAGE_API;
-    $bad_page = str_replace(' ', '_', $bad_page);
-    if ($bad_page !== "") {
-      define("TRAVIS_PRINT", "YES");
-      $page = new TestPage();
-      $page->get_text_from($bad_page);
-      AdsAbsControl::back_on();
-      Zotero::unblock_zotero();
-      $page->expand_text();
-      AdsAbsControl::give_up();
-      Zotero::block_zotero();
-      $this->assertTrue(FALSE); // prevent us from git committing with a website included
-    }
     $this->assertTrue(TRUE);
   }
 }
