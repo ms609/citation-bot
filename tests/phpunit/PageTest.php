a7488c31473125d1aaf8c7488f7d10849f991689
GlazerMann <kurt@ioiowa.com>
2022-04-19 13:52:23 -0500

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -28,6 +28,9 @@ final class PageTest extends testBaseClass {
         $page->get_text_from($writeTestPage);
         $this->assertSame($trialCitation, $page->parsed_text());
       }
+      $this->requires_secrets(function() : void {
+       $this->assertTrue(TRAVIS || $page_result); // If we have tokens and are not in TRAVIS, then should have worked
+      }
       $page->overwrite_text($trialCitation);
       $page->expand_text();
       $this->assertTrue(strpos($page->edit_summary(), 'journal, ') > 3);
