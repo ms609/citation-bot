dedf4eb3ac8648f869c2a87f28b862a6287681fd
GlazerMann <kurt@ioiowa.com>
2022-04-21 13:10:38 -0500

    fix test

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -6840,7 +6840,7 @@ T1 - This is the Title }}';
    }
  
     public function test1093DoiStuff1() : void {
-     $text = '{{cite journal|url=X|doi=10.1093/BADDDDDDDD/BADDDDDDD/junl|via=hose}}';
+     $text = '{{cite web|url=X|doi=10.1093/BADDDDDDDD/BADDDDDDD/junl|via=hose}}';
      $template = $this->make_citation($text);
      $template->forget('url');
      $this->assertNull($template->get2('url'));
@@ -6851,7 +6851,7 @@ T1 - This is the Title }}';
     }
  
     public function test1093DoiStuff2() : void {
-     $text = '{{Cite journal|url=X|doi=10.1093/BADDDDDDDD/BADDDDDDD/junl|website=hose}}';
+     $text = '{{Cite web|url=X|doi=10.1093/BADDDDDDDD/BADDDDDDD/junl|website=hose}}';
      $template = $this->make_citation($text);
      $template->forget('url');
      $this->assertNull($template->get2('url'));
@@ -6862,7 +6862,7 @@ T1 - This is the Title }}';
     }
  
     public function test1093DoiStuff3() : void {
-     $text = '{{Cite journal|url=X|doi=10.1093/BADDDDDDDD/BADDDDDDD/junl|website=hose|via=Hose}}';
+     $text = '{{Cite web|url=X|doi=10.1093/BADDDDDDDD/BADDDDDDD/junl|website=hose|via=Hose}}';
      $template = $this->make_citation($text);
      $template->forget('url');
      $this->assertNull($template->get2('url'));
@@ -6873,7 +6873,7 @@ T1 - This is the Title }}';
     }
  
     public function test1093DoiStuff4() : void {
-     $text = '{{Cite journal|url=X|doi=10.1093/BADDDDDDDD/BADDDDDDD/junl|website=kittens|via=doggies}}';
+     $text = '{{Cite web|url=X|doi=10.1093/BADDDDDDDD/BADDDDDDD/junl|website=kittens|via=doggies}}';
      $template = $this->make_citation($text);
      $template->forget('url');
      $this->assertNull($template->get2('url'));
@@ -6882,6 +6882,51 @@ T1 - This is the Title }}';
      $this->assertSame('cite document', $template->wikiname());
      $this->assertSame('kittens via doggies', $template->get2('work'));
    }
+ 
+     public function test1093DoiStuff5() : void {
+     $text = '{{cite journal|url=X|doi=10.1093/BADDDDDDDD/BADDDDDDD/junl|via=hose}}';
+     $template = $this->make_citation($text);
+     $template->forget('url');
+     $this->assertNull($template->get2('url'));
+     $this->assertNull($template->get2('via'));
+     $this->assertNull($template->get2('website'));
+     $this->assertSame('cite document', $template->wikiname());
+     $this->assertSame('hose', $template->get2('journal'));
+    }
+ 
+    public function test1093DoiStuff6() : void {
+     $text = '{{Cite journal|url=X|doi=10.1093/BADDDDDDDD/BADDDDDDD/junl|website=hose}}';
+     $template = $this->make_citation($text);
+     $template->forget('url');
+     $this->assertNull($template->get2('url'));
+     $this->assertNull($template->get2('via'));
+     $this->assertNull($template->get2('website'));
+     $this->assertSame('cite document', $template->wikiname());
+     $this->assertSame('hose', $template->get2('journal'));
+    }
+ 
+    public function test1093DoiStuff7() : void {
+     $text = '{{Cite journal|url=X|doi=10.1093/BADDDDDDDD/BADDDDDDD/junl|website=hose|via=Hose}}';
+     $template = $this->make_citation($text);
+     $template->forget('url');
+     $this->assertNull($template->get2('url'));
+     $this->assertNull($template->get2('via'));
+     $this->assertNull($template->get2('website'));
+     $this->assertSame('cite document', $template->wikiname());
+     $this->assertSame('hose', $template->get2('journal'));
+    }
+ 
+    public function test1093DoiStuff8() : void {
+     $text = '{{Cite journal|url=X|doi=10.1093/BADDDDDDDD/BADDDDDDD/junl|website=kittens|via=doggies}}';
+     $template = $this->make_citation($text);
+     $template->forget('url');
+     $this->assertNull($template->get2('url'));
+     $this->assertNull($template->get2('via'));
+     $this->assertNull($template->get2('website'));
+     $this->assertSame('cite document', $template->wikiname());
+     $this->assertSame('kittens via doggies', $template->get2('journal'));
+   }
+ 
  
     public function testFixURLinLocation() : void {
      $text = '{{cite journal|location=http://www.apple.com/indes.html}}';
