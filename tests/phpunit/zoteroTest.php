eb29d99b5db90eb57b590de74219b9277df2bce0
GlazerMann <kurt@ioiowa.com>
2022-04-24 12:23:13 -0500

    add tests

--- tests/phpunit/zoteroTest.php
+++ tests/phpunit/zoteroTest.php
@@ -758,6 +758,42 @@ final class zoteroTest extends testBaseClass {
     $this->assertSame('34–55', $template->get2('pages'));
   }
   
+  public function testZoteroResponse45() : void {
+    $text = '{{cite web}}';
+    $template = $this->make_citation($text);
+    $access_date = 0;
+    $url = '';
+    $url_kind = '';
+    $zotero_data[0] = (object) array('title' => 'This', 'itemType' => 'webpage', 'extra' => 'dfasfsdaf ADS Bibcode: 1234asdfghjklqwerty dfasfsdaf');
+    $zotero_response = json_encode($zotero_data);
+    $this->assertTrue(Zotero::process_zotero_response($zotero_response, $template, $url, $url_kind, $access_date));
+    $this->assertSame('1234asdfghjklqwerty', $template->get2('bibcode'));
+  }
+  
+  public function testZoteroResponse46() : void {
+    $text = '{{cite web}}';
+    $template = $this->make_citation($text);
+    $access_date = 0;
+    $url = '';
+    $url_kind = '';
+    $zotero_data[0] = (object) array('title' => 'This', 'itemType' => 'newspaperArticle', 'publicationTitle' => 'United States Census Bureau');
+    $zotero_response = json_encode($zotero_data);
+    $this->assertTrue(Zotero::process_zotero_response($zotero_response, $template, $url, $url_kind, $access_date));
+    $this->assertSame('United States Census Bureau', $template->get2('publisher'));
+  }
+  
+  public function testZoteroResponse47() : void {
+    $text = '{{cite web|work=DSfadsfsdf}}';
+    $template = $this->make_citation($text);
+    $access_date = 0;
+    $url = 'http://nature.org/';
+    $url_kind = '';
+    $zotero_data[0] = (object) array('title' => 'This', 'itemType' => 'report');
+    $zotero_response = json_encode($zotero_data);
+    $this->assertTrue(Zotero::process_zotero_response($zotero_response, $template, $url, $url_kind, $access_date));
+    $this->assertSame('cite journal', $template->wikiname());
+  }
+  
   public function testRemoveURLthatRedirects() : void { // This URL is a redirect -- tests code that does that
     $text = '{{cite journal|pmc=XYZ|doi=10.1021/acs.analchem.8b04567|url=http://shortdoi.org/gf7sqt|pmid=30741529|pmc=6526953|title=ISiCLE: A Quantum Chemistry Pipeline for Establishing in Silico Collision Cross Section Libraries|journal=Analytical Chemistry|volume=91|issue=7|pages=4346–4356|year=2019|last1=Colby|first1=Sean M.|last2=Thomas|first2=Dennis G.|last3=Nuñez|first3=Jamie R.|last4=Baxter|first4=Douglas J.|last5=Glaesemann|first5=Kurt R.|last6=Brown|first6=Joseph M.|last7=Pirrung|first7=Meg A.|last8=Govind|first8=Niranjan|last9=Teeguarden|first9=Justin G.|last10=Metz|first10=Thomas O.|last11=Renslow|first11=Ryan S.}}';
     $template = $this->make_citation($text);
