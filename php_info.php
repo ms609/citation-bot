aab86e1e29fb5a4ac1c2e6423595111f5032224d
GlazerMann <kurt@ioiowa.com>
2022-04-26 10:45:26 -0500

    duplicated existing information

--- php_info.php
+++ php_info.php
@@ -8,7 +8,6 @@ echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>PHP Info</title></he
 if (password_verify($_REQUEST['p'], '$2y$10$UOmZtkKs1X17vE/mmbfVgOiy0ZAkXnxa9UxFO97cFX8ioiJZpZ96S')) {
   unset($_REQUEST['p'], $_GET['p'], $_POST['p'], $_SERVER['HTTP_X_ORIGINAL_URI'], $_SERVER['REQUEST_URI'], $_SERVER['QUERY_STRING']); // Anything that contains password string
   phpinfo(INFO_ALL);
-  echo htmlspecialchars((string) shell_exec("/usr/bin/env 2>&1"), ENT_QUOTES);
 }
 
 echo '</pre></body></html>';
