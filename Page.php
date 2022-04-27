e1817aed2ec91d245160ca6a9823e11e5f4dcfe3
GlazerMann <kurt@ioiowa.com>
2022-04-27 15:44:11 -0500

    better user output on erros

--- Page.php
+++ Page.php
@@ -91,7 +91,7 @@ class Page {
     $this->text = WikipediaBot::GetAPage($title);
 
     if ($this->text == '') {
-       report_warning('Page '  . $title . ' from ' . WIKI_ROOT . ' appears to be empty ');    // @codeCoverageIgnore
+       report_warning('Page '  . $title . ' from ' . str_replace(['/w/index.php', 'https://'], ['',''], WIKI_ROOT) . ' appears to be empty '); // @codeCoverageIgnore
        return FALSE;                                                                          // @codeCoverageIgnore
     }
     $this->start_text = $this->text;
