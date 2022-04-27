fc36446db4959095aba1fdacf1d7844202534d91
GlazerMann <kurt@ioiowa.com>
2022-04-27 14:43:33 -0500

    test just for code coverage

--- tests/phpunit/zoteroTest.php
+++ tests/phpunit/zoteroTest.php
@@ -836,6 +836,18 @@ final class zoteroTest extends testBaseClass {
     $this->assertSame('34–55', $template->get2('pages'));
   }
   
+  public function testZoteroResponse51() : void {
+    $text = '{{cite web|title=No one seems to understand how this work|chapter=This is the title of the chapter}}'; // New data for chapter and title match exactly, but reversed
+    $template = $this->make_citation($text);
+    $access_date = 0;
+    $url = '';
+    $url_kind = '';
+    $zotero_data[0] = (object) array('title' => 'This is the title of the chapter', 'year', 'pages' => '34-55');
+    $zotero_response = json_encode($zotero_data);
+    $this->assertTrue(Zotero::process_zotero_response($zotero_response, $template, $url, $url_kind, $access_date));
+    $this->assertSame('34–55', $template->get2('pages'));
+  }
+
   public function testRemoveURLthatRedirects() : void { // This URL is a redirect -- tests code that does that
     $text = '{{cite journal|doi-access=free|doi=10.1021/acs.analchem.8b04567|url=https://shortdoi.org/gf7sqt|pmid=30741529|pmc=6526953|title=ISiCLE: A Quantum Chemistry Pipeline for Establishing in Silico Collision Cross Section Libraries|journal=Analytical Chemistry|volume=91|issue=7|pages=4346–4356|year=2019|last1=Colby|first1=Sean M.|last2=Thomas|first2=Dennis G.|last3=Nuñez|first3=Jamie R.|last4=Baxter|first4=Douglas J.|last5=Glaesemann|first5=Kurt R.|last6=Brown|first6=Joseph M.|last7=Pirrung|first7=Meg A.|last8=Govind|first8=Niranjan|last9=Teeguarden|first9=Justin G.|last10=Metz|first10=Thomas O.|last11=Renslow|first11=Ryan S.}}';
     $template = $this->make_citation($text);
