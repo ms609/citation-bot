79e6aa3b7a096ee98819ce18ef2dda4637cc965d
GlazerMann <kurt@ioiowa.com>
2022-04-29 13:26:37 -0500

    Update apiFunctions.php

--- apiFunctions.php
+++ apiFunctions.php
@@ -1197,7 +1197,7 @@ function get_entrez_xml(string $type, string $query) : ?object {
    } elseif ($type === "pmc") {
       $url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?tool=WikipediaCitationBot&email=" . PUBMEDUSERNAME . "&db=pmc&id=$query";
    } else {
-      report_error("Invalid type in  get_entrez_xml " . $type);
+      report_error("Invalid type passed to get_entrez_xml: " . $type);
    }
    $xml = @simplexml_load_file($url);
    if ($xml === FALSE) {
