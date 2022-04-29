f9e2b07a6ee67e7c4289a7f7682a839832b59f0f
GlazerMann <kurt@ioiowa.com>
2022-04-29 13:31:09 -0500

    Update apiFunctions.php

--- apiFunctions.php
+++ apiFunctions.php
@@ -42,7 +42,7 @@ function entrez_api(array $ids, array &$templates, string $db) : bool {   // Poi
   report_action("Using $db API to retrieve publication details: ");
   $xml = get_entrez_xml($db, implode(',', $ids));
   
-  if (!is_object($xml)) {
+  if ($xml === NULL) {
     report_warning("Error in PubMed search: No response from Entrez server");   // @codeCoverageIgnore
     return FALSE;                                                               // @codeCoverageIgnore
   }
@@ -1190,12 +1190,13 @@ function Bibcode_Response_Processing(string $return, $ch, string $adsabs_url) : 
 }
 
 function get_entrez_xml(string $type, string $query) : ?object {
+   $url =  "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/";
    if ($type === "esearch_pubmed") {
-      $url =  "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?tool=WikipediaCitationBot&email=" . PUBMEDUSERNAME . "&db=pubmed&term=$query";
+      $url .=  "esearch.fcgi?tool=WikipediaCitationBot&email=" . PUBMEDUSERNAME . "&db=pubmed&term=$query";
    } elseif ($type === "pubmed") {
-      $url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?tool=WikipediaCitationBot&email=" . PUBMEDUSERNAME . "&db=pubmed&id=$query";
+      $url .= "esummary.fcgi?tool=WikipediaCitationBot&email=" . PUBMEDUSERNAME . "&db=pubmed&id=$query";
    } elseif ($type === "pmc") {
-      $url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?tool=WikipediaCitationBot&email=" . PUBMEDUSERNAME . "&db=pmc&id=$query";
+      $url .= "esummary.fcgi?tool=WikipediaCitationBot&email=" . PUBMEDUSERNAME . "&db=pmc&id=$query";
    } else {
       report_error("Invalid type passed to get_entrez_xml: " . $type);
    }
