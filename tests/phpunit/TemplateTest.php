ea0cb589e242372e1766aa5fbe8ea3ca6fb073ed
GlazerMann <kurt@ioiowa.com>
2022-05-02 11:46:11 -0500

    fix tests

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -7407,107 +7407,115 @@ T1 - This is the Title }}';
    }
  
    public function testGoogleBooksCleanup1() : void {
-      $text = "{{cite lsa|url=https://books.google.com/booksid=12345}}";
+      $text = "{{cite LSA|url=https://books.google.com/booksid=12345}}";
       $expanded = $this->process_citation($text);
       $this->AssertSame('https://books.google.com/books?id=12345', $expanded->get2('url'));
    }
    public function testGoogleBooksCleanup2() : void {
-      $text = "{{cite lsa|url=https://books.google.com/books?vid=12345}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?vid=12345}}";
       $expanded = $this->process_citation($text);
       $this->AssertSame('https://books.google.com/books?id=12345', $expanded->get2('url'));
    }
    public function testGoogleBooksCleanup3() : void {
-      $text = "{{cite lsa|url=https://books.google.com/books?qid=12345}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?qid=12345}}";
       $expanded = $this->process_citation($text);
       $this->AssertSame('https://books.google.com/books?id=12345', $expanded->get2('url'));
    }
    public function testGoogleBooksCleanup4() : void {
-      $text = "{{cite lsa|url=https://books.google.com/?id=12345}}";
+      $text = "{{cite LSA|url=https://books.google.com/?id=12345}}";
       $expanded = $this->process_citation($text);
       $this->AssertSame('https://books.google.com/books?id=12345', $expanded->get2('url'));
    }
    public function testGoogleBooksCleanup5() : void {
-      $text = "{{cite lsa|url=https://books.google.uk.co/books?isbn=12345}}";
+      $text = "{{cite LSA|url=https://books.google.uk.co/books?isbn=12345}}";
       $expanded = $this->process_citation($text);
       $this->AssertSame('https://books.google.com/books?isbn=12345', $expanded->get2('url'));
     }
  
     public function testGoogleBooksHashCleanup1() : void {
-      $text = "{{cite lsa|url=https://books.google.com/books?id=12345&q=xyz#q=abc}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&q=xyz#q=abc}}";
       $expanded = $this->process_citation($text);
       $this->AssertSame('https://books.google.com/books?id=12345&q=abc', $expanded->get2('url'));
     }
  
     public function testGoogleBooksHashCleanup2() : void {
-      $text = "{{cite lsa|url=https://books.google.com/books?id=12345&dq=xyz#q=abc}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&dq=xyz#q=abc}}";
       $expanded = $this->process_citation($text);
       $this->AssertSame('https://books.google.com/books?id=12345&q=abc', $expanded->get2('url'));
     }
 
     public function testGoogleBooksHashCleanup3() : void {
-      $text = "{{cite lsa|url=https://books.google.com/books?id=12345&dq=abc#q=abc}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&dq=abc#q=abc}}";
       $expanded = $this->process_citation($text);
-      $this->AssertSame('https://books.google.com/books?id=12345&dq=abc', $expanded->get2('url'));
+      $this->AssertSame('https://books.google.com/books?id=12345&q=abc', $expanded->get2('url'));
     }
 
     public function testGoogleBooksHashCleanup4() : void {
-      $text = "{{cite lsa|url=https://books.google.com/books?id=12345#q=abc}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345#q=abc}}";
       $expanded = $this->process_citation($text);
       $this->AssertSame('https://books.google.com/books?id=12345&q=abc', $expanded->get2('url'));
     }
 
     public function testGoogleBooksHashCleanup5() : void {
-      $text = "{{cite lsa|url=https://books.google.com/books?id=12345&vq=abc}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&vq=abc}}";
       $expanded = $this->process_citation($text);
       $this->AssertSame('https://books.google.com/books?id=12345&q=abc', $expanded->get2('url'));
     }
  
     public function testGoogleBooksHashCleanup6() : void {
-      $text = "{{cite lsa|url=https://books.google.com/books?id=12345&vq=abc&pg=3214&q=xyz}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&vq=abc&pg=3214&q=xyz}}";
       $expanded = $this->process_citation($text);
       $this->AssertSame('https://books.google.com/books?id=12345&q=abc&pg=3214', $expanded->get2('url'));
     }
  
     public function testGoogleBooksHashCleanup7() : void {
-      $text = "{{cite lsa|url=https://books.google.com/books?id=12345&lpg=1234}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&lpg=1234}}";
       $expanded = $this->process_citation($text);
-      $this->AssertSame('{{cite lsa|url=https://books.google.com/books?id=12345&pg=1234}}', $expanded->get2('url'));
+      $this->AssertSame('https://books.google.com/books?id=12345&pg=1234', $expanded->get2('url'));
     }
 
     public function testGoogleBooksHashCleanup8() : void {
-      $text = "{{cite lsa|url=https://books.google.com/books?id=12345&q=isbn}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&q=isbn}}";
       $expanded = $this->process_citation($text);
-      $this->AssertSame('{{cite lsa|url=https://books.google.com/books?id=12345}}', $expanded->get2('url'));
+      $this->AssertSame('https://books.google.com/books?id=12345&q=isbn', $expanded->get2('url'));
      
-      $text = "{{cite lsa|url=https://books.google.com/books?id=12345&q=inauthor:34123123}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&q=isbn1234}}";
       $expanded = $this->process_citation($text);
-      $this->AssertSame('{{cite lsa|url=https://books.google.com/books?id=12345}}', $expanded->get2('url'));
+      $this->AssertSame('https://books.google.com/books?id=12345', $expanded->get2('url'));
+     
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&q=inauthor:34123123}}";
+      $expanded = $this->process_citation($text);
+      $this->AssertSame('https://books.google.com/books?id=12345', $expanded->get2('url'));
     }
 
     public function testGoogleBooksHashCleanup9() : void {
-      $text = "{{cite lsa|url=https://books.google.com/books?id=12345&dq=isbn}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&dq=isbn}}";
       $expanded = $this->process_citation($text);
-      $this->AssertSame('{{cite lsa|url=https://books.google.com/books?id=12345}}', $expanded->get2('url'));
+      $this->AssertSame('https://books.google.com/books?id=12345&q=isbn', $expanded->get2('url'));
+
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&dq=isbn1234}}";
+      $expanded = $this->process_citation($text);
+      $this->AssertSame('https://books.google.com/books?id=12345', $expanded->get2('url'));
      
-      $text = "{{cite lsa|url=https://books.google.com/books?id=12345&dq=inauthor:34123123}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&dq=inauthor:34123123}}";
       $expanded = $this->process_citation($text);
-      $this->AssertSame('{{cite lsa|url=https://books.google.com/books?id=12345}}', $expanded->get2('url'));
+      $this->AssertSame('https://books.google.com/books?id=12345', $expanded->get2('url'));
     }
  
     public function testGoogleBooksHashCleanup10() : void {
-      $text = "{{cite lsa|url=https://books.google.com/books?id=12345&d=ser&pg=3241&lpg=321&article_id=3241&sitesec=reviews}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&d=ser&pg=3241&lpg=321&article_id=3241&sitesec=reviews}}";
       $expanded = $this->process_citation($text);
-      $this->AssertSame('{{cite lsa|url=https://books.google.com/books?id=12345&sitesec=reviews}}', $expanded->get2('url'));
+      $this->AssertSame('https://books.google.com/books?id=12345&sitesec=reviews', $expanded->get2('url'));
     }
  
     public function testGoogleBooksHashCleanup11() : void {
-      $text = "{{cite lsa|url=https://books.google.com/books?id=12345&article_id=3241}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&article_id=3241}}";
       $expanded = $this->process_citation($text);
-      $this->AssertSame('{{cite lsa|url=https://books.google.com/books?id=12345&article_id=3241}}', $expanded->get2('url'));
+      $this->AssertSame('https://books.google.com/books?id=12345&article_id=3241', $expanded->get2('url'));
 
-      $text = "{{cite lsa|url=https://books.google.com/books?id=12345&article_id=3241&q=huh}}";
+      $text = "{{cite LSA|url=https://books.google.com/books?id=12345&article_id=3241&q=huh}}";
       $expanded = $this->process_citation($text);
-      $this->AssertSame('{{cite lsa|url=https://books.google.com/books?id=12345&q=huh&article_id=3241#v=onepage}}', $expanded->get2('url'));
+      $this->AssertSame('{{cite LSA|url=https://books.google.com/books?id=12345&q=huh&article_id=3241#v=onepage}}', $expanded->get2('url'));
     }
 
 }
