7ee3681646409e56170f1fd7f63dcedd19efb582
GlazerMann <kurt@ioiowa.com>
2022-05-05 15:11:29 -0500

    Duplicates line above - copy and pasted one to many times

--- Template.php
+++ Template.php
@@ -4204,10 +4204,6 @@ final class Template {
             $this->set($param, 'Google Inc.');  // Case when Google actually IS a publisher
             return;
           }
-          if (stripos($this->get('url'), 'support.google.com') !== FALSE && stripos($publisher, 'google') !== FALSE)  {
-            $this->set($param, 'Google Inc.');  // Case when Google actually IS a publisher
-            return;
-          }
           if (stripos($publisher, 'google') !== FALSE) {
             $this_host = (string) parse_url($this->get('url'), PHP_URL_HOST);
             if (stripos($this_host, 'google') === FALSE ||
