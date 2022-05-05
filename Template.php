15e62557f56f1ae4377b37c134220390cc06124d
GlazerMann <kurt@ioiowa.com>
2022-05-05 15:47:31 -0500

    fix regex

--- Template.php
+++ Template.php
@@ -3111,7 +3111,7 @@ final class Template {
       $this->forget('id');
       return;
     }
-    while (preg_match("~\b(PMID|DOI|ISBN|ISSN|ARXIV|LCCN)[\s:]*(\d[\d\s\-]*+[^\s\}\{\|,;]*)(?:[,;] )?~iu", $id, $match)) {
+    while (preg_match("~\b(PMID|DOI|ISBN|ISSN|ARXIV|LCCN)[\s:]*(\d[\d\s\-][^\s\}\{\|,;]*)(?:[,;] )?~iu", $id, $match)) {
       $the_type = strtolower($match[1]);
       $the_data = $match[2];
       $the_all  = $match[0];
