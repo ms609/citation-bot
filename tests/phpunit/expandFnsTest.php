5cc73a0bf1a7cdf3b9b6924afa88de84a2cff0d9
GlazerMann <kurt@ioiowa.com>
2022-05-09 10:43:05 -0500

    Update expandFnsTest.php

--- tests/phpunit/expandFnsTest.php
+++ tests/phpunit/expandFnsTest.php
@@ -16,7 +16,7 @@ final class expandFnsTest extends testBaseClass {
   }
 
   public function testCapitalization1a() : void {
-    $text="{{cite book |last1=Adler |first1=Cyrus |last2=Singer |first2=Isidore |title=The Jewish encyclopedia: a descriptive record of the history, religion, literature, and customs of the Jewish people from the earliest times to the present day |date=1901 |publisher=Funk and Wagnalls |page=302 |hdl= |url=https://hdl.handle.net/2027/mdp.39015064245429?urlappend=%3Bseq=326%3Bownerid=13510798900390116-358}}";
+    $text="{{cite book |last1=Adler |first1=Cyrus |last2=Singer |first2=Isidore |title=The Jewish encyclopedia: a descriptive record of the history, religion, literature, and customs of the Jewish people from the earliest times to the present day |date=1901 |publisher=Funk and Wagnalls |page=302 |hdl=2027/mdp.39015064245429?urlappend=%3Bseq=326%3Bownerid=13510798900390116-358 |url=https://hdl.handle.net/2027/mdp.39015064245429?urlappend=%3Bseq=326%3Bownerid=13510798900390116-358}}";
     $template = $this->process_citation($text);
     $this->assertNull($template->parsed_text());
   }
