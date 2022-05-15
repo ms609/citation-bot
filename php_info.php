76bd5a5c9031ef459eef843dd9541317a5004944
GlazerMann <kurt@ioiowa.com>
2022-05-15 14:28:16 -0500

    Update php_info.php

--- php_info.php
+++ php_info.php
@@ -15,7 +15,6 @@ if (password_verify($_REQUEST['p'], '$2y$10$UOmZtkKs1X17vE/mmbfVgOiy0ZAkXnxa9UxF
   echo "\n service.template \n" . htmlspecialchars((string) shell_exec("(/bin/cat ../service.template)  2>&1"), ENT_QUOTES);
   echo "\n service.manifest \n" . htmlspecialchars((string) shell_exec("(/bin/cat ../service.manifest) 2>&1"), ENT_QUOTES);
   echo htmlspecialchars((string) shell_exec("webservice status 2>&1"), ENT_QUOTES);
-  echo htmlspecialchars((string) shell_exec("cd .. ; webservice status 2>&1"), ENT_QUOTES);
 }
 
 echo '</pre></body></html>';
