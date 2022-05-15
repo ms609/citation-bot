92163dfe521b2e8bd7ee3570762de852168b7c5c
GlazerMann <kurt@ioiowa.com>
2022-05-15 16:46:17 -0500

    Update php_info.php

--- php_info.php
+++ php_info.php
@@ -9,9 +9,9 @@ if (password_verify($_REQUEST['p'], '$2y$10$UOmZtkKs1X17vE/mmbfVgOiy0ZAkXnxa9UxF
   unset($_REQUEST['p'], $_GET['p'], $_POST['p'], $_SERVER['HTTP_X_ORIGINAL_URI'], $_SERVER['REQUEST_URI'], $_SERVER['QUERY_STRING']); // Anything that contains password string
   phpinfo(INFO_ALL);
   /** @psalm-suppress ForbiddenCode */
-  echo "\n\n" . htmlspecialchars((string) shell_exec("(/bin/rm -rf ../local ../.nfs00000000050c0a6700000001 ; /usr/bin/unlink  ../.nfs00000000050c0a6700000001)  2>&1"), ENT_QUOTES);
-  echo "\n\n" . htmlspecialchars((string) shell_exec("(/bin/ls -lahtr . .. ../.config/* ../.cache ../.cache/*)  2>&1"), ENT_QUOTES);
-  
+  echo "\n\n" . htmlspecialchars((string) shell_exec("(/bin/rm -rf ../.config/ ../.nfs00000000050c0a6700000001)  2>&1"), ENT_QUOTES);
+  echo "\n\n" . htmlspecialchars((string) shell_exec("(/bin/ls -lahtr . ..  ../.cache ../.cache/*)  2>&1"), ENT_QUOTES);
+  echo "\n\n" . htmlspecialchars((string) shell_exec("/bin/ps -u tools.citations  2>&1"), ENT_QUOTES);
   echo "\n replica.my.cnf \n" . htmlspecialchars((string) shell_exec("(/bin/cat ../replica.my.cnf)  2>&1"), ENT_QUOTES);
   echo "\n service.template \n" . htmlspecialchars((string) shell_exec("(/bin/cat ../service.template)  2>&1"), ENT_QUOTES);
   echo "\n service.manifest \n" . htmlspecialchars((string) shell_exec("(/bin/cat ../service.manifest) 2>&1"), ENT_QUOTES);
