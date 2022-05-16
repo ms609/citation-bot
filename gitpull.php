3b5668d1f231f14630a7f232c8fb4dbaaeb072e1
GlazerMann <kurt@ioiowa.com>
2022-05-16 10:36:14 -0500

    do a hard press on git

--- gitpull.php
+++ gitpull.php
@@ -10,6 +10,7 @@ if (mkdir('git_pull.lock', 0700)) {
   // Fetch only updates .git, so it is very safe. That is the first half of pull. So do it as own command
   /** @psalm-suppress ForbiddenCode */
   echo htmlspecialchars((string) shell_exec("(/usr/bin/git fetch ; /usr/bin/git pull)  2>&1"), ENT_QUOTES);
+  echo htmlspecialchars((string) shell_exec("(/usr/bin/git fetch  --all; /usr/bin/git reset --hard origin/master)  2>&1"), ENT_QUOTES);
   rmdir('git_pull.lock') ;
 } else {
   echo 'lock file exists -- aborting ';
