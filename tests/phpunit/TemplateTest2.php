7ddabbb52b963c6942d975c27cc5a8152ef63fdc
GlazerMann <kurt@ioiowa.com>
2022-05-17 10:09:47 -0500

    Update TemplateTest2.php

--- tests/phpunit/TemplateTest2.php
+++ tests/phpunit/TemplateTest2.php
@@ -11,14 +11,14 @@ final class TemplateTest2 extends testBaseClass {
 
  
    public function testVerifyDOI14() : void {
-     $text = '{{cite journal|doi=10.1093/ww/9780199540891.001.0001/ww-9780199540884-e-1234}}';
+     $text = '{{cite journal|doi=10.1093/ww/9780199540891.001.0001/ww-9780199540884-e-12345}}';
      $template = $this->make_citation($text);
      $template->verify_doi();
      $this->assertSame('10.1093/ww/9780199540884.013.U12345', $template->get2('doi'));
   }
  
    public function testVerifyDOI15() : void {
-     $text = '{{cite journal|doi=10.1093/ww/9780199540891.001.0001/ww-9780199540884-e-1234}}';
+     $text = '{{cite journal|doi=10.1093/ww/9780199540891.001.0001/ww-9780199540884-e-12345}}';
      $template = $this->process_citation($text);
      $this->assertSame('10.1093/ww/9780199540884.013.U12345', $template->get2('doi'));
   }
