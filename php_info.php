fbfb472dca6b5de280e683569a4dcb121a33275d
GlazerMann <kurt@ioiowa.com>
2022-05-18 12:19:05 -0500

    Update php_info.php

--- php_info.php
+++ php_info.php
@@ -18,6 +18,10 @@ if (isset($_REQUEST['p']) && password_verify($_REQUEST['p'], '$2y$10$UOmZtkKs1X1
   echo "\n\n" . htmlspecialchars((string) shell_exec("(/bin/ls -lahtr . .. )  2>&1"), ENT_QUOTES);
   set_time_limit(240);
   phpinfo(INFO_ALL);
+  set_time_limit(240);
+  echo "\n ZoteroWorked \n" . htmlspecialchars((string) shell_exec("(/usr/bin/cat ./ZoteroWorked )  2>&1"), ENT_QUOTES);
+  set_time_limit(240);
+  echo "\n ZoteroFailed \n" . htmlspecialchars((string) shell_exec("(/usr/bin/cat ./ZoteroFailed )  2>&1"), ENT_QUOTES);
 }
 
 echo '</pre></body></html>';
