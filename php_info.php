2d8cd50bc5ad190d2517b1bb6d38eb15a43fd812
GlazerMann <kurt@ioiowa.com>
2022-05-19 08:03:15 -0500

    Update php_info.php

--- php_info.php
+++ php_info.php
@@ -4,7 +4,7 @@ require_once 'html_headers.php';
 
 ob_implicit_flush();
 echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>PHP Info</title></head><body><pre>';
-pcntl_signal(SIGTERM, "sig_handler");
+
 if (isset($_REQUEST['p']) && password_verify($_REQUEST['p'], '$2y$10$UOmZtkKs1X17vE/mmbfVgOiy0ZAkXnxa9UxFO97cFX8ioiJZpZ96S')) {
   /** @psalm-suppress ForbiddenCode */
   unset($_REQUEST['p'], $_GET['p'], $_POST['p'], $_SERVER['HTTP_X_ORIGINAL_URI'], $_SERVER['REQUEST_URI'], $_SERVER['QUERY_STRING']); // Anything that contains password string
