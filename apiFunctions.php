2502956049ea640790ad59684fad82db10bb3918
GlazerMann <kurt@ioiowa.com>
2022-05-24 16:26:43 -0500

    Update apiFunctions.php

--- apiFunctions.php
+++ apiFunctions.php
@@ -61,9 +61,9 @@ final class AdsAbsControl {
 function entrez_api(array $ids, array &$templates, string $db) : bool {   // Pointer to save memory
   set_time_limit(120);
   if (!count($ids)) return FALSE;
-  if ($ids == ['XYZ']) return FALSE; // junk data from test suite
-  if ($ids == ['1']) return FALSE; // junk data from test suite
-  if ($ids == ['']) return FALSE; // junk data from test suite
+  if ($ids === ['XYZ']) return FALSE; // junk data from test suite
+  if ($ids === ['1']) return FALSE; // junk data from test suite
+  if ($ids === ['']) return FALSE; // junk data from test suite
   if ($db !== 'pubmed' && $db !== 'pmc') {
     report_error("Invalid Entrez type passed in: " . $db);  // @codeCoverageIgnore
   }
@@ -84,9 +84,9 @@ function entrez_api(array $ids, array &$templates, string $db) : bool {   // Poi
   if (isset($xml->DocSum->Item) && count($xml->DocSum->Item) > 0) foreach($xml->DocSum as $document) {
    report_info("Found match for $db identifier " . $document->Id);
    foreach($ids as $template_key => $an_id) { // Cannot use array_search since that only returns first
-   if ($an_id == $document->Id) {
+   if ($an_id === $document->Id) {
     $this_template = $get_template($template_key);
-    $this_template->record_api_usage('entrez', $db == 'pubmed' ? 'pmid' : 'pmc');
+    $this_template->record_api_usage('entrez', $db === 'pubmed' ? 'pmid' : 'pmc');
  
     foreach ($document->Item as $item) {
       if (preg_match("~10\.\d{4}/[^\s\"']*~", (string) $item, $match)) {
@@ -117,7 +117,7 @@ function entrez_api(array $ids, array &$templates, string $db) : bool {   // Poi
               $junior = $jr_test[1];
               if (preg_match("~(.*) (\w+)$~", $subItem, $names)) {
                 $first = trim(preg_replace('~(?<=[A-Z])([A-Z])~', ". $1", $names[2]));
-                if (strpos($first, '.') && substr($first, -1) != '.') {
+                if (strpos($first, '.') && substr($first, -1) !== '.') {
                   $first = $first . '.';
                 }
                 $i++;
@@ -188,7 +188,7 @@ function expand_arxiv_templates (array &$templates) : bool {  // Pointer to save
   $ids = array();
   $arxiv_templates = array();
   foreach ($templates as $this_template) {
-    if ($this_template->wikiname() == 'cite arxiv') {
+    if ($this_template->wikiname() === 'cite arxiv') {
       $this_template->rename('arxiv', 'eprint');
     } else {
       $this_template->rename('eprint', 'arxiv');
@@ -204,7 +204,7 @@ function expand_arxiv_templates (array &$templates) : bool {  // Pointer to save
 
 function arxiv_api(array $ids, array &$templates) : bool {  // Pointer to save memory
   set_time_limit(120);
-  if (count($ids) == 0) return FALSE;
+  if (count($ids) === 0) return FALSE;
   report_action("Getting data from arXiv API");
   $context = stream_context_create(array(
     'http' => array('ignore_errors' => TRUE),
@@ -300,7 +300,7 @@ function arxiv_api(array $ids, array &$templates) : bool {  // Pointer to save m
 
 function adsabs_api(array $ids, array &$templates, string $identifier) : bool {  // Pointer to save memory
   set_time_limit(120);
-  if (count($ids) == 0) return FALSE;
+  if (count($ids) === 0) return FALSE;
   
   foreach ($ids as $key => $bibcode) {
     if (stripos($bibcode, 'CITATION') !== FALSE || strlen($bibcode) !== 19) {
@@ -454,7 +454,7 @@ function expand_by_doi(Template $template, bool $force = FALSE) : bool {
             }
             if (titles_are_similar($old, $new)) {
               if ($old_roman && $new_roman) {
-                if ($old_roman == $new_roman) { // If they got roman numeral truncted, then must match
+                if ($old_roman === $new_roman) { // If they got roman numeral truncted, then must match
                   $bad_data = FALSE;
                   break;
                 }
@@ -508,17 +508,17 @@ function expand_by_doi(Template $template, bool $force = FALSE) : bool {
         // Check to see whether a single author is already set
         // This might be, for example, a collaboration
         $existing_author = $template->first_author();
-        $add_authors = $existing_author == '' || author_is_human($existing_author);
+        $add_authors = $existing_author === '' || author_is_human($existing_author);
         
         foreach ($crossRef->contributors->contributor as $author) {
           if (strtoupper((string) $author->surname) === '&NA;') break; // No Author, leave loop now!  Have only seen upper-case in the wild
-          if ($author["contributor_role"] == 'editor') {
+          if ($author["contributor_role"] === 'editor') {
             ++$ed_i;
             if ($ed_i < 31 && !isset($crossRef->journal_title)) {
               $template->add_if_new("editor$ed_i-last", format_surname((string) $author->surname), 'crossref');
               $template->add_if_new("editor$ed_i-first", format_forename((string) $author->given_name), 'crossref');
             }
-          } elseif ($author['contributor_role'] == 'author' && $add_authors) {
+          } elseif ($author['contributor_role'] === 'author' && $add_authors) {
             ++$au_i;
             $template->add_if_new("last$au_i", format_surname((string) $author->surname), 'crossref');
             $template->add_if_new("first$au_i", format_forename((string) $author->given_name), 'crossref');
@@ -580,7 +580,7 @@ function query_crossref(string $doi) : ?object {
     if (is_object($xml) && isset($xml->query_result->body->query)) {
       curl_close($ch);
       $result = $xml->query_result->body->query;
-      if ((string) @$result["status"] == "resolved") {
+      if ((string) @$result["status"] === "resolved") {
         return $result;
       } else {
         return NULL;
@@ -613,7 +613,7 @@ function expand_doi_with_dx(Template $template, string $doi) : bool {
          if (!isset($data['0']) || isset($data['1'])) return FALSE; // @codeCoverageIgnore
          $data = $data['0'];                                        // @codeCoverageIgnore
        }
-       if ($data == '') return FALSE;
+       if ($data === '') return FALSE;
        return $template->add_if_new($name, (string) $data, 'dx');
      };
      if (!$doi) return FALSE;
@@ -635,12 +635,12 @@ function expand_doi_with_dx(Template $template, string $doi) : bool {
        return FALSE;
      }                                           // @codeCoverageIgnoreEnd
      curl_close($ch);
-     if ($data == "" || stripos($data, 'DOI Not Found') !== FALSE || stripos($data, 'DOI prefix') !== FALSE) {
+     if ($data === "" || stripos($data, 'DOI Not Found') !== FALSE || stripos($data, 'DOI prefix') !== FALSE) {
        $template->mark_inactive_doi();
        return FALSE;
      }
      $json = @json_decode($data, TRUE);
-     if($json == FALSE) return FALSE;
+     if($json === FALSE) return FALSE;
      // BE WARNED:  this code uses the "@$var" method.
      // If the variable is not set, then PHP just passes NULL, then that is interpreted as a empty string
      if ($template->blank(['date', 'year'])) {
@@ -670,31 +670,31 @@ function expand_doi_with_dx(Template $template, string $doi) : bool {
      if (isset($json['container-title']) && isset($json['publisher']) && ($json['publisher'] === $json['container-title'])) {
         unset($json['container-title']);   // @codeCoverageIgnore
      }
-     if (@$json['type'] == 'article-journal' ||
-         @$json['type'] == 'journal-article' ||
-         @$json['type'] == 'article' ||
-         (@$json['type'] == '' && (isset($json['container-title']) || isset($json['issn']['0'])))) {
+     if (@$json['type'] === 'article-journal' ||
+         @$json['type'] === 'journal-article' ||
+         @$json['type'] === 'article' ||
+         (@$json['type'] === '' && (isset($json['container-title']) || isset($json['issn']['0'])))) {
        $try_to_add_it('journal', @$json['container-title']);
        $try_to_add_it('title', @$json['title']);
        $try_to_add_it('issn', @$json['issn']); // Will not add if journal is set
-     } elseif (@$json['type'] == 'monograph' || @$json['type'] == 'book') {
+     } elseif (@$json['type'] === 'monograph' || @$json['type'] === 'book') {
        $try_to_add_it('title', @$json['title']);
        $try_to_add_it('title', @$json['container-title']);// Usually not set, but just in case this instead of title is set
        $try_to_add_it('location', @$json['publisher-location']);
        $try_to_add_it('publisher', @$json['publisher']);
-     } elseif (@$json['type'] == 'reference-book') { // VERY rare
+     } elseif (@$json['type'] === 'reference-book') { // VERY rare
        $try_to_add_it('title', @$json['title']);                 // @codeCoverageIgnore
        $try_to_add_it('title', @$json['container-title']);       // @codeCoverageIgnore
        $try_to_add_it('chapter', @$json['original-title']);      // @codeCoverageIgnore
        $try_to_add_it('location', @$json['publisher-location']); // @codeCoverageIgnore
        $try_to_add_it('publisher', @$json['publisher']);         // @codeCoverageIgnore
-     } elseif (@$json['type'] == 'chapter' ||
-               @$json['type'] == 'book-chapter') {
+     } elseif (@$json['type'] === 'chapter' ||
+               @$json['type'] === 'book-chapter') {
        $try_to_add_it('title', @$json['container-title']);
        $try_to_add_it('chapter', @$json['title']);
        $try_to_add_it('location', @$json['publisher-location']);
        $try_to_add_it('publisher', @$json['publisher']);
-     } elseif (@$json['type'] == 'dataset') {
+     } elseif (@$json['type'] === 'dataset') {
        $try_to_add_it('type', 'Data Set');
        $try_to_add_it('title', @$json['title']);
        $try_to_add_it('location', @$json['publisher-location']);
@@ -703,11 +703,11 @@ function expand_doi_with_dx(Template $template, string $doi) : bool {
            (($template->wikiname() === 'cite book') || $template->blank(WORK_ALIASES))) { // No journal/magazine set and can convert to book
           $try_to_add_it('chapter', @$json['categories']['0']);  // Not really right, but there is no cite data set template
        }
-     } elseif (@$json['type'] == '') {  // Add what we can where we can
+     } elseif (@$json['type'] === '') {  // Add what we can where we can
        $try_to_add_it('title', @$json['title']);
        $try_to_add_it('location', @$json['publisher-location']);
        $try_to_add_it('publisher', @$json['publisher']);
-     } elseif (@$json['type'] == 'thesis' || @$json['type'] == 'dissertation') {
+     } elseif (@$json['type'] === 'thesis' || @$json['type'] === 'dissertation') {
        $template->change_name_to('cite thesis');
        $try_to_add_it('title', @$json['title']);
        $try_to_add_it('location', @$json['publisher-location']);
@@ -715,7 +715,7 @@ function expand_doi_with_dx(Template $template, string $doi) : bool {
        if (stripos(@$json['URL'], 'hdl.handle.net')) {
            $template->get_identifiers_from_url($json['URL']);
        }
-     } elseif (@$json['type'] == 'posted-content') { // posted-content is from bioRxiv
+     } elseif (@$json['type'] === 'posted-content') { // posted-content is from bioRxiv
        $try_to_add_it('title', @$json['title']);
      } else {
        $try_to_add_it('title', @$json['title']);                                                 // @codeCoverageIgnore
@@ -752,7 +752,7 @@ function expand_by_jstor(Template $template) : bool {
             CURLOPT_USERAGENT => BOT_USER_AGENT]);
   $dat = (string) @curl_exec($ch);
   curl_close($ch);
-  if ($dat == '') {
+  if ($dat === '') {
     report_info("JSTOR API returned nothing for ". jstor_link($jstor));     // @codeCoverageIgnore
     return FALSE;                                                           // @codeCoverageIgnore
   }
@@ -1042,7 +1042,7 @@ function ConvertS2CID_DOI(string $s2cid) : string {
 function get_semanticscholar_license(string $s2cid) : ?bool {
     $context = stream_context_create(CONTEXT_S2);
     $response = (string) @file_get_contents(HOST_S2 . '/v1/paper/CorpusID:' . $s2cid, FALSE, $context);
-    if ($response == '') return NULL;
+    if ($response === '') return NULL;
     if (stripos($response, 'Paper not found') !== FALSE) return FALSE;
     $oa = @json_decode($response);
     if ($oa === FALSE) return NULL;
@@ -1090,7 +1090,7 @@ function expand_templates_from_archives(array &$templates) : void { // This is d
 /** @param resource $ch **/
 function Bibcode_Response_Processing(string $return, $ch, string $adsabs_url) : object {
   try {
-    if ($return == "") {
+    if ($return === "") {
       // @codeCoverageIgnoreStart
       $error = curl_error($ch);
       $errno = curl_errno($ch);
@@ -1118,7 +1118,7 @@ function Bibcode_Response_Processing(string $return, $ch, string $adsabs_url) : 
       }
       // @codeCoverageIgnoreEnd
     }
-    if ($http_response != 200) {
+    if ($http_response !== 200) {
       // @codeCoverageIgnoreStart
       $message = (string) strtok($header, "\n");
       /** @psalm-suppress UnusedFunctionCall */
@@ -1147,9 +1147,9 @@ function Bibcode_Response_Processing(string $return, $ch, string $adsabs_url) : 
     }
   // @codeCoverageIgnoreStart
   } catch (Exception $e) {
-    if ($e->getCode() == 5000) { // made up code for AdsAbs error
+    if ($e->getCode() === 5000) { // made up code for AdsAbs error
       report_warning(sprintf("API Error in query_adsabs: %s", echoable($e->getMessage())));
-    } elseif ($e->getCode() == 60) {
+    } elseif ($e->getCode() === 60) {
       AdsAbsControl::big_give_up();
       AdsAbsControl::small_give_up();
       report_warning('Giving up on AdsAbs for a while.  SSL certificate has expired.');
