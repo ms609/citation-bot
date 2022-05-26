22c81aa6b914d0f16120e707d0fa6b80c22a1b9c
GlazerMann <kurt@ioiowa.com>
2022-05-26 07:48:47 -0500

    Update apiFunctions.php

--- apiFunctions.php
+++ apiFunctions.php
@@ -703,7 +703,7 @@ function expand_doi_with_dx(Template $template, string $doi) : bool {
            (($template->wikiname() === 'cite book') || $template->blank(WORK_ALIASES))) { // No journal/magazine set and can convert to book
           $try_to_add_it('chapter', @$json['categories']['0']);  // Not really right, but there is no cite data set template
        }
-     } elseif (@$json['type'] === '') {  // Add what we can where we can
+     } elseif (@$json['type'] === '' || !isset($json['type'])) {  // Add what we can where we can
        $try_to_add_it('title', @$json['title']);
        $try_to_add_it('location', @$json['publisher-location']);
        $try_to_add_it('publisher', @$json['publisher']);
