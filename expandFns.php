eb9e5314eb44b5db8ca87a3bb6d76041608816eb
GlazerMann <kurt@ioiowa.com>
2022-05-26 16:36:47 -0500

    Make sure script does not time out

--- expandFns.php
+++ expandFns.php
@@ -83,6 +83,7 @@ function throttle_dx () : void {
 }
 
 function is_doi_works(string $doi) : ?bool {
+  set_time_limit(120);
   $doi = trim($doi);
   // And now some obvious fails
   if (strpos($doi, '/') === FALSE) return FALSE;
@@ -104,19 +105,23 @@ function is_doi_works(string $doi) : ?bool {
   throttle_dx();
 
   $context = stream_context_create(CONTEXT_INSECURE);
+  set_time_limit(120);
   $headers_test = @get_headers("https://doi.org/" . doi_encode($doi), GET_THE_HEADERS, $context);
   $context = stream_context_create(CONTEXT_INSECURE_11);
   if ($headers_test === FALSE) {
-     sleep(2);                                                                          // @codeCoverageIgnore
+     sleep(2);                                                                                        // @codeCoverageIgnore
+     set_time_limit(120);                                                                             // @codeCoverageIgnore
      $headers_test = @get_headers("https://doi.org/" . doi_encode($doi), GET_THE_HEADERS, $context);  // @codeCoverageIgnore
   }
   if ($headers_test === FALSE) {
-     sleep(5);                                                                          // @codeCoverageIgnore
+     sleep(5);                                                                                        // @codeCoverageIgnore
+     set_time_limit(120);                                                                             // @codeCoverageIgnore
      $headers_test = @get_headers("https://doi.org/" . doi_encode($doi), GET_THE_HEADERS, $context);  // @codeCoverageIgnore
   } elseif ((empty($headers_test['Location']) && empty($headers_test['location'])) || stripos($headers_test[0], '404 Not Found') !== FALSE || stripos($headers_test[0], 'HTTP/1.1 404') !== FALSE) {
-     sleep(5);                                                                          // @codeCoverageIgnore
+     sleep(5);                                                                                        // @codeCoverageIgnore
+     set_time_limit(120);                                                                             // @codeCoverageIgnore
      $headers_test = @get_headers("https://doi.org/" . doi_encode($doi), GET_THE_HEADERS, $context);  // @codeCoverageIgnore
-     if ($headers_test === FALSE) return FALSE; /** We trust previous failure **/       // @codeCoverageIgnore
+     if ($headers_test === FALSE) return FALSE; /** We trust previous failure **/                     // @codeCoverageIgnore
   }
   if (preg_match('~^10\.1038/nature\d{5}$~i', $doi) && $headers_test === FALSE) return FALSE; // Nature dropped the ball for now TODO - https://dx.doi.org/10.1038/nature05009
   if ($headers_test === FALSE) return NULL; // most likely bad, but will recheck again and again
@@ -1132,13 +1137,16 @@ function is_hdl_works(string $hdl) {
   $context = stream_context_create(CONTEXT_INSECURE_11); // HDL does 1.1 always
   usleep(100000);
   $test_url = "https://hdl.handle.net/" . $hdl;
+  set_time_limit(120);
   $headers_test = @get_headers($test_url, GET_THE_HEADERS, $context);
   if ($headers_test === FALSE) {
       sleep(3);   // @codeCoverageIgnore
+      set_time_limit(120);                                                // @codeCoverageIgnore
       $headers_test = @get_headers($test_url, GET_THE_HEADERS, $context); // @codeCoverageIgnore
   }
   if ($headers_test === FALSE) {
       sleep(8);   // @codeCoverageIgnore
+      set_time_limit(120);                                                // @codeCoverageIgnore
       $headers_test = @get_headers($test_url, GET_THE_HEADERS, $context); // @codeCoverageIgnore
   }
   if ($headers_test === FALSE) return NULL; // most likely bad, but will recheck again and again
