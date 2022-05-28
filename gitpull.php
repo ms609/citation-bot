d022a47165a8979433d79bc85eefecd2f02e0604
GlazerMann <kurt@ioiowa.com>
2022-05-27 21:09:08 -0500

    Update gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -10,9 +10,25 @@ if (mkdir('git_pull.lock', 0700)) {
   // Fetch only updates .git, so it is very safe. That is the first half of pull. So do it as own command
   /** @psalm-suppress ForbiddenCode */
   echo htmlspecialchars((string) shell_exec("(/usr/bin/git fetch  --all; /usr/bin/git reset --hard origin/master)  2>&1"), ENT_QUOTES);
-  rmdir('git_pull.lock') ;
+  rmdir('git_pull.lock');
+  exit('</pre></main></body></html>');
 } else {
-  echo 'lock file exists -- aborting ';
+  sleep(1);
+  clearstatcache();
+  if (!file_exists('git_pull.lock')) exit("try again</pre></main></body></html>");
+  sleep(1);
+  clearstatcache();
+  if (!file_exists('git_pull.lock')) exit("try again</pre></main></body></html>");
+  sleep(1);
+  clearstatcache();
+  if (!file_exists('git_pull.lock')) exit("try again</pre></main></body></html>");
+  sleep(1);
+  clearstatcache();
+  if (!file_exists('git_pull.lock')) exit("try again</pre></main></body></html>");
+  sleep(1);
+  clearstatcache();
+  if (!file_exists('git_pull.lock')) exit("try again</pre></main></body></html>");
+  rmdir('git_pull.lock');
+  exit('try again</pre></main></body></html>');
 }
-echo '</pre></main></body></html>';
 ?>
