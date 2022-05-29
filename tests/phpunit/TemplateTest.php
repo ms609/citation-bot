cadb0e540e165ccbb3595a66ce2f8be9de50c77b
GlazerMann <kurt@ioiowa.com>
2022-05-29 11:24:08 -0500

    Fix test

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -3695,8 +3695,8 @@ EP - 999 }}';
   }
  
   public function testBlankOtherThanComments() : void {
-    $text_in = "{{cite journal| title=<!-- comment1 -->  <!-- comment2 -->| journal= | issue=3 <!-- comment3 -->| volume=65 |lccn= <!-- comment4 --> cow <!-- comment5 --> }}";
-    $template = $this->make_citation($text_in);
+    $text_in = "{{cite journal| title= # # # CITATION_BOT_PLACEHOLDER_COMMENT 1 # # #  # # # CITATION_BOT_PLACEHOLDER_COMMENT 2 # # # | journal= | issue=3 # # # CITATION_BOT_PLACEHOLDER_COMMENT 3 # # #| volume=65 |lccn= # # # CITATION_BOT_PLACEHOLDER_COMMENT 4 # # # cow # # # CITATION_BOT_PLACEHOLDER_COMMENT 5 # # # }}";
+    $template = $this->make_citation($text_in); // Have to explicitly set comments above since Page() encodes and then decodes them
     $this->assertTrue($template->blank_other_than_comments('isbn'));
     $this->assertTrue($template->blank_other_than_comments('title'));
     $this->assertTrue($template->blank_other_than_comments('journal'));
@@ -3704,7 +3704,6 @@ EP - 999 }}';
     $this->assertFalse($template->blank_other_than_comments('volume'));
     $this->assertFalse($template->blank_other_than_comments('lccn'));
   }
-
  
   public function testCleanUpSomeURLS1() : void {
     $text_in = "{{cite web| url = https://www.youtube.com/watch%3Fv=9NHSOrUHE6c}}";
