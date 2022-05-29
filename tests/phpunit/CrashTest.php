4c12e84f83332d5d37e35cc5f60605a5dddfabff
GlazerMann <kurt@ioiowa.com>
2022-05-29 11:19:28 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -10,9 +10,8 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class CrashTest extends testBaseClass {
 
   public function testBlankOtherThanComments() : void {
-    $template = new Template();
     $text_in = "{{cite journal| title= # # # CITATION_BOT_PLACEHOLDER_COMMENT 1 # # #  # # # CITATION_BOT_PLACEHOLDER_COMMENT 2 # # # | journal= | issue=3 # # # CITATION_BOT_PLACEHOLDER_COMMENT 3 # # #| volume=65 |lccn= # # # CITATION_BOT_PLACEHOLDER_COMMENT 4 # # # cow # # # CITATION_BOT_PLACEHOLDER_COMMENT 5 # # # }}";
-    $page = $this->make_citation($text_in);
+    $template = $this->make_citation($text_in);
     $this->assertTrue($template->blank_other_than_comments('isbn'));
     $this->assertTrue($template->blank_other_than_comments('title'));
     $this->assertTrue($template->blank_other_than_comments('journal'));
