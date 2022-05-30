c65a1e94f3ba0eeae026f87bc1a6e6f794911b8c
GlazerMann <kurt@ioiowa.com>
2022-05-30 15:59:36 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -10,20 +10,32 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class CrashTest extends testBaseClass {
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
-      $text = '{{cite document|chapter=xyx|title=cvzxc}}';
+      $text = '{{cite document}}';
       $template = $this->make_citation($text);
+      $template->set('title', 'dsfas');
+      $template->set('chapter', 'dsfas');
       $template->final_tidy();
    
-      $text = '{{cite document|chapter=xyx|title=cvzxc|series=dsfadsdsf}}';
+      $text = '{{cite document}}';
       $template = $this->make_citation($text);
+      $template->set('title', 'dsfas');
+      $template->set('chapter', 'dsfas');
+      $template->set('series', 'dsdsdfssfas');
       $template->final_tidy();
    
-      $text = '{{cite document|chapter=xyx|title=cvzxc|work=dsfadsdsf}}';
+      $text = '{{cite document}}';
       $template = $this->make_citation($text);
+      $template->set('title', 'dsfas');
+      $template->set('chapter', 'dsfas');
+      $template->set('work', 'dsdsdfssfas');
       $template->final_tidy();
    
-      $text = '{{cite document|chapter=xyx|title=cvzxc|work=dsfadsdsf|series=4532453dsfadfas}}';
+      $text = '{{cite document}}';
       $template = $this->make_citation($text);
+      $template->set('title', 'dsfas');
+      $template->set('chapter', 'dsfas');
+      $template->set('work', 'dsdsdfssfas');
+      $template->set('series', 'dsd344dfssfas');
       $template->final_tidy();
   }
 
