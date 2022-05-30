2b7772cf2db8413c93ba3daa5243872c35957616
GlazerMann <kurt@ioiowa.com>
2022-05-30 16:06:19 -0500

    Update Template.php

--- Template.php
+++ Template.php
@@ -6050,6 +6050,7 @@ final class Template {
       if (($this->wikiname() === 'cite document' || $this->wikiname() === 'cite journal' || $this->wikiname() === 'cite web') &&
           (strpos($this->get('isbn'), '978-0-19') === 0 || strpos($this->get('isbn'), '978019') === 0 || strpos($this->get('isbn'), '978-019') === 0)) {
          $this->change_name_to('cite book',  TRUE, TRUE);
+              echo __LINE__ . " SHoulD "HAVE \n";
       }
       echo __LINE__ . " ". $this->name . "\n";
       if ($this->blank('pmc-embargo-date')) $this->forget('pmc-embargo-date'); // Do at the very end, so we do not delete it, then add it later in a different position
@@ -6200,6 +6201,7 @@ final class Template {
       echo __LINE__ . " ". $this->name . "\n";
       if (($this->wikiname() === 'cite journal' || $this->wikiname() === 'cite document' || $this->wikiname() === 'cite web') && $this->has('chapter')) { // At least avoid a template error
         $this->change_name_to('cite book',  TRUE, TRUE);
+         echo __LINE__ . " SHoulD "HAVE \n";
       }
       echo __LINE__ . " ". $this->name . "\n";
       if (($this->wikiname() === 'cite web' || $this->wikiname() === 'cite news') &&
