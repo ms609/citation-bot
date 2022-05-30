c07d9bebfbe8d2a1c64937c54706b7fedc7b5b45
GlazerMann <kurt@ioiowa.com>
2022-05-30 09:23:46 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -16,22 +16,22 @@ final class CrashTest extends testBaseClass {
     public function testFinalTidyThings1() : void {
       $text = "{{Cite web|year=|year=2000}}";
       $expanded = $this->process_citation($text);
-      echo $expanded=>parsed_text();
+      echo $expanded->parsed_text();
       $text = "{{Cite web|year=2000|year=2000}}";
       $expanded = $this->process_citation($text);
-      echo $expanded=>parsed_text();
+      echo $expanded->parsed_text();
       $text = "{{Cite web|year|year=2000}}";
       $expanded = $this->process_citation($text);
-      echo $expanded=>parsed_text();
+      echo $expanded->parsed_text();
       $text = "{{Cite web|year|year}}";
       $expanded = $this->process_citation($text);
-      echo $expanded=>parsed_text();
+      echo $expanded->parsed_text();
       $text = "{{Cite web|year|year 2000}}";
       $expanded = $this->process_citation($text);
-      echo $expanded=>parsed_text();
+      echo $expanded->parsed_text();
       $text = "{{Cite web|year 2000|year }}";
       $expanded = $this->process_citation($text);
-      echo $expanded=>parsed_text();
+      echo $expanded->parsed_text();
     }
 
 
