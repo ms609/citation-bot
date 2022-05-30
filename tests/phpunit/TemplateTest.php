ef5d81f5ba659cd7be2e57077f750fe9a399c55a
GlazerMann <kurt@ioiowa.com>
2022-05-30 09:48:15 -0500

    expand test case

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -1872,6 +1872,34 @@ final class TemplateTest extends testBaseClass {
       $text = '{{citation|year=2000|year=2001|year=2000|year=2001|year=2000}}';
       $prepared = $this->process_citation($text);
       $this->assertSame('{{citation|DUPLICATE_year=2000|DUPLICATE_year=2001|DUPLICATE_year=2000|DUPLICATE_year=2001|year=2000}}', $prepared->parsed_text());
+  
+      $text = "{{Cite web|year=|year=2000}}";
+      $expanded = $this->process_citation($text);
+      $this->assertSame('{{Cite web|year=2000}}', $expanded->parsed_text());
+
+      $text = "{{Cite web|year=2000|year=2000}}";
+      $expanded = $this->process_citation($text);
+      $this->assertSame('{{Cite web|year=2000}}', $expanded->parsed_text());
+
+      $text = "{{Cite web|year|year=2000}}";
+      $expanded = $this->process_citation($text);
+      $this->assertSame('{{Cite web|year|year=2000}}', $expanded->parsed_text());
+
+      $text = "{{Cite web|year|year}}";
+      $expanded = $this->process_citation($text);
+      $this->assertSame('{{Cite web|year|year}}', $expanded->parsed_text());
+
+      $text = "{{Cite web|year|year 2000}}";
+      $expanded = $this->process_citation($text);
+      $this->assertSame('{{Cite web|year| year=2000 }}', $expanded->parsed_text());
+
+      $text = "{{Cite web|year 2000|year }}";
+      $expanded = $this->process_citation($text);
+      $this->assertSame('{{Cite web|year | year=2000 }}', $expanded->parsed_text());
+     
+      $text = '{{citation|year=2000|year=||||||||||||||||||||||||||||||||||||||||}}';
+      $prepared = $this->process_citation($text);
+      $this->assertSame('{{citation|year=2000}}', $prepared->parsed_text());
   }
  
   public function testFixCAPSJunk() : void {
