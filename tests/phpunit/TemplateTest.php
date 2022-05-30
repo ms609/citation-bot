e6c96f6cc3fd1cb3066bf333b73ec9002411a583
GlazerMann <kurt@ioiowa.com>
2022-05-30 08:57:02 -0500

    add test

--- tests/phpunit/TemplateTest.php
+++ tests/phpunit/TemplateTest.php
@@ -3947,4 +3947,20 @@ EP - 999 }}';
     $this->assertSame("https://xy.com", $template->get2('archive-url'));
     $this->assertNull($template->get2('archive'));
   }
+ 
+  public function testModsArray() : void {
+    $text = '{{cite journal | citation_bot_placeholder_bare_url = XYX }}';
+    $template = $this->make_citation($text);
+    $template->add('title', 'Thus');
+    $this->assertNotNull($template->get2('citation_bot_placeholder_bare_url'));
+    $array = $template->modifications();
+    $expected = array ( 'modifications' =>  array ( 0 => 'title',  ),
+                        'additions' =>  array ( 0 => 'title',  ),
+                        'deletions' =>  array ( 0 => 'citation_bot_placeholder_bare_url', ),
+                        'changeonly' => array (  ),
+                        'dashes' => FALSE,
+                        'names' => FALSE,);
+    $this->assertTrue($array == $expected);
+    $this->assertNull($template->get2('citation_bot_placeholder_bare_url'));
+  }
 }
