61b49d32e5fcc5dca895ea1342bf4c71da5c8b1e
GlazerMann <kurt@ioiowa.com>
2022-05-29 20:28:26 -0500

    Update Page.php

--- Page.php
+++ Page.php
@@ -276,6 +276,7 @@ class Page {
       $this_template = $all_templates[$i];
       if (in_array($this_template->wikiname(), TEMPLATES_WE_PROCESS)) {
         $our_templates[] = $this_template;
+        $this_template->correct_param_mistakes();
         $this_template->prepare();
       } elseif (in_array($this_template->wikiname(), TEMPLATES_WE_SLIGHTLY_PROCESS)) {
         $our_templates_slight[] = $this_template;
