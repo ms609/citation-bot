d4f16fba9a1b771cc289e5492a66a33cdb7865e5
GlazerMann <kurt@ioiowa.com>
2022-05-30 08:46:57 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -10,24 +10,19 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class CrashTest extends testBaseClass {
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
-    $bad_page = BAD_PAGE_API;
-    $bad_page = str_replace(' ', '_', $bad_page);
-    if ($bad_page !== "") {
-      define("TRAVIS_PRINT", "YES");
-      Zotero::create_ch_zotero();
-      WikipediaBot::make_ch();
-      $page = new TestPage();
-      $page->get_text_from($bad_page);
-      AdsAbsControl::big_back_on();
-      AdsAbsControl::small_back_on();
-      Zotero::unblock_zotero();
-      $page->expand_text();
-      AdsAbsControl::small_give_up();
-      AdsAbsControl::big_give_up();
-      Zotero::block_zotero();
-      $this->assertTrue(FALSE); // prevent us from git committing with a website included
-    }
-    $this->assertTrue(TRUE);
+    $this->assertTrue(FALSE);
   }
+ 
+  public function testModsArray() : void {
+    $text = '{{cite journal | CITATION_BOT_PLACEHOLDER_BARE_URL = XYX }}';
+    $template = $this->make_citation($text);
+    $template->add('title', 'Thus');
+    $this->assertNotNull($template->get2('CITATION_BOT_PLACEHOLDER_BARE_URL'));
+    $array = $template->modifications();
+    var_export($array);
+    $this->assertNull($template->get2('CITATION_BOT_PLACEHOLDER_BARE_URL'));
+    $this->assertTrue(FALSE);
+  }
+
 
 }
