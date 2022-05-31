976b1d67c3f72867df9639181a2e6fd1e5269c89
GlazerMann <kurt@ioiowa.com>
2022-05-31 12:37:42 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -10,11 +10,9 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class CrashTest extends testBaseClass {
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
-    $text = '{{cite journal | doi =  10.1103/PhysRevE.103.012115 }}';  // Give OSTI, thus will not add url
-    $template = $this->make_citation($text);
-    $template->get_open_access_url();
+    $text = '{{citation|arxiv=2202.10024}}';
+    $template = $this->process_citation($text);
     $this->assertEquals('1778027', $template->get2('osti'));
-    $template->get_open_access_url();
     $this->assertNull($template->get2('url'));
   }
 
