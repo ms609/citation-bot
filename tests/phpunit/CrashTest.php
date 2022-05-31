9ac3b699540a086542ea76e6cd65e228d3254742
GlazerMann <kurt@ioiowa.com>
2022-05-31 09:08:52 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class CrashTest extends testBaseClass {
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
-    $t = $this->process_citation('{{cite journal|doi=10.11468/seikatsueisei1925.16.2_123}}');
+    $t = $this->process_citation('{{cite journal|doi=10.14989/doctor.k19250}}');
   }
 
 }
