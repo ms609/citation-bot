5b21a5a3847b5add3cd984c2b5946eac2a15a16a
GlazerMann <kurt@ioiowa.com>
2022-05-31 12:32:59 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -10,7 +10,12 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class CrashTest extends testBaseClass {
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
-    $t = $this->process_citation('{{cite journal   | last = Tencer   | first = John   | author2 = Forsberg, Kelsey Meeks   | title =  Postprocessing techniques for gradient percolation predictions on the square lattice   | journal = Phys. Rev. E    | volume = 103   | issue = 1   | year = 2021   | pages = 012115   | doi =  10.1103/PhysRevE.103.012115| pmid = 33601521  | bibcode = 2021PhRvE.103a2115T  | osti = 1778027  | s2cid = 231961701  }}');
+    $text = '{{cite journal | doi =  10.1103/PhysRevE.103.012115 }}';  // Give OSTI, thus will not add url
+    $template = $this->make_citation($text);
+    $template->get_open_access_url();
+    $this->assertEquals('1778027', $template->get2('osti'));
+    $template->get_open_access_url();
+    $this->assertNull($template->get2('url'));
   }
 
 }
