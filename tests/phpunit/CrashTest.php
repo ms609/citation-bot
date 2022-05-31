a4c2cafcfaa890d6e020bd9100a039fb5549a4b5
GlazerMann <kurt@ioiowa.com>
2022-05-31 12:38:56 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class CrashTest extends testBaseClass {
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
-    $text = '{{citation|arxiv=2202.10024}}';
+    $text = '{{citation|arxiv=2202.10024|title=TESS discovery of a sub-Neptune orbiting a mid-M dwarf TOI-2136}}';
     $template = $this->process_citation($text);
     $this->assertEquals('1778027', $template->get2('osti'));
     $this->assertNull($template->get2('url'));
