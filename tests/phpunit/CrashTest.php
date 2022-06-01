a5a6e5a07360b74fe08ba32d9621e5193327652a
GlazerMann <kurt@ioiowa.com>
2022-05-31 19:16:28 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -18,7 +18,8 @@ final class CrashTest extends testBaseClass {
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
     $text = "{{Cite web | url = https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2491514/pdf/annrcse01476-0076.pdf}}";
     $expanded = $this->process_citation($text);
-    $this->assertSame('Original code gives {{Cite web | url = https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2491514/pdf/annrcse01476-0076.pdf| pmc = 2491514}}', $expanded->parse_text());
+    $this->assertSame('Original code gives {{Cite web | url = https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2491514/pdf/annrcse01476-0076.pdf| pmc = 2491514}}',
+                      $expanded->parsed_text());
   }
 
 }
