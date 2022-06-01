febeefea91c5903b835d584b5d207fcebb3710f7
GlazerMann <kurt@ioiowa.com>
2022-06-01 17:39:21 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -16,9 +16,6 @@ final class CrashTest extends testBaseClass {
   }
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
-    $text = "{{Cite web | doi=10.25300/MISQ/2015/39.4.9}}";
-    $expanded = $this->process_citation($text);
-   
     $text = "{{Cite web | doi=10.3403/bsiso10294}}";
     $expanded = $this->process_citation($text);
     $this->assertSame('huh', $expanded->parsed_text());
