3873026b277ed719d7b76bab4c529faa50442f38
GlazerMann <kurt@ioiowa.com>
2022-06-02 09:09:14 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -12,14 +12,17 @@ final class CrashTest extends testBaseClass {
    public function testblockCI() : void {
     Zotero::create_ch_zotero();
     WikipediaBot::make_ch();
-    $this->assertTrue(FALSE);
   }
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
     var_export(is_doi_works("10.3403/bsiso10294"));
     var_export(is_doi_works("10.21236/ada614052"));
-    var_export(is_doi_works("10.25300/MISQ/2015/39.4.9"));
-    var_export(is_doi_works("10.25300/misq/2019/12338"));
+    var_export(is_doi_works("10.3403/bsiso10294"));
+    var_export(is_doi_works("10.1038/scientificamerican0302-76"));
+    var_export(is_doi_works("10.22478/ufpb.1809-4783.2020v30n3.52231"));
+    var_export(is_doi_works("10.3174/ajnr.A2583"));
+    var_export(is_doi_works("10.5373/JARDCS/V12SP5/20201874"));
+    var_export(is_doi_works("10.7861/clinmedicine.7-6-562"));
   }
 
 }
