0b0b7cb7d97760d72802392770873b560055c7bc
GlazerMann <kurt@ioiowa.com>
2022-06-02 14:58:07 -0500

    contentdirections

--- expandFns.php
+++ expandFns.php
@@ -161,7 +161,7 @@ function is_doi_works(string $doi) : ?bool {
   $resp0 = (string) @$headers_test[0];
   $resp1 = (string) @$headers_test[1];
   $resp2 = (string) @$headers_test[2];
-  if (stripos((string) @$headers_test['Location'][0] . (string) @$headers_test['Location'][1] . (string) @$headers_test['Location'][2] .(string) @$headers_test['location'][0] . (string) @$headers_test['location'][1] . (string) @$headers_test['location'][2] , 'contentdirections.com') !== FALSE) return FALSE;
+  if (stripos((string) @implode(@$headers_test['Location']) . (string) @implode(@$headers_test['location']), 'contentdirections.com') !== FALSE) return FALSE;
   if (stripos($resp0, '404 Not Found') !== FALSE         || stripos($resp0, 'HTTP/1.1 404') !== FALSE) return FALSE; // Bad
   if (stripos($resp0, '302 Found') !== FALSE             || stripos($resp0, 'HTTP/1.1 302') !== FALSE) return TRUE;  // Good
   if (stripos($resp0, '301 Moved Permanently') !== FALSE || stripos($resp0, 'HTTP/1.1 301') !== FALSE) { // Could be DOI change or bad prefix
