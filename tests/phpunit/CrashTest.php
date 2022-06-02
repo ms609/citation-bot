759d85c7c81c7aedd74e8903c1e98606b0452501
GlazerMann <kurt@ioiowa.com>
2022-06-02 09:17:31 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -16,13 +16,6 @@ final class CrashTest extends testBaseClass {
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
     var_export(is_doi_works("10.3403/bsiso10294"));
-    var_export(is_doi_works("10.21236/ada614052"));
-    var_export(is_doi_works("10.3403/bsiso10294"));
-    var_export(is_doi_works("10.1038/scientificamerican0302-76"));
-    var_export(is_doi_works("10.22478/ufpb.1809-4783.2020v30n3.52231"));
-    var_export(is_doi_works("10.3174/ajnr.A2583"));
-    var_export(is_doi_works("10.5373/JARDCS/V12SP5/20201874"));
-    var_export(is_doi_works("10.7861/clinmedicine.7-6-562"));
   }
 
 }
