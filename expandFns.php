e403f1d68ebb5ee02720075f1af878fec1193eca
GlazerMann <kurt@ioiowa.com>
2022-06-02 14:52:28 -0500

    More 10.2277 code

--- expandFns.php
+++ expandFns.php
@@ -130,6 +130,7 @@ function is_doi_works(string $doi) : ?bool {
   }
   if (preg_match('~^10\.1038/nature\d{5}$~i', $doi) && $headers_test === FALSE) return FALSE; // Nature dropped the ball for now TODO - https://dx.doi.org/10.1038/nature05009
   if ($headers_test === FALSE) { // Use CURL instead
+    if (strpos($doi, '10.2277/') === 0) return FALSE;
     $ch = curl_init();
     curl_setopt_array($ch,
             [CURLOPT_HEADER => TRUE,
@@ -160,7 +161,7 @@ function is_doi_works(string $doi) : ?bool {
   $resp0 = (string) @$headers_test[0];
   $resp1 = (string) @$headers_test[1];
   $resp2 = (string) @$headers_test[2];
-  if (stripos((string) @implode(@$headers_test['Location']) . (string) @implode(@$headers_test['location']), 'contentdirections.com') !== FALSE) return FALSE;
+  if (stripos((string) @$headers_test['Location'][0] . (string) @$headers_test['Location'][1] . (string) @$headers_test['Location'][2] .(string) @$headers_test['location'][0] . (string) @$headers_test['location'][1] . (string) @$headers_test['location'][2] , 'contentdirections.com') !== FALSE) return FALSE;
   if (stripos($resp0, '404 Not Found') !== FALSE         || stripos($resp0, 'HTTP/1.1 404') !== FALSE) return FALSE; // Bad
   if (stripos($resp0, '302 Found') !== FALSE             || stripos($resp0, 'HTTP/1.1 302') !== FALSE) return TRUE;  // Good
   if (stripos($resp0, '301 Moved Permanently') !== FALSE || stripos($resp0, 'HTTP/1.1 301') !== FALSE) { // Could be DOI change or bad prefix
