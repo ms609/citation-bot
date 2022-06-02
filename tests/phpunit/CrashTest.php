bae7a5bc44c8f285cf0e26c9f6d32fdc6b6f579e
GlazerMann <kurt@ioiowa.com>
2022-06-02 15:11:13 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -10,8 +10,8 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class CrashTest extends testBaseClass {
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
-    is_doi_works('10.2277/0521808448');
-    is_doi_works('10.2277/0521391679');
+    var_export(is_doi_works('10.2277/0521808448'));
+    var_export(is_doi_works('10.2277/0521391679'));
   }
 
 }
