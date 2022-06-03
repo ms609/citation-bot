0cce15103dd3dc58e202b8a44efb79a119d01548
GlazerMann <kurt@ioiowa.com>
2022-06-03 14:01:51 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -10,9 +10,10 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class CrashTest extends testBaseClass {
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
-   // $this->process_citation('{{Cite web |title= | year=2003 |url=https://patron.library.wisc.edu/authn/splash?url=https://www.oxfordartonline.com/groveart/view/10.1093/gao/9781884446054.001.0001/oao-9781884446054-e-7000083760 |access-date=2022-04-23 |doi=10.1093/gao/9781884446054.article.T083760 | last1=Fort | first1=Ilene Susan | title= Ten American Painters (The Ten)}}');
-    $this->process_citation('{{Cite web |title= | year=2003 | title= Ten}}');
-
+      $text = '{{Cite web |title= | year=2003 | title= Ten}}';
+      $expanded = $this->process_citation($text);
+      $text = "{{citation|year=|title=X|year=2000}}"; // Something between the two but with blank first is different code path
+      $expanded = $this->process_citation($text);
   }
 
 }
