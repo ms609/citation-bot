f30bf0012b31f3b78de85e397961b4fa6e8b8302
GlazerMann <kurt@ioiowa.com>
2022-06-03 11:46:48 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -10,8 +10,7 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class CrashTest extends testBaseClass {
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
-    var_export(is_doi_works('10.2277/0521808448'));
-    var_export(is_doi_works('10.2277/0521391679'));
+    $this->process_citation('{{Cite web |title= | year=2003 |url=https://patron.library.wisc.edu/authn/splash?url=https://www.oxfordartonline.com/groveart/view/10.1093/gao/9781884446054.001.0001/oao-9781884446054-e-7000083760 |access-date=2022-04-23 |doi=10.1093/gao/9781884446054.article.T083760 | last1=Fort | first1=Ilene Susan | title= Ten American Painters (The Ten)}}');
   }
 
 }
