e9407af308bd0644503dc391d71dd4b9cabc2e1a
GlazerMann <kurt@ioiowa.com>
2022-06-03 09:05:44 -0500

    simpler code

--- Page.php
+++ Page.php
@@ -41,16 +41,10 @@ class Page {
 
     $details = WikipediaBot::ReadDetails($title);
 
-    if (!isset($details->query)) {
-      // @codeCoverageIgnoreStart
-      $message = "Error: Could not fetch page. " . (string) @$details->error->info; 
-      report_warning($message);
-      return FALSE;
-      // @codeCoverageIgnoreEnd
-    }
     if (!isset($details->query->pages)) {
       // @codeCoverageIgnoreStart
-      $message = "Error: Did not fetch page. " . (string) @$details->error->info; 
+      $message = "Error: Could not fetch page.";
+      if (isset($details->error->info))  $message = $message . " " . (string) $details->error->info; 
       report_warning($message);
       return FALSE;
       // @codeCoverageIgnoreEnd
