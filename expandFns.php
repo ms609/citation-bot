9792e574fec51ed5940912921af677b224c64fdd
GlazerMann <kurt@ioiowa.com>
2022-06-06 14:22:30 -0500

    better regex

--- expandFns.php
+++ expandFns.php
@@ -341,7 +341,7 @@ function wikify_external_text(string $title) : string {
   if (preg_match("~^\. (.+)$~", $title, $matches)) {
     $title = trim($matches[1]);
   }
- if (preg_match("~^(.+)(\.\s)+\.$~s", $title, $matches)) {
+ if (preg_match("~^(.+)(\.\s+)\.$~s", $title, $matches)) {
     $title = trim($matches[1] . ".");
   }
   $title_orig = '';
