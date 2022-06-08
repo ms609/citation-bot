893f366f02710759dbd6f6a799ee1714f24f7819
GlazerMann <kurt@ioiowa.com>
2022-06-08 15:54:55 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -12,16 +12,15 @@ final class CrashTest extends testBaseClass {
  
   
   public function testBadPage() : void {  // Use this when debugging pages that crash the bot
-     Zotero::create_ch_zotero();
-  WikipediaBot::make_ch();
+      Zotero::create_ch_zotero();
+      WikipediaBot::make_ch();
       $this->assertTrue(FALSE);
   }
-}
 
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
-      $result = WikipediaBot::is_valid_user("David(Owner, Founder, Creator and Lead Developer)");
+     $result = WikipediaBot::is_valid_user("David(Owner, Founder, Creator and Lead Developer)");
      print_r($result);
   }
 
-
+}
