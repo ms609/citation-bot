6902455eb436f4795ebe99597c469956018b77c0
GlazerMann <kurt@ioiowa.com>
2022-06-08 15:52:24 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -10,8 +10,8 @@ require_once __DIR__ . '/../testBaseClass.php';
 final class CrashTest extends testBaseClass {
 
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
-      doi_works('10.1289/ehp.1002409');
-      doi_works('10.25300/MISQ/2013/37.2.05');
+      $result = WikipediaBot::is_valid_user("David(Owner, Founder, Creator and Lead Developer)");
+     print_r($result);
   }
 
  
