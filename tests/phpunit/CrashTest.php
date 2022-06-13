01e669201fc6276a57ca9b284671106dd3f68f4e
GlazerMann <kurt@ioiowa.com>
2022-06-13 16:25:56 -0500

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -21,7 +21,7 @@ final class CrashTest extends testBaseClass {
   public function testBadPage2() : void {  // Use this when debugging pages that crash the bot
      //$result = doi_works('10.21236/ad0012012');
      //print_r($result);
-   $this->process_citation('{{Cite journal|last=Webb|first=Thomas E F|date=July 2006|title='Dottyville'—Craiglockhart War Hospital and shell-shock treatment in the First World War|journal=Journal of the Royal Society of Medicine|volume=99|issue=7|pages=342–346|doi=|issn=0141-0768|pmc=<!-- --> |pmid=<!-- --> }}');
+   $this->process_citation("{{Cite journal|last=Webb|first=Thomas E F|date=July 2006|title='Dottyville'—Craiglockhart War Hospital and shell-shock treatment in the First World War|journal=Journal of the Royal Society of Medicine|volume=99|issue=7|pages=342–346|doi=|issn=0141-0768|pmc=<!-- --> |pmid=<!-- --> }}");
 
    
    
