9ae83b2a52f82243173ec99dd512e15f3856107c
GlazerMann <kurt@ioiowa.com>
2022-06-23 08:33:11 -0500

    PREFER_VOLUMES - some journals (#4065)

--- tests/phpunit/constantsTest.php
+++ tests/phpunit/constantsTest.php
@@ -85,9 +85,18 @@ final class constantsTest extends testBaseClass {
   public function testAllLowerCase() : void {
     $big_array = array_merge(HAS_NO_VOLUME, BAD_ACCEPTED_MANUSCRIPT_TITLES, BAD_AUTHORS,
                              PUBLISHER_ENDINGS, BAD_TITLES, IN_PRESS_ALIASES, NON_PUBLISHERS,
-                             JOURNAL_IS_BOOK_SERIES, HAS_NO_ISSUE, WORKS_ARE_PUBLISHERS);
+                             JOURNAL_IS_BOOK_SERIES, HAS_NO_ISSUE, WORKS_ARE_PUBLISHERS, PREFER_VOLUMES);
     foreach ($big_array as $actual) {
       $this->assertSame(strtolower($actual), $actual);
+    }
+  }
+  
+  public function testNoSpacesOnEnds() : void {
+    $big_array = array_merge(HAS_NO_VOLUME, BAD_ACCEPTED_MANUSCRIPT_TITLES, BAD_AUTHORS,
+                             PUBLISHER_ENDINGS, BAD_TITLES, IN_PRESS_ALIASES, NON_PUBLISHERS,
+                             JOURNAL_IS_BOOK_SERIES, HAS_NO_ISSUE, WORKS_ARE_PUBLISHERS, PREFER_VOLUMES);
+    foreach ($big_array as $actual) {
+       $this->assertSame(trim($actual), $actual);
     }
   }
   
