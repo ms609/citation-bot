3e4cdca1b6196f4e111f82a467017b002be15d7e
GlazerMann <kurt@ioiowa.com>
2022-07-06 15:20:32 -0500

    add tests

--- tests/phpunit/TemplateTest2.php
+++ tests/phpunit/TemplateTest2.php
@@ -3593,6 +3593,10 @@ final class TemplateTest2 extends testBaseClass {
      $text = '{{cite journal|title=X|periodical=X|issue=X|volume=X|pages=12-34|year=1980|last2=Him|doi=10.0000/Rubbish_bot_failure_test|bibcode=X|last1=X|first1=X}}';
      $template = $this->make_citation($text);
      $this->assertFalse($template->incomplete());
+    
+     $text = '{{citation|title=X|work=X|pages=12-34|year=1980|last2=Him|doi=10.0000/Rubbish_bot_failure_test|bibcode=X|last1=X|first1=X}}';
+     $template = $this->make_citation($text);
+     $this->assertFalse($template->incomplete());
   
      $this->requires_bibcode(function() : void {
       $text = '{{cite journal|title=X|journal=X|issue=X|volume=X|pages=12-34|year=1980|last2=Him|doi=10.0000/Rubbish_bot_failure_test|bibcode=X|last1=X|first1=X}}';
