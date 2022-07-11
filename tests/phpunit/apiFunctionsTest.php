f87170ea5bdf5e7413e3418d7179e7d6442b6e9d
GlazerMann <kurt@ioiowa.com>
2022-07-11 13:04:14 -0500

    Those DOIs now work

--- tests/phpunit/apiFunctionsTest.php
+++ tests/phpunit/apiFunctionsTest.php
@@ -971,10 +971,10 @@ final class apiFunctionsTest extends testBaseClass {
   }
   
   public function testGetBadDoiFromArxiv() : void { // If this DOI starts working or arXiv removes it, then this test will fail and not cover code anymore
-    $text = '{{cite journal|arxiv=2105.09963|doi= |last1=Gagliano |first1=Alexander |last2=Izzo |first2=Luca |last3=Kilpatrick |first3=Charles D. |last4=Mockler |first4=Brenna |author5=Wynn Vincente Jacobson-Galán |last6=Terreran |first6=Giacomo |last7=Dimitriadis |first7=Georgios |last8=Zenati |first8=Yossef |last9=Auchettl |first9=Katie |last10=Drout |first10=Maria R. |last11=Narayan |first11=Gautham |last12=Foley |first12=Ryan J. |last13=Margutti |first13=R. |last14=Rest |first14=Armin |last15=Jones |first15=D. O. |last16=Aganze |first16=Christian |last17=Aleo |first17=Patrick D. |last18=Burgasser |first18=Adam J. |last19=Coulter |first19=D. A. |last20=Gerasimov |first20=Roman |last21=Gall |first21=Christa |last22=Hjorth |first22=Jens |last23=Hsu |first23=Chih-Chun |last24=Magnier |first24=Eugene A. |last25=Mandel |first25=Kaisey S. |last26=Piro |first26=Anthony L. |last27=Rojas-Bravo |first27=César |last28=Siebert |first28=Matthew R. |last29=Stacey |first29=Holland |author30=Michael Cullen Stroh |title=An Early-Time Optical and Ultraviolet Excess in the type-Ic SN 2020oi |year=2021 |display-authors=1 }}';
+    $text = '{{cite journal|arxiv=1412.2040|doi= |last1=Duff |first1=M. J. |title=How fundamental are fundamental constants? |year=2014 }}';
     $prepared = $this->process_citation($text);
-    $this->assertSame('10.3847/1538-4357/ac35ec', $prepared->get2('doi'));
+    $this->assertSame('10.1080/00107514.2014.980093', $prepared->get2('doi'));
     $this->assertNotNull($prepared->get2('doi-broken-date')); // The DOI has to not work for this test to cover the code where a title and arxiv exist and a doi is found, but the doi does not add a title
-  } // Could try if this fails: 2201.04741   1412.2040   2105.07028  astro-ph/9708005   
+  } // Could try if this fails: astro-ph/9708005   
   
 }
