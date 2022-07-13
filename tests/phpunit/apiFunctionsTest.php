7dc0f1783217c83a0c346af6719f3e366a88d183
GlazerMann <kurt@ioiowa.com>
2022-07-13 07:33:51 -0500

    test works again

--- tests/phpunit/apiFunctionsTest.php
+++ tests/phpunit/apiFunctionsTest.php
@@ -199,9 +199,7 @@ final class apiFunctionsTest extends testBaseClass {
   public function testExpansion_doi_not_from_crossref_airiti_journal() : void {
      $expanded = $this->make_citation('{{Cite journal}}');
      expand_doi_with_dx($expanded, '10.6620/ZS.2018.57-30');
-     $this->assertSame('{{Cite journal}}', $expanded->parsed_text());
-     // this used to work $this->assertSame('{{Cite journal|year = 2018|volume = 無|issue = 57|author1 = Jun Aoyama|author2 = Sam Wouthuyzen|author3 = Michael J. Miller|author4 = Hagi Y. Sugeha|author5 = Mari Kuroki|author6 = Shun Watanabe|author7 = Augy Syahailatua|author8 = Fadly Y. Tantu|author9 = Seishi Hagihara|author10 = Triyanto|author11 = Tsuguo Otake|author12 = Katsumi Tsukamoto|title = Reproductive Ecology and Biodiversity of Freshwater Eels around Sulawesi Island Indonesia|journal = Zoological Studies}}', $expanded->parsed_text());
-  }
+     $this->assertSame('{{Cite journal| year=2018 | volume=無 | issue=57 | author1=Jun Aoyama | author2=Sam Wouthuyzen | author3=Michael J. Miller | author4=Hagi Y. Sugeha | author5=Mari Kuroki | author6=Shun Watanabe | author7=Augy Syahailatua | author8=Fadly Y. Tantu | author9=Seishi Hagihara | author10=Triyanto | author11=Tsuguo Otake | author12=Katsumi Tsukamoto | title=Reproductive Ecology and Biodiversity of Freshwater Eels around Sulawesi Island Indonesia | journal=Zoological Studies }}', $expanded->parsed_text());  }
 
   // http://www.eidr.org/
   public function testExpansion_doi_not_from_crossref_eidr_Black_Panther_Movie() : void {
