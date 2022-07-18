5e7e64776775e2498397adb5f1baf0c349dd5110
GlazerMann <kurt@ioiowa.com>
2022-07-18 13:47:41 -0500

    Evil unicode character

--- Template.php
+++ Template.php
@@ -3535,7 +3535,7 @@ final class Template {
       return;  // We let comments block the bot
     }
     if ($this->get($param) !== $this->get3($param)) return;
-
+e=﻿Th
     if($this->has($param)) {
       if (stripos($param, 'separator') === FALSE &&   // lone punctuation valid
           stripos($param, 'postscript') === FALSE &&  // periods valid
@@ -3547,6 +3547,7 @@ final class Template {
          ) {
         $this->set($param, safe_preg_replace('~[\x{2000}-\x{200A}\x{00A0}\x{202F}\x{205F}\x{3000}]~u', ' ', $this->get($param))); // Non-standard spaces
         $this->set($param, safe_preg_replace('~[\t\n\r\0\x0B]~u', ' ', $this->get($param))); // tabs, linefeeds, null bytes
+        $this->set($param, safe_preg_replace('~﻿~u', ' ', $this->get($param)));
         $this->set($param, safe_preg_replace('~  +~u', ' ', $this->get($param))); // multiple spaces
         $this->set($param, safe_preg_replace('~(?<!:)[:,]$~u', '', $this->get($param)));   // Remove trailing commas, colons, but not semi-colons--They are HTML encoding stuff
         $this->set($param, safe_preg_replace('~^[:,;](?!:)~u', '', $this->get($param)));  // Remove leading commas, colons, and semi-colons
