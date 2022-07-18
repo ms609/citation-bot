fc6d50e5f29c73a3a96ff92e4777b4d6664decad
GlazerMann <kurt@ioiowa.com>
2022-07-18 11:31:18 -0500

    better worldcat ISSN

--- Template.php
+++ Template.php
@@ -7392,10 +7392,8 @@ final class Template {
       $wonky = trim($matches[1]);
       if ($wonky === "[WorldCat.org]") {
         report_info('WorldCat temporarily unresponsive or does not have Title for ISSN ' .  echoable($issn));
-      } elseif (preg_match('~^(.+)\. \(e?Newspaper, \d{4}\) \[WorldCat.org\]$~', $wonky, $matches)) {
-        return $this->add_if_new('newspaper', trim($matches[1]));
-      } elseif (preg_match('~^ISSN \d{4}\-\d{3}[\dxX] : (.+) \(e?Newspaper, \d{4}s\) \[WorldCat.org\]$~', $wonky, $matches)) {
-        return $this->add_if_new('newspaper', trim($matches[1]));
+      } elseif (preg_match('~^(.+) \(e?Newspaper, \d{4}s?\) \[WorldCat.org\]$~', $wonky, $matches)) {
+        return $this->add_if_new('newspaper', trim($matches[1], " \n\r\t\v\x00\."));
       } else {
         report_minor_error('Unexpected title from WorldCat for ISSN ' . echoable($issn) . ' : ' . echoable($wonky));
       }
