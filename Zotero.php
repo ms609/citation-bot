f6d4288923a12eb9f42b957b88020ca91ef868b2
GlazerMann <kurt@ioiowa.com>
2022-07-23 15:40:42 -0500

    more jstor cleanup

--- Zotero.php
+++ Zotero.php
@@ -1159,7 +1159,7 @@ public static function find_indentifiers_in_urls(Template $template, ?string $ur
        }
      }
      // Remove junk from URLs
-     while (preg_match('~^https?://www\.jstor\.org/stable/(.+)(?:&ved=|&usg=|%3Fseq%3D1|\?)~i', $url, $matches)) {
+     while (preg_match('~^https?://www\.jstor\.org/stable/(.+)(?:&ved=|&usg=|%3Fseq%3D1|\?|#metadata_info_tab_contents)~i', $url, $matches)) {
        $url = 'https://www.jstor.org/stable/' . $matches[1] ;
        if (is_null($url_sent)) {
          $template->set($url_type, $url); // Update URL with cleaner one
