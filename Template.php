0dcebf4c2313dd3be18b23e42ee29c094b3f820b
GlazerMann <kurt@ioiowa.com>
2022-07-25 08:29:23 -0500

    More junk data

--- Template.php
+++ Template.php
@@ -373,7 +373,7 @@ final class Template {
               $the_title = '';
               $bad_data = TRUE;
           }
-          if ($the_title === 'null' || $the_title === '[No title found]' || $the_title === 'Archived copy' ||
+          if ($the_title === 'null' || $the_title === '[No title found]' || $the_title === 'Archived copy' || $the_title === 'JSTOR' ||
               $the_title === 'ShieldSquare Captcha' || $the_title === 'Shibboleth Authentication Request') { // title=none is often because title is "reviewed work....
               $this->rename('title', 'CITATION_BOT_PLACEHOLDER_title');
               $the_title = '';
