9f23a905ae2fd7c7f694f9f04b938220fbaa6f20
GlazerMann <kurt@ioiowa.com>
2022-08-13 16:10:17 -0500

    Rare Google URL component

--- Template.php
+++ Template.php
@@ -7393,7 +7393,7 @@ final class Template {
              break;
           case "hl": case "safe": case "q": case "tbm": case "start": case "ludocid":
           case "cshid": case "stick": case "as_eq": case "kgmid": case "as_drrb":
-          case "as_scoring": case "gl": case "rllag": case "lsig":
+          case "as_scoring": case "gl": case "rllag": case "lsig": case "lpsid":
              $url .=  $part . "&" ;
              break;
           default:
