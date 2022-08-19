d89f97be41f48441c822e761770d6b77d996e887
GlazerMann <kurt@ioiowa.com>
2022-08-19 15:03:26 -0500

    Hard to tell apart

--- expandFns.php
+++ expandFns.php
@@ -1273,9 +1273,15 @@ function numberToRomanRepresentation(int $number) : string { // https://stackove
 }
 
 function convert_to_utf8(string $value) : string {
-    $encode =  mb_detect_encoding($value, ["UTF-8", "GB2312", "WINDOWS-1252", "ISO-2022-JP"]);
-    if ($encode === FALSE || $encode === 'UTF-8') return $value;
-    $new_value = mb_convert_encoding($value, "UTF-8", $encode);
+    $encode1 =  mb_detect_encoding($value, ["UTF-8", "EUC-KR", "EUC-CN", "ISO-2022-JP", "WINDOWS-1252"]);
+    if ($encode1 === FALSE || $encode1 === 'UTF-8') return $value;
+    $encode2 =  mb_detect_encoding($value, ["UTF-8", "EUC-CN", "EUC-KR", "ISO-2022-JP", "WINDOWS-1252"]);
+    if ($encode1 !=== $encode2) return $value;
+    $encode3 =  mb_detect_encoding($value, ["UTF-8", "ISO-2022-JP", "EUC-CN", "EUC-KR", "WINDOWS-1252"]);
+    if ($encode1 !=== $encode3) return $value;
+    $encode4 =  mb_detect_encoding($value, ["UTF-8", "WINDOWS-1252", "ISO-2022-JP", "EUC-CN", "EUC-KR"]);
+    if ($encode1 !=== $encode4) return $value;
+    $new_value = mb_convert_encoding($value, "UTF-8", $encode1);
     if ($new_value == "") return $value;
     return $new_value;
 }
