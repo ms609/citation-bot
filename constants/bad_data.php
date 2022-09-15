5ea02c587314337a9195c81610c5775cc2035c4e
GlazerMann <kurt@ioiowa.com>
2022-09-15 08:06:08 -0500

    More bad titles

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -126,7 +126,8 @@ const BAD_ZOTERO_TITLES = ['Browse publications', 'Central Authentication Servic
                                  '404 | ', '页面不存在', 'De pagina is niet gevonden', '404 - ', ' Stranica nije pronađena',
                                  '404 Page', '404. The page', "wasn't found on this server", '404. The URL', 
                                  'ShieldSquare', '404 Not Found', "404页面", "Sign up | LinkedIn", 'The-star.co.kr',
-			         'Connecting to the iTunes Store', '500 Internal Server Error'];
+			         'Connecting to the iTunes Store', '500 Internal Server Error', 'DomainMarket.com',
+			         'bluehost.com'];
 
 const CANONICAL_PUBLISHER_URLS = array ('elsevier.com', 'springer.com', 'sciencedirect.com', 'tandfonline.com',
                                 'taylorandfrancis.com', 'wiley.com', 'sagepub.com', 'sagepublications.com',
