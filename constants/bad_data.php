50243bb6ee47f6de187c9ad4887a10a433c73423
GlazerMann <kurt@ioiowa.com>
2022-10-17 07:04:56 -0500

    Bulletin of the United States National Museum

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -9,7 +9,7 @@ const HAS_NO_VOLUME = array("zookeys", "studia hibernica", "analecta hibernica",
 // oceanic linguistics special publications has the problem that issues will not show up within temlpates
 const HAS_NO_ISSUE = array("special papers in palaeontology", "oceanic linguistics special publications");  // Some journals have volumes only, no issue numbers
 const PREFER_VOLUMES = array("illinois classical studies"); // When issue=volume, drop issue.  JSTOR calls volumes issues
-const PREFER_ISSUES = array("mammalian species");
+const PREFER_ISSUES = array("mammalian species", "bulletin of the united states national museum");
 const BAD_ACCEPTED_MANUSCRIPT_TITLES = array("oup accepted manuscript", "placeholder for bad pdf file", 
                                              "placeholder", "symbolic placeholder", "[placeholder]", 
                                              "placeholder for arabic language transliteration", "article not found");
