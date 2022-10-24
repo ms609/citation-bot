61eafa9461eaa0a5464f2c48cfbecb14d21e3377
GlazerMann <kurt@ioiowa.com>
2022-10-24 09:21:07 -0500

    Update bad_data.php

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -779,7 +779,8 @@ const NON_JOURNAL_WEBSITES = array('-news.co.uk/', '.ajc.com/', '.al.com/',
 	                  'xxlmag.com/', 'xzqh.org/', 'yahoo.co.jp/', 'yahoo.com/', 'yandex.ru/',
 	                  'ynszxc.net/', 'yourentertainmentnow.com/', 'youtube.com/',
 	                  'yuzhnoye.com/', 'zap2it.com/', 'zdnet.com/', 'zeenews.india.com/',
-	                  'zerohedge.com/', 'zhanqi.tv/', 'zoominfo.com/', 'hdr.undp.org/', '.nist.gov/');
+	                  'zerohedge.com/', 'zhanqi.tv/', 'zoominfo.com/', 'hdr.undp.org/', '.nist.gov/',
+	                  'ngdc.noaa.gov/');
                      // Just a list of ones that are obvious.  Add ones that time-out as we find them
                      // bbm.ca is short enough that we add /bbm.ca/ and .bbm.ca/ since we don't want to grab too many sites
 
