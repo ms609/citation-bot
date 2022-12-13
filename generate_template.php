16cfc24ded04b750e58548c8d5d8b1151c264e2c
GlazerMann <kurt@ioiowa.com>
2022-12-13 07:09:51 -0600

    clean up comments

--- generate_template.php
+++ generate_template.php
@@ -15,9 +15,13 @@ $t->parse_text('{{cite web }}');
 if (count($_GET) !== 1) exit('Exactly one parameters must be passed</pre></body></html>');
 foreach ($_GET as $param=>$value) {
   /** The user sent this in, so we declare it to not be tainted, and do some checking */
-  /** @psalm-taint-escape ssrf */   /** @psalm-taint-escape has_quotes */   /** @psalm-taint-escape html */
+  /** @psalm-taint-escape ssrf
+      @psalm-taint-escape has_quotes
+      @psalm-taint-escape html */
   $value = strtolower($value);
-  /** @psalm-taint-escape ssrf */   /** @psalm-taint-escape has_quotes */   /** @psalm-taint-escape html */
+  /** @psalm-taint-escape ssrf
+      @psalm-taint-escape has_quotes
+      @psalm-taint-escape html */
   $param = strtolower($param);
   if (strlen($value) === 0) exit('Unset parameter error</pre></body></html>');
   if ((strpos($value, "'") !== FALSE ) || (strpos($value, '"') !== FALSE ) || (strpos($value, "|") !== FALSE ) || (strpos($value, " ") !== FALSE )) {
