47523c47ed78309d5d616b744120dbd6d9a1474e
GlazerMann <kurt@ioiowa.com>
2022-12-12 19:19:57 -0600

    taint escape

--- generate_template.php
+++ generate_template.php
@@ -14,10 +14,10 @@ $t = new Template();
 $t->parse_text('{{cite web }}');
 if (count($_GET) !== 1) exit('Exactly one parameters must be passed</pre></body></html>');
 foreach ($_GET as $param=>$value) {
-  /** The user sent this in, so we declare it to not be tainted */
-  /** @psalm-taint-escape ssrf */ /** @psalm-taint-escape quotes */
+  /** The user sent this in, so we declare it to not be tainted, and do some checking */
+  /** @psalm-taint-escape ssrf */   /** @psalm-taint-escape has_quotes */   /** @psalm-taint-escape html */
   $value = strtolower($value);
-  /** @psalm-taint-escape ssrf */
+  /** @psalm-taint-escape ssrf */   /** @psalm-taint-escape has_quotes */   /** @psalm-taint-escape html */
   $param = strtolower($param);
   if (strlen($value) === 0) exit('Unset parameter error</pre></body></html>');
   if ((strpos($value, "'") !== FALSE ) || (strpos($value, '"') !== FALSE ) || (strpos($value, "|") !== FALSE ) || (strpos($value, " ") !== FALSE )) {
