b4b07d40f35a96f03769b4df2c0a0f2e74cadcfb
GlazerMann <kurt@ioiowa.com>
2022-12-26 12:16:40 -0600

    use "empty" (#4116)

--- Template.php
+++ Template.php
@@ -2699,7 +2699,7 @@ final class Template {
         if ($part_start[0] === 'page')     $part_start[0] = 'pg';
         switch ($part_start[0]) {
           case "dq": case "pg": case "lpg": case "q": case "printsec": case "cd": case "vq": case "jtp": case "sitesec": case "article_id":
-            if (!isset($part_start[1]) || $part_start[1] === '') {
+            if (empty($part_start[1])) {
                 $removed_redundant++;
                 $removed_parts .= $part;
             } else {
@@ -7426,7 +7426,7 @@ final class Template {
         if (isset($part_start[1]) && $part_start[1] === '') {
           $part_start[0] = "donotaddmeback"; // Do not add blank ones
         }
-        $it_is_blank = ((string) @$part_start[1] === "");
+        $it_is_blank = (empty($part_start[1]));
         switch ($part_start[0]) {
           case "aq": case "aqi": case "bih": case "biw": case "client":
           case "as": case "useragent": case "as_brr":
