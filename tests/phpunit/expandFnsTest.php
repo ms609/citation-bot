fc89b1cb7efbd3ea7955ed47f88d2874ee920a5b
GlazerMann <kurt@ioiowa.com>
2023-01-18 10:08:58 -0600

    Add test

--- tests/phpunit/expandFnsTest.php
+++ tests/phpunit/expandFnsTest.php
@@ -422,8 +422,9 @@ final class expandFnsTest extends testBaseClass {
   }
 
   public function testForDOIGettingFixed() : void { // These do not work, but it would be nice if they did
-    // Nature dropped ball on these - and they should break
+    // Nature dropped ball on these - and they should break or lead someplace
     $this->assertTrue(doi_works('10.1038/nature05009'));
+    $this->assertFalse(@get_headers("https://doi.org/10.1038/nature05009", GET_THE_HEADERS, stream_context_create(CONTEXT_INSECURE)));
     // https://search.informit.org/doi/10.3316/aeipt.20772 and such
     $this->assertFalse(doi_works('10.3316/informit.550258516430914'));
     $this->assertFalse(doi_works('10.3316/ielapa.347150294724689'));
