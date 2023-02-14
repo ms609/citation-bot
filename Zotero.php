08872d21f3113820935de7273309563f89b72ad9
GlazerMann <kurt@ioiowa.com>
2023-02-14 07:39:25 -0600

    drop arxiv links

--- Zotero.php
+++ Zotero.php
@@ -1556,7 +1556,7 @@ public static function find_indentifiers_in_urls(Template $template, ?string $ur
           quietly('report_modification', "Converting URL to arXiv parameter");
           $ret = $template->add_if_new('arxiv', $arxiv_id[0]); // Have to add before forget to get cite type right
           if (is_null($url_sent)) {
-            if ($template->has_good_free_copy()) $template->forget($url_type);
+            if ($template->has_good_free_copy() || $template->has('arxiv')  || $template->has('eprint')) $template->forget($url_type);
           }
           return $ret;
         }
