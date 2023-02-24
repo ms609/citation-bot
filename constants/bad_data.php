f833ee628399c98b9abaa67bab6ea6ebf7f3e635
GlazerMann <kurt@ioiowa.com>
2023-02-24 08:22:32 -0600

    Sify - add as website parameter

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -396,6 +396,7 @@ const HOSTNAME_MAP  = array('public.ebookcentral.proquest.com' => '[[ProQuest]]'
 			    'wikinews.org' => '[[Wikinews]]',
 			    'royal.uk' => 'The Royal Family',
 			    'cinemaexpress.com' => '[[Cinema Express]]',
+			    'sify.com' => '[[Sify]]',
                            ); // Be warned, some website host a seperate sunday edition, etc.  Be careful and when in doubt link to hostname
 
 const NO_DATE_WEBSITES = array('wikipedia.org', 'web.archive.org', 'perma-archives.org', 'webarchive.proni.gov.uk', 'perma.cc',
