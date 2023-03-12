29f48276ccd488210eeb7cb1504626c17ebd37b8
GlazerMann <kurt@ioiowa.com>
2023-03-12 09:16:24 -0500

    YA sub-template

--- Template.php
+++ Template.php
@@ -3220,7 +3220,7 @@ final class Template {
           case "usgs index id": case "us patent": case "us trademark": case "zdb":
           case "subscription required": case "ncid": case "wikileaks cable":
           case "bhl page": case "internet archive": case "youtube": case "nypl":
-          case "bnf": case "dnb-idn": case "nara catalog record": case "urn":
+          case "bnf": case "dnb-idn": case "nara catalog record": case "urn": case "viaf":
           case "so-vid": case "philpapers": case "iccu": case "hathitrust": case "allmusic":
           case "hal": case "icd11": case "coden": case "blcat": case "cobiss.bih":
           case "cobiss.rs": case "cobiss.sr": case "harvtxt": case "mathnet": case "eissn":
