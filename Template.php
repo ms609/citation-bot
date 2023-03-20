90c00222f85d9c0b8c8225ef5b30803c9aa2950f
GlazerMann <kurt@ioiowa.com>
2023-03-20 12:56:04 -0500

    sub-template

--- Template.php
+++ Template.php
@@ -3246,7 +3246,7 @@ final class Template {
           case "dggs citation id": case "harvp": case "nla": case "catkey": case "hyphen":
           case "mit libraries": case "epa national catalog": case "unt key": case "eram":
           case "regreq": case "nobr": case "subscription": case "uspl": case "small":
-          case "rism": case "jan": case "nbsp":
+          case "rism": case "jan": case "nbsp": case "abbr":
           case "genbank": case "better source needed": case "free access": case "required subscription":
           case "fahrplan-ch": case "incomplete short citation": case "music":
           case "gbooks": // TODO - should use
