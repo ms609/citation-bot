e7923803f51277447ee4871aaa0c46b66dbac5ea
GlazerMann <kurt@ioiowa.com>
2023-03-28 07:53:51 -0500

    more bad google urls

--- Template.php
+++ Template.php
@@ -2627,6 +2627,9 @@ final class Template {
        if ($this->has($url_type) && preg_match('~^https?://books\.google\.[^/]+/booksid=(.+)$~', $this->get($url_type), $matches)) {
          $this->set($url_type, 'https://books.google.com/books?id=' . $matches[1]);
        }
+       if ($this->has($url_type) && preg_match('~^https?://books\.google\.[^/\?]+\?id=(.+)$~', $this->get($url_type), $matches)) {
+         $this->set($url_type, 'https://books.google.com/books?id=' . $matches[1]);
+       }
        if ($this->has($url_type) && preg_match('~^https?://books\.google\.[^/]+\/books\/about\/[^/]+\.html$~', $this->get($url_type), $matches)) {
          $this->forget($url_type);
        }
