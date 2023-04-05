f3d72ff4c3e60750a150c2a9f99a6962fbecabd8
GlazerMann <kurt@ioiowa.com>
2023-04-05 14:13:59 -0500

    google books

--- Template.php
+++ Template.php
@@ -2641,7 +2641,7 @@ final class Template {
        if ($this->has($url_type) && preg_match('~^https?://(?:books|www)\.google\.[^/]+\/books\/edition\/[a-zA-Z0-9\_]+\/?$~', $this->get($url_type), $matches)) {
          $this->forget($url_type);
        }
-       if ($this->has($url_type) && preg_match('~^https?://(?:books|www)\.google\.[^/]+\/books\/edition\/([a-zA-Z0-9\_]+)\/\?(.+)$~', $this->get($url_type), $matches)) {
+       if ($this->has($url_type) && preg_match('~^https?://(?:books|www)\.google\.[^/]+\/books\/edition\/[a-zA-Z0-9\_]+\/([a-zA-Z0-9\-]+)\/\?(.+)$~', $this->get($url_type), $matches)) {
          $this->set($url_type, 'https://books.google.com/books?id=' . $matches[1] . '&' . $matches[2]);
        }
        if ($this->has($url_type) && preg_match('~^https?://books\.google\..*id\&\#61\;.*$~', $this->get($url_type), $matches)) {
