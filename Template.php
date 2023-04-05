770ee0bac8d13ae785299b7a27884352a4f603db
GlazerMann <kurt@ioiowa.com>
2023-04-05 14:02:52 -0500

    oops

--- Template.php
+++ Template.php
@@ -2638,10 +2638,10 @@ final class Template {
        if ($this->has($url_type) && preg_match('~^https?://books\.google\.[^/]+\/books\/about\/[^/]+\.html$~', $this->get($url_type), $matches)) {
          $this->forget($url_type);
        }
-       if ($this->has($url_type) && preg_match('~^https?://(?:books|www)\.google\.[^/]+\/books\/edition\/[a-zA-Z0-1\_]+\/?$~', $this->get($url_type), $matches)) {
+       if ($this->has($url_type) && preg_match('~^https?://(?:books|www)\.google\.[^/]+\/books\/edition\/[a-zA-Z0-9\_]+\/?$~', $this->get($url_type), $matches)) {
          $this->forget($url_type);
        }
-       if ($this->has($url_type) && preg_match('~^https?://(?:books|www)\.google\.[^/]+\/books\/edition\/([a-zA-Z0-1\_]+)\/\?(.+)$~', $this->get($url_type), $matches)) {
+       if ($this->has($url_type) && preg_match('~^https?://(?:books|www)\.google\.[^/]+\/books\/edition\/([a-zA-Z0-9\_]+)\/\?(.+)$~', $this->get($url_type), $matches)) {
          $this->set($url_type, 'https://books.google.com/books?id=' . $matches[1] . '&' . $matches[2]);
        }
        if ($this->has($url_type) && preg_match('~^https?://books\.google\..*id\&\#61\;.*$~', $this->get($url_type), $matches)) {
