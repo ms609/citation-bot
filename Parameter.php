1de78ee130c392b1cca68466859cff38be096697
GlazerMann <kurt@ioiowa.com>
2023-04-06 21:56:35 -0500

    Update Parameter.php

--- Parameter.php
+++ Parameter.php
@@ -63,6 +63,7 @@ final class Parameter {
         $this->post = $match[2];
       }
     }
+    if ($text !== $this->pre . $this->param . this->eq . $this->val . $this->post) echo "\n HOSED $text \n;
 
   }
 
