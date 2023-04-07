b219b209e0c5703aeed84fef7ee86af03bdf4f49
GlazerMann <kurt@ioiowa.com>
2023-04-06 21:52:16 -0500

    add test

--- tests/phpunit/ParameterTest.php
+++ tests/phpunit/ParameterTest.php
@@ -159,5 +159,10 @@ final class ParameterTest extends testBaseClass {
     $this->assertSame('{{citation|format=Joe}}', $template->parsed_text());
   }
   
+  public function testOddSpaces() : void { // TODO
+    $text = "{{Infobox settlement\n| image_skyline            = \n \n| image_caption            = \n}}";
+    $template = $this->process_citation($text);
+    $this->assertSame($text, $template->parsed_text());
+  }
 }
 
