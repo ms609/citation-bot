79c96cd5c5b0ba4e4e5ad522841724519c72f6cc
GlazerMann <kurt@ioiowa.com>
2023-04-11 16:38:44 -0500

    fixed

--- tests/phpunit/ParameterTest.php
+++ tests/phpunit/ParameterTest.php
@@ -159,7 +159,7 @@ final class ParameterTest extends testBaseClass {
     $this->assertSame('{{citation|format=Joe}}', $template->parsed_text());
   }
   
-  public function testOddSpaces() : void { // TODO
+  public function testOddSpaces() : void {
     $text = "{{Infobox settlement\n| image_skyline            = \n \n| image_caption            = \n}}";
     $template = $this->process_citation($text);
     $this->assertSame($text, $template->parsed_text());
