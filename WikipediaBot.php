76d5ecb60b75a66140cb4d50822aab4946fc4ccd
GlazerMann <kurt@ioiowa.com>
2023-04-17 15:24:58 -0500

    Use function

--- WikipediaBot.php
+++ WikipediaBot.php
@@ -96,7 +96,7 @@ final class WikipediaBot {
       } elseif (strpos((string) @$response->error->info, 'The authorization headers in your request are not valid') !== FALSE) {
         report_error('There is something wrong with your Oauth tokens');  // @codeCoverageIgnore
       } else {
-        file_put_contents('CodeCoverage', html_entity_decode((string) @$response->error->info) . "\n", FILE_APPEND); // Good to know about about these things
+        bot_debug_log(html_entity_decode((string) @$response->error->info)); // Good to know about about these things
         report_warning('API call failed: ' . echoable((string) @$response->error->info) . '.  Will sleep and move on.');
       }
       sleep (10);
