a2d27016162fa66fa0307772a0d9bfe7c0fb3d66
GlazerMann <kurt@ioiowa.com>
2023-04-20 09:16:32 -0500

    Big Jobs

--- kill_big_job.php
+++ kill_big_job.php
@@ -1,18 +1,18 @@
 <?php
 declare(strict_types=1);
-exit(); // Disabled this API in setup.php
 set_time_limit(120);
 $started = @session_start();
 @setcookie(session_name(),session_id(),time()+(7*24*3600)); // since we do not call WikipediaBot().authenticate_user()
 
 require_once 'html_headers.php';
+require_once 'big_jobs.php';
 
 if (!$started) {
  echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Killing the big job</title></head><body><main><pre>Could not even access session</pre></main></body></html>';
-} elseif ((string) @$_SESSION['big_and_busy'] !== 'BLOCK4') {
+} elseif (!big_jobs_exists()) {
  echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Killing the big job</title></head><body><main><pre>No exiting large job found</pre></main></body></html>';
 } else {
- $_SESSION['kill_the_big_job'] = "YES";
+ big_jobs_kill();
  echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Killing the big job</title></head><body><main><pre>Existing large job flagged for stopping</pre></main></body></html>';
 }
 ?>
