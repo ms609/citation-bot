99f0322b46b2cd816f4fd664b5378c4062299e8c
GlazerMann <kurt@ioiowa.com>
2023-04-20 09:18:02 -0500

    Update setup.php

--- setup.php
+++ setup.php
@@ -119,42 +119,6 @@ function check_blocked() : void {
   }
 }
 
-function unlock_user() : void {
-  @session_start();
-  unset($_SESSION['big_and_busy']);     
-  @session_write_close();
-}
-
-function check_overused() : void {
- return;
-  /**  No longer enforcing - TODO figure out some way to get this to work.  Seems to just hang.  Also, re-enable kill_big_job.php
- if (!HTML_OUTPUT) return;
- if (isset($_SESSION['big_and_busy']) && $_SESSION['big_and_busy'] === 'BLOCK4') {
-   echo '</pre><div style="text-align:center"><h1>Run blocked by your existing big run.</h1></div><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>';
-   exit();
- }
- ob_start(); // Buffer output for big jobs
- @session_start();
- define('BIG_JOB_MODE', 'YES');
- register_shutdown_function('unlock_user');
- $_SESSION['big_and_busy'] = 'BLOCK4';
- @session_write_close();
- **/
-}
-
-function check_killed() : void {
- if(!defined('BIG_JOB_MODE')) return;
- @session_start(['read_and_close' => TRUE]);
- if (isset($_SESSION['kill_the_big_job'])) {
-   @session_start();
-   unset($_SESSION['kill_the_big_job']);
-   unset($_SESSION['big_and_busy']);
-   @session_write_close();
-   echo '</pre><div style="text-align:center"><h1>Run killed as requested.</h1></div><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>';
-   exit();
- }
-}
-
 define("MAX_TRIES", 2);
 require_once 'Comment.php';
 require_once 'user_messages.php';
