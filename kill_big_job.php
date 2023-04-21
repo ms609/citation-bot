37092e5db54862d8936c4da0d9f8280332576e97
GlazerMann <kurt@ioiowa.com>
2023-04-21 15:11:35 -0500

    paranoid

--- kill_big_job.php
+++ kill_big_job.php
@@ -7,6 +7,8 @@ $started = @session_start();
 require_once 'html_headers.php';
 require_once 'big_jobs.php';
 
+ob_implicit_flush();
+
 if (!$started) {
  echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Killing the big job</title></head><body><main><pre>Could not even access session</pre></main></body></html>';
 } elseif (!big_jobs_exists()) {
