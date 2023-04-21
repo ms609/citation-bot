80db93c5bd8668b688ca740d488ffab0f9189158
GlazerMann <kurt@ioiowa.com>
2023-04-21 15:18:27 -0500

    error with static

--- big_jobs.php
+++ big_jobs.php
@@ -1,18 +1,16 @@
 <?php
 
-static $big_jobs_lock_file;
-
 function big_jobs_name() : string {
   $user = $_SESSION['citation_bot_user_id'];
   return "./user_locks/" . str_replace(["'", "="], '', base64_encode($user));
 }
 
 function big_jobs_we_died() : void {
-  @fclose($big_jobs_lock_file);
   @unlink(big_jobs_name());
 }
 
 function big_jobs_check_overused(int $page_count) : void {
+ static $lock_file; // Force file handle to stay open
  if (!HTML_OUTPUT) return;
  if (!WikipediaBot::NonStandardMode()) return; // TOTO - enable for everyone
  if ($page_count < 50) return; // Used to be BIG_RUN constant
@@ -27,7 +25,6 @@ function big_jobs_check_overused(int $page_count) : void {
    exit();
  } else {
    define('BIG_JOB_MODE', 'YES');
-   $big_jobs_lock_file = $lock_file; // Force file handle to stay open
    register_shutdown_function('big_jobs_we_died'); // We now have a lock file that will magically go away when code dies/quits
  }
 }
