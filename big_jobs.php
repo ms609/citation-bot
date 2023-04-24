b9420a2c961c61584bca5200a6552c7822adab03
GlazerMann <kurt@ioiowa.com>
2023-04-24 10:17:40 -0500

    Update big_jobs.php

--- big_jobs.php
+++ big_jobs.php
@@ -1,12 +1,14 @@
 <?php
 
 function big_jobs_name() : string {
+  $version = ""; // So we can reset everyone
   $user = $_SESSION['citation_bot_user_id'];
-  return "./user_locks/" . str_replace(["'", "="], '', base64_encode($user));
+  return "./user_locks/" . str_replace(["'", "="], '', base64_encode($user) . $version);
 }
 
 /** @param resource $lock_file **/
 function big_jobs_we_died($lock_file) : void {
+  @clearstatcache();
   @fclose($lock_file);
   @unlink(big_jobs_name());
 }
@@ -14,9 +16,9 @@ function big_jobs_we_died($lock_file) : void {
 function big_jobs_check_overused(int $page_count) : void {
  static $lock_file; // Force file handle to stay open
  if (!HTML_OUTPUT) return;
+ clearstatcache();
  if ($page_count < 50) return; // Used to be BIG_RUN constant
  $fn = big_jobs_name();
- clearstatcache();
  if (file_exists($fn) && (filemtime($fn) > (time()-3600))) { // More than an hour
     unlink($fn);
  }
@@ -33,8 +35,8 @@ function big_jobs_check_overused(int $page_count) : void {
 function big_jobs_check_killed() : void {
  if (!HTML_OUTPUT) return;
  if (!defined('BIG_JOB_MODE')) return;
- $fn = big_jobs_name() . '_kill_job';
  clearstatcache();
+ $fn = big_jobs_name() . '_kill_job';
  if (file_exists($fn)) {
    echo '</pre><div style="text-align:center"><h1>Run killed as requested.</h1></div><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>';
    @unlink($fn);
@@ -53,5 +55,6 @@ function big_jobs_exists() : bool {
 }
 
 function big_jobs_kill() : void {
+ clearstatcache();
  touch(big_jobs_name() . '_kill_job');
 }
