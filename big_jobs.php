30e2e438e59881010785f640b85541066534ecf4
GlazerMann <kurt@ioiowa.com>
2023-04-24 10:21:24 -0500

    Update big_jobs.php (#4183)

--- big_jobs.php
+++ big_jobs.php
@@ -1,20 +1,24 @@
 <?php
 
 function big_jobs_name() : string {
+  $version = ""; // So we can reset everyone
   $user = $_SESSION['citation_bot_user_id'];
-  return "./user_locks/" . str_replace(["'", "="], '', base64_encode($user));
+  return "./user_locks/" . str_replace(["'", "="], '', base64_encode($user) . $version);
 }
 
-function big_jobs_we_died() : void {
+/** @param resource $lock_file **/
+function big_jobs_we_died($lock_file) : void {
+  @clearstatcache();
+  @fclose($lock_file);
   @unlink(big_jobs_name());
 }
 
 function big_jobs_check_overused(int $page_count) : void {
  static $lock_file; // Force file handle to stay open
  if (!HTML_OUTPUT) return;
+ clearstatcache();
  if ($page_count < 50) return; // Used to be BIG_RUN constant
  $fn = big_jobs_name();
- clearstatcache();
  if (file_exists($fn) && (filemtime($fn) > (time()-3600))) { // More than an hour
     unlink($fn);
  }
@@ -24,15 +28,15 @@ function big_jobs_check_overused(int $page_count) : void {
    exit();
  } else {
    define('BIG_JOB_MODE', 'YES');
-   register_shutdown_function('big_jobs_we_died'); // We now have a lock file that will magically go away when code dies/quits
+   register_shutdown_function('big_jobs_we_died', $lock_file); // We now have a lock file that will magically go away when code dies/quits
  }
 }
 
 function big_jobs_check_killed() : void {
  if (!HTML_OUTPUT) return;
  if (!defined('BIG_JOB_MODE')) return;
- $fn = big_jobs_name() . '_kill_job';
  clearstatcache();
+ $fn = big_jobs_name() . '_kill_job';
  if (file_exists($fn)) {
    echo '</pre><div style="text-align:center"><h1>Run killed as requested.</h1></div><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>';
    @unlink($fn);
@@ -51,5 +55,6 @@ function big_jobs_exists() : bool {
 }
 
 function big_jobs_kill() : void {
+ clearstatcache();
  touch(big_jobs_name() . '_kill_job');
 }
