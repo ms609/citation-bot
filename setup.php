a00ac47606489a7c42f918779ebf15c9964b6f41
GlazerMann <kurt@ioiowa.com>
2023-05-27 16:41:14 -0500

    Just in case error is VERY eearly

--- setup.php
+++ setup.php
@@ -12,7 +12,7 @@ if (file_exists('git_pull.lock')) {
  */
 
 function bot_debug_log(string $log_this) : void {
-  file_put_contents('CodeCoverage', echoable(WikipediaBot::GetLastUser()) . " :: " . echoable(Page::$last_title) . " :: $log_this\n", FILE_APPEND);
+  if (function_exists('echoable')) file_put_contents('CodeCoverage', echoable(WikipediaBot::GetLastUser()) . " :: " . echoable(Page::$last_title) . " :: $log_this\n", FILE_APPEND);
 }
 
 if (isset($_REQUEST["wiki_base"])){
