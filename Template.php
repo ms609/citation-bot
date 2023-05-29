f6bf39ae1f54668dbbc99a0365c862f03d6bd5b5
GlazerMann <kurt@ioiowa.com>
2023-05-28 19:25:57 -0500

    more ignored stuff

--- Template.php
+++ Template.php
@@ -3223,7 +3223,7 @@ final class Template {
           case "genbank": case "better source needed": case "free access": case "required subscription":
           case "fahrplan-ch": case "incomplete short citation": case "music": case "bar-ads":
           case "subscription or libraries": case "gallica": case "gnd": case "ncbibook":
-          case "spaces":
+          case "spaces": case "ndash":
           case "gbooks": // TODO - should use
           case "isbnt": case "issn link": case "lccn8": // Assume not normal template for a reason
           case "google books": // Usually done for fancy formatting and because already has title-link/url
