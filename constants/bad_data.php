7a48859c2a266b8e32f91cbc99c4a5a7c637c2a3
GlazerMann <kurt@ioiowa.com>
2023-06-11 06:29:22 -0500

    rottentomatoes.com has bad dates

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -407,7 +407,7 @@ const NO_DATE_WEBSITES = array('wikipedia.org', 'web.archive.org', 'perma-archiv
                               'waybackmachine.org', 'siarchives.si.edu', 'gutenberg.org', 'archive.fo', 'archive.today', 'archive.ph',
                               'oireachtas.ie', 'webarchive.nla.gov.au', 'ebooks.adelaide.edu.au', 'archive.md', 'imdb.com',
                               'apps.des.qld.gov.au', 'billboard.com', 'music.apple.com', 'spotify.com', 'fred.stlouisfed.org',
-                              'simonsfoundation.org', 'chroniclingamerica.loc.gov', 'github.com');
+                              'simonsfoundation.org', 'chroniclingamerica.loc.gov', 'github.com', 'rottentomatoes.com');
 
 const ZOTERO_AVOID_REGEX = array("twitter\.",               // This should be {{cite tweet}}
                                  // Zotero seems to be doing better now "youtube\.", "youtu\.be",
