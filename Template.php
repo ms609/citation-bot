5e4247d6770ab22369e8557cd7ede21b49da4d9d
GlazerMann <kurt@ioiowa.com>
2023-06-12 08:29:54 -0500

    newspaperarchive

--- Template.php
+++ Template.php
@@ -5142,6 +5142,9 @@ final class Template {
                 }
               }
           }
+          if (stripos($this->get($param), 'https://access.newspaperarchive.com/') === 0) {
+            $this->set($param, str_ireplace('https://access.newspaperarchive.com/', 'https://www.newspaperarchive.com/', $this->get($param));
+          }
 
           clean_up_oxford_stuff($this, $param);
 
