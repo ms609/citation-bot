28fd32c3bdb87a86a7faf0172f831e073df7bf82
GlazerMann <kurt@ioiowa.com>
2023-07-05 06:54:08 -0500

    restore_italics

--- expandFns.php
+++ expandFns.php
@@ -436,7 +436,10 @@ function restore_italics (string $text) : string {
                          'NetWare', 'LaserJet', 'MacWorks', 'PostScript', 'PageMaker', 'ClarisWorks', 'HyperCard',
                          'PowerPoint', 'WorldWideWeb', 'EchoStar', 'BellSouth', 'EastEnders', 'SpaceCamp',
                          'HarperCollins', 'SeaTac', 'PricewaterhouseCoopers', 'DataLab', 'DuBois', 
-                         'ScaFi', 
+                         'ScaFi', '(Mc', '(Mac', 'BioShock', 'BreadTube', 'PewDiePie', 'LepVax', 'CrossMab', 'GyneFix',
+                         'ProsPeCtsi', 'sCIentIfICi', 'PsyCholoGyi', 'DiGeorge', 'AstraZeneca', 'SemCluster',
+                         'VirtuReal', '-Mc', '-Mac', 'NexGard', 'AmBisome', 'AmbiOnp', 'SnapShot', 'LentiGlobin',
+                         
                        ), '', $padded) !== $padded) return $text; // Words with capitals in the middle, but not the first character
   $new = safe_preg_replace('~([a-z]+)([A-Z][a-z]+\b)~', "$1 ''$2''", $text);
   if ($new === $text) {
