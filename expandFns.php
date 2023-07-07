2246fdb63cb6fca6cf4374dbe47959218ca9e650
GlazerMann <kurt@ioiowa.com>
2023-07-07 12:58:12 -0500

    Update expandFns.php

--- expandFns.php
+++ expandFns.php
@@ -515,7 +515,7 @@ function restore_italics (string $text) : string {
                          'HyPhy', 'MrBayes', 'SunTag', 'InterPro', 'SmProt', 'ChikDenMaZika', 'LitCovid',
                          'GeneTree', 'GenAge', 'QnAs', 'BiDil', 'iAge', 'DevSec', 'SecOps', 'DevcOps',
                          'LeafCutter', 'CyBase','OxPhos', 'ArrayExpress', 'BepiColombo', 'RuleMonkey',
-                         'OxyCo',
+                         'OxyCo', 'CdZnTe', 
                        ), '', $padded) !== $padded) return $text; // Words with capitals in the middle, but not the first character
   $new = safe_preg_replace('~([a-z]+)([A-Z][a-z]+\b)~', "$1 ''$2''", $text);
   if ($new === $text) {
