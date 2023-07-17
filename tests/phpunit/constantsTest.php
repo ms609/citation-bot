fc82b1c9a32898f6ec28dc3108faf14020c955e5
GlazerMann <kurt@ioiowa.com>
2023-07-17 11:11:07 -0500

    Update constantsTest.php

--- tests/phpunit/constantsTest.php
+++ tests/phpunit/constantsTest.php
@@ -503,8 +503,8 @@ final class constantsTest extends testBaseClass {
     for ($i = 0; $i < count(ITALICS_HARDCODE_OUT); $i++) {
       $this->assertSame(0, substr_count("'''", ITALICS_HARDCODE_IN[$i]));
       $this->assertSame(0, substr_count("'''", ITALICS_HARDCODE_OUT[$i]));
-      $in = str_replace(["'", " "], ['', ''], ITALICS_HARDCODE_IN[$i]);
-      $out = str_replace(["'", " ", ': '], ['', '', ''], ITALICS_HARDCODE_OUT[$i]);
+      $in  = str_replace(["'", " ", ':'], ['', '', ''], ITALICS_HARDCODE_IN[$i]);
+      $out = str_replace(["'", " ", ':'], ['', '', ''], ITALICS_HARDCODE_OUT[$i]);
       $this->assertSame($in, $out); // Same once spaces and single quotes are removed
     }
  }
