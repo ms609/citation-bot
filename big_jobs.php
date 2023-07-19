03c36938dd69ba485b0f78d1a2af70eb0be981cd
GlazerMann <kurt@ioiowa.com>
2023-07-19 09:16:13 -0500

    Fix locking - clearing directory and file cache

--- big_jobs.php
+++ big_jobs.php
@@ -1,17 +1,21 @@
 <?php
 declare(strict_types=1);
 
-function big_jobs_name() : string {
+function big_jobs_name() : string { // NEVER save this string. Always use this function so that clearstatcache is called
   $version = "_1"; // So we can reset everyone, and we are 100% sure we do not get just the directory name
-  $user = (string) @$_SESSION['citation_bot_user_id']; // Sometimes is not set - no idea how
+  $start = "/dev/shm/";
+  $user = (string) @$_SESSION['citation_bot_user_id']; // Sometimes is not set - clearly bogus                           
   $user = base64_encode($user); // Sanitize - will now just be a-zA-Z0-9/+ and padded with = and surrounded by quotes because of PHP
   $user = str_replace(["'", "=", '"', "/"], ["", "", "", "_"], $user); // Sanitize more
-  return "/dev/shm/" . $user . $version; 
+  $file = $start . $user . $version;
+  @clearstatcache();
+  @clearstatcache(TRUE, $start);
+  @clearstatcache(TRUE, $file);
+  return $file;
 }
 
 /** @param resource $lock_file **/
 function big_jobs_we_died($lock_file) : void {
-  @clearstatcache();
   @fclose($lock_file);
   @unlink(big_jobs_name());
   flush();
@@ -21,7 +25,6 @@ function big_jobs_check_overused(int $page_count) : void {
  static $lock_file; // Force file handle to stay open
  if (!HTML_OUTPUT) return;
  if ($page_count < 50) return; // Used to be BIG_RUN constant
- clearstatcache();
  $fn = big_jobs_name();
  if (file_exists($fn) && (filemtime($fn) > (time()-3600))) { // More than an hour
     @unlink($fn);
@@ -45,7 +48,6 @@ function big_jobs_check_overused(int $page_count) : void {
 function big_jobs_check_killed() : void {
  if (!HTML_OUTPUT) return;
  if (!defined('BIG_JOB_MODE')) return;
- clearstatcache();
  $fn = big_jobs_name() . '_kill_job';
  if (file_exists($fn)) {
    echo '</pre><div style="text-align:center"><h1>Run killed as requested.</h1></div><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>';
@@ -61,7 +63,6 @@ function big_jobs_check_killed() : void {
 }
 
 function big_jobs_kill() : bool {
- clearstatcache();
  if (!file_exists(big_jobs_name())) return FALSE;
  touch(big_jobs_name() . '_kill_job');
  flush();
