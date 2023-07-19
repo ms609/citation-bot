aaf65bdcdc01e36d82041adb090118987025eec8
GlazerMann <kurt@ioiowa.com>
2023-07-18 19:34:53 -0500

    Make sure we close the lock file

--- big_jobs.php
+++ big_jobs.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+$big_jobs_lock_file;
+
 function big_jobs_name() : string {
   $version = "_1"; // So we can reset everyone, and we are 100% sure we do not get just the directory name
   $user = (string) @$_SESSION['citation_bot_user_id']; // Sometimes is not set - no idea how
@@ -9,16 +11,15 @@ function big_jobs_name() : string {
   return "./user_locks/" . $user . $version; 
 }
 
-/** @param resource $lock_file **/
-function big_jobs_we_died($lock_file) : void {
+function big_jobs_we_died() : void {
+  global $big_jobs_lock_file;
   @clearstatcache(TRUE);
-  @fclose($lock_file);
+  @fclose(@$big_jobs_lock_file);
   @unlink(big_jobs_name());
   flush();
 }
 
 function big_jobs_check_overused(int $page_count) : void {
- static $lock_file; // Force file handle to stay open
  if (!HTML_OUTPUT) return;
  if ($page_count < 50) return; // Used to be BIG_RUN constant
  clearstatcache(TRUE);
@@ -27,13 +28,14 @@ function big_jobs_check_overused(int $page_count) : void {
     @unlink($fn);
     flush();
  }
- $lock_file = fopen($fn, 'w+');
- if ($lock_file === FALSE) {
+ global $big_jobs_lock_file;
+ $big_jobs_lock_file = fopen($fn, 'w+');
+ if ($big_jobs_lock_file === FALSE) {
    echo '</pre><div style="text-align:center"><h1>Run blocked by your existing big run.</h1></div><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>';
    exit();
  } else {
    define('BIG_JOB_MODE', 'YES');
-   register_shutdown_function('big_jobs_we_died', $lock_file); // We now have a lock file that will magically go away when code dies/quits
+   register_shutdown_function('big_jobs_we_died');
  }
 }
 
@@ -45,8 +47,7 @@ function big_jobs_check_killed() : void {
  if (file_exists($fn)) {
    echo '</pre><div style="text-align:center"><h1>Run killed as requested.</h1></div><footer><a href="./" title="Use Citation Bot again">Another</a>?</footer></body></html>';
    @unlink($fn);
-   @unlink(big_jobs_name());
-   flush();
+   big_jobs_we_died();
    exit();
  }
  $fn = big_jobs_name();
