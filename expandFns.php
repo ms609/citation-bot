c82455620b41a844f510dccfe15e1bea674e88a3
GlazerMann <kurt@ioiowa.com>
2023-07-22 09:29:35 -0500

    more xml

--- expandFns.php
+++ expandFns.php
@@ -359,6 +359,9 @@ function wikify_external_text(string $title) : string {
   $originalTags = array('<title>', '</title>', '</ title>', 'From the Cover: ');
   $wikiTags = array('','','','');
   $title = str_ireplace($originalTags, $wikiTags, $title);
+  $originalTags = array('<inf>', '</inf>');
+  $wikiTags = array('<sub>', '</sub>');
+  $title = str_ireplace($originalTags, $wikiTags, $title);
   $originalTags = array('.<br>', '.</br>', '.</ br>', '.<p>', '.</p>', '.</ p>', '.<strong>', '.</strong>', '.</ strong>');
   $wikiTags = array('. ','. ','. ','. ','. ','. ','. ','. ','. ');
   $title = str_ireplace($originalTags, $wikiTags, $title);
