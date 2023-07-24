48c3a988c9857cb111a9f41f45485ce218b70d02
GlazerMann <kurt@ioiowa.com>
2023-07-24 12:00:50 -0500

    volume/issue demix

--- Template.php
+++ Template.php
@@ -6849,6 +6849,7 @@ final class Template {
               preg_match("~^(\d+) \((\d+ Suppl \d+)\)$~i", $data, $matches) ||
               preg_match("~^Vol\.?(\d+)\((\d+)\)$~", $data, $matches) ||
               preg_match("~^(\d+) +\(No(?:\.|\. | )(\d+)\)$~i", $data, $matches) ||
+              preg_match("~^(\d+):(\d+)$~", $data, $matches) ||
               preg_match("~^(\d+) +\(Iss(?:\.|\. | )(\d+)\)$~i", $data, $matches)
          ) {
          $possible_volume=$matches[1];
