f990b4d9d421b0d9d1c83c583dc74becb4064c53
GlazerMann <kurt@ioiowa.com>
2023-10-24 15:54:15 -0500

    keep pdf

--- Zotero.php
+++ Zotero.php
@@ -1222,7 +1222,7 @@ public static function find_indentifiers_in_urls(Template $template, ?string $ur
            return TRUE;
          }
        }
-       if (is_null($url_sent) && get_semanticscholar_license($s2cid) === FALSE) {
+       if (is_null($url_sent) && get_semanticscholar_license($s2cid) === FALSE && stripos($url, 'pdf') === FALSE) {
          report_warning('Removing un-licensed Semantic Scholar URL that was converted to S2CID parameter');
          $template->forget($url_type);
          return TRUE;
