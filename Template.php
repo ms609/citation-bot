9ec9d3ab91040daf4c72e869424d603062247730
GlazerMann <kurt@ioiowa.com>
2023-12-04 10:09:54 -0600

    Update Template.php

--- Template.php
+++ Template.php
@@ -363,7 +363,7 @@ final class Template {
               $bad_data = TRUE;
           }
           $ieee_insanity = FALSE;
-          if (conference_doi($this->get('doi') && $this->wikiname() === 'cite journal') &&
+          if (conference_doi($this->get('doi')) && ($this->wikiname() === 'cite journal') &&
               ($this->has('isbn') ||
               (stripos($the_title, 'proceedings') !== FALSE && stripos($the_journal, 'proceedings') !== FALSE) ||
               (stripos($the_title, 'Conference') !== FALSE && stripos($the_journal, 'Conference') !== FALSE) ||
