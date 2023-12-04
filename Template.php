1c6a0919a1231d9b25a97d9cc50a8e43ae2b1729
GlazerMann <kurt@ioiowa.com>
2023-12-04 10:47:26 -0600

    Surtees Society

--- Template.php
+++ Template.php
@@ -1353,6 +1353,7 @@ final class Template {
           if ($this->has('journal') && str_equivalent($this->get('journal'), $value)) return FALSE;
           if ($this->has('title') && str_equivalent($this->get('title'), $value)) return FALSE;
           if ($value === 'A Penguin book') return FALSE;
+          if ($value === 'Surtees Society') return FALSE;
           if ($value === 'Also known as:Official records of the Union and Confederate armies') return FALSE;
           return $this->add($param_name, $value);
         }
