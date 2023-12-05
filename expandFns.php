f7b12a6ee5c6667a542fae0bc86022c69db10a08
Maxim Masiutin <maxim@masiutin.com>
2023-12-05 16:44:48 +0200

    An option to save output to files rather than sending to Wikipedia

--- expandFns.php
+++ expandFns.php
@@ -1216,28 +1216,47 @@ function edit_a_list_of_pages(array $pages_in_category, WikipediaBot $api, strin
     big_jobs_check_killed();
     $done++;
     if ($page->get_text_from($page_title) && $page->expand_text()) {
-      report_phase("Writing to " . echoable($page_title) . '... ');
-      $attempts = 0;
-      if ($total === 1) {
-        $edit_sum = $edit_summary_end;
-      } else {
-        $edit_sum = $edit_summary_end . (string) $done . '/' . (string) $total . ' ';
-      }
-      while (!$page->write($api, $edit_sum) && $attempts < MAX_TRIES) ++$attempts;
-      if ($attempts < MAX_TRIES) {
-        $last_rev = WikipediaBot::get_last_revision($page_title);
-        html_echo(
-        "\n  <a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . "&diff=prev&oldid="
-        . $last_rev . ">diff</a>" .
-        " | <a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . "&action=history>history</a>", 
-        "\n" . WIKI_ROOT . "?title=" . urlencode($page_title) . "&diff=prev&oldid=". $last_rev . "\n");
-        $final_edit_overview .=
-          "\n [ <a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . "&diff=prev&oldid="
-        . $last_rev . ">diff</a>" .
-        " | <a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . "&action=history>history</a> ] " . "<a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . ">" . echoable($page_title) . "</a>";
-      } else {
-        report_warning("Write failed.");
-        $final_edit_overview .= "\n Write failed.      " . "<a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . ">" . echoable($page_title) . "</a>";
+      if (SAVETOFILES_MODE)
+      {
+        // Sanitize file name by replacing characters that are not allowed on most file systems to underscores, and also replace path characters
+        // And add .md extension to avoid troubles with devices such as 'con' or 'aux'
+        $filename = preg_replace('/[\/\\:*?"<>|]/', '_', $page_title) . '.md';
+        report_phase("Saving to file " . echoable($filename));
+        $body = $page->parsed_text();
+        $bodylen = strlen($body);
+        if (file_put_contents($filename, $body)===$bodylen)
+        {
+          report_phase("Saved to file " . echoable($filename));
+        } else
+        {
+          report_warning("Save to file failed.");
+        }
+        unset($body);
+      } else
+      {
+        report_phase("Writing to " . echoable($page_title) . '... ');
+        $attempts = 0;
+        if ($total === 1) {
+          $edit_sum = $edit_summary_end;
+        } else {
+          $edit_sum = $edit_summary_end . (string) $done . '/' . (string) $total . ' ';
+        }
+        while (!$page->write($api, $edit_sum) && $attempts < MAX_TRIES) ++$attempts;
+        if ($attempts < MAX_TRIES) {
+          $last_rev = WikipediaBot::get_last_revision($page_title);
+          html_echo(
+          "\n  <a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . "&diff=prev&oldid="
+          . $last_rev . ">diff</a>" .
+          " | <a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . "&action=history>history</a>", 
+          "\n" . WIKI_ROOT . "?title=" . urlencode($page_title) . "&diff=prev&oldid=". $last_rev . "\n");
+          $final_edit_overview .=
+            "\n [ <a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . "&diff=prev&oldid="
+          . $last_rev . ">diff</a>" .
+          " | <a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . "&action=history>history</a> ] " . "<a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . ">" . echoable($page_title) . "</a>";
+        } else {
+          report_warning("Write failed.");
+          $final_edit_overview .= "\n Write failed.      " . "<a href=" . WIKI_ROOT . "?title=" . urlencode($page_title) . ">" . echoable($page_title) . "</a>";
+        }
       }
     } else {
       report_phase($page->parsed_text() ? "No changes required. \n\n    # # # " : "Blank page. \n\n    # # # ");
