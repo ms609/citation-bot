1300caad457bd8886a5a5aa85156d4b1df51abf7
GlazerMann <kurt@ioiowa.com>
2023-12-19 16:26:10 -0600

    Update Template.php

--- Template.php
+++ Template.php
@@ -221,6 +221,9 @@ final class Template {
 		return base64_decode($this->get(strtolower('CITATION_BOT_PLACEHOLDER_BARE_URL')));
 	  }
 	}
+	if (stripos(trim($this->name), '#invoke:') === 0) {
+		return '{{' . $this->name . '|' . $this->join_params() . '}}';
+	}
 	return '{{' . $this->name . $this->join_params() . '}}';
   }
 
