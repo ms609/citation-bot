47080efa97be2a723ea24d34ab374fccf2a6f8f1
GlazerMann <kurt@ioiowa.com>
2023-12-20 10:03:07 -0600

    Update apiFunctions.php

--- apiFunctions.php
+++ apiFunctions.php
@@ -1531,6 +1531,11 @@ function query_adsabs(string $options) : object {
       $header = ''; 
       $body = '';
       curlGetResponse($adsabs_url, $return, $ch, $http_response_code, $header, $body);
+    } (catch Exception $e) {
+      $return = '';
+      $http_response_code = 6000;
+      $header = ''; 
+      $body = '';
     }
     $response = Bibcode_Response_Processing($adsabs_url, $http_response_code, $header, $body);
     return $response;
