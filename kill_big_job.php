6758f00011a616bf55d3717d46a647f28fe49653
GlazerMann <kurt@ioiowa.com>
2024-01-10 08:45:37 -0600

    Update kill_big_job.php

--- kill_big_job.php
+++ kill_big_job.php
@@ -1,12 +1,11 @@
 <?php
 declare(strict_types=1);
+ob_implicit_flush();
 set_time_limit(120);
 $started = @session_start(['read_and_close' => TRUE]);
 
 require_once 'html_headers.php';
 require_once 'big_jobs.php';
-
-ob_implicit_flush();
 
 if (!isset($_SESSION['citation_bot_user_id'])) {
  echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Killing the big job</title></head><body><main><pre>You are not logged in</pre></main></body></html>';
