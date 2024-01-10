34cfe73eb999bd92fe8ec91e3ac644b3bac53c64
GlazerMann <kurt@ioiowa.com>
2024-01-10 08:42:48 -0600

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -21,7 +21,6 @@ function death_time(string $err) : never {
   $name = (string) @session_name();
   $id = (string) @session_id();
   @session_destroy(); // This is really bad news. Do this before we kill the cookie
-  flush(); // Paranoid
   @setcookie($name, $id, @time()-42000, "", "", TRUE, TRUE);
   echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Authentifcation System Failure</title></head><body><main>' . echoable($err) . '</main></body></html>';
   exit(0);
