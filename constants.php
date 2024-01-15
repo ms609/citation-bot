d36f736d98266a7d2527a1fb2a8e28dfd0055481
GlazerMann <kurt@ioiowa.com>
2024-01-15 16:28:32 -0600

    Update constants.php

--- constants.php
+++ constants.php
@@ -64,9 +64,7 @@ function curl_init_array(float $time, array $ops) : CurlHandle {
 	// 1 - Global Defaults
 	// TODO curl_setopt_array($ch, []);
 	// 2 - Default Time by ratio
-	curl_setopt_array(
-	  [CURLOPT_TIMEOUT => BOT_HTTP_TIMEOUT * $time,
-	   CURLOPT_CONNECTTIMEOUT => BOT_CONNECTION_TIMEOUT * $time]);
+	curl_setopt_array($ch, [CURLOPT_TIMEOUT => BOT_HTTP_TIMEOUT * $time, CURLOPT_CONNECTTIMEOUT => BOT_CONNECTION_TIMEOUT * $time]);
 	// 3 - Specific options and overrides (after time, just in case ratio is different for both parts)
 	curl_setopt_array($ch, $ops);
 	return $ch;
