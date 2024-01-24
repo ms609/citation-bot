c29616b171f0978936a45e4d92dc397887446c6f
GlazerMann <kurt@ioiowa.com>
2024-01-24 07:16:41 -0600

    Update italics.php

--- constants/italics.php
+++ constants/italics.php
@@ -418,7 +418,7 @@ const CAMEL_CASE = array('DeSoto', 'PubChem', 'BitTorrent', 'Al2O3', 'NiMo', 'Cu
 			 'RiceWiki', 'SciLite', 'ScispaCy', 'WormBase', 'VirFinder', 'UniProt', 'GaiaData', 'FoodOn',
 			 'GaiaHundred', 'WikiJournal', 'StarCraft', 'EuResist', 'PhageScope', 'CoVaMa', 'EuroMyositis',
 			 'DavEnd', 'CoLaus', 'MinerAlocorticoid', 'miRiad', 'kaSenzangakhona', 'BioChirality', 'DeGeer',
-			 'ReFocus:', 'FusionCatcher', 'cFos', 'ReOrienting'
+			 'ReFocus:', 'FusionCatcher', 'cFos', 'ReOrienting', 'NatureServe',
 		       );
 
 const ITALICS_HARDCODE_IN  = ["The Myth ofPiers Plowman", "BioelectromagnetismPrinciples and Applications", "Practice inThe Nice Valour",
