844bbf81e938e6bd0eb425b5eb89e4f941d4661c
GlazerMann <kurt@ioiowa.com>
2024-02-16 13:42:46 -0600

    Update gadgetapi.php

--- gadgetapi.php
+++ gadgetapi.php
@@ -32,13 +32,14 @@ try {
  $page->parse_text($originalText);
  $page->expand_text();
  $newText = $page->parsed_text();
- if ($newText === "") $newText = $originalText; // Something went very wrong
+ if ($newText === "") throw new Exception('text lost');  // @codeCoverageIgnore
 
  //Modify edit summary to identify bot-assisted edits
  if ($newText !== $originalText) {
    if ($editSummary) $editSummary .= ' | '; // Add pipe if already something there.
    $editSummary .=  str_replace('Use this bot', 'Use this tool', $page->edit_summary()) . '| #UCB_Gadget ';
  }
+ unset($originalText, $page);
 
  /**
    * @psalm-taint-escape html
@@ -48,7 +49,7 @@ try {
    'expandedtext' => $newText,
    'editsummary' => $editSummary
  );
- unset($newText, $editSummary, $originalText, $page); // Free memory before encoding
+ unset($newText, $editSummary);
  ob_end_clean();
 
  echo (string) @json_encode($result);
@@ -56,3 +57,5 @@ try {
  @ob_end_clean();@ob_end_clean();@ob_end_clean(); // @codeCoverageIgnore
  // Above is paranoid panic code.  So paranoid that we even empty buffers two extra times
 }
+
+?>
