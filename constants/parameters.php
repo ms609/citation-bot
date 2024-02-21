2d7056ae6343ad4accb51910bdc166179f31e5af
GlazerMann <kurt@ioiowa.com>
2024-02-21 16:55:46 -0600

    Update parameters.php

--- constants/parameters.php
+++ constants/parameters.php
@@ -514,9 +514,10 @@ const PARAMETER_LIST = array(
      'timecaption', 'titlelink', 'ref name', 'ISBN13', 'booktitle', 'chapterurl', 'episodelink',
      'isbn13', 'mailinglist', 'mapurl', 'nopp', 'publicationdate', 'publicationplace',
      'serieslink', 'sid', 'note', 'do', 'lien', 'lay-date', 'lay-format', 'lay-source',
-     'lay-url', 'transcripturl', 'pages totales', 'ppprefix', 'pprefix');
+     'lay-url', 'transcripturl', 'pages totales', 'ppprefix', 'pprefix', 'web', 'middle');
 
 // sid is actually page range in some no english wiki's but do not fix since so close to id=
 // lien is French template parameter, but also English work
+// web could be website or url
 
 
