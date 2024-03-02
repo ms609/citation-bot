9ff53e7da73c399179a49e7fa956bd1bd76af427
GlazerMann <kurt@ioiowa.com>
2024-03-02 15:39:45 -0600

    Update expandFns.php

--- expandFns.php
+++ expandFns.php
@@ -194,6 +194,9 @@ function is_doi_works(string $doi) : ?bool {
   if ($headers_test === FALSE) {
      return NULL; // most likely bad - note that NULL means do not add or remove doi-broken-date from pages
   }
+  if (stripos($doi, '10.1126/scidip.') === 0) {
+      if ((string) @$headers_test['1'] === 'HTTP/1.1 404 Forbidden') unset($headers_test['1']); // https://doi.org/10.1126/scidip.ado5059
+  }
   if (interpret_doi_header($headers_test) !== FALSE) {
        return interpret_doi_header($headers_test);
   }
@@ -208,9 +211,6 @@ function is_doi_works(string $doi) : ?bool {
 function interpret_doi_header(array $headers_test) : ?bool {
   if (empty($headers_test['Location']) && empty($headers_test['location'])) return FALSE; // leads nowhere
  
-  if (stripos($doi, '10.1126/scidip.') === 0) {
-      if ((string) @$headers_test['1'] === 'HTTP/1.1 404 Forbidden') unset($headers_test['1']); // https://doi.org/10.1126/scidip.ado5059
-  }
   if ((string) @$headers_test['1'] === 'HTTP/1.1 302 Redirected') unset($headers_test['2']); // https://doi.org/10.1107/S2056989021000116
   if ((string) @$headers_test['1'] === 'HTTP/1.1 301 Moved Permanently') unset($headers_test['2']); // https://doi.org/10.5152/UCD.2016.3683
 
