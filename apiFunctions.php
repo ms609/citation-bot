c6f92387ebe2510df3c9dcf8b68dbb3eac23cbf0
GlazerMann <kurt@ioiowa.com>
2024-03-08 08:46:41 -0600

    Update apiFunctions.php

--- apiFunctions.php
+++ apiFunctions.php
@@ -109,7 +109,7 @@ function entrez_api(array $ids, array &$templates, string $db) : void {   // Poi
        $an_id = -3333; // Do not use this
    }
    if ($an_id == $document->Id) {
-    $this_template = $templates($template_key);
+    $this_template = $templates[$template_key];
     $this_template->record_api_usage('entrez', $db === 'pubmed' ? 'pmid' : 'pmc');
 
     foreach ($document->Item as $item) {
