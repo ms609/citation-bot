22a400a584c4ba6126c2231bba09452e730dd1b5
GlazerMann <kurt@ioiowa.com>
2024-04-09 12:25:14 -0500

    Update null_doi.php

--- constants/null_doi.php
+++ constants/null_doi.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 const NULL_DOI_ANNOYING = // TODO - manually check these from time to time - Last Check 10 MAR 2024
 array(
+'10.7454/irhs.v1i1.50' => TRUE,
+'10.21143/jhp.vol8.no3.772' => TRUE,
 '10.1001/jama.275.17.1339' => TRUE,
 '10.1080/10798587.2007.10642974' => TRUE,
 '10.1139/gen-41-4-543' => TRUE,
@@ -347,7 +349,7 @@ array(
 '10.2104/mbr07056' => TRUE,
 '10.21040/eom/2016.2.7' => TRUE,
 '10.21082/blpn.v12n2.2006.p83-88' => TRUE,
-'10.21143/jhp.vol8.no3.772' => TRUE,
+'10.21143/jhp.vol8.no3.772' => TRUE, // DB Error
 '10.21154/justicia.v12i2.328' => TRUE,
 '10.21271/ZJPAS.31.2.12' => TRUE,
 '10.2136/sh2012-53-6-lf' => TRUE, // published landing page
@@ -643,7 +645,7 @@ array(
 '10.7182/prtr.13.3.a368158t7662j463' => TRUE,
 '10.7313/upo9781904761679.011' => TRUE,
 '10.7313/upo9781907284991.018' => TRUE,
-'10.7454/irhs.v1i1.50' => TRUE,
+'10.7454/irhs.v1i1.50' => TRUE, // DB Error
 '10.7556/jaoa' => TRUE,
 '10.7575/aiac.ijalel.v.6n.3p.71' => TRUE,
 '10.7815/ijorcs.26.2012.051' => TRUE,
@@ -656,6 +658,7 @@ const NULL_DOI_BUT_GOOD = array(  // TODO - these need to be manually double che
                                   // Note that a failed url on one computer does not mean it is gone, it might just be you
                                   // You can use hdl.handle.org to show you the final url - russia and other countries firewall
                                   // Also, you can use various archive services to see (hdl.handle.org might not work while doi.org does work even!!!)
+'10.1016/b978-0-12-386454-3.00012-9' => TRUE,
 '10.1001/archpsyc.60.5.497' => TRUE,
 '10.1002/047084289X.rt358.pub2' => TRUE,
 '10.1002/0471221902' => TRUE,
