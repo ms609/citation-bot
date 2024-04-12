2e9fa87f0bce6e516a559133da883a4ab1e8561a
GlazerMann <kurt@ioiowa.com>
2024-04-12 10:17:31 -0500

    Update mistakes.php

--- constants/mistakes.php
+++ constants/mistakes.php
@@ -513,6 +513,7 @@ const COMMON_MISTAKES = array ( // Common mistakes that aren't picked up by the 
   "link to author" => "author-link",
   "access-daet" => "access-date",
   "werk" => "work",
+  "abruf" => "access-date",			       
 ); // Lots of stuff that just existed
 
 const COMMON_MISTAKES_TOOL = array(
