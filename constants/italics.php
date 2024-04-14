75af7c5763ffd131888d5299de8c35d0d7de53f7
GlazerMann <kurt@ioiowa.com>
2024-04-14 15:01:19 -0500

    Update italics.php

--- constants/italics.php
+++ constants/italics.php
@@ -462,7 +462,7 @@ const ITALICS_HARDCODE_IN  = ["The Myth ofPiers Plowman", "BioelectromagnetismPr
 	     "the Binary Dynamics ofRomeo and Juliet", "llegada de los IncasAproximaciones desde", "Biomedical LawLegal and Extra",
 	     "Homo erectusPleistocene Evidence from ", "Technical Issues andFuture Directions", "on EarthJoseph Smith", "Brain and MemoryModulation and ",
 	     "Qusayr AmraArt and the", " ofDrosophilamotor circuits", "Understanding GenocideThe Social", "New Supreme CourtNational and",
-	     "the Limits of EmpireOpium and",
+	     "the Limits of EmpireOpium and", "Roman EmpireVolume I: Maximilian",
 ];
 const ITALICS_HARDCODE_OUT = ["The Myth of ''Piers Plowman''", "Bioelectromagnetism: Principles and Applications", "Practice in ''The Nice Valour''",
 	     "Tweeting the ''Jihad'': Social Media", " of Sectarianism: Community", " for Wonder: Nineteenth Century Travel ",
@@ -490,5 +490,5 @@ const ITALICS_HARDCODE_OUT = ["The Myth of ''Piers Plowman''", "Bioelectromagnet
 	     "the Binary Dynamics of ''Romeo and Juliet''", "llegada de los Incas. Aproximaciones desde", "Biomedical Law: Legal and Extra",
 	     "Homo erectus: Pleistocene Evidence from ", "Technical Issues and Future Directions", "on Earth: Joseph Smith", "Brain and Memory: Modulation and ",
 	     "Qusayr Amra: Art and the", " of ''Drosophila'' motor circuits", "Understanding Genocide: The Social", "New Supreme Court: National and",
-	     "the Limits of Empire: Opium and",
+	     "the Limits of Empire: Opium and", "Roman Empire: Volume I: Maximilian",
 ];
