860946cd55ebcd615615b9f264468c040439c973
GlazerMann <kurt@ioiowa.com>
2024-04-18 13:26:38 -0500

    silly code coverage tests

--- tests/phpunit/apiFunctionsTest.php
+++ tests/phpunit/apiFunctionsTest.php
@@ -836,5 +836,21 @@ final class apiFunctionsTest extends testBaseClass {
     sleep(2);
     $this->assertTrue(get_semanticscholar_license('52813129'));
   }
-	
+
+  public function testUse_ISSN() : void {
+    $prepared = $this->process_citation('{{cite news|issn=0140-0460 }}');
+    $this->assertSame('[[The Times]]', $prepared->get2('newspaper')); 
+
+    $prepared = $this->process_citation('{{cite news|issn=0190-8286 }}');
+    $this->assertSame('[[The Washington Post]]', $prepared->get2('newspaper')); 
+
+    $prepared = $this->process_citation('{{cite news|issn=0362-4331 }}');
+    $this->assertSame('[[The New York Times]]', $prepared->get2('newspaper')); 
+
+    $prepared = $this->process_citation('{{cite news|issn=0163-089X }}');
+    $this->assertSame('[[The Wall Street Journal]]', $prepared->get2('newspaper')); 
+
+    $prepared = $this->process_citation('{{cite news|issn=1092-0935 }}');
+    $this->assertSame('[[The Wall Street Journal]]', $prepared->get2('newspaper')); 
+  }
 }
