d42e0d6e0ef7da27458d8e7a72d0b06601b23dac
GlazerMann <kurt@ioiowa.com>
2024-04-19 08:05:55 -0500

    Whole bunch of google books tests

--- tests/phpunit/expandFnsTest.php
+++ tests/phpunit/expandFnsTest.php
@@ -596,5 +596,65 @@ final class expandFnsTest extends testBaseClass {
     $this->assertSame($out, mb_substr_replace($in, 'X', 3, 1));
     $this->assertNotSame($out, substr_replace($in, 'X', 3, 1));
   }
-	  
+
+  public function testGoogleBookNormalize1() : void {
+    $removed_redundant = 0;
+    $removed_parts = '';
+    $gid = array();
+    $url_in  = 'https://books.google.com/books?id=m8W2AgAAQBAJ&bsq=1234';
+    $url_out = 'https://books.google.com/books?id=m8W2AgAAQBAJ&q=1234';
+    normalize_google_books($url_in, $removed_redundant, $removed_parts, $gid); // Reference passed
+    $this->assertSame($url_out, $url_in);
+  }
+
+  public function testGoogleBookNormalize1() : void {
+    $removed_redundant = 0;
+    $removed_parts = '';
+    $gid = array();
+    $url_in  = 'https://books.google.com/books?id=m8W2AgAAQBAJ&bsq=1234&q=abc';
+    $url_out = 'https://books.google.com/books?id=m8W2AgAAQBAJ&q=abc';
+    normalize_google_books($url_in, $removed_redundant, $removed_parts, $gid); // Reference passed
+    $this->assertSame($url_out, $url_in);
+  }
+
+  public function testGoogleBookNormalize2() : void {
+    $removed_redundant = 0;
+    $removed_parts = '';
+    $gid = array();
+    $url_in  = 'https://books.google.com/books?id=m8W2AgAAQBAJ#PPA333,M1';
+    $url_out = 'https://books.google.com/books?id=m8W2AgAAQBAJ&pg=PA333';
+    normalize_google_books($url_in, $removed_redundant, $removed_parts, $gid); // Reference passed
+    $this->assertSame($url_out, $url_in);
+  }
+ 
+  public function testGoogleBookNormalize3() : void {
+    $removed_redundant = 0;
+    $removed_parts = '';
+    $gid = array();
+    $url_in  = 'https://books.google.com/books?id=m8W2AgAAQBAJ#PP333,M1';
+    $url_out = 'https://books.google.com/books?id=m8W2AgAAQBAJ&pg=PP333';
+    normalize_google_books($url_in, $removed_redundant, $removed_parts, $gid); // Reference passed
+    $this->assertSame($url_out, $url_in);
+  }
+ 
+  public function testGoogleBookNormalize4() : void {
+    $removed_redundant = 0;
+    $removed_parts = '';
+    $gid = array();
+    $url_in  = 'https://books.google.com/books?id=m8W2AgAAQBAJ#PPT333,M1';
+    $url_out = 'https://books.google.com/books?id=m8W2AgAAQBAJ&pg=PT333';
+    normalize_google_books($url_in, $removed_redundant, $removed_parts, $gid); // Reference passed
+    $this->assertSame($url_out, $url_in);
+  }
+
+  public function testGoogleBookNormalize5() : void {
+    $removed_redundant = 0;
+    $removed_parts = '';
+    $gid = array();
+    $url_in  = 'https://books.google.com/books?id=m8W2AgAAQBAJ#PR333,M1';
+    $url_out = 'https://books.google.com/books?id=m8W2AgAAQBAJ&pg=PR333';
+    normalize_google_books($url_in, $removed_redundant, $removed_parts, $gid); // Reference passed
+    $this->assertSame($url_out, $url_in);
+  }
+	
 }
