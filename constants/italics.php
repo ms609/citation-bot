9fb72e823e1338772cf2a5893043594199bb8210
GlazerMann <kurt@ioiowa.com>
2024-04-23 10:08:32 -0500

    Update italics.php

--- constants/italics.php
+++ constants/italics.php
@@ -463,7 +463,7 @@ const ITALICS_HARDCODE_IN  = ["The Myth ofPiers Plowman", "BioelectromagnetismPr
 	     "Homo erectusPleistocene Evidence from ", "Technical Issues andFuture Directions", "on EarthJoseph Smith", "Brain and MemoryModulation and ",
 	     "Qusayr AmraArt and the", " ofDrosophilamotor circuits", "Understanding GenocideThe Social", "New Supreme CourtNational and",
 	     "the Limits of EmpireOpium and", "Roman EmpireVolume I: Maximilian", "History DerailedCentral and Eastern ",
-	     " Last CampaignBritain and the ",
+	     " Last CampaignBritain and the ", " and ConditionalsNew and Revised ",
 ];
 const ITALICS_HARDCODE_OUT = ["The Myth of ''Piers Plowman''", "Bioelectromagnetism: Principles and Applications", "Practice in ''The Nice Valour''",
 	     "Tweeting the ''Jihad'': Social Media", " of Sectarianism: Community", " for Wonder: Nineteenth Century Travel ",
@@ -492,5 +492,5 @@ const ITALICS_HARDCODE_OUT = ["The Myth of ''Piers Plowman''", "Bioelectromagnet
 	     "Homo erectus: Pleistocene Evidence from ", "Technical Issues and Future Directions", "on Earth: Joseph Smith", "Brain and Memory: Modulation and ",
 	     "Qusayr Amra: Art and the", " of ''Drosophila'' motor circuits", "Understanding Genocide: The Social", "New Supreme Court: National and",
 	     "the Limits of Empire: Opium and", "Roman Empire: Volume I: Maximilian", "History Derailed: Central and Eastern ",
-	     " Last Campaign: Britain and the ",
+	     " Last Campaign: Britain and the ", " and Conditionals: New and Revised ",
 ];
