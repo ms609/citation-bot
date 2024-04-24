12d9291d35a6dbfd66caa44171fb62850a7fd1af
GlazerMann <kurt@ioiowa.com>
2024-04-24 13:42:53 -0500

    Update linked_pages.php

--- linked_pages.php
+++ linked_pages.php
@@ -26,7 +26,7 @@ if ($page_name === '') {
   report_warning('Nothing requested on webform -- OR -- page name got lost during initial authorization ');
   bot_html_footer();
   exit();
-} elseif (substr($page_name, 0, 5) !== 'User:' && !in_array($api->get_the_user(), ['Headbomb', 'AManWithNoPlan'])) { // Do not let people run willy-nilly
+} elseif (substr($page_name, 0, 5) !== 'User:' && !in_array($api->get_the_user(), ['Headbomb', 'AManWithNoPlan'], TRUE)) { // Do not let people run willy-nilly
   report_warning('API only intended for User generated pages for fixing specific issues ');
   bot_html_footer();
   exit();
@@ -56,7 +56,7 @@ foreach($links as $link) {
     if (isset($link['exists']) && ($link['ns'] === 0 || $link['ns'] === 118)) {  // normal and draft articles only
 	$linked_page = (string) $link['*'];
 	$linked_page = str_replace(' ', '_', $linked_page);
-	if(!in_array($linked_page, AVOIDED_LINKS) && stripos($linked_page, 'disambiguation') === FALSE) {
+	if(!in_array($linked_page, AVOIDED_LINKS, TRUE) && stripos($linked_page, 'disambiguation') === FALSE) {
 	    $pages_in_category[] = $linked_page;
 	}
     }
