3802217cb3b75efb8ce543004df0a275f304d4c2
GlazerMann <kurt@ioiowa.com>
2024-04-27 17:32:18 -0500

    Update ZZZLastTests.php

--- tests/phpunit/ZZZLastTests.php
+++ tests/phpunit/ZZZLastTests.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  */
 require_once __DIR__ . '/../testBaseClass.php';
 
-final class ZZZLastTests extends testBaseClass {
+final readonly class ZZZLastTests extends testBaseClass {
 
   protected function setUp(): void {
    if (BAD_PAGE_API !== '') {
