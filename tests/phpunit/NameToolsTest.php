c7ecfc1af97b3d2b09713d87fae493c48cea3c6a
GlazerMann <kurt@ioiowa.com>
2024-04-28 10:42:35 -0500

    Update NameToolsTest.php

--- tests/phpunit/NameToolsTest.php
+++ tests/phpunit/NameToolsTest.php
@@ -17,7 +17,7 @@ final class NameToolsTest extends testBaseClass {
 
   public function testFillCache() : void {
     $this->fill_cache();
-    $this->assertTrue(TRUE);
+    $this->assertTrue(true);
   }
 
   public function testFormatMultipleAuthors1() : void {
