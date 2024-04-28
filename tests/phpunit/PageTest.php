fcc614c95cdab6a2f21135dec009cb842b3c6445
GlazerMann <kurt@ioiowa.com>
2024-04-28 10:42:42 -0500

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -17,7 +17,7 @@ final class PageTest extends testBaseClass {
 
   public function testFillCache() : void {
     $this->fill_cache();
-    $this->assertTrue(TRUE);
+    $this->assertTrue(true);
   }
 
   public function testPageChangeSummary1() : void {
