884b6eafa6c367fa2f83f0577c1957f1e24a3b2e
GlazerMann <kurt@ioiowa.com>
2024-04-28 10:47:53 -0500

    Update gadgetTest.php

--- tests/phpunit/gadgetTest.php
+++ tests/phpunit/gadgetTest.php
@@ -16,7 +16,7 @@ final class gadgetTest extends testBaseClass {
 
   public function testFillCache() : void {
     $this->fill_cache();
-    $this->assertTrue(TRUE);
+    $this->assertTrue(true);
   }
 
   public function testGadget() : void {
