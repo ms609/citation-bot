631bc67d7972d6809475e441e54d059ef080cf6a
GlazerMann <kurt@ioiowa.com>
2024-04-29 11:45:02 -0500

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -218,17 +218,17 @@ final class PageTest extends testBaseClass {
       $page->get_text_from($writeTestPage);
       $origText = $page->parsed_text();
       $trialCitation = '{{Cite journal | title Bot Testing | ' .
-	'doi_broken_date=1986-01-01 | doi = 10.1038/nature09068}}';
+        'doi_broken_date=1986-01-01 | doi = 10.1038/nature09068}}';
       $page->overwrite_text($trialCitation);
       $page_result = $page->write($api, "Testing bot write function");
       if (TRAVIS && !$page_result) {
         // ! API call failed: '''Your IP address is in a range which has been blocked on all wikis.''' The block was made by [//meta.wikimedia.org/wiki/User:Jon_Kolbert Jon Kolbert] (meta.wikimedia.org). The reason given is ''[[m:NOP|Open Proxy]]: Colocation webhost - Contact [[m:Special:Contact/stewards|stewards]] if you are affected ''. * Start of block: 02:23, 27 October 2019 * Expiration of block: 02:23, 27 October 2021
-	$page->get_text_from($writeTestPage);
-	$this->assertSame($origText, $page->parsed_text());
+        $page->get_text_from($writeTestPage);
+        $this->assertSame($origText, $page->parsed_text());
       } else {
-	// Double check we can read it back
-	$page->get_text_from($writeTestPage);
-	$this->assertSame($trialCitation, $page->parsed_text());
+        // Double check we can read it back
+        $page->get_text_from($writeTestPage);
+        $this->assertSame($trialCitation, $page->parsed_text());
       }
       $this->requires_secrets(function() : void {
        $this->assertTrue(TRAVIS || $page_result); // If we have tokens and are not in TRAVIS, then should have worked
@@ -238,9 +238,9 @@ final class PageTest extends testBaseClass {
       $this->assertTrue(strpos($page->edit_summary(), 'journal, ') > 3);
       $this->assertTrue(strpos($page->edit_summary(), ' Removed ') > 3);
       if ($page_result) {
-	 $this->assertTrue($page->write($api));
+         $this->assertTrue($page->write($api));
       } else {
-	 $this->assertFalse($page->write($api));
+         $this->assertFalse($page->write($api));
       }
       $page->get_text_from($writeTestPage);
       $this->assertTrue(strpos($page->parsed_text(), 'Nature') > 5); // This can fail, if the page gets edited and is no longer {{Cite journal | doi = 10.1038/nature09068 | title=Bot Testing | date=2010 | pmid=20505727 | last1=Smith | first1=M. R. | last2=Caron | first2=J. B. | journal=Nature | volume=465 | issue=7297 | pages=469–472 | hdl=1807/32368 | s2cid=4421029 }}
@@ -264,8 +264,8 @@ final class PageTest extends testBaseClass {
 
   public function testEmptyPage() : void {
       foreach (['', '  ', " \n ", '  move along, nothing to see here ', '  move along, nothing to see here {{}} ', ' }}}}{{{{ ', '{{{{}}', '{{{{    }}', '{{{{}}}}}}}}'] as $text) {
-	$page = $this->process_page($text);
-	$this->assertSame($text, $page->parsed_text());
+        $page = $this->process_page($text);
+        $this->assertSame($text, $page->parsed_text());
       }
   }
 
