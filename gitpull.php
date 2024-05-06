5f9941c83392617b129b2e22e58cd92e88a27058
GlazerMann <kurt@ioiowa.com>
2024-05-06 08:57:56 -0500

    Update gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -1,13 +1,12 @@
 <?php
+
 declare(strict_types=1);
 
 set_time_limit(120);
-
 ob_implicit_flush(true);
-
 require_once 'html_headers.php';
-
 echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Git Pull</title></head><body><main><pre>';
+
 clearstatcache();
 if (@mkdir('git_pull.lock', 0700)) {
   /** @psalm-suppress ForbiddenCode */
