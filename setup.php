15bcecbf8508673d1e6efd7ece761f1a4440f549
GlazerMann <kurt@ioiowa.com>
2024-05-13 08:16:56 -0500

    allow users to pass key

--- setup.php
+++ setup.php
@@ -9,9 +9,9 @@ ob_implicit_flush(true);
 flush();
 
 if (file_exists('git_pull.lock')) {
-  sleep(5);
-  echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Citation Bot: error</title></head><body><h1>GIT pull in progress - please retry again in a moment</h1></body></html>';
-  exit;
+    sleep(5);
+    echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Citation Bot: error</title></head><body><h1>GIT pull in progress - please retry again in a moment</h1></body></html>';
+    exit;
 }
 
 /*
@@ -20,23 +20,23 @@ if (file_exists('git_pull.lock')) {
  */
 
 function bot_debug_log(string $log_this): void {
-  @clearstatcache(); // Deal with multiple writers, but not so paranoid that we get a file lock
-  if (function_exists('echoable')) {
-     // Avoid making a new huge string, so do not combine
-    file_put_contents('CodeCoverage', echoable(WikipediaBot::GetLastUser()) . " :: " . echoable(Page::$last_title) . " :: ", FILE_APPEND);
-    file_put_contents('CodeCoverage', $log_this, FILE_APPEND);
-    file_put_contents('CodeCoverage', "\n", FILE_APPEND);
-  }
+    @clearstatcache(); // Deal with multiple writers, but not so paranoid that we get a file lock
+    if (function_exists('echoable')) {
+        // Avoid making a new huge string, so do not combine
+        file_put_contents('CodeCoverage', echoable(WikipediaBot::GetLastUser()) . " :: " . echoable(Page::$last_title) . " :: ", FILE_APPEND);
+        file_put_contents('CodeCoverage', $log_this, FILE_APPEND);
+        file_put_contents('CodeCoverage', "\n", FILE_APPEND);
+    }
 }
 
 if (isset($_REQUEST["wiki_base"])){
-  $wiki_base = trim((string) $_REQUEST["wiki_base"]);
-  if (!in_array($wiki_base, ['en', 'simple'], true)) {
-     echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Citation Bot: error</title></head><body><h1>Unsupported wiki requested - aborting</h1></body></html>';
-     exit;
-  }
+    $wiki_base = trim((string) $_REQUEST["wiki_base"]);
+    if (!in_array($wiki_base, ['en', 'simple'], true)) {
+        echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Citation Bot: error</title></head><body><h1>Unsupported wiki requested - aborting</h1></body></html>';
+        exit;
+    }
 } else {
-  $wiki_base = 'en';
+    $wiki_base = 'en';
 }
 define("WIKI_ROOT", 'https://'. $wiki_base . '.wikipedia.org/w/index.php');
 define("API_ROOT", 'https://'. $wiki_base . '.wikipedia.org/w/api.php');
@@ -50,71 +50,75 @@ include_once './vendor/autoload.php';
 define("TRAVIS", (bool) getenv('CI') || defined('__PHPUNIT_PHAR__') || defined('PHPUNIT_COMPOSER_INSTALL') || (strpos((string) @$_SERVER['argv'][0], 'phpunit') !== false));
 
 if ((string) @$_REQUEST["page"] . (string) @$argv[1] === "User:AManWithNoPlan/sandbox3") { // Specific page to make sure this code path keeps working
-  define('EDIT_AS_USER', true);
+    define('EDIT_AS_USER', true);
 }
 
 if (TRAVIS || isset($argv)) {
-  define("HTML_OUTPUT", false);
+    define("HTML_OUTPUT", false);
 } else {
-  define("HTML_OUTPUT", true);
+    define("HTML_OUTPUT", true);
 }
 
 // This is needed because the Gadget API expects only JSON back, therefore ALL output from the citation bot is thrown away
 if (strpos((string) @$_SERVER['PHP_SELF'], '/gadgetapi.php') === false) {
-  define("FLUSHING_OKAY", true);
+    define("FLUSHING_OKAY", true);
 } else {
-  define("FLUSHING_OKAY", false);
+    define("FLUSHING_OKAY", false);
 }
 
 if (isset($_REQUEST["slow"]) || TRAVIS || (isset($argv) && in_array('--slow', $argv, true))) {
-  define("SLOW_MODE", true);
+    define("SLOW_MODE", true);
 } else {
-  define("SLOW_MODE", false);
+    define("SLOW_MODE", false);
 }
 
 if (isset($argv) && in_array('--savetofiles', $argv, true)) {
-  define("SAVETOFILES_MODE", true);
+    define("SAVETOFILES_MODE", true);
 } else {
-  define("SAVETOFILES_MODE", false);
+    define("SAVETOFILES_MODE", false);
 }
 
 if (file_exists('env.php')) {
-  // Set the environment variables with putenv(). Remember to set permissions (not readable!)
-  ob_start();
-  /** @psalm-suppress MissingFile */
-  include_once 'env.php';
-  $env_output = trim(ob_get_contents());
-  if ($env_output) {
-    bot_debug_log("got this:\n" . $env_output);  // Something unexpected, so log it
-  }
-  unset($env_output);
-  ob_end_clean();
+    // Set the environment variables with putenv(). Remember to set permissions (not readable!)
+    ob_start();
+    /** @psalm-suppress MissingFile */
+    include_once 'env.php';
+    $env_output = trim(ob_get_contents());
+    if ($env_output) {
+        bot_debug_log("got this:\n" . $env_output);  // Something unexpected, so log it
+    }
+    unset($env_output);
+    ob_end_clean();
 }
 
 if (!mb_internal_encoding('UTF-8') || !mb_regex_encoding('UTF-8')) { /** @phpstan-ignore-line */ /** We are very paranoid */
-  echo 'Unable to set encoding';
-  exit;
+    echo 'Unable to set encoding';
+    exit;
 }
 
 ini_set("memory_limit", "3648M"); // Use Megabytes to match memory usage check code
 ini_set("pcre.backtrack_limit", "1425000000");
 ini_set("pcre.recursion_limit", "425000000");
 if (isset($_REQUEST["pcre"]) || (strpos((string) @$_SERVER['PHP_SELF'], '/gadgetapi.php') !== false)) { // Willing to take slight performance penalty on Gadget
-  ini_set("pcre.jit", "0");
+    ini_set("pcre.jit", "0");
 }
 
-define("PHP_ADSABSAPIKEY", (string) getenv("PHP_ADSABSAPIKEY"));
+if (isset($_REQUEST['PHP_ADSABSAPIKEY'])) {
+    define('PHP_ADSABSAPIKEY', (string) $_REQUEST['PHP_ADSABSAPIKEY']);
+} else {
+    define('PHP_ADSABSAPIKEY', (string) getenv('PHP_ADSABSAPIKEY'));
+}
 
 $env_limit_action = trim((string) getenv("PHP_ADSABSAPILIMITACTION"));
 if ($env_limit_action !== '') {
-  define("PHP_ADSABSAPILIMITACTION", $env_limit_action);
+    define("PHP_ADSABSAPILIMITACTION", $env_limit_action);
 }
 unset($env_limit_action);
 
 if ((string) getenv("PHP_S2APIKEY") !== "") {
-  define("HEADER_S2", ["x-api-key: " . (string) getenv("PHP_S2APIKEY")]);
+    define("HEADER_S2", ["x-api-key: " . (string) getenv("PHP_S2APIKEY")]);
 } else {
-  define("HEADER_S2", []);
+    define("HEADER_S2", []);
 }
 
 // see https://www.ncbi.nlm.nih.gov/books/NBK25497/ for more information
@@ -123,19 +127,19 @@ $nlm_tool = "WikipediaCitationBot";
 $nlm_apikey = (string) getenv("NLM_APIKEY");
 $nlm_email = (string) getenv("NLM_EMAIL");
 if (!(strpos($nlm_email, '@') > 0)) {
-  $nlm_email = PUBMEDUSERNAME;
+    $nlm_email = PUBMEDUSERNAME;
 }
 if (strlen($nlm_apikey) < 8) {
-  $nlm_apikey = "";
+    $nlm_apikey = "";
 }
 define ("NLM_LOGIN", "tool=" . urlencode($nlm_tool) . "&email=" . urlencode($nlm_email) . (($nlm_apikey === "") ? "" : ("&api_key=" . urlencode($nlm_apikey))));
 unset($nlm_email, $nlm_apikey, $nlm_tool);
 
 function check_blocked(): void {
-  if (!TRAVIS && ! WikipediaBot::is_valid_user('Citation_bot')) {
-    echo '</pre><div style="text-align:center"><h1>The Citation Bot is currently blocked because of disagreement over its usage.</h1><br/><h2><a href="https://en.wikipedia.org/wiki/User_talk:Citation_bot" title="Join the discussion" target="_blank">Please join in the discussion</a></h2></div><footer><a href="./" title="Use Citation Bot again">Another&nbsp;page</a>?</footer></body></html>';
-    exit;
-  }
+    if (!TRAVIS && ! WikipediaBot::is_valid_user('Citation_bot')) {
+        echo '</pre><div style="text-align:center"><h1>The Citation Bot is currently blocked because of disagreement over its usage.</h1><br/><h2><a href="https://en.wikipedia.org/wiki/User_talk:Citation_bot" title="Join the discussion" target="_blank">Please join in the discussion</a></h2></div><footer><a href="./" title="Use Citation Bot again">Another&nbsp;page</a>?</footer></body></html>';
+        exit;
+    }
 }
 
 define("MAX_TRIES", 2);
@@ -151,13 +155,13 @@ require_once 'apiFunctions.php';
 require_once 'Page.php';
 
 if (isset($argv)) {
-  define("MAX_PAGES", 1000000);
+    define("MAX_PAGES", 1000000);
 } else {
-  define("MAX_PAGES", 3850);
+    define("MAX_PAGES", 3850);
 }
 
 if (!TRAVIS) { // This is explicity "tested" in test suite
-  Zotero::create_ch_zotero();
-  WikipediaBot::make_ch();
+    Zotero::create_ch_zotero();
+    WikipediaBot::make_ch();
 }
 
