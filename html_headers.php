507caaf9db8d0de15ab2c6dee4660236318d6d7f
GlazerMann <kurt@ioiowa.com>
2024-05-18 15:10:59 -0500

    clean up (#4620)

--- html_headers.php
+++ html_headers.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-@header('Cache-Control: no-store, private');
-@header('Content-type: text/html; charset=utf-8');
-@header('Expires: 0');
-@header('Pragma: no-cache');
-@header('X-Accel-Buffering: no');
-@header('X-Accel-Expires: 0');
+header('Cache-Control: no-store, private');
+header('Content-type: text/html; charset=utf-8');
+header('Expires: 0');
+header('Pragma: no-cache');
+header('X-Accel-Buffering: no');
+header('X-Accel-Expires: 0');
