68e3cc87d91be4c32cd61d3370cb34bd00167609
GlazerMann <kurt@ioiowa.com>
2024-05-29 19:17:48 -0500

    cookie jar (#4629)

--- Template.php
+++ Template.php
@@ -7145,10 +7145,7 @@ final class Template
        $this->set($param, $matches[1]);
       }
       if (preg_match("~^https?://proquest\.umi\.com/.*$~", $this->get($param), $matches)) {
-       $ch = bot_curl_init(1.5, [
-        CURLOPT_COOKIEFILE => 'cookie.txt', // Needed for proquest
-        CURLOPT_URL => $matches[0],
-       ]);
+       $ch = bot_curl_init(1.5, [CURLOPT_URL => $matches[0]]);
        if (bot_curl_exec($ch) !== "") {
         $redirectedUrl = (string) curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); // Final URL
         if (preg_match("~^https?://.+(\.proquest\.com/docview/\d{4,})(?:|/abstract.*|/fulltext.*|/preview.*)$~", $redirectedUrl, $matches) || preg_match("~^https?://.+(\.proquest\.com/openurl/handler/.+)$~", $redirectedUrl, $matches)) {
