9f135865f759302ae67bd75fde0c1d01bcbc49dc
GlazerMann <kurt@ioiowa.com>
2024-05-31 14:43:45 -0500

    Update constants.php

--- constants.php
+++ constants.php
@@ -54,7 +54,7 @@ function curl_limit_page_size(CurlHandle $_ch, int $_DE = 0, int $down = 0, int 
     }
     return 0;
 }
-/** @param array<int, int|string|bool> $ops */
+/** @param array<int, int|string|bool|array<int, string>> $ops */
 function bot_curl_init(float $time, array $ops): CurlHandle {
     $ch = curl_init();
     // 1 - Global Defaults
