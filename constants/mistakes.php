0504091e18c6659ceb58d3f288acfc8debf7e8bf
GlazerMann <kurt@ioiowa.com>
2024-06-12 07:18:52 -0500

    Update mistakes.php

--- constants/mistakes.php
+++ constants/mistakes.php
@@ -767,6 +767,11 @@ const COMMON_MISTAKES = [ // Common mistakes that aren't picked up by the levens
     'אתר' => 'website',
     'כותרת' => 'title',
     'תאריך' => 'date',
+    'accesssdate' => 'access-date',
+    'firts' => 'first',
+    'pub;isher' => 'publisher',
+    'transtitle' => 'trans-title',
+    'websote' => 'website',
 ]; // Lots of stuff that just existed
 
 const COMMON_MISTAKES_TOOL = [
