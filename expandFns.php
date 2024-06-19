b6a62985eed79eb8a056f373c35dac1cb93e6940
GlazerMann <kurt@ioiowa.com>
2024-06-19 08:45:05 -0500

    Update expandFns.php

--- expandFns.php
+++ expandFns.php
@@ -678,15 +678,10 @@ function str_equivalent(string $str1, string $str2): bool {
     if (str_i_same(str_remove_irrelevant_bits($str1), str_remove_irrelevant_bits($str2))) {
         return true;
     }
-    $simple1 = trim(str_replace(['    ', '   ', '  '], [' ', ' ', ' '], trim(str_replace(['-', '.', '   ', '  ', '[[', ']]'], [' ', ' ', ' ', ' ', ' ', ' '], strtolower($str1))));
-    $simple2 = trim(str_replace(['    ', '   ', '  '], [' ', ' ', ' '], trim(str_replace(['-', '.', '   ', '  ', '[[', ']]'], [' ', ' ', ' ', ' ', ' ', ' '], strtolower($str2))));
-    if (in_array($simple1, JOURNAL_IS_BOOK_SERIES, true) && in_array($simple2, JOURNAL_IS_BOOK_SERIES, true)) {
-        // Special case for book series
-        $simple1 = trim(str_replace(['(clifton, n j )', '(san diego, calif )', 'advances in ', 'experimental', 'pharmacology', 'methods in ', 'immunology', 'methods ', 'molecular', 'medicine', 'biology', ' of ', 'enzymology', 'biol', ' and '],
-                                    ['', '', 'adv ', 'exp', 'pharmacol', 'meth ', 'immunol', 'meth ', 'mol', 'med', 'bio', ' ', 'enzymol', 'bio', ' '], $simple1);
-        $simple2 = trim(str_replace(['(clifton, n j )', '(san diego, calif )', 'advances in ', 'experimental', 'pharmacology', 'methods in ', 'immunology', 'methods ', 'molecular', 'medicine', 'biology', ' of ', 'enzymology', 'biol', ' and '],
-                                    ['', '', 'adv ', 'exp', 'pharmacol', 'meth ', 'immunol', 'meth ', 'mol', 'med', 'bio', ' ', 'enzymol', 'bio', ' '], $simple2);
-        if ($simple1 === $simple2) {
+    if (string_is_book_series($str1) && string_is_book_series($str1)) { // Both series, but not the same
+        $str1 = trim(str_replace(COMPARE_SERIES_IN, COMPARE_SERIES_OUT, str2lower($str1)));
+        $str2 = trim(str_replace(COMPARE_SERIES_IN, COMPARE_SERIES_OUT, str2lower($str2)));
+        if ($str1 === $str2) {
             return true;
         }
     }
