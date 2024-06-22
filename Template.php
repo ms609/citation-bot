1b6d765d28555cd7d4ba2378f48059e615e35fe6
GlazerMann <kurt@ioiowa.com>
2024-06-22 13:15:15 -0500

    reuters

--- Template.php
+++ Template.php
@@ -6669,8 +6669,8 @@ final class Template
       return;
      }
      $title = $this->get($param);
-     if (preg_match('~^(.+) # # # CITATION_BOT_PLACEHOLDER_TEMPLATE \d+ # # # Reuters$~', $title, $matches)) {
-      if (stripos($this->get('agency') . $this->get('work') . $this->get('website') . $this->get('newspaper') . $this->get('website')  . $this->get('publiser'), 'Reuters') !== false) {
+     if (preg_match('~^(.+) # # # CITATION_BOT_PLACEHOLDER_TEMPLATE \d+ # # # Reuters(?:\.com)$~i', $title, $matches)) {
+      if (stripos($this->get('agency') . $this->get('work') . $this->get('website') . $this->get('newspaper') . $this->get('website') . $this->get('publiser'), 'reuters') !== false) {
        $title = $matches[1];
        $this->set('title', $title);
       }
