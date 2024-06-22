ff1f7efe7972c9a54703cbbe55ff71b2fa49d779
GlazerMann <kurt@ioiowa.com>
2024-06-21 19:35:45 -0500

    conference

--- Template.php
+++ Template.php
@@ -6008,8 +6008,8 @@ final class Template
       return;
      }
      $the_title = $this->get('title');
-     $the_param = str_ireplace(['Proc. ', 'Conf. '], ['Proceedings ', 'Conference '], $the_param);
-     $the_title = str_ireplace(['Proc. ', 'Conf. '], ['Proceedings ', 'Conference '], $the_title);
+     $the_param = str_ireplace(['Proc. of ', 'Conf. on', 'Proc. ', 'Conf. '], ['Proceedings of', 'Conference on', 'Proceedings of', 'Conference on'], $the_param);
+     $the_title = str_ireplace(['Proc. of ', 'Conf. on', 'Proc. ', 'Conf. '], ['Proceedings of', 'Conference on', 'Proceedings of', 'Conference on'], $the_title);
      foreach (CONFERENCE_LIST as $conf) {
       if (stripos($the_title, $conf) !== false && stripos($the_title, $the_param) !== false) {
        $this->forget($param);
