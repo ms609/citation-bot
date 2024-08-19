65b98acfc2d8319a5c5f3671017de35ef4e12059
GlazerMann <kurt@ioiowa.com>
2024-08-19 10:39:59 -0500

    Update parameters.php

--- constants/parameters.php
+++ constants/parameters.php
@@ -542,7 +542,7 @@ const DEAD_PARAMETERS = ['PPPrefix', 'PPrefix', 'Ref', 'albumlink', 'albumtype',
     'plainurl', 'sous-titre', 'arşivengelli',  'pdf', 'trans-first', 'trans-last', 'image',
     'alt isbn', 'died_date', 'issn2', 'subtitle', 'url texte', 'with', 'wo', 'father',
     'journal-link', 'align', 'certyear', 'relyear', 'chapter-author', 'restricted',
-    'total_pages', 'total-pages', 'broken-link', 'broken-url',
+    'total_pages', 'total-pages', 'broken-link', 'broken-url', 'lien périodique',
 ];
 
 // sid is actually page range in some no english wiki's but do not fix since so close to id=
