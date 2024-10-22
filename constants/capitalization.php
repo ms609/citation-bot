fd06f2f2fd2421787cced15963b0b96f322cbbc0
GlazerMann <kurt@ioiowa.com>
2024-10-22 18:36:33 -0500

    Update capitalization.php

--- constants/capitalization.php
+++ constants/capitalization.php
@@ -498,6 +498,7 @@ const UC_SMALL_WORDS = [ /* The following will be automatically updated to alpha
 // For ones that start with lower-case, include both ELife and Elife versions in misspelled array
 
 const JOURNAL_ACRONYMS = [ /* The following will be automatically updated to alphabetical order */
+    ' Feminismo/s ',
     ' (and the Middle East) ',
     ' (BBA) ',
     ' (online ed.) ',
@@ -1023,6 +1024,7 @@ const JOURNAL_ACRONYMS = [ /* The following will be automatically updated to alp
     /* The above will be automatically updated to alphabetical order */
 ];
 const UCFIRST_JOURNAL_ACRONYMS = [ /* The following will be automatically updated to alphabetical order */
+    ' Feminismo/S ',
     ' (And the Middle East) ',
     ' (Bba) ',
     ' (online Ed.) ',
