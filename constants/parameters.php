ead47e9b2bdb3c28d8556a5a4303843ff37aa14a
GlazerMann <kurt@ioiowa.com>
2024-11-01 08:14:14 -0500

    More bad things that we cannot fix - so ignore

--- constants/parameters.php
+++ constants/parameters.php
@@ -546,7 +546,8 @@ const DEAD_PARAMETERS = ['PPPrefix', 'PPrefix', 'Ref', 'albumlink', 'albumtype',
     'authors', 'plume', 'loc', 'allpages', 'archive-access', 'original', 'laysummary',
     'archiveservice', 'reviewer', 'hrsg', 'address', 'archived', 'history access',
     'lt', 'aircraft_type', 'catalog', 'release', 'illustratore', 'libris', 'tirage',
-    'comment', 'see', 'cnbc', 'web page', 'nolink', 'world', 'tom', 'names',
+    'comment', 'see', 'cnbc', 'web page', 'nolink', 'world', 'tom', 'names', 'additional_title',
+    'crid', 'column',
 ];
 
 // sid is actually page range in some no english wiki's but do not fix since so close to id=
