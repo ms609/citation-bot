a9d00ccf2b8ae96c6be429270f19ec6293adb164
GlazerMann <kurt@ioiowa.com>
2024-11-01 14:32:02 -0500

    Update zoteroTest.php

--- tests/phpunit/zoteroTest.php
+++ tests/phpunit/zoteroTest.php
@@ -2,19 +2,21 @@
 declare(strict_types=1);
 
 /*
-    * Tests for Zotero.php - some of these work even when zotero fails because they check for the absence of bad data
-    */
+ * Tests for Zotero.php - some of these work even when zotero fails because they check for the absence of bad data
+ */
 
 require_once __DIR__ . '/../testBaseClass.php';
+
+/**
+ * @long
+ */
+
 final class zoteroTest extends testBaseClass {
 
     protected function setUp(): void {
         if (BAD_PAGE_API !== '') {
             $this->markTestSkipped();
         }
-        $this->getTestResultObject()->setTimeoutForSmallTests(60);
-        $this->getTestResultObject()->setTimeoutForMediumTests(120);
-        $this->getTestResultObject()->setTimeoutForLargeTests(180);
     }
 
     public function testFillCache(): void {
