623872b1abdc4078ba598679994b2f27ac26a366
GlazerMann <kurt@ioiowa.com>
2024-11-04 11:24:09 -0600

    Update capitalization.php

--- constants/capitalization.php
+++ constants/capitalization.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 if (get_included_files()[0] === __FILE__) {
-	exit;
+    exit;
 }
 
 const LC_SMALL_WORDS = [ /* The following will be automatically updated to alphabetical order */
