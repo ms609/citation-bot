c227f3b39e5dc9b5bc6011e4866e99db172a7681
GlazerMann <kurt@ioiowa.com>
2024-11-04 11:16:28 -0600

    Update ZZZLastTests.php

--- tests/phpunit/ZZZLastTests.php
+++ tests/phpunit/ZZZLastTests.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 /*
  * Final Test
  */
+
+if (get_included_files()[0] === __FILE__) {
+    exit;
+}
+
 require_once __DIR__ . '/../testBaseClass.php';
 
 final class ZZZLastTests extends testBaseClass {
