97bebc9b37ed7cca1d1dc24ce8693ec2a808f24c
GlazerMann <kurt@ioiowa.com>
2024-11-04 11:16:18 -0600

    Update WikipediaBotTest.php

--- tests/phpunit/WikipediaBotTest.php
+++ tests/phpunit/WikipediaBotTest.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
  * Tests for WikipediaBot.php
  */
 
+if (get_included_files()[0] === __FILE__) {
+    exit;
+}
+
 require_once __DIR__ . '/../testBaseClass.php';
 
     final class WikipediaBotTest extends testBaseClass {
