375f05cc9bf47bbdbf5d1178e835850b2db03aa7
GlazerMann <kurt@ioiowa.com>
2024-11-08 07:00:46 -0600

    Update parameters.php

--- constants/parameters.php
+++ constants/parameters.php
@@ -551,7 +551,7 @@ const DEAD_PARAMETERS = ['PPPrefix', 'PPrefix', 'Ref', 'albumlink', 'albumtype',
     'archiveservice', 'reviewer', 'hrsg', 'address', 'archived', 'history access',
     'lt', 'aircraft_type', 'catalog', 'release', 'illustratore', 'libris', 'tirage',
     'comment', 'see', 'cnbc', 'web page', 'nolink', 'world', 'tom', 'names', 'additional_title',
-    'crid', 'column', 'réimpression',
+    'crid', 'column', 'réimpression', 'orig-title',
 ];
 
 // sid is actually page range in some no english wiki's but do not fix since so close to id=
