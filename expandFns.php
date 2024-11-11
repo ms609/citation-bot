6faf478514d8a4cce28b469b81aedb92cc3c5db8
GlazerMann <kurt@ioiowa.com>
2024-11-11 11:25:27 -0600

    Update expandFns.php

--- expandFns.php
+++ expandFns.php
@@ -2,10 +2,6 @@
 
 declare(strict_types=1);
 
-if (get_included_files()[0] === __FILE__) {
-    exit;
-}
-
 require_once 'constants.php';           // @codeCoverageIgnore
 require_once 'Template.php';            // @codeCoverageIgnore
 require_once 'big_jobs.php';            // @codeCoverageIgnore
