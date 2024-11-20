3550aa5572a1f708849f010295da7eba6c83ea36
GlazerMann <kurt@ioiowa.com>
2024-11-20 09:03:32 -0600

    More tests

--- tests/phpunit/expandFnsTest.php
+++ tests/phpunit/expandFnsTest.php
@@ -533,6 +533,20 @@ final class expandFnsTest extends testBaseClass {
                 $changes = $changes . "In Both: " . $doi . "                ";
             }
         }
+        foreach (NULL_DOI_LIST as $doi => $value) {
+            foreach (NULL_DOI_STARTS_BAD as $bad_start) {
+                if (stripos($doi, $bad_start) === 0) {
+                    $changes = $changes . "In bad and bad start: " . $doi . "                ";
+                }
+            }
+        }
+        foreach (NULL_DOI_BUT_GOOD as $doi => $value) {
+            foreach (NULL_DOI_STARTS_BAD as $bad_start) {
+                if (stripos($doi, $bad_start) === 0) {
+                    $changes = $changes . "In good and bad start: " . $doi . "                ";
+                }
+            }
+        }
         foreach (NULL_DOI_ANNOYING as $doi => $value) {
             if (!isset(NULL_DOI_LIST[$doi])) {
                 $changes = $changes . "Not in main null list: " . $doi . "           ";
