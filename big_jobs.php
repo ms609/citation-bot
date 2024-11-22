e9ad26af2ad59d977790e0225b67c7745d6efdd8
GlazerMann <kurt@ioiowa.com>
2024-11-22 07:58:11 -0600

    smalled "big"

--- big_jobs.php
+++ big_jobs.php
@@ -36,7 +36,7 @@ function big_jobs_we_died($lock_file): void {
 
 function big_jobs_check_overused(int $page_count): void {
     static $lock_file; // Force file handle to stay open
-    if (!HTML_OUTPUT || $page_count < 50) {
+    if (!HTML_OUTPUT || $page_count < 5) {
         return;
     }
     $fn = big_jobs_name();
