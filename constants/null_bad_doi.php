59e6105420dc485d638d85d0fadae247b0918c22
GlazerMann <kurt@ioiowa.com>
2024-12-06 15:24:53 -0600

    Update null_bad_doi.php

--- constants/null_bad_doi.php
+++ constants/null_bad_doi.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 // TODO - manually check these from time to time - Last Check 7 NOV 2024
 const NULL_DOI_ANNOYING = [
+    '10.1558/cam.v8i1.67' => true, // 404
     '10.1001/archderm.133.1.49' => true, // 404
     '10.1001/archderm.134.11.1484' => true, // 404
     '10.1001/archderm.136.10.1272-a' => true, // 404
@@ -688,7 +689,7 @@ const NULL_DOI_STARTS_BAD = [ // These need one test in expandFnsTest.php "Deal 
 const NULL_DOI_LIST = [
     '10.1017/s0021911800112653' => true,
     '10.1186/s12863-015-0203-2' => true,
-    '10.1558/cam.v8i1.67' => true,
+    '10.1558/cam.v8i1.67' => true, // 404
     '10.33182/ml.v12i3.283' => true,
     '10.1001/archderm.133.1.49' => true, // 404
     '10.1001/archderm.134.11.1484' => true, // 404
