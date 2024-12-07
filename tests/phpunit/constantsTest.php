0b0f5118f6a6930443f8c7dda522fb378dda8acb
GlazerMann <kurt@ioiowa.com>
2024-12-07 11:39:00 -0600

    fix test correctly this time

--- tests/phpunit/constantsTest.php
+++ tests/phpunit/constantsTest.php
@@ -567,7 +567,7 @@ final class constantsTest extends testBaseClass {
             $tem = str_replace(' ', '_', $tem);
             if (WikipediaBot::is_redirect($tem) !== 0 &&
                 $tem !== 'Template:Cite_paper' && $tem !== 'Template:cite_paper' && // We use code to clean up cite paper
-                $tem !== 'Template:Lccn' && $tem !== 'Template:lccn') { // We remove one layer of re-direct
+                $tem !== 'Template:LCCN') { // We remove one layer of re-direct, but not both
                 $errors = $errors . '   Is now a redirect:' . $convert[1];
             }
         }
