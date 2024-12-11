bcfe973394fc223cd8c518c6f1118f1c135b25f6
GlazerMann <kurt@ioiowa.com>
2024-12-11 16:25:27 -0600

    in_array MUST be strict

--- process_page.php
+++ process_page.php
@@ -16,7 +16,7 @@ require_once 'setup.php';
 
 if (isset($argv[1])) {
     $pages = $argv[1];
-    if (in_array($pages, ['page_list.txt', 'page_list2.txt'])) {
+    if (in_array($pages, ['page_list.txt', 'page_list2.txt'], true)) {
         $pages = trim((string) file_get_contents($pages));
     }
 } elseif (isset($_GET["page"])) {
