69e7d0d5d2381aac5155277556eeed3b715d3491
GlazerMann <kurt@ioiowa.com>
2024-12-11 17:31:43 -0600

    Update Template.php

--- Template.php
+++ Template.php
@@ -15,9 +15,6 @@ require_once 'constants.php';
 require_once 'NameTools.php';
 // @codeCoverageIgnoreEnd
 
-
-
-
 const REJECT_NEW = ['null', 'n/a', 'undefined', '0 0', '(:none)', '-'];
 const GOOFY_TITLES = ['Archived copy', "{title}", 'ScienceDirect', 'Google Books', 'None', 'usurped title'];
 const BAD_NEW_PAGES = ['0', '0-0', '0–0'];
@@ -25,114 +22,27 @@ const BAD_ISBN = ['9780918678072', '978-0-918678-07-2', '0918678072', '0-918678-
 const SHORT_STRING = ['the', 'and', 'a', 'for', 'in', 'on', 's', 're', 't', 'an', 'as', 'at', 'and', 'but', 'how', 'why', 'by', 'when', 'with', 'who', 'where', ''];
 const RIS_IS_BOOK = ['CHAP', 'BOOK', 'EBOOK', 'ECHAP', 'EDBOOK', 'DICT', 'ENCYC', 'GOVDOC'];
 const RIS_IS_FULL_BOOK = ['BOOK', 'EBOOK', 'EDBOOK'];
-const 
-
-GOOD_FREE
-['publisher', 'projectmuse', 'have free']
-
-BAD_OA_URL
-['10.4135/9781529742343', '10.1017/9781108859745']
-
-ENGLISH_WIKI
-['en', 'simple', 'mdwiki']
-
-REMOVE_SEMI
-['date', 'year', 'location', 'publisher', 'issue', 'number', 'page', 'pages', 'pp', 'p', 'volume']
-
-REMOVE_PERIOD
-['date', 'year', 'issue', 'number', 'page', 'pages', 'pp', 'p', 'volume']
-
-LINK_LIST
-['authorlink', 'chapterlink', 'contributorlink', 'editorlink', 'episodelink', 'interviewerlink', 'inventorlink', 'serieslink', 'subjectlink', 'titlelink', 'translatorlink']
-
-BAD_AGENT
-['United States Food and Drug Administration', 'Surgeon General of the United States', 'California Department of Public Health']
-
-BAD_AGENT_PUBS
-['United States Department of Health and Human Services', 'California Tobacco Control Program', ''
-
-NO_LANGS
-['n', 'no', 'live', 'alive', 'কার্যকর', 'hayır', 'não', 'nao', 'false']
-
-YES_LANGS
-['y', 'yes', 'dead', 'si', 'sì', 'ja', 'evet', 'ei tööta', 'sim', 'ano', 'true']
-
-PDF_LINKS
-['pdf', 'portable document format', '[[portable document format|pdf]]', '[[portable document format]]', '[[pdf]]']
-
-DEPARMENTS
-        [
-         'local',
-         'editorial',
-         'international',
-         'national',
-         'communication',
-         'letter to the editor',
-         'review',
-         'coronavirus',
-         'race & reckoning',
-         'politics',
-         'opinion',
-         'opinions',
-         'investigations',
-         'tech',
-         'technology',
-         'world',
-         'sports',
-         'world',
-         'arts & entertainment',
-         'arts',
-         'entertainment',
-         'u.s.',
-         'n.y.',
-         'business',
-         'science',
-         'health',
-         'books',
-         'style',
-         'food',
-         'travel',
-         'real estate',
-         'magazine',
-         'economy',
-         'markets',
-         'life & arts',
-         'uk news',
-         'world news',
-         'health news',
-         'lifestyle',
-         'photos',
-         'education',
-         'arts',
-         'life',
-         'puzzles',
-        ]
-        
-        
-BAD_VIA
-         [
-          '',
-          'project muse',
-          'wiley',
-          'springer',
-          'questia',
-          'elsevier',
-          'wiley online library',
-          'wiley interscience',
-          'interscience',
-          'sciencedirect',
-          'science direct',
-          'ebscohost',
-          'proquest',
-          'google scholar',
-          'google',
-          'bing',
-          'yahoo',
-         ]        
-        
-        
-        VOL_NUM
-        ['volume', 'issue', 'number']
+const GOOD_FREE = ['publisher', 'projectmuse', 'have free'];
+const BAD_OA_URL = ['10.4135/9781529742343', '10.1017/9781108859745'];
+const ENGLISH_WIKI = ['en', 'simple', 'mdwiki'];
+const REMOVE_SEMI = ['date', 'year', 'location', 'publisher', 'issue', 'number', 'page', 'pages', 'pp', 'p', 'volume'];
+const REMOVE_PERIOD = ['date', 'year', 'issue', 'number', 'page', 'pages', 'pp', 'p', 'volume'];
+const LINK_LIST = ['authorlink', 'chapterlink', 'contributorlink', 'editorlink', 'episodelink', 'interviewerlink', 'inventorlink', 'serieslink', 'subjectlink', 'titlelink', 'translatorlink'];
+const BAD_AGENT = ['United States Food and Drug Administration', 'Surgeon General of the United States', 'California Department of Public Health'];
+const BAD_AGENT_PUBS = ['United States Department of Health and Human Services', 'California Tobacco Control Program', ''];
+const NO_LANGS = ['n', 'no', 'live', 'alive', 'কার্যকর', 'hayır', 'não', 'nao', 'false'];
+const YES_LANGS = ['y', 'yes', 'dead', 'si', 'sì', 'ja', 'evet', 'ei tööta', 'sim', 'ano', 'true'];
+const PDF_LINKS = ['pdf', 'portable document format', '[[portable document format|pdf]]', '[[portable document format]]', '[[pdf]]'];
+const DEPARMENTS = ['local', 'editorial', 'international', 'national', 'communication', 'letter to the editor',
+        'review', 'coronavirus', 'race & reckoning', 'politics', 'opinion', 'opinions', 'investigations', 'tech',
+        'technology', 'world', 'sports', 'world', 'arts & entertainment', 'arts', 'entertainment', 'u.s.', 'n.y.',
+        'business', 'science', 'health', 'books', 'style', 'food', 'travel', 'real estate', 'magazine', 'economy',
+        'markets', 'life & arts', 'uk news', 'world news', 'health news', 'lifestyle', 'photos', 'education',
+        'arts', 'life', 'puzzles'];
+const BAD_VIA = [ '', 'project muse', 'wiley', 'springer', 'questia', 'elsevier', 'wiley online library',
+        'wiley interscience', 'interscience', 'sciencedirect', 'science direct', 'ebscohost', 'proquest',
+        'google scholar', 'google', 'bing', 'yahoo'] 
+const VOL_NUM = ['volume', 'issue', 'number'];
 
 final class Template
 {
