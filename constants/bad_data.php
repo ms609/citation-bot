0bbbbace5f015c0a65514fcdd8c3b723016dc2fe
GlazerMann <kurt@ioiowa.com>
2024-12-13 12:26:01 -0600

    even more twitter

--- constants/bad_data.php
+++ constants/bad_data.php
@@ -11279,6 +11279,7 @@ const NON_JOURNAL_WEBSITES = [
     'twin2.co.jp/',
     'twitch.tv/',
     'twitter.com/',
+    '/x.com',
     'twreporter.org/',
     'twtd.co.uk/',
     'txexecutions.org/',
