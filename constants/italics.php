d86490cdcfdc4d8ce0615092f5de8b3a6e42c1b2
GlazerMann <kurt@ioiowa.com>
2024-12-16 08:50:51 -0600

    Update italics.php

--- constants/italics.php
+++ constants/italics.php
@@ -538,7 +538,7 @@ const CAMEL_CASE = [
     'poreCov', 'PyArmadillo', 'QuasiFlow', 'RustBelt', 'AutoDock', 'TractoFlow',
     'libRoadRunner', 'pyJac', 'myGrid', 'Cp2TiCl', 'myExperiment',
     'CicArVarDB', 'InDel database', 'BioMoby', 'maxdBrowse', 'GazePlotter',
-    'ScanGraph',
+    'ScanGraph', 'MusicBrainz', 'WontBe', 'InChIs', 'PolyChord',
 ];
 
 const ITALICS_HARDCODE_IN  = [
