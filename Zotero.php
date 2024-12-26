9eec972380442964c9fdf78b54247b84f1090e9a
GlazerMann <kurt@ioiowa.com>
2024-12-26 10:36:14 -0600

    Be less trusting of Zotero and log stuff

--- Zotero.php
+++ Zotero.php
@@ -945,7 +945,12 @@ final class Zotero {
                     (stripos((string) $result->publicationTitle, ' edition') === false)) { // Do not add if "journal" includes "edition"
                     if (str_replace(NON_JOURNALS, '', (string) $result->publicationTitle) === (string) $result->publicationTitle) {
                         if (str_ireplace(NON_JOURNAL_WEBSITES, '', $url) === $url || $template->wikiname() === 'cite journal') {
-                            $template->add_if_new('journal', (string) $result->publicationTitle);
+                            if (str_ireplace(CANONICAL_PUBLISHER_URLS, '', $url) === $url) {
+                                bot_debug_log('Possible journal URL: ' . $url);
+                                $template->add_if_new('work', (string) $result->publicationTitle);
+                            } else {
+                                $template->add_if_new('journal', (string) $result->publicationTitle);
+                            }
                         } else {
                             $template->add_if_new('work', (string) $result->publicationTitle);
                         }
