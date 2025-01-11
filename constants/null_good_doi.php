0e5309842570a8f4087deb2ae7720f6b98f05952
GlazerMann <kurt@ioiowa.com>
2025-01-11 13:20:59 -0600

    Update null_good_doi.php

--- constants/null_good_doi.php
+++ constants/null_good_doi.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 // TODO - these need to be manually double checked once in a great while
 //  We cannot add hdls to this list, since we need the final URL
 const NULL_DOI_BUT_GOOD = [
+    '10.1090/s0002-9904-1939-06980-2' => true,
     '10.1000/100' => true,
     '10.1001/archfami.9.10.1195' => true,
     '10.1001/archgenpsychiatry.2009.141' => true,
