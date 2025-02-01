bce0cf08aa06bc77d2a2c408ccd2b7586e93cde8
GlazerMann <kurt@ioiowa.com>
2025-01-31 20:42:25 -0600

    No need to cache this

--- expandFns.php
+++ expandFns.php
@@ -80,6 +80,9 @@ function doi_works(string $doi): ?bool {
     if (TRUST_DOI_GOOD && isset(NULL_DOI_BUT_GOOD[$doi])) {
         return true;
     }
+    if (isset(NULL_DOI_ANNOYING[$doi])) {
+        return false;
+    }
     if (strlen($doi) > HandleCache::MAX_HDL_SIZE) {
         return null;   // @codeCoverageIgnore
     }
