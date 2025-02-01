28fa80bc07fe6d1ac345104b2880d2fb985823a5
GlazerMann <kurt@ioiowa.com>
2025-02-01 09:50:10 -0600

    Deal with gone dois

--- expandFns.php
+++ expandFns.php
@@ -83,6 +83,13 @@ function doi_works(string $doi): ?bool {
     if (isset(NULL_DOI_ANNOYING[$doi])) {
         return false;
     }
+    if (!TRAVIS) {
+        foreach (NULL_DOI_STARTS_BAD as $bad_start) {
+            if (stripos($doi, $bad_start) === 0) {
+                return false; // all gone
+            }
+        }
+    }
     if (strlen($doi) > HandleCache::MAX_HDL_SIZE) {
         return null;   // @codeCoverageIgnore
     }
