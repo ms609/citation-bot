62297d18f108d314689d8556219b70a6053613af
GlazerMann <kurt@ioiowa.com>
2025-02-10 10:40:08 -0600

    over-truncated

--- expandFns.php
+++ expandFns.php
@@ -2321,6 +2321,7 @@ function doi_is_bad (string $doi): bool {
         $doi === '10.7722/j' ||         // over-truncated
         $doi === '10.1002/bies' ||      // over-truncated
         $doi === '10.1002/job' ||       // over-truncated
+        $doi === '10.5465/ame' ||       // over-truncated
         strpos($doi, '10.5779/hypothesis') === 0 || // SPAM took over
         strpos($doi, '10.5555/') === 0 || // Test DOI prefix
         strpos($doi, '10.5860/choice.') === 0 || // Paywalled book review
