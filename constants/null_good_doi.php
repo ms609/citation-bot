ec2e5fb810901b46f61df147c445ed2bdf4c5b2c
GlazerMann <kurt@ioiowa.com>
2025-04-08 20:20:29 -0500

    Update null_good_doi.php

--- constants/null_good_doi.php
+++ constants/null_good_doi.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 // TODO - these need to be manually double checked once in a great while
 //  We cannot add hdls to this list, since we need the final URL
 const NULL_DOI_BUT_GOOD = [
+    '10.21272/mmi.2020.4-07' => true,
     '10.1000/100' => true,
     '10.1001/archderm.138.12.1584' => true,
     '10.1001/archderm.140.7.819' => true,
