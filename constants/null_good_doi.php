7430eff29b9b6a4a6f88f1e22650b2f9bc4d1776
GlazerMann <kurt@ioiowa.com>
2025-04-13 16:32:12 -0500

    Update null_good_doi.php

--- constants/null_good_doi.php
+++ constants/null_good_doi.php
@@ -7,6 +7,8 @@ declare(strict_types=1);
 // TODO - these need to be manually double checked once in a great while
 //  We cannot add hdls to this list, since we need the final URL
 const NULL_DOI_BUT_GOOD = [
+    '10.7751/telopea20055705' => true,
+    '10.7751/telopea20065733' => true,
     '10.1000/100' => true,
     '10.1001/archderm.138.12.1584' => true,
     '10.1001/archderm.140.7.819' => true,
