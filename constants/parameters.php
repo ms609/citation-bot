6ec585d423e238e489247c924e2c50c95c5d49e9
GlazerMann <kurt@ioiowa.com>
2025-04-30 15:41:27 -0500

    Update parameters.php

--- constants/parameters.php
+++ constants/parameters.php
@@ -553,6 +553,7 @@ const DEAD_PARAMETERS = ['PPPrefix', 'PPrefix', 'Ref', 'albumlink', 'albumtype',
     'star', 'band', 'title-volume', 'death_date', 'dodeurl', 'license', 'username', 'bsite',
     'cite', 'flag', 'access-website', 'resumo-url', 'url-resumo', 'subtitulo', 'total-páginas',
     'firtitle', 'plaintitle', 'unused_data', 'cite web', 'cite-web', 'compiler', 'circulation',
+    'refname', 'parliament', 'en ligne le',
 ];
 
 // sid is actually page range in some no english wiki's but do not fix since so close to id=
