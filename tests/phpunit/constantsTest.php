f311b01a226c2e5936151adad7910e860fb9c897
GlazerMann <kurt@ioiowa.com>
2025-07-06 14:45:59 -0500

    gone

--- tests/phpunit/constantsTest.php
+++ tests/phpunit/constantsTest.php
@@ -125,7 +125,7 @@ final class constantsTest extends testBaseClass {
                                  PUBLISHER_ENDINGS, BAD_TITLES, IN_PRESS_ALIASES, NON_PUBLISHERS,
                                  JOURNAL_IS_BOOK_SERIES, WORKS_ARE_PUBLISHERS, PREFER_VOLUMES,
                                  PREFER_ISSUES, PARAMETER_LIST, LOTS_OF_EDITORS,
-                                 TEMPLATES_WE_HARV, FLATTENED_AUTHOR_PARAMETERS, TEMPLATES_VCITE,
+                                 TEMPLATES_WE_HARV, FLATTENED_AUTHOR_PARAMETERS,
                                  TEMPLATES_WE_CHAPTER_URL, TEMPLATES_WE_RENAME, TEMPLATES_WE_BARELY_PROCESS,
                                  TEMPLATES_WE_SLIGHTLY_PROCESS, TEMPLATES_WE_PROCESS);
         foreach ($big_array as $actual) {
