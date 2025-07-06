8f44b352f2f6805e48848936fbd32853ae3ca6bb
GlazerMann <kurt@ioiowa.com>
2025-07-06 14:57:57 -0500

    Update PageTest.php

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../testBaseClass.php';
 
-final class PageTest extends tecite stBaseClass {
+final class PageTest extends testBaseClass {
 
     protected function setUp(): void {
         if (BAD_PAGE_API !== '') {
