e6122f49ee415e4a247bc6f794164ba6660e150b
GlazerMann <kurt@ioiowa.com>
2025-11-30 16:52:18 -0600

    Deal with in test suite

--- tests/testBaseClass.php
+++ tests/testBaseClass.php
@@ -51,8 +51,7 @@ abstract class testBaseClass extends PHPUnit\Framework\TestCase {
              $this->testing_skip_bibcode = false;
         }
         if (!getenv('PHP_OAUTH_CONSUMER_TOKEN') || !getenv('PHP_OAUTH_CONSUMER_SECRET') ||
-                !getenv('PHP_OAUTH_ACCESS_TOKEN') || !getenv('PHP_OAUTH_ACCESS_SECRET') || 
-                !WikipediaBot::is_valid_user('Citation_bot')) {
+             !getenv('PHP_OAUTH_ACCESS_TOKEN') || !getenv('PHP_OAUTH_ACCESS_SECRET')) {
              $this->testing_skip_wiki = true;
         } else {
              $this->testing_skip_wiki = false;
