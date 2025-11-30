d12aab7716ef15b009afad8c98472fbd6fba827c
GlazerMann <kurt@ioiowa.com>
2025-11-30 14:25:26 -0600

    Update testBaseClass.php

--- tests/testBaseClass.php
+++ tests/testBaseClass.php
@@ -46,7 +46,7 @@ abstract class testBaseClass extends PHPUnit\Framework\TestCase {
         }
         if (!getenv('PHP_OAUTH_CONSUMER_TOKEN') || !getenv('PHP_OAUTH_CONSUMER_SECRET') ||
                 !getenv('PHP_OAUTH_ACCESS_TOKEN') || !getenv('PHP_OAUTH_ACCESS_SECRET') ||
-                !WikipediaBot::is_valid_user('Citation_bot'))) {
+                !WikipediaBot::is_valid_user('Citation_bot')) {
              $this->testing_skip_wiki = true;
         } else {
              $this->testing_skip_wiki = false;
