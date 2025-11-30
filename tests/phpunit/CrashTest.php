226a64e6ffdf99ecfd889c3d3036b018ec08e32b
GlazerMann <kurt@ioiowa.com>
2025-11-30 14:52:06 -0600

    Update CrashTest.php

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -9,23 +9,23 @@ require_once __DIR__ . '/../testBaseClass.php';
 
 final class CrashTest extends testBaseClass {
 
+    protected function setUp(): void {
+        if (BAD_PAGE_API === '') {
+            $this->markTestSkipped();
+        }
+        parent::setUp();
+    }
+
     public function testBadPage2(): void {  // Use this when debugging pages that crash the bot
         $page = new TestPage();
         $bad_page = BAD_PAGE_API;
         $bad_page = str_replace(' ', '_', $bad_page);
-        if ($bad_page !== "") {
-            Zotero::create_ch_zotero();
-            WikipediaBot::make_ch();
-            $page->get_text_from($bad_page);
-            AdsAbsControl::big_back_on();
-            AdsAbsControl::small_back_on();
-            Zotero::unblock_zotero();
-            $page->expand_text();
-            AdsAbsControl::small_give_up();
-            AdsAbsControl::big_give_up();
-            Zotero::block_zotero();
-            $this->assertTrue(false); // help prevent us from git committing with a website included
-        }
-        $this->assertTrue(true);
+        $page->get_text_from($bad_page);
+        // Turn everything on
+        AdsAbsControl::big_back_on();
+        AdsAbsControl::small_back_on();
+        Zotero::unblock_zotero();
+        $page->expand_text();
+        $this->assertTrue(false); // help prevent us from git committing with a website included
     }
 }
