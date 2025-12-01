72cdd1f229a3d632cfe211666352a45bb9c75c5c
GlazerMann <kurt@ioiowa.com>
2025-11-30 20:41:05 -0600

    avoid "skip"

--- tests/phpunit/CrashTest.php
+++ tests/phpunit/CrashTest.php
@@ -9,15 +9,12 @@ require_once __DIR__ . '/../testBaseClass.php';
 
 final class CrashTest extends testBaseClass {
 
-    protected function setUp(): void {
-        if (BAD_PAGE_API === '') {
-            $this->markTestSkipped();
-        }
-        parent::setUp();
-    }
-
     public function testBadPage(): void {  // Use this when using PHPUnit to debug pages that crash the bot - There are better ways to debug it, but this allows debugging using GitHub's actions
         $page = new TestPage();
+        if (BAD_PAGE_API === '') {
+            $this->assertTrue(true);
+            return;
+        }
         $bad_page = BAD_PAGE_API;
         $bad_page = str_replace(' ', '_', $bad_page);
         $page->get_text_from($bad_page);
