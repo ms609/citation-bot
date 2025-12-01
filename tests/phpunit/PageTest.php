97db68d36f73a2fe0ef65259fe31fd7040cb647d
GlazerMann <kurt@ioiowa.com>
2025-12-01 12:58:47 -0600

    This fails without secrets

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -215,6 +215,10 @@ final class PageTest extends testBaseClass {
         $origText = $page->parsed_text();
         $trialCitation = '{{Cite journal | title Bot Testing | doi_broken_date=1986-01-01 | doi = 10.1038/nature09068}}';
         $page->overwrite_text($trialCitation);
+        $page_result = false;
+        $this->requires_secrets(function(): void {
+            $page_result = $page->write($api, "Testing bot write function");
+        });
         $page_result = $page->write($api, "Testing bot write function");
         if (TRAVIS && !$page_result) {
             // ! API call failed: '''Your IP address is in a range which has been blocked on all wikis.''' The block was made by [//meta.wikimedia.org/wiki/User:Jon_Kolbert Jon Kolbert] (meta.wikimedia.org). The reason given is ''[[m:NOP|Open Proxy]]: Colocation webhost - Contact [[m:Special:Contact/stewards|stewards]] if you are affected ''. * Start of block: 02:23, 27 October 2019 * Expiration of block: 02:23, 27 October 2021
