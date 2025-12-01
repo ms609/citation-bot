175b6304574ed715e07906ba7d593e6e33bdf85b
GlazerMann <kurt@ioiowa.com>
2025-12-01 14:37:57 -0600

    testBotExpandWrite - only with secret

--- tests/phpunit/PageTest.php
+++ tests/phpunit/PageTest.php
@@ -208,38 +208,36 @@ final class PageTest extends testBaseClass {
     }
 
     public function testBotExpandWrite(): void {
-        $api = new WikipediaBot();
-        $page = new TestPage();
-        $writeTestPage = 'User:Blocked Testing Account/writetest';
-        $page->get_text_from($writeTestPage);
-        $origText = $page->parsed_text();
-        $trialCitation = '{{Cite journal | title Bot Testing | doi_broken_date=1986-01-01 | doi = 10.1038/nature09068}}';
-        $page->overwrite_text($trialCitation);
-        $page_result = false;
-        $this->requires_secrets(function($api): void {
-            $page_result = $page->write($api, "Testing bot write function");
-        });
-        if (TRAVIS && !$page_result) {
-            // ! API call failed: '''Your IP address is in a range which has been blocked on all wikis.''' The block was made by [//meta.wikimedia.org/wiki/User:Jon_Kolbert Jon Kolbert] (meta.wikimedia.org). The reason given is ''[[m:NOP|Open Proxy]]: Colocation webhost - Contact [[m:Special:Contact/stewards|stewards]] if you are affected ''. * Start of block: 02:23, 27 October 2019 * Expiration of block: 02:23, 27 October 2021
-            $page->get_text_from($writeTestPage);
-            $this->assertSame($origText, $page->parsed_text());
-        } else {
-            // Double check we can read it back
-            $page->get_text_from($writeTestPage);
-            $this->assertSame($trialCitation, $page->parsed_text());
-        }
         $this->requires_secrets(function(): void {
+            $api = new WikipediaBot();
+            $page = new TestPage();
+            $writeTestPage = 'User:Blocked Testing Account/writetest';
+            $page->get_text_from($writeTestPage);
+            $origText = $page->parsed_text();
+            $trialCitation = '{{Cite journal | title Bot Testing | doi_broken_date=1986-01-01 | doi = 10.1038/nature09068}}';
+            $page->overwrite_text($trialCitation);
+            $page_result = false;
+            $page_result = $page->write($api, "Testing bot write function");
+            if (TRAVIS && !$page_result) {
+                // ! API call failed: '''Your IP address is in a range which has been blocked on all wikis.''' The block was made by [//meta.wikimedia.org/wiki/User:Jon_Kolbert Jon Kolbert] (meta.wikimedia.org). The reason given is ''[[m:NOP|Open Proxy]]: Colocation webhost - Contact [[m:Special:Contact/stewards|stewards]] if you are affected ''. * Start of block: 02:23, 27 October 2019 * Expiration of block: 02:23, 27 October 2021
+                $page->get_text_from($writeTestPage);
+                $this->assertSame($origText, $page->parsed_text());
+            } else {
+                // Double check we can read it back
+                $page->get_text_from($writeTestPage);
+                $this->assertSame($trialCitation, $page->parsed_text());
+            }
             $this->assertTrue(TRAVIS || $page_result); // If we have tokens and are not in TRAVIS, then should have worked
-        });
-        $page->overwrite_text($trialCitation);
-        $page->expand_text();
-        $this->assertTrue(strpos($page->edit_summary(), 'journal, ') > 3);
-        $this->assertTrue(strpos($page->edit_summary(), ' Removed ') > 3);
-        if ($page_result) {
-            $this->assertTrue($page->write($api));
+            $page->overwrite_text($trialCitation);
+            $page->expand_text();
+            $this->assertTrue(strpos($page->edit_summary(), 'journal, ') > 3);
+            $this->assertTrue(strpos($page->edit_summary(), ' Removed ') > 3);
+            if ($page_result) {
+                $this->assertTrue($page->write($api));
+            }
+            $page->get_text_from($writeTestPage);
+            $this->assertTrue(strpos($page->parsed_text(), 'Nature') > 5); // This can fail, if the page gets edited and is no longer {{Cite journal | doi = 10.1038/nature09068 | title=Bot Testing | date=2010 | pmid=20505727 | last1=Smith | first1=M. R. | last2=Caron | first2=J. B. | journal=Nature | volume=465 | issue=7297 | pages=469–472 | hdl=1807/32368 | s2cid=4421029 }}
         }
-        $page->get_text_from($writeTestPage);
-        $this->assertTrue(strpos($page->parsed_text(), 'Nature') > 5); // This can fail, if the page gets edited and is no longer {{Cite journal | doi = 10.1038/nature09068 | title=Bot Testing | date=2010 | pmid=20505727 | last1=Smith | first1=M. R. | last2=Caron | first2=J. B. | journal=Nature | volume=465 | issue=7297 | pages=469–472 | hdl=1807/32368 | s2cid=4421029 }}
     }
 
     public function testNobots(): void {
