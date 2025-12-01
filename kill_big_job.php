473accc36e48b1afcbbeab598d2dedad324be284
GlazerMann <kurt@ioiowa.com>
2025-12-01 08:25:56 -0600

    Update kill_big_job.php

--- kill_big_job.php
+++ kill_big_job.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 header("Access-Control-Allow-Origin: null"); // Humans only
 
-ob_implicit_flush(true);
 set_time_limit(120);
 session_start(['read_and_close' => true]);
 
 require_once 'big_jobs.php';
+
+ob_implicit_flush(true);
 
 if (!isset($_SESSION['citation_bot_user_id'])) {
     echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Killing the big job</title></head><body><main><pre>You are not logged in</pre></main></body></html>';
