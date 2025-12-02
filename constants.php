fb70a041ce337cd95cb9721bc083f67cc28bd0cc
GlazerMann <kurt@ioiowa.com>
2025-12-01 19:29:44 -0600

    Update constants.php

--- constants.php
+++ constants.php
@@ -99,3 +99,32 @@ function bot_curl_exec(CurlHandle $ch): string {
     curl_setopt($ch, CURLOPT_REFERER, WIKI_ROOT . "title=" . Page::$last_title);
     return (string) @curl_exec($ch);
 }
+
+const REJECT_NEW = ['null', 'n/a', 'undefined', '0 0', '(:none)', '-'];
+const GOOFY_TITLES = ['Archived copy', "{title}", 'ScienceDirect', 'Google Books', 'None', 'usurped title'];
+const BAD_NEW_PAGES = ['0', '0-0', '0–0'];
+const BAD_ISBN = ['9780918678072', '978-0-918678-07-2', '0918678072', '0-918678-07-2'];
+const SHORT_STRING = ['the', 'and', 'a', 'for', 'in', 'on', 's', 're', 't', 'an', 'as', 'at', 'but', 'how', 'why', 'by', 'when', 'with', 'who', 'where', ''];
+const RIS_IS_BOOK = ['CHAP', 'BOOK', 'EBOOK', 'ECHAP', 'EDBOOK', 'DICT', 'ENCYC', 'GOVDOC', 'RPRT'];
+const RIS_IS_FULL_BOOK = ['BOOK', 'EBOOK', 'EDBOOK'];
+const GOOD_FREE = ['publisher', 'projectmuse', 'have free'];
+const BAD_OA_URL = ['10.4135/9781529742343', '10.1017/9781108859745'];
+const REMOVE_SEMI = ['date', 'year', 'location', 'publisher', 'issue', 'number', 'page', 'pages', 'pp', 'p', 'volume'];
+const REMOVE_PERIOD = ['date', 'year', 'issue', 'number', 'page', 'pages', 'pp', 'p', 'volume'];
+const LINK_LIST = ['authorlink', 'chapterlink', 'contributorlink', 'editorlink', 'episodelink', 'interviewerlink', 'inventorlink', 'serieslink', 'subjectlink', 'titlelink', 'translatorlink'];
+const BAD_AGENT = ['United States Food and Drug Administration', 'Surgeon General of the United States', 'California Department of Public Health'];
+const BAD_AGENT_PUBS = ['United States Department of Health and Human Services', 'California Tobacco Control Program', ''];
+const NO_LANGS = ['n', 'no', 'live', 'alive', 'কার্যকর', 'hayır', 'não', 'nao', 'false'];
+const YES_LANGS = ['y', 'yes', 'dead', 'si', 'sì', 'ja', 'evet', 'ei tööta', 'sim', 'ano', 'true'];
+const PDF_LINKS = ['pdf', 'portable document format', '[[portable document format|pdf]]', '[[portable document format]]', '[[pdf]]'];
+const DEPARTMENTS = ['local', 'editorial', 'international', 'national', 'communication', 'letter to the editor',
+        'review', 'coronavirus', 'race & reckoning', 'politics', 'opinion', 'opinions', 'investigations', 'tech',
+        'technology', 'world', 'sports', 'arts & entertainment', 'arts', 'entertainment', 'u.s.', 'n.y.',
+        'business', 'science', 'health', 'books', 'style', 'food', 'travel', 'real estate', 'magazine', 'economy',
+        'markets', 'life & arts', 'uk news', 'world news', 'health news', 'lifestyle', 'photos', 'education',
+        'life', 'puzzles'];
+const BAD_VIA = [ '', 'project muse', 'wiley', 'springer', 'questia', 'elsevier', 'wiley online library',
+        'wiley interscience', 'interscience', 'sciencedirect', 'science direct', 'ebscohost', 'proquest',
+        'google scholar', 'google', 'bing', 'yahoo'];
+const VOL_NUM = ['volume', 'issue', 'number'];
+
