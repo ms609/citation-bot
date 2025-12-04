6f7f95153a58c24ba27692a2f71a5724399751e6
GlazerMann <kurt@ioiowa.com>
2025-12-03 20:44:54 -0600

    Update gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -7,23 +7,19 @@ header("Access-Control-Allow-Origin: null");
 /** @psalm-suppress MissingFile */
 require_once 'env.php';
 
-if (($_GET['password'] ?? '') !== (string) @getenv('DEPLOY_PASSWORD') ) {
-    http_response_code(403);
-    exit('Incorrect password. Please add ?password=YOUR_PASSWORD to the URL. You can set the password in your .env file (DEPLOY_PASSWORD).');
-}
-
 ob_implicit_flush(true);
 flush();
-
 clearstatcache(true);
-if (@mkdir('git_pull.lock', 0700)) {
-    sleep(1); // paranoid
+
+if (($_GET['password'] ?? '') !== (string) @getenv('DEPLOY_PASSWORD') ) {
+    http_response_code(403);
+    $git_hub = 'Incorrect password. Please add ?password=YOUR_PASSWORD to the URL. You can set the password in your .env file (DEPLOY_PASSWORD).';
+} elseif (@mkdir('git_pull.lock', 0700)) {
     /** @psalm-suppress ForbiddenCode */
     $git_hub = htmlspecialchars((string) shell_exec("(/usr/bin/git fetch  --all; /usr/bin/git reset --hard origin/master)  2>&1"), ENT_QUOTES);
-    sleep(2);
     rmdir('git_pull.lock');
 } else {
-    sleep(3);
+    sleep(1);
     $git_hub = "Please try again";
 }
 echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Git Pull</title></head><body><main><pre>', $git_hub, '</pre></main></body></html>';
