ad9eebeb8efe67a16e151728ccbaf5495e1408a5
GlazerMann <kurt@ioiowa.com>
2025-12-04 11:17:03 -0600

    Cannot send response code

--- gitpull.php
+++ gitpull.php
@@ -7,19 +7,18 @@ header("Access-Control-Allow-Origin: null");
 /** @psalm-suppress MissingFile */
 require_once 'env.php';
 
-clearstatcache(true);
+const LOCK_DIR = 'git_pull.lock';
+
+clearstatcache(true, LOCK_DIR);
 
 if (($_GET['password'] ?? '') !== (string) @getenv('DEPLOY_PASSWORD') ) {
-    http_response_code(403);
     $git_hub = 'Incorrect password. Please add ?password=YOUR_PASSWORD to the URL. You can set the password in your .env file (DEPLOY_PASSWORD).';
-} elseif (@mkdir('git_pull.lock', 0700)) {
-    http_response_code(200);
+} elseif (@mkdir(LOCK_DIR, 0700)) {
     /** @psalm-suppress ForbiddenCode */
     $git_hub = htmlspecialchars((string) shell_exec("(/usr/bin/git fetch  --all; /usr/bin/git reset --hard origin/master)  2>&1"), ENT_QUOTES);
-    rmdir('git_pull.lock');
+    rmdir(LOCK_DIR);
 } else {
-    http_response_code(409);
-    $git_hub = "Please try again";
+    $git_hub = "Please try again - lock file found";
 }
 echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>Git Pull</title></head><body><main><pre>', $git_hub, '</pre></main></body></html>';
 flush(); // paranoid about disk I/O
