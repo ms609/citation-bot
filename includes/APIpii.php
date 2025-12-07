2ee6fd34b7f591380203ee6f069df5fdc0b89417
GlazerMann <kurt@ioiowa.com>
2025-12-07 11:44:22 -0600

    Create APIpii.php

---
+++ includes/APIpii.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+    function get_doi_from_pii(string $pii): string {
+        static CurlHandle? $ch_pii;
+        if (null === $ch_pii) {
+        	$ch_pii = bot_curl_init($time, []);
+        }
+        curl_setopt($ch_pii, CURLOPT_URL, "https://api.elsevier.com/content/object/pii/" . $pii);
+        $ch_return = (string) bot_curl_exec($ch_pii);
+        if (preg_match('~<prism:doi>(10\..+)<\/prism:doi>~', $ch_return, $match)) {
+            return $match[1];
+        }
+        return '';
+    }
