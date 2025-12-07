f7737f2312b96a2e2753c8467932db7c74692c1c
GlazerMann <kurt@ioiowa.com>
2025-12-06 22:01:48 -0600

    Update apiFunctions.php

--- apiFunctions.php
+++ apiFunctions.php
@@ -8,14 +8,14 @@ require_once 'user_messages.php';
 require_once 'Template.php';
 require_once 'NameTools.php';
 
-require_once 'APIdoi.php';
-require_once 'APIS2.php';
-require_once 'APIBibCode.php';
-require_once 'APIPubMed.php';
-require_once 'APIgoogle.php';
-require_once 'APIunpaywall.php';
-require_once 'APIjstor.php';
-require_once 'APIarXiv.php';
-require_once 'APIarchives.php';
+require_once 'includes/APIdoi.php';
+require_once 'includes/APIS2.php';
+require_once 'includes/APIBibCode.php';
+require_once 'includes/APIPubMed.php';
+require_once 'includes/APIgoogle.php';
+require_once 'includes/APIunpaywall.php';
+require_once 'includes/APIjstor.php';
+require_once 'includes/APIarXiv.php';
+require_once 'includes/APIarchives.php';
 
 // @codeCoverageIgnoreEnd
