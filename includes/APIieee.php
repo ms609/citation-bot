bf14445c08206c8952883b66f424adcb96c43795
GlazerMann <kurt@ioiowa.com>
2025-12-07 12:40:22 -0600

    Create APIieee.php

---
+++ includes/APIieee.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+    /**
+        @param array<Template> $templates
+    */
+    function query_ieee_webpages(array &$templates): void {  // Pointer to save memory
+        static $ch_ieee;
+        if (null === $ch_ieee) {
+            $ch_ieee = bot_curl_init($time, [CURLOPT_USERAGENT => 'curl']); // IEEE requires JavaScript, unless curl is specified
+        }
+        foreach (['url', 'chapter-url', 'chapterurl'] as $kind) {
+            foreach ($templates as $template) {
+                set_time_limit(120);
+                /** @psalm-taint-escape ssrf */
+                $the_url = $template->get($kind);
+                if (preg_match("~^https://ieeexplore\.ieee\.org/document/(\d{5,})$~", $the_url, $matches_url)) {
+                    curl_setopt(self::$ch_ieee, CURLOPT_URL, $the_url);
+                    if ($template->blank('doi')) {
+                        usleep(100000); // 0.10 seconds
+                        $return = bot_curl_exec(self::$ch_ieee);
+                        if ($return !== "" && preg_match_all('~"doi":"(10\.\d{4}/[^\s"]+)"~', $return, $matches, PREG_PATTERN_ORDER)) {
+                            $dois = array_unique($matches[1]);
+                            if (count($dois) === 1) {
+                                if ($template->add_if_new('doi', $dois[0])) {
+                                    if (strpos($template->get('doi'), $matches_url[1]) !== false && doi_works($template->get('doi'))) {
+                                        $template->forget($kind);
+                                    }
+                                }
+                            }
+                        }
+                    } elseif (doi_works($template->get('doi'))) {
+                        usleep(100000); // 0.10 seconds
+                        $return = bot_curl_exec(self::$ch_ieee);
+                        if ($return !== "" && strpos($return, "<title> -  </title>") !== false) {
+                            report_forget("Existing IEEE no longer works - dropping URL"); // @codeCoverageIgnore
+                            $template->forget($kind);                   // @codeCoverageIgnore
+                        }
+                    }
+                }
+            }
+        }
+    }
