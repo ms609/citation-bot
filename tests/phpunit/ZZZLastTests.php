341ffc21f3ee1c1182cc2bdeb62d3dda2ca6a5c9
GlazerMann <kurt@ioiowa.com>
2025-12-14 12:41:55 -0600

    add a "test"

--- tests/phpunit/ZZZLastTests.php
+++ tests/phpunit/ZZZLastTests.php
@@ -9,6 +9,23 @@ require_once __DIR__ . '/../testBaseClass.php';
 
 final class ZZZLastTests extends testBaseClass {
 
+    public function testCoverageFaker(): void {
+        require __DIR__ . '../../includes/setup.php';
+        require __DIR__ . '../../includes/constants.php';
+        require __DIR__ . '../../includes/constants/bad_data.php';
+        require __DIR__ . '../../includes/constants/capitalization.php';
+        require __DIR__ . '../../includes/constants/isbn.php';
+        require __DIR__ . '../../includes/constants/italics.php';
+        require __DIR__ . '../../includes/constants/math.php';
+        require __DIR__ . '../../includes/constants/mistakes.php';
+        require __DIR__ . '../../includes/constants/null_bad_doi.php';
+        require __DIR__ . '../../includes/constants/null_good_doi.php';
+        require __DIR__ . '../../includes/constants/parameters.php';
+        require __DIR__ . '../../includes/constants/regular_expressions.php';
+        require __DIR__ . '../../includes/constants/translations.php';
+        $this->assertTrue(true);
+    }
+
     public function testFlushCacheAtEnd(): void {
         $pg = new TestPage(); unset($pg);    // Fill page name with test name for debugging
         HandleCache::free_memory();
