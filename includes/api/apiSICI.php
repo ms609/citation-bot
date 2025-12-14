faa68a93dbfe11adc50d161f2cc56013bcd2c081
GlazerMann <kurt@ioiowa.com>
2025-12-14 14:22:13 -0600

    Update apiSICI.php

--- includes/api/apiSICI.php
+++ includes/api/apiSICI.php
@@ -2,17 +2,17 @@
 
 declare(strict_types=1);
 
-    function use_sici(): void
+    function use_sici(Template $template): void
     {
-        if (preg_match(REGEXP_SICI, urldecode($this->parsed_text()), $sici)) {
+        if (preg_match(REGEXP_SICI, urldecode($template->parsed_text()), $sici)) {
             quietly('report_action', "Extracting information from SICI");
-            $this->add_if_new('issn', $sici[1]); // Check whether journal is set in add_if_new
-            $this->add_if_new('year', (string) (int) $sici[2]);
-            $this->add_if_new('volume', (string) (int) $sici[5]);
+            $template->add_if_new('issn', $sici[1]); // Check whether journal is set in add_if_new
+            $template->add_if_new('year', (string) (int) $sici[2]);
+            $template->add_if_new('volume', (string) (int) $sici[5]);
             if ($sici[6]) {
-                $this->add_if_new('issue', (string) (int) $sici[6]);
+                $template->add_if_new('issue', (string) (int) $sici[6]);
             }
-            $this->add_if_new('pages', (string) (int) $sici[7]);
+            $template->add_if_new('pages', (string) (int) $sici[7]);
             report_action("Found and used SICI");
         }
     }
