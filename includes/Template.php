9140d317b778e9881bfec7bae0fa2178de825b18
GlazerMann <kurt@ioiowa.com>
2025-12-15 16:02:29 -0600

    pull out ISBN code (#5100)

--- includes/Template.php
+++ includes/Template.php
@@ -16,6 +16,7 @@ require_once 'includes/NameTools.php';
 require_once 'includes/IsThisChapter.php';
 require_once 'includes/api/APIsici.php';
 require_once 'includes/isConference.php';
+require_once 'includes/isbn13.php';
 // @codeCoverageIgnoreEnd
 
 final class Template
@@ -7227,42 +7228,11 @@ final class Template
 
     private function isbn10Toisbn13(string $isbn10, bool $ignore_year): string
     {
-        $isbn10 = mb_trim($isbn10); // Remove leading and trailing spaces
-        $test = str_replace(['—', '?', '–', '-', '?', ' '], '', $isbn10);
-        if (mb_strlen($test) < 10 || mb_strlen($test) > 13) {
-            return $isbn10;
+        $year = $this->year_int();
+        if ($ignore_year) {
+            $year = 3000;
         }
-        $isbn10 = str_replace('x', 'X', $isbn10);
-        if (preg_match("~^[0-9Xx ]+$~", $isbn10) === 1) {
-            // Uses spaces
-            $isbn10 = str_replace(' ', '-', $isbn10);
-        }
-        $isbn10 = str_replace(['—', '?', '–', '-', '?'], '-', $isbn10); // Standardize dahses : en dash, horizontal bar, em dash, minus sign, figure dash, to hyphen.
-        if (preg_match("~[^0-9Xx\-]~", $isbn10) === 1) {
-            return $isbn10;
-        } // Contains invalid characters
-        if (mb_substr($isbn10, -1) === "-" || mb_substr($isbn10, 0, 1) === "-") {
-            return $isbn10;
-        } // Ends or starts with a dash
-        if ($this->year_int() < 2007 && !$ignore_year) {
-            return $isbn10;
-        } // Older books does not have ISBN-13, see [[WP:ISBN]]
-        $isbn13 = str_replace('-', '', $isbn10); // Remove dashes to do math
-        if (mb_strlen($isbn13) !== 10) {
-            return $isbn10;
-        } // Might be an ISBN 13 already, or rubbish
-        $isbn13 = '978' . mb_substr($isbn13, 0, -1); // Convert without check digit - do not need and might be X
-        if (preg_match("~[^0123456789]~", $isbn13) === 1) {
-            return $isbn10;
-        } // Not just numbers
-        $sum = 0;
-        for ($count = 0; $count < 12; $count++) {
-            $sum = $sum + intval($isbn13[$count]) * ($count % 2 ? 3 : 1); // Depending upon even or odd, we multiply by 3 or 1 (strange but true)
-        }
-        $sum = (10 - ($sum % 10)) % 10;
-        $isbn13 = '978' . '-' . mb_substr($isbn10, 0, -1) . (string) $sum; // Assume existing dashes (if any) are right
-        quietly('report_modification', "Converted ISBN10 to ISBN13");
-        return $isbn13;
+        return changeisbn10Toisbn13($isbn10, $year);
     }
 
     /** @return array<string> */
