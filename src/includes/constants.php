0637883aed434f9828a038d12dc14e201df49b3c
GlazerMann <kurt@ioiowa.com>
2025-12-16 07:49:54 -0600

    Update constants.php

--- src/includes/constants.php
+++ src/includes/constants.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 // @codeCoverageIgnoreStart
 // all new constant files needed listed here
-require_once 'src/includes/constants/bad_data.php';
-require_once 'src/includes/constants/capitalization.php';
-require_once 'src/includes/constants/math.php';
-require_once 'src/includes/constants/mistakes.php';
-require_once 'src/includes/constants/parameters.php';
-require_once 'src/includes/constants/regular_expressions.php';
-require_once 'src/includes/constants/italics.php';
-require_once 'src/includes/constants/isbn.php';
-require_once 'src/includes/constants/null_good_doi.php';
-require_once 'src/includes/constants/null_bad_doi.php';
-require_once 'src/includes/constants/translations.php';
+require_once __DIR__ . '/includes/constants/bad_data.php';
+require_once __DIR__ . '/includes/constants/capitalization.php';
+require_once __DIR__ . '/includes/constants/math.php';
+require_once __DIR__ . '/includes/constants/mistakes.php';
+require_once __DIR__ . '/includes/constants/parameters.php';
+require_once __DIR__ . '/includes/constants/regular_expressions.php';
+require_once __DIR__ . '/includes/constants/italics.php';
+require_once __DIR__ . '/includes/constants/isbn.php';
+require_once __DIR__ . '/includes/constants/null_good_doi.php';
+require_once __DIR__ . '/includes/constants/null_bad_doi.php';
+require_once __DIR__ . '/includes/constants/translations.php';
 // @codeCoverageIgnoreEnd
 
 const PIPE_PLACEHOLDER = '# # # CITATION_BOT_PLACEHOLDER_PIPE # # #';
