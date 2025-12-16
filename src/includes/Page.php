7d0d182b48f226ff5788a035caf2bd22d98e1148
GlazerMann <kurt@ioiowa.com>
2025-12-16 07:48:04 -0600

    Update Page.php

--- src/includes/Page.php
+++ src/includes/Page.php
@@ -9,23 +9,23 @@ declare(strict_types=1);
  * handle collected page modifications, and save the edited page text.
  */
 
-require_once 'src/includes/Comment.php';       // @codeCoverageIgnoreStart
-require_once 'src/includes/Template.php';
-require_once 'src/includes/constants.php';
-require_once 'src/includes/api/APIdoi.php';
-require_once 'src/includes/api/APIS2.php';
-require_once 'src/includes/api/APIBibCode.php';
-require_once 'src/includes/api/APIPubMed.php';
-require_once 'src/includes/api/APIgoogle.php';
-require_once 'src/includes/api/APIunpaywall.php';
-require_once 'src/includes/api/APIjstor.php';
-require_once 'src/includes/api/APIarXiv.php';
-require_once 'src/includes/api/APIarchives.php';
-require_once 'src/includes/expandFns.php';
-require_once 'src/includes/user_messages.php';
-require_once 'src/includes/api/APIzotero.php';
-require_once 'src/includes/citeodnb.php';
-require_once 'src/includes/api/APIissn.php'; // @codeCoverageIgnoreEnd
+require_once __DIR__ . '/includes/Comment.php';       // @codeCoverageIgnoreStart
+require_once __DIR__ . '/includes/Template.php';
+require_once __DIR__ . '/includes/constants.php';
+require_once __DIR__ . '/includes/api/APIdoi.php';
+require_once __DIR__ . '/includes/api/APIS2.php';
+require_once __DIR__ . '/includes/api/APIBibCode.php';
+require_once __DIR__ . '/includes/api/APIPubMed.php';
+require_once __DIR__ . '/includes/api/APIgoogle.php';
+require_once __DIR__ . '/includes/api/APIunpaywall.php';
+require_once __DIR__ . '/includes/api/APIjstor.php';
+require_once __DIR__ . '/includes/api/APIarXiv.php';
+require_once __DIR__ . '/includes/api/APIarchives.php';
+require_once __DIR__ . '/includes/expandFns.php';
+require_once __DIR__ . '/includes/user_messages.php';
+require_once __DIR__ . '/includes/api/APIzotero.php';
+require_once __DIR__ . '/includes/citeodnb.php';
+require_once __DIR__ . '/includes/api/APIissn.php'; // @codeCoverageIgnoreEnd
 
 const UNPROTECTED_PAGE = ["autoconfirmed", "extendedconfirmed", "editautoreviewprotected"];
 const PROTECTED_PAGE = ["sysop", "templateeditor"];
