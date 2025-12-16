33072f88d7637484bec5263157c06a8cfe365641
GlazerMann <kurt@ioiowa.com>
2025-12-16 13:11:58 -0600

    Update setup.php

--- src/includes/setup.php
+++ src/includes/setup.php
@@ -179,7 +179,7 @@ require_once __DIR__ . '/Parameter.php';
 require_once __DIR__ . '/TextTools.php';
 require_once __DIR__ . '/WebTools.php';
 require_once __DIR__ . '/doiTools.php';
-require_once __DIR__ . '/MiscTools';
+require_once __DIR__ . '/miscTools.php';
 require_once __DIR__ . '/Template.php';
 require_once __DIR__ . '/api/APIzotero.php';
 require_once __DIR__ . '/api/APIdoi.php';
