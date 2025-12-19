6656c55207341611aeed528be74713e29b360b54
GlazerMann <kurt@ioiowa.com>
2025-12-19 14:02:22 -0600

    Update issnTest.php

--- tests/phpunit/includes/api/issnTest.php
+++ tests/phpunit/includes/api/issnTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '../../../testBaseClass.php';
+require_once __DIR__ . '/../../../testBaseClass.php';
 
 final class issnTest extends testBaseClass {
     public function testUse_ISSN(): void {
