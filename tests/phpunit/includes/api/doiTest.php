60f2dc0396d864de226d61cb73669b3d59d5e7c9
GlazerMann <kurt@ioiowa.com>
2025-12-19 14:02:03 -0600

    Update doiTest.php

--- tests/phpunit/includes/api/doiTest.php
+++ tests/phpunit/includes/api/doiTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '../../../testBaseClass.php';
+require_once __DIR__ . '/../../../testBaseClass.php';
 
 final class doiTest extends testBaseClass {
     public function testExpansion_doi_not_from_crossrefRG(): void {
