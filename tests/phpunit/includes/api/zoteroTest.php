c95b7d477c7d69bec994d8d74250d78cf20b1ebe
GlazerMann <kurt@ioiowa.com>
2025-12-19 15:22:14 -0600

    Update zoteroTest.php

--- tests/phpunit/includes/api/zoteroTest.php
+++ tests/phpunit/includes/api/zoteroTest.php
@@ -1200,11 +1200,4 @@ final class zoteroTest extends testBaseClass {
         $this->assertNull($template->get2('url'));
     }
 
-    public function testPII(): void {
-        $pg = new TestPage(); unset($pg); // Fill page name with test name for debugging
-        $pii = 'S0960076019302699';
-        $doi_expect = '10.1016/j.jsbmb.2019.105494';
-        $doi = get_doi_from_pii($pii);
-        $this->assertSame($doi_expect, $doi);
-    }
 }
