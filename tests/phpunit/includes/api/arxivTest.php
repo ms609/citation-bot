80291a9ed6576963119471576eb98a59d7abc07d
GlazerMann <kurt@ioiowa.com>
2025-12-19 15:19:26 -0600

    Update arxivTest.php

--- tests/phpunit/includes/api/arxivTest.php
+++ tests/phpunit/includes/api/arxivTest.php
@@ -18,4 +18,24 @@ final class arxivTest extends testBaseClass {
         $this->assertSame('10.1093/mnras/290.2.L28', $prepared->get2('doi'));
         $this->assertNotNull($prepared->get2('doi-broken-date')); // The DOI has to not work for this test to cover the code where a title and arxiv exist and a doi is found, but the doi does not add a title
     }
+
+    public function testArxivExpansion(): void {
+        $text = "{{Cite web | http://uk.arxiv.org/abs/0806.0013}}"
+                    . "{{Cite arxiv | eprint = 0806.0013 | class=forgetit|publisher=uk.arxiv}}"
+                    . '{{Cite arxiv |arxiv=1609.01689 | title = Accelerating Nuclear Configuration Interaction Calculations through a Preconditioned Block Iterative Eigensolver|class=cs.NA | year = 2016| last1 = Shao| first1 = Meiyue | display-authors = etal}}'
+                    . '{{cite arXiv|eprint=hep-th/0303241}}'; // tests line feeds
+
+        $expanded = $this->process_page($text);
+        $templates = $expanded->extract_object('Template');
+        $this->assertSame('cite journal', $templates[0]->wikiname());
+        $this->assertSame('0806.0013', $templates[0]->get2('arxiv'));
+        $this->assertSame('cite journal', $templates[1]->wikiname());
+        $this->assertSame('0806.0013', $templates[1]->get2('arxiv'));
+        $this->assertNull($templates[1]->get2('class'));
+        $this->assertNull($templates[1]->get2('eprint'));
+        $this->assertNull($templates[1]->get2('publisher'));
+        $this->assertSame('2016', $templates[2]->get2('year'));
+        $this->assertSame('Pascual Jordan, his contributions to quantum mechanics and his legacy in contemporary local quantum physics', $templates[3]->get2('title'));
+    }
+
 }
