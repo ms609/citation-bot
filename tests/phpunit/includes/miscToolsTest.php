158096f5096415a663a22ac129f198c3c031e69e
GlazerMann <kurt@ioiowa.com>
2025-12-19 14:05:13 -0600

    Update and rename miscTools.php to miscToolsTest.php

--- tests/phpunit/includes/miscToolsTest.php
+++ tests/phpunit/includes/miscToolsTest.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../../testBaseClass.php';
 
-final class miscTools extends testBaseClass {
+final class miscToolsTest extends testBaseClass {
     public function testcheck_memory_usage(): void {
         $pg = new TestPage(); unset($pg); // Fill page name with test name for debugging
         check_memory_usage('testcheck_memory_usage');
