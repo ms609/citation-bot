c7bf537ae5a294c8de2de41ccb9cf6baf4c6e348
GlazerMann <kurt@ioiowa.com>
2025-12-19 15:28:06 -0600

    Create ieeeTest.php

---
+++ tests/phpunit/includes/api/ieeeTest.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/../../../testBaseClass.php';
+
+final class ieeeTest extends testBaseClass {
+
+    public function testIEEEdoi(): void {
+        $url = "https://ieeexplore.ieee.org/document/4242344";
+        $template = $this->process_citation('{{cite journal | url = ' . $url . ' }}');
+        if ($template->get('doi') === "") {
+            sleep(5);
+            $template = $this->process_citation('{{cite journal | url = ' . $url . ' }}');
+        }
+        $this->assertSame('10.1109/ISSCC.2007.373373', $template->get2('doi'));
+    }
+
+    public function testIEEEdropBadURL(): void {
+        $template = $this->process_citation('{{cite journal | url = https://ieeexplore.ieee.org/document/4242344341324324123412343214 |doi =10.1109/ISSCC.2007.373373 }}');
+        $this->assertNull($template->get2('url'));
+    } 
+}
