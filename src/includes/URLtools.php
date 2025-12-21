7ad2e653c92251224a244563b858f73621282dde
GlazerMann <kurt@ioiowa.com>
2025-12-21 14:56:22 -0600

    normalize variables names (#5147)

--- src/includes/URLtools.php
+++ src/includes/URLtools.php
@@ -24,16 +24,16 @@ function drop_urls_that_match_dois(array &$templates): void {  // Pointer to sav
         $doi = $template->get_without_comments_and_placeholders('doi');
         if ($template->has('url')) {
             $url = $template->get('url');
-            $url_kind = 'url';
+            $url_type = 'url';
         } elseif ($template->has('chapter-url')) {
             $url = $template->get('chapter-url');
-            $url_kind = 'chapter-url';
+            $url_type = 'chapter-url';
         } elseif ($template->has('chapterurl')) {
             $url = $template->get('chapterurl'); // @codeCoverageIgnore
-            $url_kind = 'chapterurl';      // @codeCoverageIgnore
+            $url_type = 'chapterurl';      // @codeCoverageIgnore
         } else {
             $url = '';
-            $url_kind = '';
+            $url_type = '';
         }
         if ($doi && // IEEE code does not require "not incomplete"
             $url &&
@@ -41,7 +41,7 @@ function drop_urls_that_match_dois(array &$templates): void {  // Pointer to sav
             $template->blank(DOI_BROKEN_ALIASES) &&
             preg_match("~^https?://ieeexplore\.ieee\.org/document/\d{5,}/?$~", $url) && mb_strpos($doi, '10.1109') === 0) {
             report_forget("Existing IEEE resulting from equivalent DOI; dropping URL");
-            $template->forget($url_kind);
+            $template->forget($url_type);
         }
 
         if ($doi &&
@@ -53,46 +53,46 @@ function drop_urls_that_match_dois(array &$templates): void {  // Pointer to sav
                 set_time_limit(120);
             if (str_ireplace(PROXY_HOSTS_TO_DROP, '', $url) !== $url && $template->get('doi-access') === 'free') {
                 report_forget("Existing proxy URL resulting from equivalent free DOI; dropping URL");
-                $template->forget($url_kind);
+                $template->forget($url_type);
             } elseif (str_ireplace(PROXY_HOSTS_TO_ALWAYS_DROP, '', $url) !== $url && $template->get('doi-access') === 'free') {
                 report_forget("Existing proxy URL resulting from equivalent free DOI; dropping URL");
-                $template->forget($url_kind);
+                $template->forget($url_type);
             } elseif (str_ireplace(PROXY_HOSTS_TO_ALWAYS_DROP, '', $url) !== $url && $template->blank(['archive-url', 'archiveurl'])) {
                 report_forget("Existing proxy URL resulting from equivalent DOI; fixing URL");
-                $template->set($url_kind, "https://dx.doi.org/" . doi_encode($doi));
+                $template->set($url_type, "https://dx.doi.org/" . doi_encode($doi));
             } elseif (preg_match('~www.sciencedirect.com/science/article/B[^/\-]*\-[^/\-]+\-[^/\-]+/~', $url)) {
                 report_forget("Existing Invalid ScienceDirect URL when DOI is present; fixing URL");
-                $template->set($url_kind, "https://dx.doi.org/" . doi_encode($doi));
+                $template->set($url_type, "https://dx.doi.org/" . doi_encode($doi));
             } elseif (preg_match('~www.sciencedirect.com/science/article/pii/\S{0,16}$~i', $url)) { // Too Short
                 report_forget("Existing Invalid ScienceDirect URL when DOI is present; fixing URL");
-                $template->set($url_kind, "https://dx.doi.org/" . doi_encode($doi));
+                $template->set($url_type, "https://dx.doi.org/" . doi_encode($doi));
             } elseif (preg_match('~www.springerlink.com/content~i', $url)) { // Dead website
                 report_forget("Existing Invalid Springer Link URL when DOI is present; fixing URL");
-                $template->set($url_kind, "https://dx.doi.org/" . doi_encode($doi));
+                $template->set($url_type, "https://dx.doi.org/" . doi_encode($doi));
             } elseif (str_ireplace('insights.ovid.com/pubmed', '', $url) !== $url && $template->has('pmid')) {
                 report_forget("Existing OVID URL resulting from equivalent PMID and DOI; dropping URL");
-                $template->forget($url_kind);
+                $template->forget($url_type);
             } elseif ($template->has('pmc') && str_ireplace('iopscience.iop.org', '', $url) !== $url) {
                 report_forget("Existing IOP URL resulting from equivalent DOI; dropping URL");
-                $template->forget($url_kind);;
-                $template->set($url_kind, "https://dx.doi.org/" . doi_encode($doi));
+                $template->forget($url_type);;
+                $template->set($url_type, "https://dx.doi.org/" . doi_encode($doi));
             } elseif (str_ireplace('wkhealth.com', '', $url) !== $url) {
                 report_forget("Existing Outdated WK Health URL resulting from equivalent DOI; fixing URL");
-                $template->set($url_kind, "https://dx.doi.org/" . doi_encode($doi));
+                $template->set($url_type, "https://dx.doi.org/" . doi_encode($doi));
             } elseif ($template->has('pmc') && str_ireplace('bmj.com/cgi/pmidlookup', '', $url) !== $url && $template->has('pmid') && $template->get('doi-access') === 'free' && mb_stripos($url, 'pdf') === false) {
                 report_forget("Existing The BMJ URL resulting from equivalent PMID and free DOI; dropping URL");
-                $template->forget($url_kind);
-            } elseif ($template->get('doi-access') === 'free' && $template->get('url-status') === 'dead' && $url_kind === 'url') {
+                $template->forget($url_type);
+            } elseif ($template->get('doi-access') === 'free' && $template->get('url-status') === 'dead' && $url_type === 'url') {
                 report_forget("Existing free DOI; dropping dead URL");
-                $template->forget($url_kind);
+                $template->forget($url_type);
             } elseif (doi_works($template->get('doi')) &&
                         !preg_match(REGEXP_DOI_ISSN_ONLY, $template->get('doi')) &&
-                        $url_kind !== '' &&
-                        (str_ireplace(CANONICAL_PUBLISHER_URLS, '', $template->get($url_kind)) !== $template->get($url_kind)) &&
+                        $url_type !== '' &&
+                        (str_ireplace(CANONICAL_PUBLISHER_URLS, '', $template->get($url_type)) !== $template->get($url_type)) &&
                         $template->has_good_free_copy() &&
-                        (mb_stripos($template->get($url_kind), 'pdf') === false)) {
+                        (mb_stripos($template->get($url_type), 'pdf') === false)) {
                 report_forget("Existing canonical URL resulting in equivalent free DOI/pmc; dropping URL");
-                $template->forget($url_kind);
+                $template->forget($url_type);
             } elseif (mb_stripos($url, 'pdf') === false && $template->get('doi-access') === 'free' && $template->has('pmc')) {
                 curl_setopt($ch_dx, CURLOPT_URL, "https://dx.doi.org/" . doi_encode($doi));
                 $ch_return = bot_curl_exec($ch_dx);
@@ -112,7 +112,7 @@ function drop_urls_that_match_dois(array &$templates): void {  // Pointer to sav
                     if (mb_stripos($url_short, $redirectedUrl_doi) !== false ||
                         mb_stripos($redirectedUrl_doi, $url_short) !== false) {
                         report_forget("Existing canonical URL resulting from equivalent free DOI; dropping URL");
-                        $template->forget($url_kind);
+                        $template->forget($url_type);
                     } else { // See if $url redirects
                         /** @psalm-taint-escape ssrf */
                         $the_url = $url;
@@ -124,7 +124,7 @@ function drop_urls_that_match_dois(array &$templates): void {  // Pointer to sav
                             if (mb_stripos($redirectedUrl_url, $redirectedUrl_doi) !== false ||
                                             mb_stripos($redirectedUrl_doi, $redirectedUrl_url) !== false) {
                                 report_forget("Existing canonical URL resulting from equivalent free DOI; dropping URL");
-                                $template->forget($url_kind);
+                                $template->forget($url_type);
                             }
                         }
                     }
@@ -132,11 +132,11 @@ function drop_urls_that_match_dois(array &$templates): void {  // Pointer to sav
                 unset($ch_return);
             }
         }
-        $url = $template->get($url_kind);
+        $url = $template->get($url_type);
         if ($url && !$template->profoundly_incomplete() && str_ireplace(PROXY_HOSTS_TO_ALWAYS_DROP, '', $url) !== $url) {
             if (!$template->blank_other_than_comments('pmc')) {
                 report_forget("Existing proxy URL resulting from equivalent PMC; dropping URL");
-                $template->forget($url_kind);
+                $template->forget($url_type);
             }
         }
     }
