e0e8883f7bcf342d0f63a0425bf7a9bb9f349a4b
GlazerMann <kurt@ioiowa.com>
2025-12-22 09:00:46 -0600

    give s2 more time (#5156)

--- tests/phpunit/includes/api/S2apiTest.php
+++ tests/phpunit/includes/api/S2apiTest.php
@@ -13,19 +13,22 @@ final class S2apiTest extends testBaseClass {
     }
 
     public function testS2CIDlicenseFalse(): void {
-        sleep(2);
+        sleep(4);
         $this->assertFalse(get_semanticscholar_license('94502986'));
     }
 
     public function testS2CIDlicenseTrue(): void {
-        sleep(2);
+        sleep(4);
         $this->assertFalse(get_semanticscholar_license('52813129'));
-        sleep(2);
+    }
+
+    public function testS2CIDlicenseTrue2(): void {
+        sleep(4);
         $this->assertTrue(get_semanticscholar_license('73436496'));
     }
 
     public function testSemanticscholar2(): void {
-        sleep(2);
+        sleep(4);
         $text = '{{cite web|url=https://www.semanticscholar.org/paper/The-Holdridge-life-zones-of-the-conterminous-United-Lugo-Brown/406120529d907d0c7bf96125b83b930ba56f29e4}}';
         $template = $this->process_citation($text);
         $this->assertSame('10.1046/j.1365-2699.1999.00329.x', mb_strtolower($template->get('doi')));
@@ -36,7 +39,7 @@ final class S2apiTest extends testBaseClass {
     }
 
     public function testSemanticscholar3(): void {
-        sleep(2);
+        sleep(4);
         $text = '{{cite web|url=https://pdfs.semanticscholar.org/8805/b4d923bee9c9534373425de81a1ba296d461.pdf }}';
         $template = $this->process_citation($text);
         $this->assertSame('10.1007/978-3-540-78646-7_75', $template->get2('doi'));
@@ -47,7 +50,7 @@ final class S2apiTest extends testBaseClass {
     }
 
     public function testSemanticscholar4(): void { // s2cid does not match and ALL CAPS
-        sleep(2);
+        sleep(4);
         $text = '{{cite web|url=https://semanticscholar.org/paper/861fc89e94d8564adc670fbd35c48b2d2f487704|S2CID=XXXXXX}}';
         $template = $this->process_citation($text);
         $this->assertNull($template->get2('doi'));
@@ -58,7 +61,7 @@ final class S2apiTest extends testBaseClass {
     }
 
     public function testSemanticscholar41(): void { // s2cid does not match and ALL CAPS AND not cleaned up with initial tidy
-        sleep(2);
+        sleep(4);
         $text = '{{cite web|url=https://semanticscholar.org/paper/861fc89e94d8564adc670fbd35c48b2d2f487704|S2CID=XXXXXX}}';
         $template = $this->make_citation($text);
         $template->get_identifiers_from_url();
@@ -67,7 +70,7 @@ final class S2apiTest extends testBaseClass {
     }
 
     public function testSemanticscholar42(): void {
-        sleep(2);
+        sleep(4);
         $text = '{{cite web|url=https://semanticscholar.org/paper/861fc89e94d8564adc670fbd35c48b2d2f487704|pmc=32414}}'; // has a good free copy
         $template = $this->make_citation($text);
         $template->get_identifiers_from_url();
@@ -76,7 +79,7 @@ final class S2apiTest extends testBaseClass {
     }
 
     public function testSemanticscholar5(): void {
-        sleep(2);
+        sleep(4);
         $text = '{{cite web|s2cid=1090322}}';
         $template = $this->process_citation($text);
         $this->assertSame('10.1007/978-3-540-78646-7_75', $template->get2('doi'));
