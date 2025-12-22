9e3f8ba35f7e3e1e2174ece214b687c318dda6cd
GlazerMann <kurt@ioiowa.com>
2025-12-22 12:58:22 -0600

    Fix cookie files path to always be that same - web, github, CLI, etc (#5161)

--- src/includes/bot_curl.php
+++ src/includes/bot_curl.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+const COOKIE_FILE_PATH = __DIR__ . '/cookie.txt'; // Proquest needs
+
 function curl_limit_page_size(CurlHandle $_ch, int $_DE = 0, int $down = 0, int $_UE = 0, int $_Up = 0): int {
     // MOST things are sane, some things are stupidly large like S2 json data or archived PDFs
     // If $down exceeds max-size of 128MB, returning non-0 breaks the connection!
@@ -33,8 +35,8 @@ function bot_curl_init(float $time, array $ops): CurlHandle {
         CURLOPT_HEADEROPT => CURLHEADER_UNIFIED,
         CURLOPT_PROGRESSFUNCTION => 'curl_limit_page_size',
         CURLOPT_NOPROGRESS => "0",
-        CURLOPT_COOKIEJAR => 'cookie.txt', // Needed for proquest
-        CURLOPT_COOKIEFILE => 'cookie.txt', // Needed for proquest
+        CURLOPT_COOKIEJAR => COOKIE_FILE_PATH,
+        CURLOPT_COOKIEFILE => COOKIE_FILE_PATH,
         // 2 - Default Time by ratio
         CURLOPT_TIMEOUT => BOT_HTTP_TIMEOUT * $time,
         CURLOPT_CONNECTTIMEOUT => BOT_CONNECTION_TIMEOUT * $time,
