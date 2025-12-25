a2925571c2393ff85d6988b8c3f5c3871099ae71
GlazerMann <kurt@ioiowa.com>
2025-12-25 15:40:44 -0600

    spliut up issnTest.php

--- tests/phpunit/includes/api/issnTest.php
+++ tests/phpunit/includes/api/issnTest.php
@@ -4,19 +4,27 @@ declare(strict_types=1);
 require_once __DIR__ . '/../../../testBaseClass.php';
 
 final class issnTest extends testBaseClass {
-    public function testUse_ISSN(): void {
+    public function testUse_ISSN_1(): void {
         $prepared = $this->process_citation('{{cite news|issn=0140-0460 }}');
         $this->assertSame('[[The Times]]', $prepared->get2('newspaper'));
+    }
 
+    public function testUse_ISSN_2(): void {
         $prepared = $this->process_citation('{{cite news|issn=0190-8286 }}');
         $this->assertSame('[[The Washington Post]]', $prepared->get2('newspaper'));
+    }
 
+    public function testUse_ISSN_3(): void {
         $prepared = $this->process_citation('{{cite news|issn=0362-4331 }}');
         $this->assertSame('[[The New York Times]]', $prepared->get2('newspaper'));
+    }
 
+    public function testUse_ISSN_4(): void {
         $prepared = $this->process_citation('{{cite news|issn=0163-089X }}');
         $this->assertSame('[[The Wall Street Journal]]', $prepared->get2('newspaper'));
+    }
 
+    public function testUse_ISSN_5(): void {
         $prepared = $this->process_citation('{{cite news|issn=1092-0935 }}');
         $this->assertSame('[[The Wall Street Journal]]', $prepared->get2('newspaper'));
     }
