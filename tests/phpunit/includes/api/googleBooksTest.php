c59b83474da6ce5c0414f545ff01bf9d6f0bb5ef
GlazerMann <kurt@ioiowa.com>
2025-12-26 16:00:20 -0600

    Update and rename GoogleBooksTest.php to googleBooksTest.php

--- tests/phpunit/includes/api/googleBooksTest.php
+++ tests/phpunit/includes/api/googleBooksTest.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../../../testBaseClass.php';
 
-final class GoogleBooksTest extends testBaseClass {
+final class googleBooksTest extends testBaseClass {
     public function testGoogleBookNormalize0(): void {
         $pg = new TestPage(); // Fill page name with test name for debugging
         unset($pg);
