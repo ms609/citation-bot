3f8ba31d54cc4c2b97fecb0883598df357008387
GlazerMann <kurt@ioiowa.com>
2025-12-26 16:04:36 -0600

    Update zzzLastTest.php

--- tests/phpunit/includes/api/zzzLastTest.php
+++ tests/phpunit/includes/api/zzzLastTest.php
@@ -2,10 +2,10 @@
 declare(strict_types=1);
 
 /*
- * Final Test
+ * Final Test - thus burried deep in the test suite. Does not need to be last, but quite a ways in.
  */
 
-require_once __DIR__ . '/../testBaseClass.php';
+require_once __DIR__ . '../../../testBaseClass.php';
 
 final class zzzLastTest extends testBaseClass {
 
