42a5a596362fa5868809796def6912f761e103e8
GlazerMann <kurt@ioiowa.com>
2025-12-26 15:52:54 -0600

    Update and rename tests/phpunit/ZZZLastTest.php to tests/phpunit/includes/api/zzzLastTest.php

--- tests/phpunit/includes/api/zzzLastTest.php
+++ tests/phpunit/includes/api/zzzLastTest.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../testBaseClass.php';
 
-final class ZZZLastTest extends testBaseClass {
+final class zzzLastTest extends testBaseClass {
 
     public function testFlushCacheAtEnd(): void {
         $pg = new TestPage(); // Fill page name with test name for debugging
