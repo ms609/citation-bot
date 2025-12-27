48afc16483a69a968851e8d9f58c6236c4dff08f
GlazerMann <kurt@ioiowa.com>
2025-12-27 14:18:53 -0600

    Update ieeeTest.php

--- tests/phpunit/includes/api/ieeeTest.php
+++ tests/phpunit/includes/api/ieeeTest.php
@@ -9,7 +9,7 @@ final class ieeeTest extends testBaseClass {
         $url = "https://ieeexplore.ieee.org/document/4242344";
         $template = $this->process_citation('{{cite journal | url = ' . $url . ' }}');
         if ($template->get('doi') === "") {
-            sleep(5);
+            sleep(run_type_mods(10, 10, 10, 5, 5))
             $template = $this->process_citation('{{cite journal | url = ' . $url . ' }}');
         }
         $this->assertSame('10.1109/ISSCC.2007.373373', $template->get2('doi'));
