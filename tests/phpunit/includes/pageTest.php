ab9b0a74e8b8e0bfe5c07f6f8758f5f5652eb017
GlazerMann <kurt@ioiowa.com>
2025-12-27 17:57:57 -0600

    pubmed timout (#5273)

--- tests/phpunit/includes/pageTest.php
+++ tests/phpunit/includes/pageTest.php
@@ -133,6 +133,7 @@ final class pageTest extends testBaseClass {
     }
 
     public function testVancNames1(): void {
+        $this->sleep_pubmed();
         $page = $this->process_page('{|}{{cs1 config|name-list-style=vanc}}<!-- -->{{{|}}}{{cite journal | title = Food Ingredients That Inhibit Cholesterol Absorption | journal = Preventive Nutrition and Food Science | volume = | issue =  | pages = 67–80 | date = June 2017 | pmid = 28702423 | pmc = 5503415 | doi = <!-- --> | vauthors = }}');
         $this->assertSame(     '{|}{{cs1 config|name-list-style=vanc}}<!-- -->{{{|}}}{{cite journal | title = Food Ingredients That Inhibit Cholesterol Absorption | journal = Preventive Nutrition and Food Science | volume = 22| issue =  2| pages = 67–80 | date = June 2017 | pmid = 28702423 | pmc = 5503415 | doi = <!-- --> | vauthors = Jesch ED, Carr TP }}', $page->parsed_text());
         $this->assertSame('Alter: volume, issue. Add: authors 1-2. Removed parameters. | [[:en:WP:UCB|Use this bot]]. [[:en:WP:DBUG|Report bugs]]. ', $page->edit_summary());
