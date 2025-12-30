9210ce629ec51fe343fb79534d066d399c42e833
GlazerMann <kurt@ioiowa.com>
2025-12-30 13:55:49 -0600

    Update piiAPITest.php

--- tests/phpunit/includes/api/piiAPITest.php
+++ tests/phpunit/includes/api/piiAPITest.php
@@ -6,8 +6,7 @@ require_once __DIR__ . '/../../../testBaseClass.php';
 final class piiAPITest extends testBaseClass {
 
     public function testPII(): void {
-        $pg = new TestPage(); // Fill page name with test name for debugging
-        unset($pg);
+        new TestPage(); // Fill page name with test name for debugging
         $pii = 'S0960076019302699';
         $doi_expect = '10.1016/j.jsbmb.2019.105494';
         $doi = get_doi_from_pii($pii);
