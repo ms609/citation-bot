8afb4cb06d612a30a337c1555e6a55afbbe2877b
GlazerMann <kurt@ioiowa.com>
2025-12-30 13:53:16 -0600

    Update parameterTest.php

--- tests/phpunit/includes/parameterTest.php
+++ tests/phpunit/includes/parameterTest.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/../../testBaseClass.php';
 final class parameterTest extends testBaseClass {
 
     public function testValueWithPipeAndTrailingNewline(): void {
-        TestPage(); // Fill page name with test name for debugging
+        new TestPage(); // Fill page name with test name for debugging
         $text = "last1 = [[:en:Bigwig# # # CITATION_BOT_PLACEHOLDER_PIPE # # #SomeoneFamous]]\n";
         $parameter = $this->parameter_parse_text_helper($text);
         $this->assertSame('', $parameter->pre);
