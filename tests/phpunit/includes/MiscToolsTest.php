27686dc70159487e72328fc55f6076d0a62b2393
GlazerMann <kurt@ioiowa.com>
2025-12-30 14:38:17 -0600

    Update MiscToolsTest.php

--- tests/phpunit/includes/MiscToolsTest.php
+++ tests/phpunit/includes/MiscToolsTest.php
@@ -5,8 +5,7 @@ require_once __DIR__ . '/../../testBaseClass.php';
 
 final class MiscToolsTest extends testBaseClass {
     public function testcheck_memory_usage(): void {
-        $pg = new TestPage(); // Fill page name with test name for debugging
-        unset($pg);
+        new TestPage(); // Fill page name with test name for debugging
         check_memory_usage('testcheck_memory_usage');
         $this->assertFaker();
     }
