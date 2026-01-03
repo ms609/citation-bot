7a173e6f0b056ad2c8ffe39f8b420e75b298a330
GlazerMann <kurt@ioiowa.com>
2026-01-03 17:51:14 -0600

    rel="noopener noreferrer"

--- src/includes/setup.php
+++ src/includes/setup.php
@@ -162,7 +162,7 @@ function check_blocked(): void {
         if (defined('EDIT_AS_USER')) {
             echo '</pre><div style="text-align:center"><h1>Citation Bot is currently blocked because of a malfunction or disagreement about its edits - so BE CAREFUL.</h1></div><pre>';
         } else {
-            echo '</pre><div style="text-align:center"><h1>Citation Bot is currently blocked because of a malfunction or disagreement about its edits.</h1><br/><h1>Alternatively, the bot has not been fully enabled on this wiki yet.</h1><h2><a href="https://en.wikipedia.org/wiki/User_talk:Citation_bot" title="Join the discussion" target="_blank"  aria-label="Join the discussion (opens a new window)">Please join in the discussion</a></h2></div><footer><a href="./" title="Use Citation Bot again"> Edit another page</a>?</footer></body></html>';
+            echo '</pre><div style="text-align:center"><h1>Citation Bot is currently blocked because of a malfunction or disagreement about its edits.</h1><br/><h1>Alternatively, the bot has not been fully enabled on this wiki yet.</h1><h2><a href="https://en.wikipedia.org/wiki/User_talk:Citation_bot" title="Join the discussion" target="_blank" rel="noopener noreferrer" aria-label="Join the discussion (opens a new window)">Please join in the discussion</a></h2></div><footer><a href="./" title="Use Citation Bot again"> Edit another page</a>?</footer></body></html>';
             exit;
         }
     }
