ab5ff3bb8f3af376af021ba18adb796ec76ab8da
redalert2fan <42489779+redalert2fan@users.noreply.github.com>
2026-01-10 20:48:14 +0100

    Update parameters.php

--- src/includes/constants/parameters.php
+++ src/includes/constants/parameters.php
@@ -435,7 +435,7 @@ const CITE_BOOK_UNSUPPORTED_PARAMS = [
     'magazine',
 ];
 
-/** Parameters allowed by cite bioRxiv template
+/** Parameters allowed by cite bioRxiv template per Wikipedia documentation at https://en.wikipedia.org/wiki/Template:Cite_bioRxiv */
 const CITE_BIORXIV_ALLOWED_PARAMS = [
     'biorxiv',
     'title',
