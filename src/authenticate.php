add7bbcf345eeda8db41ebef217e79c2326982e3
GlazerMann <kurt@ioiowa.com>
2026-01-14 19:32:16 -0600

    All html as UTF-8, even errors etc (#5379)

--- src/authenticate.php
+++ src/authenticate.php
@@ -11,7 +11,7 @@ use MediaWiki\OAuthClient\Token;
 /** The two ways we leave this script */
 function death_time(string $err): never {
     unset($_SESSION['access_key'], $_SESSION['access_secret'], $_SESSION['citation_bot_user_id'], $_SESSION['request_key'], $_SESSION['request_secret']);
-    echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>Authentication System Failure</title></head><body><main>', $err, '</main></body></html>';
+    echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><meta name="viewport" content="width=device-width, initial-scale=1.0" /><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>Authentication System Failure</title></head><body><main>', $err, '</main></body></html>';
     exit(0);
 }
 
