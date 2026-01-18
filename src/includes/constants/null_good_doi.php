a9ce9443363fc872d9cf461e5d26e04c2f00e80d
GlazerMann <kurt@ioiowa.com>
2026-01-18 14:56:51 -0600

    Update null_good_doi.php

--- src/includes/constants/null_good_doi.php
+++ src/includes/constants/null_good_doi.php
@@ -6,6 +6,9 @@ declare(strict_types=1);
  * Note that a failed url on one computer does not mean it is gone, it might just be you. You can use hdl.handle.org to show you the final url - russia and other countries firewall. These need to be manually double checked once in a great while. We cannot add hdls to this list, since we need the final URL
  */
 const NULL_DOI_BUT_GOOD = [
+    '10.18632/aging.100521' => true,
+    '10.18632/aging.100920' => true,
+    '10.18632/aging.101549' => true,
     '10.1000/100' => true,
     '10.1001/archderm.134.11.1484' => true,
     '10.1001/archderm.134.11.1487' => true,
