4ae003489f78920d67690c5f9c2d68eef2eb7150
GlazerMann <kurt@ioiowa.com>
2026-01-31 17:11:19 -0600

    Update WikiThings.php

--- src/includes/WikiThings.php
+++ src/includes/WikiThings.php
@@ -9,8 +9,8 @@ declare(strict_types=1);
 // Do not ever remove CITATION_BOT_PLACEHOLDER from strings, there are lots of REGEX and stripos() calls in the code
 
 abstract class WikiThings {
-    public const TREAT_IDENTICAL_SEPARATELY = false;  // The contents of theses items never get edited, so this is safe
-    private string $rawtext; // Uninitialized.  Will crash if read before set; which is good.
+    public const bool TREAT_IDENTICAL_SEPARATELY = false;  // The contents of theses items never get edited, so this is safe
+    private string|null $rawtext; // Uninitialized.  Will crash if read before set; which is good.
 
     public function parse_text(string $text): void {
         $this->rawtext = $text;
@@ -22,41 +22,41 @@ abstract class WikiThings {
 }
 
 final class Comment extends WikiThings {
-    public const PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_COMMENT %s # # #';
-    public const REGEXP = ['~<!--[^\<\>\-]*?-->~us', '~<!--[\s\S]*?-->~us'];
+    public const string PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_COMMENT %s # # #';
+    public const string REGEXP = ['~<!--[^\<\>\-]*?-->~us', '~<!--[\s\S]*?-->~us'];
 }
 
 final class Nowiki extends WikiThings {
-    public const PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_NOWIKI %s # # #';
-    public const REGEXP = ['~<nowiki>[^\<\>]*?</nowiki>~us', '~<nowiki>[\s\S]*?</nowiki>~us'];
+    public const string PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_NOWIKI %s # # #';
+    public const string REGEXP = ['~<nowiki>[^\<\>]*?</nowiki>~us', '~<nowiki>[\s\S]*?</nowiki>~us'];
 }
 
 final class Chemistry extends WikiThings {
-    public const PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_CHEMISTRY %s # # #';
-    public const REGEXP = ['~<chem>[\s\S]*?</chem>~us'];
+    public const string PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_CHEMISTRY %s # # #';
+    public const string REGEXP = ['~<chem>[\s\S]*?</chem>~us'];
 }
 
 final class Mathematics extends WikiThings {
-    public const PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_MATHEMATICS %s # # #';
-    public const REGEXP = ['~<math(?:| chem)(?:| display=.inline.|display=.block.)>[\s\S]*?</math>~us'];
+    public const string PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_MATHEMATICS %s # # #';
+    public const string REGEXP = ['~<math(?:| chem)(?:| display=.inline.|display=.block.)>[\s\S]*?</math>~us'];
 }
 
 final class Musicscores extends WikiThings {
-    public const PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_MUSIC %s # # #';
-    public const REGEXP = ['~<score>[\s\S]*?</score>~us'];
+    public const string PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_MUSIC %s # # #';
+    public const string REGEXP = ['~<score>[\s\S]*?</score>~us'];
 }
 
 final class Preformated extends WikiThings {
-    public const PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_PREFORMAT %s # # #';
-    public const REGEXP = ['~<pre>[\s\S]*?</pre>~us'];
+    public const string PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_PREFORMAT %s # # #';
+    public const string REGEXP = ['~<pre>[\s\S]*?</pre>~us'];
 }
 
 final class SingleBracket extends WikiThings {
-    public const PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_SINGLE_BRACKET %s # # #';
-    public const REGEXP = ['~(?<!\{)\{[^\{\}]+\}(?!\})~us'];
+    public const string PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_SINGLE_BRACKET %s # # #';
+    public const string REGEXP = ['~(?<!\{)\{[^\{\}]+\}(?!\})~us'];
 }
 
 final class TripleBracket extends WikiThings {
-    public const PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_TRIPLE_BRACKET %s # # #';
-    public const REGEXP = ['~(?<!\{)\{\{\{[^\{\}]+\}\}\}(?!\})~us'];
+    public const string PLACEHOLDER_TEXT = '# # # CITATION_BOT_PLACEHOLDER_TRIPLE_BRACKET %s # # #';
+    public const string REGEXP = ['~(?<!\{)\{\{\{[^\{\}]+\}\}\}(?!\})~us'];
 }
