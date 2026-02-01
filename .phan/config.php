1ed977c043a0462b63520f2fba3e02877996b8bf
GlazerMann <kurt@ioiowa.com>
2026-01-31 18:39:29 -0600

    Update config.php

--- .phan/config.php
+++ .phan/config.php
@@ -16,6 +16,6 @@ return [
         './vendor'
     ],
     'directory_list' => [
-        './src/includes/constants', './src/includes/api', './src/includes', './src', './vendor/mediawiki', './tests'
+        './vendor/mediawiki', './vendor/phpunit', './src/includes/constants', './src/includes/api', './src/includes', './src', './tests'
     ],
 ];
