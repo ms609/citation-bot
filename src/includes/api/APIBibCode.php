df4ab49f9cf5fcc07064b02d01bbbf6af7280a04
GlazerMann <kurt@ioiowa.com>
2026-02-07 07:26:33 -0600

    Update APIBibCode.php

--- src/includes/api/APIBibCode.php
+++ src/includes/api/APIBibCode.php
@@ -497,7 +497,7 @@ function Bibcode_Response_Processing(array $curl_opts, string $adsabs_url): obje
         $header_length = (int) curl_getinfo($ch, CURLINFO_HEADER_SIZE);
         if ($http_response_code === 0 || $header_length === 0) {
             throw new Exception('Size of zero from AdsAbs website'); // @codeCoverageIgnore
-        }   
+        }
         $header = mb_substr($return, 0, $header_length, '8bit');  // These are byte counts, not character counts
         $body = mb_substr($return, $header_length, '8bit');       // These are byte counts, not character counts
         unset($return);
