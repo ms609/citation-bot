21f9c334d590ead296e6252c0f376bed6029c99e
GlazerMann <kurt@ioiowa.com>
2026-02-07 07:31:46 -0600

    Update doiTools.php

--- src/includes/doiTools.php
+++ src/includes/doiTools.php
@@ -139,7 +139,7 @@ function is_doi_active(string $doi): ?bool {
     $return = bot_curl_exec($ch);
     $header_length = (int) curl_getinfo($ch, CURLINFO_HEADER_SIZE);
     $header = mb_substr($return, 0, $header_length, '8bit');  // Byte count, not characters
-    $body = mb_substr($return, $header_length, '8bit');  // Byte count, not characters
+    $body = mb_substr($return, $header_length, null, '8bit'); // Byte count, not characters
     $response_code = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
     if ($header === "" || ($response_code === 503) || ($response_code === 429)) {
         sleep(4);                                                             // @codeCoverageIgnoreStart
@@ -150,7 +150,7 @@ function is_doi_active(string $doi): ?bool {
         $response_code = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
         $header_length = (int) curl_getinfo($ch, CURLINFO_HEADER_SIZE);
         $header = mb_substr($return, 0, $header_length, '8bit');  // Byte count, not characters
-        $body = mb_substr($return, $header_length, '8bit');  // Byte count, not characters
+        $body = mb_substr($return, $header_length, null, '8bit'); // Byte count, not characters
     }                                                                       // @codeCoverageIgnoreEnd
     if ($response_code === 429) {  // WE are still getting blocked
         sleep(run_type_mods(10, 2, 2, 1, 2));   // @codeCoverageIgnore
