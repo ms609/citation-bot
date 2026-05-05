ebeabe33c867213a61e842bdf0729af205348182
redalert2fan <42489779+redalert2fan@users.noreply.github.com>
2026-05-05 15:30:53 +0200

    Skip testLatexMathInTitle on arXiv API null response (rate limiting) (#5560)

--- tests/phpunit/includes/TemplatePart3Test.php
+++ tests/phpunit/includes/TemplatePart3Test.php
@@ -707,6 +707,9 @@ EP - 999 }}';
         $text = "{{Cite arxiv|eprint=1801.03103}}";
         $expanded = $this->process_citation($text);
         $title = $expanded->get2('title');
+        if ($title === null) {
+            $this->markTestSkipped('arXiv API did not respond (rate limit or outage)');
+        }
         // For some reason we sometimes get the first one - probably just ARXIV
         $title1 = 'A Candidate $z\sim10$ Galaxy Strongly Lensed into a Spatially Resolved Arc';
         $title2 = "RELICS: A Candidate ''z'' ∼ 10 Galaxy Strongly Lensed into a Spatially Resolved Arc";
