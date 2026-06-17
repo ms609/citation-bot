d0da39f29b625114f79bf476f158d0b263dc6cd6
redalert2fan <42489779+redalert2fan@users.noreply.github.com>
2026-06-17 09:59:39 +0200

    Normalize non-breaking hyphen (U+2011) to regular hyphen in tidy_parameter

--- src/includes/Template.php
+++ src/includes/Template.php
@@ -3625,6 +3625,8 @@ final class Template
                 $this->set($param, safe_preg_replace('~^\=+\s*(?![^a-zA-Z0-9\[\'\"])~u', '', $this->get($param))); // Remove leading ='s sign if in front of letter or number
                 $this->set($param, safe_preg_replace('~&#x2010;~u', '-', $this->get($param)));
                 $this->set($param, safe_preg_replace('~\x{2010}~u', '-', $this->get($param)));
+                $this->set($param, safe_preg_replace('~&#x2011;~u', '-', $this->get($param)));
+                $this->set($param, safe_preg_replace('~\x{2011}~u', '-', $this->get($param)));
                 $this->set($param, safe_preg_replace('~&#x2013;~u', '&ndash;', $this->get($param)));
                 $this->set($param, safe_preg_replace('~&#x2014;~u', '&mdash;', $this->get($param)));
                 $this->set($param, safe_preg_replace('~&#x00026;~u', '&', $this->get($param)));
