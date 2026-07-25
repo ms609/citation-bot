b364aa04a4d4cb4832f3d3ebf085d87d48334395
GlazerMann <kurt@ioiowa.com>
2026-07-25 13:19:36 -0500

    curl_close does nothing in PHP 8 (#5683)

--- tests/phpunit/includes/BotCurlTest.php
+++ tests/phpunit/includes/BotCurlTest.php
@@ -14,14 +14,12 @@ final class BotCurlTest extends testBaseClass {
         $ch = curl_init();
         $this->assertNotFalse($ch);
         $this->assertSame(0, curl_limit_page_size($ch, 0, 0, 0, 0));
-        curl_close($ch);
     }
 
     public function testCurlLimitPageSizeSmallPayload(): void {
         $ch = curl_init();
         $this->assertNotFalse($ch);
         $this->assertSame(0, curl_limit_page_size($ch, 0, 1000, 0, 0));
-        curl_close($ch);
     }
 
     public function testCurlLimitPageSizeAtExactLimit(): void {
@@ -29,44 +27,37 @@ final class BotCurlTest extends testBaseClass {
         $ch = curl_init();
         $this->assertNotFalse($ch);
         $this->assertSame(0, curl_limit_page_size($ch, 0, 134217728, 0, 0));
-        curl_close($ch);
     }
 
     public function testCurlLimitPageSizeOneByteOverLimit(): void {
         $ch = curl_init();
         $this->assertNotFalse($ch);
         $this->assertSame(1, curl_limit_page_size($ch, 0, 134217729, 0, 0));
-        curl_close($ch);
     }
 
     public function testCurlLimitPageSizeLargePayload(): void {
         $ch = curl_init();
         $this->assertNotFalse($ch);
         $this->assertSame(1, curl_limit_page_size($ch, 0, 500000000, 0, 0));
-        curl_close($ch);
     }
 
     public function testBotCurlInitReturnsCurlHandle(): void {
         $ch = bot_curl_init(1.0, []);
         $this->assertInstanceOf(CurlHandle::class, $ch);
-        curl_close($ch);
     }
 
     public function testBotCurlInitWithUrl(): void {
         $ch = bot_curl_init(1.0, [CURLOPT_URL => 'http://example.com']);
         $this->assertInstanceOf(CurlHandle::class, $ch);
-        curl_close($ch);
     }
 
     public function testBotCurlInitWithHalfTimeScale(): void {
         $ch = bot_curl_init(0.5, []);
         $this->assertInstanceOf(CurlHandle::class, $ch);
-        curl_close($ch);
     }
 
     public function testBotCurlInitWithZeroTimeScale(): void {
         $ch = bot_curl_init(0.0, []);
         $this->assertInstanceOf(CurlHandle::class, $ch);
-        curl_close($ch);
     }
 }
