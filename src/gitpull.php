55abeb6163317c5bfe1b7f6666581c8e846c31d0
GlazerMann <kurt@ioiowa.com>
2026-07-26 20:21:28 -0500

    call getenv only once

--- src/gitpull.php
+++ src/gitpull.php
@@ -11,7 +11,9 @@ const LOCK_DIR = 'git_pull.lock';
 
 clearstatcache(true, LOCK_DIR);
 
-if (($_GET['password'] ?? '') !== (string) @getenv('DEPLOY_PASSWORD') && @getenv('DEPLOY_PASSWORD') !== false ) {
+$deployPassword = @getenv('DEPLOY_PASSWORD');
+
+if ($deployPassword !== false && ($_GET['password'] ?? '') !== (string) $deployPassword) {
     $git_hub = 'Incorrect password. Please add ?password=YOUR_PASSWORD to the URL. You can set the password in your env.php file (DEPLOY_PASSWORD).';
 } elseif (@mkdir(LOCK_DIR, 0700)) {
     /** @psalm-suppress ForbiddenCode */
