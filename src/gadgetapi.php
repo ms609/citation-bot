2db3f38a86329c4cd4a62a531aaf3a492e1e8520
GlazerMann <kurt@ioiowa.com>
2026-07-27 07:35:41 -0500

    Enhance origin validation and CORS header handling

--- src/gadgetapi.php
+++ src/gadgetapi.php
@@ -10,13 +10,13 @@ try {
     require_once __DIR__ . '/includes/setup.php';
 
     $origin = mb_strtolower((string) @$_SERVER['HTTP_ORIGIN']);
-    if (!CI && $origin !== 'https://mdwiki.org' && !str_ends_with($origin, '.wikipedia.org')) {
+    if ((!CI && $origin !== 'https://mdwiki.org' && !str_ends_with($origin, '.wikipedia.org')) || (preg_match('~[\;\,\s]~', $origin))) {
         throw new Exception('not a wiki');    // @codeCoverageIgnore
     }
-    unset($origin);
 
-    @header('Access-Control-Allow-Origin: *'); // Needed for gadget to work right
+    @header('Access-Control-Allow-Origin: ' . $origin);
     @header('Content-Type: application/json');
+    unset($origin);
 
     if (!is_string(@$_POST['text']) || !is_string(@$_POST['summary'])) {
         throw new Exception('not a string');    // @codeCoverageIgnore
