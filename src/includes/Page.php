797061f4a6dc15afc04a9f2843cb1e4e4753cef0
GlazerMann <kurt@ioiowa.com>
2026-08-17 13:47:58 -0500

    Enhance name-list-style conditions in Page.php

--- src/includes/Page.php
+++ src/includes/Page.php
@@ -1037,11 +1037,11 @@ class Page {
         $pattern = '/{{\s*?cs1\s*?config[^}]*?name-list-style\s*?=\s*?(\w+)\b[^}]*?}}/im';
         if (preg_match($pattern, $this->text, $matches)) {
             $s = mb_strtolower($matches[1]); // We ONLY deal with first one
-            if ($s === 'default' || $s === 'none') {
+            if ($s === 'default' || $s === 'none' || $s === 'serial' || $s === 'and' ) {
                 $name_list_style = VancStyle::NAME_LIST_STYLE_DEFAULT;
             } elseif ($s === 'vanc') {
                 $name_list_style = VancStyle::NAME_LIST_STYLE_VANC;
-            } elseif ($s === 'amp') {
+            } elseif ($s === 'amp' || $s === '&' || $s === 'ampersand') {
                 $name_list_style = VancStyle::NAME_LIST_STYLE_AMP;
             } elseif ($s !== '') {
                 bot_debug_log('Weird name-list-style found: ' . echoable($s));
