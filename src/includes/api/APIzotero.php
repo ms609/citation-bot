7aa5e96e75afe40638608e2cfec0fd39a320cc09
GlazerMann <kurt@ioiowa.com>
2026-08-22 13:42:31 -0500

    Fix URL encoding for Zotero API bug
    https://phabricator.wikimedia.org/T413651
    
    https://github.com/ms609/citation-bot/issues/5326

--- src/includes/api/APIzotero.php
+++ src/includes/api/APIzotero.php
@@ -191,6 +191,8 @@ final class Zotero {
             }
         }
 
+        /** Deal with https://phabricator.wikimedia.org/T413651 bug in zotero */
+        $url=str_replace("-", "%2D", $url);
         /** @psalm-taint-escape ssrf */
         $the_url = CITOID_ZOTERO . urlencode($url);
         curl_setopt(self::$zotero_ch, CURLOPT_URL, $the_url);
