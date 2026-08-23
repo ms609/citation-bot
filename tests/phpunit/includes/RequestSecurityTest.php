673e70f22a30d26cfa3c5e6eac05d6baea72d2f0
GlazerMann <kurt@ioiowa.com>
2026-08-22 20:32:44 -0500

    Update RequestSecurityTest.php

--- tests/phpunit/includes/RequestSecurityTest.php
+++ tests/phpunit/includes/RequestSecurityTest.php
@@ -159,6 +159,7 @@ final class RequestSecurityTest extends PHPUnit\Framework\TestCase {
         $forms = $xpath->query('//form[@method="post"]');
         $this->assertNotFalse($forms);
         $this->assertCount(1, $forms);
+        /** @var DOMElement $posted_from */
         $posted_form = $forms->item(0);
         $this->assertInstanceOf(DOMElement::class, $posted_form);
         $this->assertSame($action, $posted_form->getAttribute('action'));
