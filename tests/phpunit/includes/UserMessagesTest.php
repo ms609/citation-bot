1fe078d3960fcb55ab4feacadc9fd20b3142be39
GlazerMann <kurt@ioiowa.com>
2026-08-23 16:01:47 -0500

    Rename test methods to include '2' suffix

--- tests/phpunit/includes/UserMessagesTest.php
+++ tests/phpunit/includes/UserMessagesTest.php
@@ -116,49 +116,49 @@ final class UserMessagesTest extends testBaseClass {
         $this->assertSame('', $output);
     }
 
-    public function testReportPhaseRunsWithoutError(): void {
+    public function testReportPhaseRunsWithoutError2(): void {
         ob_start();
         report_phase('test phase');
         $this->assertSame('', (string) ob_get_clean());
     }
 
-    public function testReportActionRunsWithoutError(): void {
+    public function testReportActionRunsWithoutError2(): void {
         ob_start();
         report_action('test action');
         $this->assertSame('', (string) ob_get_clean());
     }
 
-    public function testReportInfoRunsWithoutError(): void {
+    public function testReportInfoRunsWithoutError2(): void {
         ob_start();
         report_info('test info');
         $this->assertSame('', (string) ob_get_clean());
     }
 
-    public function testReportInactionRunsWithoutError(): void {
+    public function testReportInactionRunsWithoutError2(): void {
         ob_start();
         report_inaction('test inaction');
         $this->assertSame('', (string) ob_get_clean());
     }
 
-    public function testReportWarningRunsWithoutError(): void {
+    public function testReportWarningRunsWithoutError2(): void {
         ob_start();
         report_warning('test warning');
         $this->assertSame('', (string) ob_get_clean());
     }
 
-    public function testReportModificationRunsWithoutError(): void {
+    public function testReportModificationRunsWithoutError2(): void {
         ob_start();
         report_modification('test modification');
         $this->assertSame('', (string) ob_get_clean());
     }
 
-    public function testReportAddRunsWithoutError(): void {
+    public function testReportAddRunsWithoutError2(): void {
         ob_start();
         report_add('test add');
         $this->assertSame('', (string) ob_get_clean());
     }
 
-    public function testReportForgetRunsWithoutError(): void {
+    public function testReportForgetRunsWithoutError2(): void {
         ob_start();
         report_forget('test forget');
         $this->assertSame('', (string) ob_get_clean());
