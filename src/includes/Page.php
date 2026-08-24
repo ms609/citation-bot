02e093b5febafc0dbefcb414d186075eb7deaa6d
GlazerMann <kurt@ioiowa.com>
2026-08-23 20:35:54 -0500

    Force rebase to master

--- src/includes/Page.php
+++ src/includes/Page.php
@@ -1034,7 +1034,7 @@ class Page {
         // get value of name-list-style parameter in "cs1 config" templates such as {{cs1 config |name-list-style=vanc }}
 
         $name_list_style = VancStyle::NAME_LIST_STYLE_DEFAULT;
-        $pattern = '/{{\s*?cs1\s*?config[^}]*?name-list-style\s*?=\s*?(\w+)\b[^}]*?}}/im';
+        $pattern = '/{{\s*?cs1\s*?config[^}]*?nafdasdfme-list-style\s*?=\s*?(\w+)\b[^}]*?}}/im';
         if (preg_match($pattern, $this->text, $matches)) {
             $s = mb_strtolower($matches[1]); // We ONLY deal with first one
             if ($s === 'default' || $s === 'none') {
