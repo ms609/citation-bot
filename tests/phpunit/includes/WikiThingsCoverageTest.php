ca213eaea0e437ec1a457371da88916fa3f19d3b
GlazerMann <kurt@ioiowa.com>
2026-08-24 12:57:59 -0500

    Suppress PhanParamSuspiciousOrder warnings in tests

--- tests/phpunit/includes/WikiThingsCoverageTest.php
+++ tests/phpunit/includes/WikiThingsCoverageTest.php
@@ -23,79 +23,97 @@ final class WikiThingsCoverageTest extends testBaseClass {
         $this->assertSame('# # # CITATION_BOT_PLACEHOLDER_TRIPLE_BRACKET %s # # #', TripleBracket::PLACEHOLDER_TEXT);
     }
 
-    /** @phan-suppress-begin PhanParamSuspiciousOrder */
-
     public function testCommentFastRegexpMatchesSimpleComment(): void {
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(Comment::REGEXP[0], '<!-- simple comment -->'));
     }
 
     public function testCommentFallbackRegexpMatchesMultilineComment(): void {
         $text = "<!-- first line\nsecond <tag> line -->";
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(Comment::REGEXP[1], $text));
     }
 
     public function testCommentRegexpDoesNotMatchUnclosedComment(): void {
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(0, preg_match(Comment::REGEXP[1], '<!-- unfinished'));
     }
 
     public function testNowikiFastRegexpMatchesSimpleContent(): void {
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(Nowiki::REGEXP[0], '<nowiki>{{cite web}}</nowiki>'));
     }
 
     public function testNowikiFallbackRegexpMatchesNestedAngleBrackets(): void {
         $text = '<nowiki><ref>{{cite web}}</ref></nowiki>';
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(Nowiki::REGEXP[1], $text));
     }
 
     public function testChemistryRegexpMatchesMultilineContent(): void {
         $text = "<chem>H2O +\nCO2</chem>";
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(Chemistry::REGEXP[0], $text));
     }
 
     public function testMathematicsRegexpMatchesPlainMath(): void {
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(Mathematics::REGEXP[0], '<math>x^2</math>'));
     }
 
     public function testMathematicsRegexpMatchesMathChemVariant(): void {
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(Mathematics::REGEXP[0], '<math chem>H_2O</math>'));
     }
 
     public function testMathematicsRegexpMatchesInlineDisplayAttribute(): void {
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(Mathematics::REGEXP[0], '<math display="inline">x</math>'));
     }
 
     public function testMathematicsRegexpMatchesBlockDisplayAttribute(): void {
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(Mathematics::REGEXP[0], '<math display="block">x</math>'));
     }
 
     public function testMusicScoreRegexpMatchesContent(): void {
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(Musicscores::REGEXP[0], '<score>c d e f</score>'));
     }
 
     public function testPreformattedRegexpMatchesMultilineContent(): void {
         $text = "<pre>line 1\n{{cite journal}}\nline 3</pre>";
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(Preformated::REGEXP[0], $text));
     }
 
     public function testSyntaxHighlightRegexpIsCaseInsensitive(): void {
         $text = '<SyntaxHighlight lang="php"><?php echo 1; ?></SyntaxHighlight>';
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(SyntaxHighlight::REGEXP[0], $text));
     }
 
     public function testSyntaxHighlightRegexpAcceptsAttributes(): void {
         $text = '<syntaxhighlight lang="php" line>echo "x";</syntaxhighlight>';
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(SyntaxHighlight::REGEXP[0], $text));
     }
 
     public function testSingleBracketRegexpMatchesExactlyOneBracePair(): void {
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(SingleBracket::REGEXP[0], '{value}'));
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(0, preg_match(SingleBracket::REGEXP[0], '{{value}}'));
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(0, preg_match(SingleBracket::REGEXP[0], '{{{value}}}'));
     }
 
     public function testTripleBracketRegexpMatchesExactlyThreeBracePairs(): void {
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(1, preg_match(TripleBracket::REGEXP[0], '{{{value}}}'));
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(0, preg_match(TripleBracket::REGEXP[0], '{{value}}'));
+        // @phan-suppress-next-line PhanParamSuspiciousOrder
         $this->assertSame(0, preg_match(TripleBracket::REGEXP[0], '{{{{value}}}}'));
     }
 
@@ -104,8 +122,4 @@ final class WikiThingsCoverageTest extends testBaseClass {
         $this->assertFalse(Nowiki::TREAT_IDENTICAL_SEPARATELY);
         $this->assertFalse(Mathematics::TREAT_IDENTICAL_SEPARATELY);
     }
-
-    /** @phan-suppress-end PhanParamSuspiciousOrder */
-
 }
-
