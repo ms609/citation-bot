dbd72179f98db70539ba12b8a24ffab155d672c9
GlazerMann <kurt@ioiowa.com>
2026-08-29 10:19:15 -0500

    Update LOCK_DIR to use __DIR__ for path

--- src/gitpull.php
+++ src/gitpull.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/includes/PublicConfig.php';
 enforce_public_request_configuration(is_string($_SERVER['HTTP_HOST'] ?? null) ? $_SERVER['HTTP_HOST'] : null);
 send_configured_cors_header(is_string($_SERVER['HTTP_ORIGIN'] ?? null) ? $_SERVER['HTTP_ORIGIN'] : null);
 
-const LOCK_DIR = 'git_pull.lock';
+const LOCK_DIR = __DIR__ . '/git_pull.lock';
 
 clearstatcache(true, LOCK_DIR);
 
