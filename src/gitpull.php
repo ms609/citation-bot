190e0e4558ab127fc3a6d586375e1aa027232d99
GlazerMann <kurt@ioiowa.com>
2026-08-30 08:29:48 -0500

    Do not work, if password is not set

--- src/gitpull.php
+++ src/gitpull.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+// We minimize include files so that this works even if we break the deployment
+
 /** @psalm-suppress MissingFile */
 require_once __DIR__ . '/env.php';
 
@@ -9,6 +11,7 @@ require_once __DIR__ . '/includes/PublicConfig.php';
 
 enforce_public_request_configuration(is_string($_SERVER['HTTP_HOST'] ?? null) ? $_SERVER['HTTP_HOST'] : null);
 send_configured_cors_header(is_string($_SERVER['HTTP_ORIGIN'] ?? null) ? $_SERVER['HTTP_ORIGIN'] : null);
+@header('Cache-Control: no-store');
 
 const LOCK_DIR = __DIR__ . '/git_pull.lock';
 
@@ -20,12 +23,15 @@ $password_in = $_GET['password'] ?? '';
 
 if (!is_string($password_in)) {
     $git_hub = 'Invalid password type.';
+} elseif ($deployPassword === false) {
+    $git_hub = 'Error: No DEPLOY_PASSWORD is configured.'
 } elseif ($deployPassword !== false && !hash_equals($password_in, (string) $deployPassword)) {
     $git_hub = 'Incorrect password. Please add ?password=YOUR_PASSWORD to the URL. You can set the password in your env.php file (DEPLOY_PASSWORD).';
 } elseif (@mkdir(LOCK_DIR, 0700)) {
     register_shutdown_function(static function (): void {
         if (is_dir(LOCK_DIR)) {
             @rmdir(LOCK_DIR);
+            clearstatcache(true, LOCK_DIR);
         }
     });
     try {
@@ -37,9 +43,6 @@ if (!is_string($password_in)) {
     } finally {
         @rmdir(LOCK_DIR);
         clearstatcache(true, LOCK_DIR);
-    }
-    if ($deployPassword === false) {
-        $git_hub = $git_hub . '\nWarning: No DEPLOY_PASSWORD was required.';
     }
 } else {
     $git_hub = "Please try again - lock file found";
