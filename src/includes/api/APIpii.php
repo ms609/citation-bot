eb9531a7a7512bd2784fb5208c37862f9d7124db
GlazerMann <kurt@ioiowa.com>
2026-08-30 10:09:04 -0500

    Refactor bot_curl_init to include max response bytes

--- src/includes/api/APIpii.php
+++ src/includes/api/APIpii.php
@@ -13,8 +13,7 @@ function get_doi_from_pii(string $pii): string {
     static $ch_pii;
     if ($ch_pii === null) {
         $time = (float) run_type_mods(1, 3, 3, 3, 3);
-        $ch_pii = bot_curl_init($time, []);
-        bot_curl_set_max_response_bytes($ch_pii, 2 * 1024 * 1024);
+        $ch_pii = bot_curl_init($time, [], 2 * 1024 * 1024);
     }
     curl_setopt($ch_pii, CURLOPT_URL, "https://api.elsevier.com/content/object/pii/" . $pii);
     return parse_pii_doi_response(bot_curl_exec($ch_pii));
