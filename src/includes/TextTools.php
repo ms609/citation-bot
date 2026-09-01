28a847537c71dd71193108153803ba7eb0ee24d5
GlazerMann <kurt@ioiowa.com>
2026-09-01 16:47:19 -0500

    Add files via upload

--- src/includes/TextTools.php
+++ src/includes/TextTools.php
@@ -400,8 +400,8 @@ function normalize_c1_quotes(string $str): string {
 
     // Handle invalid UTF-8 (raw bytes from Windows-1252)
     if (!mb_check_encoding($str, 'UTF-8')) {
-        $str = (string) preg_replace('/[\x91\x92]/', "'", $str);
-        $str = (string) preg_replace('/[\x93\x94]/', '"', $str);
+        $str = (string) preg_replace('~[\x91\x92]~', "'", $str);
+        $str = (string) preg_replace('~[\x93\x94]~', '"', $str);
     }
 
     // Handle valid UTF-8 control characters (U+0091-U+0094)
