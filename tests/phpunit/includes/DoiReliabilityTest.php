33b962dd99e9d1ebefb171f93650b0ccf367b3c0
GlazerMann <kurt@ioiowa.com>
2026-09-01 20:17:21 -0500

    Fix expected value in throttle delay test

--- tests/phpunit/includes/DoiReliabilityTest.php
+++ tests/phpunit/includes/DoiReliabilityTest.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../../testBaseClass.php';
 
 final class DoiReliabilityTest extends testBaseClass {
     public function testThrottleDelayUsesSecondsToMicrosecondsConversion(): void {
-        $this->assertSame(50000, dx_throttle_delay(10.0, 10.0));
+        $this->assertSame(40000, dx_throttle_delay(10.0, 10.0));
         $half_delay = dx_throttle_delay(10.020, 10.0);
         $this->assertGreaterThanOrEqual(19999, $half_delay);
         $this->assertLessThanOrEqual(20001, $half_delay);
