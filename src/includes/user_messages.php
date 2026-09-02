f2578acd9f27790bfd8c6b513b45001ad1030636
GlazerMann <kurt@ioiowa.com>
2026-09-02 07:03:18 -0500

    Specify UTF-8 encoding in htmlspecialchars
    Specify UTF-8 encoding in htmlspecialchars function.

--- src/includes/user_messages.php
+++ src/includes/user_messages.php
@@ -112,7 +112,7 @@ function echoable(?string $string): string {
      * @psalm-taint-escape has_quotes
      */
     $string = (string) $string;
-     return HTML_OUTPUT ? htmlspecialchars($string, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5) : $string;
+    return HTML_OUTPUT ? htmlspecialchars($string, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5, 'UTF-8') : $string;
 }
 
 function pubmed_link(string $identifier, string $pm): string {
