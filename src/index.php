c9bc264ce5f1fdc4d883710a6516daf81e5add45
GlazerMann <kurt@ioiowa.com>
2026-09-10 16:47:26 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -84,7 +84,7 @@ session_write_close();
         <option value="ru">ru.wikipedia.org</option>
         <option value="sr">sr.wikipedia.org</option>
         <!-- Need to do better international treatment <option value="vi">vi.wikipedia.org</option> -->
-        <option value="mdwiki">MDWiki.org</option>
+        <!-- <option value="mdwiki">MDWiki.org</option> still not authorized -->
       </select>
     </p>
     <div role="status" aria-live="polite" id="botStatus" class="sr-only"></div>
