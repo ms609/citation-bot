47ea5f31da7dfb86fab32fa55ca51a7253855d5c
AManWithNoPlan <wikiamanwithnoplan@gmail.com>
2026-09-14 10:08:04 -0500

    fix phan errors

--- src/includes/api/APIzotero.php
+++ src/includes/api/APIzotero.php
@@ -77,7 +77,7 @@ final class Zotero {
         return urlencode($url);
     }
 
-    private static function normalize_zotero_result(object $result, string $url): bool {
+    private static function normalize_zotero_result(stdClass $result, string $url): bool {
         $scalar_fields = [
             'title',
             'publicationTitle',
@@ -525,7 +525,7 @@ final class Zotero {
         }
         if (is_array($result)) {
             $result = (object) $result;
-        } elseif (!is_object($result)) {
+        } elseif (!$result instanceof stdClass) {
             report_warning(
                 "Zotero JSON item was not an object for URL " . echoable($url) . ": " .
                 echoable(self::raw_response_excerpt($zotero_response))
