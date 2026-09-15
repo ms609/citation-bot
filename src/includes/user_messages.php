74c5d5e648ffbbb6e74bb1bbec53fca47f70d229
GlazerMann <kurt@ioiowa.com>
2026-09-15 08:31:39 -0500

    Add try-catch to report_error to force exit (#6008)

--- src/includes/user_messages.php
+++ src/includes/user_messages.php
@@ -76,14 +76,18 @@ function report_inline(string $text): void {
  * @codeCoverageIgnore
  */
 function report_error(string $text): never {
-    if (CI) {
-        trigger_error($text, E_USER_WARNING);
-    } elseif (function_exists('bot_debug_log')) {
-        bot_debug_log($text);  // Code logfile, if defined
-        report_warning($text); // To the user
-    } else {
-        report_warning($text); // To the user
-        trigger_error($text, E_USER_WARNING);  // System Logfile
+    try {
+        if (CI) {
+            trigger_error($text, E_USER_WARNING);
+        } elseif (function_exists('bot_debug_log')) {
+            bot_debug_log($text);  // Code logfile, if defined
+            report_warning($text); // To the user
+        } else {
+            report_warning($text); // To the user
+            trigger_error($text, E_USER_WARNING);  // System Logfile
+        }
+    } catch (Throwable) {
+        ;
     }
     exit(1);
 }
