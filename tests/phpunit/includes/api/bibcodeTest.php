997718b6c92091d71bfb93e0474e19cc7cee7b7a
GlazerMann <kurt@ioiowa.com>
2026-09-18 14:54:24 -0500

    Fix test that I broke

--- tests/phpunit/includes/api/bibcodeTest.php
+++ tests/phpunit/includes/api/bibcodeTest.php
@@ -538,6 +538,7 @@ final class bibcodeTest extends testBaseClass {
             if ($found_bibcode === null) { // Once more time
                 sleep(22);
                 expand_by_adsabs($expanded);
+                $found_bibcode = $expanded->get2('bibcode');
             }
             if ($found_bibcode === null) {
                 $this->markTestSkipped('AdsAbs API did not respond (rate limit or outage)');
